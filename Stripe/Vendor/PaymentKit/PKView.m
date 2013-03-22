aa3194611faa207ea72847fea4295fe5f2b1a967
Martin Conte Mac Donell <Reflejo@gmail.com>
2013-03-22 10:11:10 -0700

    Changed how the background of the textfield is created in order to support iOS < 6.0

--- Stripe/Vendor/PaymentKit/PKView.m
+++ Stripe/Vendor/PaymentKit/PKView.m
@@ -84,8 +84,7 @@
     
     UIImageView *backgroundImageView = [[UIImageView alloc] initWithFrame:self.bounds];
     backgroundImageView.image = [[UIImage imageNamed:@"textfield"]
-                                 resizableImageWithCapInsets:UIEdgeInsetsMake(0, 8, 0, 8)
-                                 resizingMode:UIImageResizingModeStretch];
+                                 resizableImageWithCapInsets:UIEdgeInsetsMake(0, 8, 0, 8)];
     [self addSubview:backgroundImageView];
     
     self.innerView = [[UIView alloc] initWithFrame:CGRectMake(40, 12, self.frame.size.width - 40, 20)];
