61cde0447a294f4fcc045380d13195b75d69bc58
Kieran Lafferty <kieran.lafferty@gmail.com>
2013-07-03 09:56:13 -0400

    Revert "Removed STPView"
    This reverts commit cdf3f29c719b57c0a34d0ec825c908fba82f8074.

---
+++ Stripe/STPView.h
@@ -0,0 +1,34 @@
+//
+//  STPView.h
+//
+//  Created by Alex MacCaw on 2/14/13.
+//  Copyright (c) 2013 Stripe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Stripe.h"
+#import "PKView.h"
+
+#define STPCardErrorUserMessage NSLocalizedString(@"Your card is invalid", @"Error when the card is not valid")
+
+typedef void (^STPTokenBlock)(STPToken* token, NSError* error);
+
+@class STPView;
+
+@protocol STPViewDelegate <NSObject>
+@optional
+- (void) stripeView:(STPView*)view withCard:(PKCard *)card isValid:(BOOL)valid;
+@end
+
+@interface STPView : UIView <PKViewDelegate>
+
+- (id)initWithFrame: (CGRect)frame andKey: (NSString*)stripeKey;
+
+@property IBOutlet PKView* paymentView;
+@property (copy) NSString* key;
+@property id <STPViewDelegate> delegate;
+@property (readonly) BOOL pending;
+
+- (void)createToken:(STPTokenBlock)block;
+
+@end
