93b1cefaefe6783b9f78c21c24b1130e8fff79a8
Howard Wilson <howard@watsonbox.net>
2014-07-14 19:28:06 +0200

    Add missing Foundation.h import to STPAPIConnection.h

--- Stripe/STPAPIConnection.h
+++ Stripe/STPAPIConnection.h
@@ -5,6 +5,8 @@
 //  Created by Phil Cohen on 4/9/14.
 //
 
+#import <Foundation/Foundation.h>
+
 typedef void (^APIConnectionCompletionBlock)(NSURLResponse *response, NSData *body, NSError *requestError);
 
 // Like NSURLConnection but verifies that the server isn't using a revoked certificate.
