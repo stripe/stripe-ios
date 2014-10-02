5dafce61a2c6e2c75b8058644ce81394b3567785
Jack Flintermann <jack@stripe.com>
2014-10-02 02:04:59 -0700

    initial wkwebview impl

---
+++ Stripe/STPCheckoutLegacyViewController.h
@@ -0,0 +1,13 @@
+//
+//  STPCheckoutLegacyViewController.h
+//  StripeExample
+//
+//  Created by Jack Flintermann on 10/2/14.
+//  Copyright (c) 2014 Stripe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STPCheckoutLegacyViewController : UIViewController
+
+@end
---
+++ Stripe/STPCheckoutModernViewController.h
@@ -0,0 +1,13 @@
+//
+//  STPCheckoutModernViewController.h
+//  StripeExample
+//
+//  Created by Jack Flintermann on 10/2/14.
+//  Copyright (c) 2014 Stripe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STPCheckoutModernViewController : UIViewController
+
+@end
