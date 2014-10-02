13d863be4e7550c41d6b77b84a307853c4c5745a
Charles Scalesse <charles@zola.com>
2014-10-02 12:14:37 -0400

    Adding additional properties to STPBankAccount and a getter for last4

--- Stripe/STPBankAccount.h
+++ Stripe/STPBankAccount.h
@@ -15,10 +15,13 @@
 @property (nonatomic) NSString *routingNumber;
 @property (nonatomic) NSString *country;
 
+@property (nonatomic, readonly) NSString *bankAccountId;
 @property (nonatomic, readonly) NSString *last4;
 @property (nonatomic, readonly) NSString *bankName;
-@property (nonatomic, readonly) BOOL validated;
 @property (nonatomic, readonly) NSString *fingerprint;
+@property (nonatomic, readonly) NSString *currency;
+@property (nonatomic, readonly) BOOL validated;
+@property (nonatomic, readonly) BOOL disabled;
 
 - (instancetype)initWithAttributeDictionary:(NSDictionary *)attributeDictionary;
 
--- Stripe/STPBankAccount.m
+++ Stripe/STPBankAccount.m
@@ -12,9 +12,13 @@
 @interface STPBankAccount ()
 
 @property (nonatomic, readwrite) NSString *object;
+@property (nonatomic, readwrite) NSString *bankAccountId;
 @property (nonatomic, readwrite) NSString *last4;
 @property (nonatomic, readwrite) NSString *bankName;
+@property (nonatomic, readwrite) NSString *fingerprint;
+@property (nonatomic, readwrite) NSString *currency;
 @property (nonatomic, readwrite) BOOL validated;
+@property (nonatomic, readwrite) BOOL disabled;
 
 @end
 
@@ -33,6 +37,7 @@
 - (instancetype)initWithAttributeDictionary:(NSDictionary *)attributeDictionary {
     self = [self init];
     if (self) {
+        // sanitize NSNull
         NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
         [attributeDictionary enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
             if (obj != [NSNull null]) {
@@ -40,12 +45,29 @@
             }
         }];
         
+        self.object = dictionary[@"object"];
+        self.bankAccountId = dictionary[@"id"];
         self.last4 = dictionary[@"last4"];
         self.bankName = dictionary[@"bank_name"];
         self.country = dictionary[@"country"];
+        self.fingerprint = dictionary[@"fingerprint"];
+        self.currency = dictionary[@"currency"];
         self.validated = [dictionary[@"validated"] boolValue];
+        self.disabled = [dictionary[@"disabled"] boolValue];
     }
     return self;
+}
+
+#pragma mark - Getters
+
+- (NSString *)last4 {
+    if (_last4) {
+        return _last4;
+    } else if (self.accountNumber && self.accountNumber.length >= 4) {
+        return [self.accountNumber substringFromIndex:(self.accountNumber.length - 4)];
+    } else {
+        return nil;
+    }
 }
 
 #pragma mark - STPFormEncodeProtocol
@@ -87,7 +109,8 @@
         && [self.routingNumber ?: @"" isEqualToString:bankAccount.routingNumber ?: @""]
         && [self.country ?: @"" isEqualToString:bankAccount.country ?: @""]
         && [self.last4 ?: @"" isEqualToString:bankAccount.last4 ?: @""]
-        && [self.bankName ?: @"" isEqualToString:bankAccount.bankName ?: @""];
+        && [self.bankName ?: @"" isEqualToString:bankAccount.bankName ?: @""]
+        && [self.currency ?: @"" isEqualToString:bankAccount.currency ?: @""];
 }
 
 @end
