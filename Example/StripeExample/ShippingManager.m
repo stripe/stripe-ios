7bf60ced7ae7fca83be9458f8e683b989f7f3f66
Jack Flintermann <jack@stripe.com>
2014-10-23 18:17:27 -0400

    improvements to example app

--- Example/StripeExample/ShippingManager.m
+++ Example/StripeExample/ShippingManager.m
@@ -12,43 +12,45 @@
 @implementation ShippingManager
 
 - (NSArray *)defaultShippingMethods {
-    return [self domesticShippingMethods];
+    return [self californiaShippingMethods];
 }
 
 - (void)fetchShippingCostsForAddress:(ABRecordRef)address completion:(void (^)(NSArray *shippingMethods, NSError *error))completion {
     // you could, for example, go to UPS here and calculate shipping costs to that address.
     ABMultiValueRef addressValues = ABRecordCopyValue(address, kABPersonAddressProperty);
-    NSString *country;
+    NSString *state;
     if (ABMultiValueGetCount(addressValues) > 0) {
         CFDictionaryRef dict = ABMultiValueCopyValueAtIndex(addressValues, 0);
-        country = CFDictionaryGetValue(dict, kABPersonAddressCountryKey);
+        state = CFDictionaryGetValue(dict, kABPersonAddressStateKey);
     }
-    if (!country) {
+    if (!state) {
         completion(nil, [NSError new]);
     }
-    if ([country isEqualToString:@"US"]) {
-        completion([self domesticShippingMethods], nil);
+    if ([state isEqualToString:@"CA"]) {
+        completion([self californiaShippingMethods], nil);
     } else {
         completion([self internationalShippingMethods], nil);
     }
 }
 
-- (NSArray *)domesticShippingMethods {
-    PKShippingMethod *normalItem = [PKShippingMethod summaryItemWithLabel:@"Llama Domestic Shipping" amount:[NSDecimalNumber decimalNumberWithString:@"20.00"]];
+- (NSArray *)californiaShippingMethods {
+    PKShippingMethod *normalItem =
+        [PKShippingMethod summaryItemWithLabel:@"Llama California Shipping" amount:[NSDecimalNumber decimalNumberWithString:@"20.00"]];
     normalItem.detail = @"3-5 Business Days";
+    normalItem.identifier = normalItem.label;
     PKShippingMethod *expressItem =
-        [PKShippingMethod summaryItemWithLabel:@"Llama Domestic Express Shipping" amount:[NSDecimalNumber decimalNumberWithString:@"30.00"]];
+        [PKShippingMethod summaryItemWithLabel:@"Llama California Express Shipping" amount:[NSDecimalNumber decimalNumberWithString:@"30.00"]];
     expressItem.detail = @"Next Day";
+    expressItem.identifier = expressItem.label;
     return @[normalItem, expressItem];
 }
 
 - (NSArray *)internationalShippingMethods {
-    PKShippingMethod *normalItem =
-        [PKShippingMethod summaryItemWithLabel:@"Llama International Shipping" amount:[NSDecimalNumber decimalNumberWithString:@"40.00"]];
+    PKShippingMethod *normalItem = [PKShippingMethod summaryItemWithLabel:@"Llama US Shipping" amount:[NSDecimalNumber decimalNumberWithString:@"40.00"]];
     normalItem.detail = @"3-5 Business Days";
     normalItem.identifier = normalItem.label;
     PKShippingMethod *expressItem =
-        [PKShippingMethod summaryItemWithLabel:@"Llama International Express Shipping" amount:[NSDecimalNumber decimalNumberWithString:@"50.00"]];
+        [PKShippingMethod summaryItemWithLabel:@"Llama US Express Shipping" amount:[NSDecimalNumber decimalNumberWithString:@"50.00"]];
     expressItem.detail = @"Next Day";
     expressItem.identifier = expressItem.label;
     return @[normalItem, expressItem];
