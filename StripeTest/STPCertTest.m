df3abf477b29d380cf84df53176c41c281e32acf
Jack Flintermann <jack@stripe.com>
2015-01-05 19:50:16 -0500

    refactor cert tests a bit

--- StripeTest/STPCertTest.m
+++ StripeTest/STPCertTest.m
@@ -12,17 +12,8 @@
 
 NSString *const STPExamplePublishableKey = @"bad_key";
 
-typedef NS_ENUM(NSInteger, StripeCertificateFailMethod) {
-    StripeCertificateFailMethodNoError = 0,
-    StripeCertificateFailMethodExpired,
-    StripeCertificateFailMethodMismatched,
-    StripeCertificateFailMethodRevoked,
-    NumStripeCertificateFailMethods
-};
-
-@interface STPAPIClient(Failure)
-@property(nonatomic, readwrite)NSURL *apiURL;
-- (void)setFailureMethod:(StripeCertificateFailMethod)method;
+@interface STPAPIClient (Failure)
+@property (nonatomic, readwrite) NSURL *apiURL;
 @end
 
 @interface STPCertTest : XCTestCase
@@ -33,82 +24,62 @@ typedef NS_ENUM(NSInteger, StripeCertificateFailMethod) {
 - (void)testNoError {
     XCTestExpectation *expectation = [self expectationWithDescription:@"Token creation"];
     STPAPIClient *client = [[STPAPIClient alloc] initWithPublishableKey:STPExamplePublishableKey];
-    [client setFailureMethod:StripeCertificateFailMethodNoError];
     [client createTokenWithData:[NSData new]
-                             completion:^(STPToken *token, NSError *error) {
-                                 [expectation fulfill];
-                                 // Note that this API request *will* fail, but it will return error
-                                 // messages from the server and not be blocked by local cert checks
-                                 XCTAssertNil(token, @"Expected no token");
-                                 XCTAssertNotNil(error, @"Expected error");
-                             }];
+                     completion:^(STPToken *token, NSError *error) {
+                         [expectation fulfill];
+                         // Note that this API request *will* fail, but it will return error
+                         // messages from the server and not be blocked by local cert checks
+                         XCTAssertNil(token, @"Expected no token");
+                         XCTAssertNotNil(error, @"Expected error");
+                     }];
     [self waitForExpectationsWithTimeout:5.0f handler:nil];
 }
-
-- (void)testCertificateErrorWithMethod:(StripeCertificateFailMethod)method {
-    XCTestExpectation *expectation = [self expectationWithDescription:@"Token creation"];
-    STPAPIClient *client = [[STPAPIClient alloc] initWithPublishableKey:STPExamplePublishableKey];
-    [client setFailureMethod:method];
-    [client createTokenWithData:[NSData new]
-                             completion:^(STPToken *token, NSError *error) {
-                                 [expectation fulfill];
-                                 XCTAssertNil(token, @"Expected no response");
-                                 XCTAssertNotNil(error, @"Expected error");
-
-                                 // Revoked errors are a bit special.
-                                 if (method == StripeCertificateFailMethodRevoked) {
-                                     XCTAssertEqualObjects(error.domain, StripeDomain, @"Revoked errors specifically are in the Stripe domain");
-                                     XCTAssertEqual(error.code, STPConnectionError, @"Revoked errors should generate the right code.");
-                                 } else {
-                                     XCTAssertEqualObjects(error.domain, @"NSURLErrorDomain", @"Error should be NSURLErrorDomain");
-                                     XCTAssertNotNil(error.userInfo[@"NSURLErrorFailingURLPeerTrustErrorKey"],
-                                                     @"There should be a secTustRef for Foundation HTTPS errors");
-                                 }
-                             }];
-
-    [self waitForExpectationsWithTimeout:5.0f handler:nil];
-}
-
-// These are broken into separate methods to make test reports nicer.
 
 - (void)testExpired {
-    [self testCertificateErrorWithMethod:StripeCertificateFailMethodExpired];
+    [self createTokenWithBaseURL:[NSURL URLWithString:@"https://testssl-expire.disig.sk/index.en.html"]
+                      completion:^(STPToken *token, NSError *error) {
+                          XCTAssertNil(token, @"Token should be nil.");
+                          XCTAssertEqualObjects(error.domain, @"NSURLErrorDomain", @"Error should be NSURLErrorDomain");
+                          XCTAssertNotNil(error.userInfo[@"NSURLErrorFailingURLPeerTrustErrorKey"],
+                                          @"There should be a secTustRef for Foundation HTTPS errors");
+                      }];
 }
 
 - (void)testMismatched {
-    [self testCertificateErrorWithMethod:StripeCertificateFailMethodMismatched];
+    [self createTokenWithBaseURL:[NSURL URLWithString:@"https://mismatched.stripe.com"]
+                      completion:^(STPToken *token, NSError *error) {
+                          XCTAssertNil(token, @"Token should be nil.");
+                          XCTAssertEqualObjects(error.domain, @"NSURLErrorDomain", @"Error should be NSURLErrorDomain");
+                          XCTAssertNotNil(error.userInfo[@"NSURLErrorFailingURLPeerTrustErrorKey"],
+                                          @"There should be a secTustRef for Foundation HTTPS errors");
+                      }];
 }
 
 - (void)testRevoked {
-    [self testCertificateErrorWithMethod:StripeCertificateFailMethodRevoked];
+    [self createTokenWithBaseURL:[NSURL URLWithString:@"https://revoked.stripe.com:444"]
+                      completion:^(STPToken *token, NSError *error) {
+                          XCTAssertNil(token, @"Token should be nil.");
+                          XCTAssertEqualObjects(error.domain, StripeDomain, @"Revoked errors specifically are in the Stripe domain");
+                          XCTAssertEqual(error.code, STPConnectionError, @"Revoked errors should generate the right code.");
+                      }];
+}
+
+// helper method
+- (void)createTokenWithBaseURL:(NSURL *)baseURL completion:(STPCompletionBlock)completion {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Token creation"];
+    STPAPIClient *client = [[STPAPIClient alloc] initWithPublishableKey:STPExamplePublishableKey];
+    client.apiURL = baseURL;
+    [client createTokenWithData:[NSData new]
+                     completion:^(STPToken *token, NSError *error) {
+                         [expectation fulfill];
+                         completion(token, error);
+                     }];
+
+    [self waitForExpectationsWithTimeout:5.0f handler:nil];
 }
 
 @end
 
-@implementation STPAPIClient(Failure)
-
+@implementation STPAPIClient (Failure)
 @dynamic apiURL;
-
-- (void)setFailureMethod:(StripeCertificateFailMethod)failureMethod {
-    NSURL *url;
-    switch (failureMethod) {
-            break;
-        case StripeCertificateFailMethodExpired:
-            url = [NSURL URLWithString:@"https://testssl-expire.disig.sk/index.en.html"];
-            break;
-        case StripeCertificateFailMethodMismatched:
-            url = [NSURL URLWithString:@"https://mismatched.stripe.com"];
-            break;
-        case StripeCertificateFailMethodRevoked:
-            url = [NSURL URLWithString:@"https://revoked.stripe.com:444"];
-            break;
-        case StripeCertificateFailMethodNoError:
-        default:
-            break;
-    }
-    if (url) {
-        self.apiURL = url;
-    }
-}
-
 @end
