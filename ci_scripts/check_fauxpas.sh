6a02b9ab85c08d8f8a0ff03cf6b361c1a7536ae6
Jack Flintermann <jack@stripe.com>
2015-01-07 22:50:53 -0500

    rename test dir

--- ci_scripts/check_fauxpas.sh
+++ ci_scripts/check_fauxpas.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [[ "$TRAVIS_SECURE_ENV_VARS" != "true" ]]; then
+if [[ $CI && "$TRAVIS_SECURE_ENV_VARS" != "true" ]]; then
   echo "Skipping Faux Pas linting."
   exit 0
 fi
 
 echo "Linting with Faux Pas..."
-fauxpas check "./Stripe Tests/Stripe Tests.xcodeproj" --scheme "iOS Tests" --configFile "./ci_scripts/FauxPasConfig/main.fauxpas.json" --minErrorStatusSeverity Concern && fauxpas check "./Stripe Tests/Stripe Tests.xcodeproj" --scheme "OSX Tests" --configFile "./ci_scripts/FauxPasConfig/main.fauxpas.json" --minErrorStatusSeverity Concern
+fauxpas check "./Tests/Stripe Tests.xcodeproj" --scheme "iOS Tests" --configFile "./ci_scripts/FauxPasConfig/main.fauxpas.json" --minErrorStatusSeverity Concern && fauxpas check "./Tests/Stripe Tests.xcodeproj" --scheme "OSX Tests" --configFile "./ci_scripts/FauxPasConfig/main.fauxpas.json" --minErrorStatusSeverity Concern
