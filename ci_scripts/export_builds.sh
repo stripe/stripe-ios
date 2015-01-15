2c87d13a046d5eff319abaa6a25be0cc4e14de74
Jack Flintermann <jack@stripe.com>
2015-01-14 23:47:07 -0500

    fix build scripts

--- ci_scripts/export_builds.sh
+++ ci_scripts/export_builds.sh
@@ -1,19 +1,26 @@
 BUILDDIR="${PWD}/build"
+rm -rf $BUILDDIR
 
 xcodebuild build -workspace Stripe.xcworkspace -scheme StripeOSX -configuration Release OBJROOT=$BUILDDIR SYMROOT=$BUILDDIR | xcpretty -c
 cd $BUILDDIR/Release
-zip -r StripeOSX.zip Stripe.framework
+mkdir StripeOSX
+mv Stripe.framework StripeOSX
+ditto -ck --rsrc --sequesterRsrc --keepParent StripeOSX StripeOSX.zip
 cp StripeOSX.zip $BUILDDIR
 cd -
 
 xcodebuild build -workspace Stripe.xcworkspace -scheme StripeiOSStaticFramework -configuration Release OBJROOT=$BUILDDIR SYMROOT=$BUILDDIR | xcpretty -c
 cd $PWD/build/Release-iphonesimulator
-zip -r StripeiOS.zip Stripe.framework
+mkdir StripeiOS
+mv Stripe.framework StripeiOS
+ditto -ck --rsrc --sequesterRsrc --keepParent StripeiOS StripeiOS.zip
 cp StripeiOS.zip $BUILDDIR
 cd -
 
 xcodebuild build -workspace Stripe.xcworkspace -scheme StripeiOSStaticFrameworkWithoutApplePay -configuration Release OBJROOT=$BUILDDIR SYMROOT=$BUILDDIR | xcpretty -c
 cd $PWD/build/Release-iphonesimulator
-zip -r StripeiOS-WithoutApplePay.zip Stripe.framework
+mkdir StripeiOS-WithoutApplePay
+mv Stripe.framework StripeiOS-WithoutApplePay
+ditto -ck --rsrc --sequesterRsrc --keepParent StripeiOS-WithoutApplePay StripeiOS-WithoutApplePay.zip
 cp StripeiOS-WithoutApplePay.zip $BUILDDIR
 cd -
