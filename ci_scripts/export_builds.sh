91e2c7582becfa7bdb8e212a17bdb247d7caea2e
Jack Flintermann <jack@stripe.com>
2015-01-19 14:07:32 -0800

    rename stripe to stripeosx

--- ci_scripts/export_builds.sh
+++ ci_scripts/export_builds.sh
@@ -4,7 +4,7 @@ rm -rf $BUILDDIR
 xcodebuild build -workspace Stripe.xcworkspace -scheme StripeOSX -configuration Release OBJROOT=$BUILDDIR SYMROOT=$BUILDDIR | xcpretty -c
 cd $BUILDDIR/Release
 mkdir StripeOSX
-mv Stripe.framework StripeOSX
+mv StripeOSX.framework StripeOSX
 ditto -ck --rsrc --sequesterRsrc --keepParent StripeOSX StripeOSX.zip
 cp StripeOSX.zip $BUILDDIR
 cd -
