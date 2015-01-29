1f1c588fc73d7617f80168146b687e6d21d89389
Chris Eidhof <chris@eidhof.nl>
2015-01-29 19:43:41 +0100

    Fix success case

--- Stripe/Checkout/STPCheckoutViewController.m
+++ Stripe/Checkout/STPCheckoutViewController.m
@@ -142,10 +142,11 @@
         [self.checkoutDelegate checkoutController:self
                                    didCreateToken:token
                                        completion:^(STPBackendChargeResult status, NSError *error) {
+                                           self.backendChargeSuccessful = (status == STPBackendChargeResultSuccess);
+
                                            if (status == STPBackendChargeResultSuccess) {
                                                [adapter evaluateJavaScript:payload[@"success"]];
                                            } else {
-                                               self.backendChargeSuccessful = (status == STPBackendChargeResultSuccess);
                                                self.backendChargeError = error;
                                                NSString *encodedError = @"";
                                                if (error.localizedDescription) {
