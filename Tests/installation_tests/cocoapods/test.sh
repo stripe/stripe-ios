f4a244510b60fcbee4985d2daf7b9414b1a2fc6c
Jack Flintermann <jack@stripe.com>
2015-05-07 16:57:06 -0400

    build cocoapods test for simulator

--- Tests/installation_tests/cocoapods/test.sh
+++ Tests/installation_tests/cocoapods/test.sh
@@ -2,4 +2,4 @@
 
 echo "Checking test Cocoapods app..."
 cd $(dirname $0)
-pod install && set -o pipefail && xcodebuild build -workspace CocoapodsTest.xcworkspace -scheme CocoapodsTest | xcpretty -c
+pod install && set -o pipefail && xcodebuild build -workspace CocoapodsTest.xcworkspace -scheme CocoapodsTest -sdk iphonesimulator | xcpretty -c
