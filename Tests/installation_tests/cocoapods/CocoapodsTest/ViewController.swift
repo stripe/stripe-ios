d81a6fcf4dc655c5cdc445d3531c13de33c58921
Jack Flintermann <jack@stripe.com>
2015-05-07 15:37:04 -0400

    add cocoapods installation test

---
+++ Tests/installation_tests/cocoapods/CocoapodsTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CocoapodsTest
+//
+//  Created by Jack Flintermann on 5/7/15.
+//  Copyright (c) 2015 stripe. All rights reserved.
+//
+
+import UIKit
+import Stripe
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
