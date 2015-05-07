a7c5ce35db4e9c832803fb5629515f52f5768370
Jack Flintermann <jack@stripe.com>
2015-05-07 16:04:19 -0400

    add cocoapods test to travis builds

---
+++ Tests/installation_tests/cocoapods/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "Checking test Cocoapods app..."
+cd $(dirname $0)
+pod install && set -o pipefail && xcodebuild build -workspace CocoapodsTest.xcworkspace -scheme CocoapodsTest | xcpretty -c
