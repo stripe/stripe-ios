672c101cbf206ccf14ed901b283a622ddb1f0abb
Jack Flintermann <jack@stripe.com>
2015-05-08 13:20:44 -0400

    archs

--- Tests/installation_tests/cocoapods/test.sh
+++ Tests/installation_tests/cocoapods/test.sh
@@ -2,4 +2,4 @@
 
 echo "Checking test Cocoapods app..."
 cd $(dirname $0)
-pod install && set -o pipefail && xcodebuild build -workspace CocoapodsTest.xcworkspace -scheme CocoapodsTest -sdk iphonesimulator -configuration Release ONLY_ACTIVE_ARCH=NO | xcpretty -c
+pod install && set -o pipefail && xcodebuild build -workspace CocoapodsTest.xcworkspace -scheme CocoapodsTest -sdk iphonesimulator -configuration Release ONLY_ACTIVE_ARCH=NO ARCHS=x86_64 | xcpretty -c
