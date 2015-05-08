9bb9684894cbd7eb10f82fae9f158c317f503729
Jack Flintermann <jack@stripe.com>
2015-05-08 13:47:55 -0400

    tweaks

--- Tests/installation_tests/cocoapods/test.sh
+++ Tests/installation_tests/cocoapods/test.sh
@@ -4,4 +4,4 @@ echo "Checking test Cocoapods app..."
 cd $(dirname $0)
 rm -rf Pods
 rm Podfile.lock
-pod install && set -o pipefail && xctool build -workspace CocoapodsTest.xcworkspace -scheme CocoapodsTest -sdk iphonesimulator -configuration Release ONLY_ACTIVE_ARCH=NO ARCHS=x86_64
+pod install && set -o pipefail && xctool build -workspace CocoapodsTest.xcworkspace -scheme CocoapodsTest
