f5dec5b6880ed45fc6e0cfe480159495b69a2ffb
Jack Flintermann <jack@stripe.com>
2015-05-15 16:43:32 -0400

    iphonesimulator; disable other tests

--- Tests/installation_tests/manual_installation/test.sh
+++ Tests/installation_tests/manual_installation/test.sh
@@ -8,9 +8,9 @@ BUILDDIR="$(cd $(dirname $0); pwd)/build"
 rm -rf $BUILDDIR
 mkdir $BUILDDIR
 
-xcodebuild build -workspace "${PROJECTDIR}/Stripe.xcworkspace" -scheme StripeiOSStaticFramework -configuration Release OBJROOT=$BUILDDIR SYMROOT=$BUILDDIR | xcpretty -c
+xcodebuild build -workspace "${PROJECTDIR}/Stripe.xcworkspace" -scheme StripeiOSStaticFramework -configuration Release OBJROOT=$BUILDDIR SYMROOT=$BUILDDIR -sdk iphonesimulator | xcpretty -c
 
 rm -rf ./ManualInstallationTest/Frameworks/Stripe.framework
 mv $BUILDDIR/Release-iphonesimulator/Stripe.framework ./ManualInstallationTest/Frameworks
 
-xctool build -project ManualInstallationTest.xcodeproj -scheme ManualInstallationTest
+xctool build -project ManualInstallationTest.xcodeproj -scheme ManualInstallationTest -sdk iphonesimulator
