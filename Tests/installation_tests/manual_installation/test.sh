c3fed9a6cd3c6463cf5dd60c5127bb3337579706
Jack Flintermann <jack@stripe.com>
2015-05-15 17:16:43 -0400

    create frameworks dir

--- Tests/installation_tests/manual_installation/test.sh
+++ Tests/installation_tests/manual_installation/test.sh
@@ -11,12 +11,13 @@ mkdir $BUILDDIR
 
 xcodebuild build -workspace "${PROJECTDIR}/Stripe.xcworkspace" -scheme StripeiOSStaticFramework -configuration Release OBJROOT=$BUILDDIR SYMROOT=$BUILDDIR -sdk iphonesimulator | xcpretty -c
 
-rm -rf $TESTDIR/ManualInstallationTest/Frameworks/Stripe.framework
 echo "Copying framework..."
+rm -rf $TESTDIR/ManualInstallationTest/Frameworks
+mkdir $TESTDIR/ManualInstallationTest/Frameworks
+mv $BUILDDIR/Release-iphonesimulator/Stripe.framework $TESTDIR/ManualInstallationTest/Frameworks
 echo $BUILDDIR
 echo $TESTDIR
 find $BUILDDIR
 find $TESTDIR
-mv $BUILDDIR/Release-iphonesimulator/Stripe.framework $TESTDIR/ManualInstallationTest/Frameworks
 
 xctool build -project "${TESTDIR}/ManualInstallationTest.xcodeproj" -scheme ManualInstallationTest -sdk iphonesimulator
