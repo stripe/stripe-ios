f6c068a25b999c067259b59fd5ddb3bad830444d
Jack Flintermann <jack@stripe.com>
2015-05-15 16:49:25 -0400

    fix relative directories in manual installation script

--- Tests/installation_tests/manual_installation/test.sh
+++ Tests/installation_tests/manual_installation/test.sh
@@ -3,6 +3,7 @@
 echo "Checking test manual installation app..."
 
 PROJECTDIR="$(cd $(dirname $0)/../../..; pwd)"
+TESTDIR="$(cd $(dirname $0); pwd)"
 BUILDDIR="$(cd $(dirname $0); pwd)/build"
 
 rm -rf $BUILDDIR
@@ -10,7 +11,7 @@ mkdir $BUILDDIR
 
 xcodebuild build -workspace "${PROJECTDIR}/Stripe.xcworkspace" -scheme StripeiOSStaticFramework -configuration Release OBJROOT=$BUILDDIR SYMROOT=$BUILDDIR -sdk iphonesimulator | xcpretty -c
 
-rm -rf ./ManualInstallationTest/Frameworks/Stripe.framework
-mv $BUILDDIR/Release-iphonesimulator/Stripe.framework ./ManualInstallationTest/Frameworks
+rm -rf $TESTDIR/ManualInstallationTest/Frameworks/Stripe.framework
+mv $BUILDDIR/Release-iphonesimulator/Stripe.framework $TESTDIR/ManualInstallationTest/Frameworks
 
-xctool build -project ManualInstallationTest.xcodeproj -scheme ManualInstallationTest -sdk iphonesimulator
+xctool build -project "${TESTDIR}/ManualInstallationTest.xcodeproj" -scheme ManualInstallationTest -sdk iphonesimulator
