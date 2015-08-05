ebafbeca50943721bfdd491451b36e2715ec2e6d
Jack Flintermann <jack@stripe.com>
2015-08-05 18:33:06 -0400

    carthage test script fix

--- Tests/installation_tests/carthage/test.sh
+++ Tests/installation_tests/carthage/test.sh
@@ -9,7 +9,7 @@ cd $TESTDIR
 GIT_REPO=`cd "../../.."; pwd`
 cd $TESTDIR
 
-GIT_BRANCH=`git branch | sed -n '/\* /s///p'`
+GIT_BRANCH=${TRAVIS_COMMIT-`git branch | sed -n '/\* /s///p'`}
 
 rm -f "$TESTDIR/Cartfile*"
 echo "git \"$GIT_REPO\" \"$GIT_BRANCH\"" > "$TESTDIR/Cartfile"
