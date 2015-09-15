07f3f9e29579ff63b90f2ba92cd50166c01ec8d0
Jack Flintermann <jack@stripe.com>
2015-09-15 14:54:15 -0400

    fix payment card text field test

--- Tests/Tests/STPPaymentCardTextFieldTest.m
+++ Tests/Tests/STPPaymentCardTextFieldTest.m
@@ -18,8 +18,18 @@
 
 - (void)testIntrinsicContentSize {
     STPPaymentCardTextField *textField = [STPPaymentCardTextField new];
+    
+    UIFont *iOS8SystemFont = [UIFont fontWithName:@"HelveticaNeue" size:18];
+    textField.font = iOS8SystemFont;
     XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.height, 44, 0.1);
     XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.width, 252, 0.1);
+    
+    UIFont *iOS9SystemFont = [UIFont fontWithName:@".SFUIText-Regular" size:18];
+    if (iOS9SystemFont) {
+        textField.font = iOS9SystemFont;
+        XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.height, 44, 0.1);
+        XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.width, 265, 0.1);
+    }
     
     textField.font = [UIFont fontWithName:@"Avenir" size:44];
     XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.height, 60, 0.1);
