f087ed5ab11a39ffdf4f1b178a29d3c152dfa16c
Jack Flintermann <jack@stripe.com>
2015-09-15 18:25:19 -0400

    add beta fauxpas support

--- ci_scripts/install_fauxpas.sh
+++ ci_scripts/install_fauxpas.sh
@@ -7,3 +7,5 @@ brew install caskroom/cask/brew-cask
 brew cask install fauxpas
 $HOME/Applications/FauxPas.app/Contents/Resources/install-cli-tools
 fauxpas updatelicense "organization-seat" "Stripe, Inc" $FAUX_PAS_LICENSE
+# Enable beta xcode 7 support
+defaults write org.hasseg.fauxpas experimentalXcode7SupportEnabled -bool yes
