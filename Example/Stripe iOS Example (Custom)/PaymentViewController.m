8957a9be3e463d9d36882b96c4b501dbe56816b7
Ben Guo <bg@stripe.com>
2016-01-05 16:28:54 -0500

    add testing UI

--- Example/Stripe iOS Example (Custom)/PaymentViewController.m
+++ Example/Stripe iOS Example (Custom)/PaymentViewController.m
@@ -13,6 +13,13 @@
 @interface PaymentViewController () <STPPaymentCardTextFieldDelegate>
 @property (weak, nonatomic) STPPaymentCardTextField *paymentTextField;
 @property (weak, nonatomic) UIActivityIndicatorView *activityIndicator;
+
+// UI for testing setCard (TODO: remove)
+@property (strong, nonatomic) UITextField *numberField;
+@property (strong, nonatomic) UITextField *expMonthField;
+@property (strong, nonatomic) UITextField *expYearField;
+@property (strong, nonatomic) UITextField *cvcField;
+@property (strong, nonatomic) UIButton *setButton;
 @end
 
 @implementation PaymentViewController
@@ -45,6 +52,41 @@
     activityIndicator.hidesWhenStopped = YES;
     self.activityIndicator = activityIndicator;
     [self.view addSubview:activityIndicator];
+
+    // UI for testing setCard (TODO: remove)
+    self.numberField = [UITextField new];
+    self.numberField.placeholder = @"Number";
+    self.expMonthField = [UITextField new];
+    self.expMonthField.placeholder = @"Exp Month";
+    self.expYearField = [UITextField new];
+    self.expYearField.placeholder = @"Exp Year";
+    self.cvcField = [UITextField new];
+    self.cvcField.placeholder = @"CVC";
+    NSArray<UITextField *> *fields = @[self.numberField, self.expMonthField, self.expYearField, self.cvcField];
+    for (UITextField *field in fields) {
+        field.borderStyle = UITextBorderStyleLine;
+        field.keyboardType = UIKeyboardTypeNumberPad;
+        field.frame = CGRectMake(0, 0, 300, 40);
+    }
+    self.setButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.setButton setTitle:@"setCard" forState:UIControlStateNormal];
+    [self.setButton addTarget:self action:@selector(setCard:) forControlEvents:UIControlEventTouchUpInside];
+    [self.setButton sizeToFit];
+    [self.view addSubview:self.numberField];
+    [self.view addSubview:self.expMonthField];
+    [self.view addSubview:self.expYearField];
+    [self.view addSubview:self.cvcField];
+    [self.view addSubview:self.setButton];
+}
+
+// UI for testing setCard (TODO: remove)
+- (void)setCard:(UIButton *)button {
+    STPCardParams *card = [[STPCardParams alloc] init];
+    card.number = self.numberField.text;
+    card.expMonth = [self.expMonthField.text integerValue];
+    card.expYear = [self.expYearField.text integerValue];
+    card.cvc = self.cvcField.text;
+    [self.paymentTextField setCard:card];
 }
 
 - (void)viewDidLayoutSubviews {
@@ -54,6 +96,14 @@
     self.paymentTextField.frame = CGRectMake(padding, padding, width, 44);
     
     self.activityIndicator.center = self.view.center;
+
+    // UI for testing setCard (TODO: remove)
+    CGFloat centerX = self.paymentTextField.center.x;
+    self.numberField.center = CGPointMake(centerX, CGRectGetMaxY(self.paymentTextField.frame) + padding*2);
+    self.expMonthField.center = CGPointMake(centerX, CGRectGetMaxY(self.numberField.frame) + padding);
+    self.expYearField.center = CGPointMake(centerX, CGRectGetMaxY(self.expMonthField.frame) + padding);
+    self.cvcField.center = CGPointMake(centerX, CGRectGetMaxY(self.expYearField.frame) + padding);
+    self.setButton.center = CGPointMake(centerX, CGRectGetMaxY(self.cvcField.frame) + padding);
 }
 
 - (void)paymentCardTextFieldDidChange:(nonnull STPPaymentCardTextField *)textField {
