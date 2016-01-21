85c1e20992e2c6dd4a61a54ac7c58badecbcfa81
Jack Flintermann <jack@stripe.com>
2016-01-20 19:13:53 -0500

    fix stppaymentcardtextfield tests

--- Tests/Tests/STPPaymentCardTextFieldTest.m
+++ Tests/Tests/STPPaymentCardTextFieldTest.m
@@ -22,18 +22,18 @@
     UIFont *iOS8SystemFont = [UIFont fontWithName:@"HelveticaNeue" size:18];
     textField.font = iOS8SystemFont;
     XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.height, 44, 0.1);
-    XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.width, 252, 0.1);
+    XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.width, 257, 0.1);
     
     UIFont *iOS9SystemFont = [UIFont fontWithName:@".SFUIText-Regular" size:18];
     if (iOS9SystemFont) {
         textField.font = iOS9SystemFont;
         XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.height, 44, 0.1);
-        XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.width, 265, 0.1);
+        XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.width, 270, 0.1);
     }
     
     textField.font = [UIFont fontWithName:@"Avenir" size:44];
     XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.height, 60, 0.1);
-    XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.width, 483, 0.1);
+    XCTAssertEqualWithAccuracy(textField.intrinsicContentSize.width, 488, 0.1);
 }
 
 @end
