23394c3646ecafc705b71824375999a1e0277030
Jack Flintermann <jack@stripe.com>
2016-01-20 19:24:38 -0500

    fixes to check_version script

--- ci_scripts/check_version.rb
+++ ci_scripts/check_version.rb
@@ -1,20 +1,15 @@
 #!/usr/bin/env ruby
 
 puts "Checking that version is set correctly..."
-git_version = `git describe`.strip.split("-").first
+git_version = `git describe`.strip.split("-").first # this is the most recent tag
 file_version = File.open('VERSION').first.strip
 search_result = `grep #{file_version} ./Stripe/PublicHeaders/STPAPIClient.h`
+
 if search_result.length == 0
-  abort("VERSION does not match STPSDKVersion in STPAPIClient.h") 
+  abort("VERSION does not match STPSDKVersion in STPAPIClient.h")
 end
 
 if ENV["TRAVIS_BRANCH"] == "master"
-
-  if ENV["TRAVIS_TAG"]
-    if git_version != "v#{file_version}"
-      abort("Current git tag does not match VERSION on a tag build!")
-    end
-  end
 
   if git_version > "v#{file_version}"
     abort("Current git tag is greater than VERSION, did you forget to increment it?")
