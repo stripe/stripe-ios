f94544e4e6ac3341f2cf80ea151227d4fb1cf15e
Jack Flintermann <jack@stripe.com>
2016-02-05 18:03:32 -0500

    6.2.0

--- Stripe/PublicHeaders/STPAPIClient.h
+++ Stripe/PublicHeaders/STPAPIClient.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-static NSString *const __nonnull STPSDKVersion = @"6.1.0";
+static NSString *const __nonnull STPSDKVersion = @"6.2.0";
 
 @class STPBankAccount, STPBankAccountParams, STPCard, STPCardParams, STPToken;
 
