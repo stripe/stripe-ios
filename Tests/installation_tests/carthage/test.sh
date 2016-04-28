4d776328959fbe254aa4c1214934ad63bdba26b7
Jack Flintermann <jack@stripe.com>
2016-04-28 14:44:41 -0400

    simpler fauxpas install

--- Tests/installation_tests/carthage/test.sh
+++ Tests/installation_tests/carthage/test.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-echo "Checking test Carthage app (with frameworks)..."
+echo "Checking test Carthage app..."
 
 TESTDIR="$(cd $(dirname $0); pwd)"
-echo $TESTDIR
 cd $TESTDIR
 
 GIT_REPO=`cd "../../.."; pwd`
@@ -14,6 +13,6 @@ GIT_BRANCH=${TRAVIS_COMMIT-`git branch | sed -n '/\* /s///p'`}
 rm -f "$TESTDIR/Cartfile*"
 echo "git \"$GIT_REPO\" \"$GIT_BRANCH\"" > "$TESTDIR/Cartfile"
 
-carthage update
+carthage bootstrap --platform ios --configuration Release --no-use-binaries
 
 xctool build -project "$TESTDIR/CarthageTest.xcodeproj" -scheme CarthageTest -sdk iphonesimulator
--- ci_scripts/install_fauxpas.sh
+++ ci_scripts/install_fauxpas.sh
@@ -3,9 +3,6 @@ if [[ $CI && "$TRAVIS_SECURE_ENV_VARS" != "true" ]]; then
   exit 0
 fi
 
-brew update; brew cleanup; brew cask cleanup
-brew uninstall --force brew-cask; brew update
-brew tap caskroom/cask
 brew cask install fauxpas
 $HOME/Applications/FauxPas.app/Contents/Resources/install-cli-tools
 fauxpas updatelicense "organization-seat" "Stripe, Inc" $FAUX_PAS_LICENSE
