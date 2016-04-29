1c687a0817f3b491d5b333f1eae9f06af077c2bb
Jack Flintermann <jack@stripe.com>
2016-04-29 14:22:45 -0400

    properly install fauxpas

--- ci_scripts/install_fauxpas.sh
+++ ci_scripts/install_fauxpas.sh
@@ -6,6 +6,7 @@ fi
 brew update; brew cleanup; brew cask cleanup
 brew uninstall --force brew-cask; brew update
 brew tap caskroom/cask
+brew cask install fauxpas
 
 $HOME/Applications/FauxPas.app/Contents/Resources/install-cli-tools
 fauxpas updatelicense "organization-seat" "Stripe, Inc" $FAUX_PAS_LICENSE
