b235b696cd5e131f5d93c6651e988ae49e0cb116
Ben Guo <bg@stripe.com>
2016-07-14 13:24:15 -0400

    add STPCustomerDeserializer tests

---
+++ Tests/Tests/STPTestUtils.h
@@ -0,0 +1,15 @@
+//
+//  STPTestUtils.h
+//  Stripe
+//
+//  Created by Ben Guo on 7/14/16.
+//  Copyright © 2016 Stripe, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STPTestUtils : NSObject
+
++ (NSDictionary *)jsonNamed:(NSString *)name;
+
+@end
