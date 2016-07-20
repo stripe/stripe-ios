7b94602c23faff965f3557b6f1a4cde14b7bef41
Jack Flintermann <jack@stripe.com>
2016-07-20 03:41:51 +0100

    reorder tests for faster parallel execution

--- Tests/installation_tests/manual_installation/test.sh
+++ Tests/installation_tests/manual_installation/test.sh
@@ -11,11 +11,11 @@ BUILDDIR="$(cd $(dirname $0); pwd)/build"
 rm -rf $BUILDDIR
 mkdir $BUILDDIR
 
-xcodebuild build -workspace "${PROJECTDIR}/Stripe.xcworkspace" -scheme StripeiOSStaticFramework -configuration Debug OBJROOT=$BUILDDIR SYMROOT=$BUILDDIR -sdk iphonesimulator | xcpretty -c
+xcodebuild build -workspace "${PROJECTDIR}/Stripe.xcworkspace" -scheme StripeiOSStaticFramework -configuration Release OBJROOT=$BUILDDIR SYMROOT=$BUILDDIR -sdk iphonesimulator | xcpretty -c
 
 rm -rf $TESTDIR/ManualInstallationTest/Frameworks
 mkdir $TESTDIR/ManualInstallationTest/Frameworks
-mv $BUILDDIR/Debug-iphonesimulator/Stripe.framework $TESTDIR/ManualInstallationTest/Frameworks
-mv $BUILDDIR/Debug-iphonesimulator/Stripe.bundle $TESTDIR/ManualInstallationTest/Frameworks/Stripe.framework
+mv $BUILDDIR/Release-iphonesimulator/Stripe.framework $TESTDIR/ManualInstallationTest/Frameworks
+mv $BUILDDIR/Release-iphonesimulator/Stripe.bundle $TESTDIR/ManualInstallationTest/Frameworks/Stripe.framework
 
 set -o pipefail && xcodebuild test -project "${TESTDIR}/ManualInstallationTest.xcodeproj" -scheme ManualInstallationTest -sdk iphonesimulator | xcpretty -c
