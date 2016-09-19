c7f5b093acd99e57ddc3586f20260829fc7f4a3e
Ben Guo <bg@stripe.com>
2016-09-19 15:31:04 -0400

    remove duplicate STPPaymentContext tests

--- Tests/Tests/STPPaymentContextTest.m
+++ Tests/Tests/STPPaymentContextTest.m
@@ -13,6 +13,7 @@
 #import "NSDecimalNumber+Stripe_Currency.h"
 
 @interface STPPaymentContext (Testing)
+@property(nonatomic) PKShippingMethod *selectedShippingMethod;
 - (PKPaymentRequest *)buildPaymentRequest;
 @end
 
@@ -96,11 +97,87 @@
     XCTAssertTrue([itemTotalAmount isEqualToNumber:correctTotalAmount]);
 }
 
-- (void)testSummaryItems_generateAmount {
+- (void)testSetPaymentAmount_generateSummaryItemsShippingMethod {
+    STPPaymentContext *context = [[STPPaymentContext alloc] initWithAPIAdapter:[TestSTPBackendAPIAdapter new]];
+    context.paymentAmount = 100;
+    context.configuration.companyName = @"Foo Company";
+    PKShippingMethod *method = [PKShippingMethod new];
+    method.amount = [NSDecimalNumber decimalNumberWithString:@"5.99"];
+    method.label = @"FedEx";
+    method.detail = @"foo";
+    method.identifier = @"123";
+    context.selectedShippingMethod = method;
+
+    NSArray<PKPaymentSummaryItem *> *items = [context paymentSummaryItems];
+    XCTAssertEqual((int)[items count], 2);
+    PKPaymentSummaryItem *item1 = items[0];
+    XCTAssertEqualObjects(item1.label, @"FedEx");
+    XCTAssertEqualObjects(item1.amount, [NSDecimalNumber decimalNumberWithString:@"5.99"]);
+    PKPaymentSummaryItem *item2 = items[1];
+    XCTAssertEqualObjects(item2.label, @"Foo Company");
+    XCTAssertEqualObjects(item2.amount, [NSDecimalNumber decimalNumberWithString:@"6.99"]);
+}
+
+- (void)testSummaryItemsToSummaryItems_shippingMethod {
+    STPPaymentContext *context = [[STPPaymentContext alloc] initWithAPIAdapter:[TestSTPBackendAPIAdapter new]];
+    PKPaymentSummaryItem *item1 = [PKPaymentSummaryItem new];
+    item1.amount = [NSDecimalNumber decimalNumberWithString:@"1.00"];
+    item1.label = @"foo";
+    PKPaymentSummaryItem *item2 = [PKPaymentSummaryItem new];
+    item2.amount = [NSDecimalNumber decimalNumberWithString:@"9.00"];
+    item2.label = @"bar";
+    PKPaymentSummaryItem *item3 = [PKPaymentSummaryItem new];
+    item3.amount = [NSDecimalNumber decimalNumberWithString:@"10.00"];
+    item3.label = @"baz";
+    context.paymentSummaryItems = @[item1, item2, item3];
+    PKShippingMethod *method = [PKShippingMethod new];
+    method.amount = [NSDecimalNumber decimalNumberWithString:@"5.99"];
+    method.label = @"FedEx";
+    method.detail = @"foo";
+    method.identifier = @"123";
+    context.selectedShippingMethod = method;
+
+    NSArray<PKPaymentSummaryItem *> *items = [context paymentSummaryItems];
+    XCTAssertEqual((int)[items count], 4);
+    PKPaymentSummaryItem *resultItem1 = items[0];
+    XCTAssertEqualObjects(resultItem1.label, @"foo");
+    XCTAssertEqualObjects(resultItem1.amount, [NSDecimalNumber decimalNumberWithString:@"1.00"]);
+    PKPaymentSummaryItem *resultItem2 = items[1];
+    XCTAssertEqualObjects(resultItem2.label, @"bar");
+    XCTAssertEqualObjects(resultItem2.amount, [NSDecimalNumber decimalNumberWithString:@"9.00"]);
+    PKPaymentSummaryItem *resultItem3 = items[2];
+    XCTAssertEqualObjects(resultItem3.label, @"FedEx");
+    XCTAssertEqualObjects(resultItem3.amount, [NSDecimalNumber decimalNumberWithString:@"5.99"]);
+    PKPaymentSummaryItem *resultItem4 = items[3];
+    XCTAssertEqualObjects(resultItem4.label, @"baz");
+    XCTAssertEqualObjects(resultItem4.amount, [NSDecimalNumber decimalNumberWithString:@"15.99"]);
+}
+
+- (void)testAmountToAmount_shippingMethod_usd {
+    STPPaymentContext *context = [[STPPaymentContext alloc] initWithAPIAdapter:[TestSTPBackendAPIAdapter new]];
+    context.paymentAmount = 100;
+    PKShippingMethod *method = [PKShippingMethod new];
+    method.amount = [NSDecimalNumber decimalNumberWithString:@"5.99"];
+    method.label = @"FedEx";
+    method.detail = @"foo";
+    method.identifier = @"123";
+    context.selectedShippingMethod = method;
+    NSInteger amount = context.paymentAmount;
+    XCTAssertEqual(amount, 699);
+}
+
+- (void)testSummaryItems_generateAmountDecimalCurrency {
     STPPaymentContext *context = [[STPPaymentContext alloc] initWithAPIAdapter:[TestSTPBackendAPIAdapter new]];
     context.paymentSummaryItems = [self testSummaryItems];
     context.paymentCurrency = @"USD";
     XCTAssertTrue(context.paymentAmount == 10000);
+}
+
+- (void)testSummaryItems_generateAmountNoDecimalCurrency {
+    STPPaymentContext *context = [[STPPaymentContext alloc] initWithAPIAdapter:[TestSTPBackendAPIAdapter new]];
+    context.paymentSummaryItems = [self testSummaryItems];
+    context.paymentCurrency = @"JPY";
+    XCTAssertTrue(context.paymentAmount == 100);
 }
 
 @end
