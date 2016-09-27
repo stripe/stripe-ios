8e0082f39a0b75bf5aab4f343b098da3887cd166
Ben Guo <bg@stripe.com>
2016-09-27 17:17:36 -0400

    fix navigation bar theme visual bug

--- Stripe/UINavigationBar+Stripe_Theme.m
+++ Stripe/UINavigationBar+Stripe_Theme.m
@@ -17,7 +17,7 @@ static NSInteger const STPNavigationBarHairlineViewTag = 787473;
 - (void)stp_setTheme:(STPTheme *)theme {
     [self stp_hairlineImageView].hidden = YES;
     [self stp_artificialHairlineView].backgroundColor = theme.tertiaryBackgroundColor;
-    self.barTintColor = theme.secondaryBackgroundColor;
+    self.barTintColor = theme.primaryBackgroundColor;
     self.tintColor = theme.accentColor;
 
     if ([STPColorUtils colorIsBright:theme.secondaryBackgroundColor]) {
