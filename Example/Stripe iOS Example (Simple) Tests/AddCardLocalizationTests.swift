24e32cc9bae5fe3d157e4c9910fd6ab6c9174ba3
Brian Dorfman <bdorfman@stripe.com>
2016-10-14 16:21:37 -0700

    Scaffolding for UI l10n tests

---
+++ Example/Stripe iOS Example (Simple) Tests/AddCardLocalizationTests.swift
@@ -0,0 +1,30 @@
+//
+//  AddCardLocalizationTests.swift
+//  Stripe iOS Example (Simple)
+//
+//  Created by Brian Dorfman on 10/14/16.
+//  Copyright © 2016 Stripe. All rights reserved.
+//
+
+import FBSnapshotTestCase
+
+class AddCardLocalizationTests: FBSnapshotTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+    
+    func testExample() {
+        
+    }
+    
+    func testPerformanceExample() {
+        
+    }
+    
+}
