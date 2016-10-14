fca9c1fe3f8f2635ef3c5804941df09e7131bb4e
Brian Dorfman <bdorfman@stripe.com>
2016-10-14 16:21:37 -0700

    Add localization snapshot tests

---
+++ Tests/Tests/STPAddCardViewControllerLocalizationTests.m
@@ -0,0 +1,80 @@
+//
+//  STPAddCardViewControllerLocalizationTests.m
+//  Stripe
+//
+//  Created by Brian Dorfman on 10/17/16.
+//  Copyright © 2016 Stripe, Inc. All rights reserved.
+//
+
+#import <FBSnapshotTestCase/FBSnapshotTestCase.h>
+#import <Stripe/Stripe.h>
+
+@interface STPAddCardViewControllerLocalizationTests : FBSnapshotTestCase
+
+@end
+
+@implementation STPAddCardViewControllerLocalizationTests
+
+//- (void)setUp {
+//    [super setUp];
+//
+//    self.recordMode = YES;
+//}
+
+- (void)performSnapshotTestForLanguage:(NSString *)language {
+    
+    STPPaymentConfiguration *config = [STPPaymentConfiguration new];
+    config.publishableKey = @"test";
+    config.companyName = @"Test Company";
+    config.requiredBillingAddressFields = STPBillingAddressFieldsFull;
+    config.additionalPaymentMethods = STPPaymentMethodTypeAll;
+    config.smsAutofillDisabled = NO;
+    
+    [STPLocalizationUtils overrideLanguageTo:language];
+    
+    STPAddCardViewController *addCardVC = [[STPAddCardViewController alloc] initWithConfiguration:config
+                                                                                            theme:[STPTheme defaultTheme]];
+    
+    UINavigationController *navController = [UINavigationController new];
+    navController.view.frame = CGRectMake(0, 0, 320, 750);
+    [navController pushViewController:addCardVC animated:NO];
+    
+    FBSnapshotVerifyView(navController.view, nil)
+    
+    [STPLocalizationUtils overrideLanguageTo:nil];
+}
+
+- (void)testGerman {
+    [self performSnapshotTestForLanguage:@"de"];
+}
+
+- (void)testEnglish {
+    [self performSnapshotTestForLanguage:@"en"];
+}
+
+- (void)testSpanish {
+    [self performSnapshotTestForLanguage:@"es"];
+}
+
+- (void)testFrench {
+    [self performSnapshotTestForLanguage:@"fr"];
+}
+
+- (void)testItalian {
+    [self performSnapshotTestForLanguage:@"it"];
+}
+
+- (void)testJapanese {
+    [self performSnapshotTestForLanguage:@"ja"];
+}
+
+- (void)testDutch {
+    [self performSnapshotTestForLanguage:@"nl"];
+}
+
+- (void)testChinese {
+    [self performSnapshotTestForLanguage:@"zh-Hans"];
+}
+
+
+@end
