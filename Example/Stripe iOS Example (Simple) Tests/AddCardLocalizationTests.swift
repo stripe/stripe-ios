0026c22c9ceb131e0f30a725288816690de8a24f
Brian Dorfman <bdorfman@stripe.com>
2016-10-14 17:29:21 -0700

    Add localization snapshot tests

--- Example/Stripe iOS Example (Simple) Tests/AddCardLocalizationTests.swift
+++ Example/Stripe iOS Example (Simple) Tests/AddCardLocalizationTests.swift
@@ -7,24 +7,88 @@
 //
 
 import FBSnapshotTestCase
+import Stripe
+
+class TestAPIClient: NSObject, STPBackendAPIAdapter {
+    static let sharedClient = TestAPIClient()
+    @objc func retrieveCustomer(_ completion: @escaping STPCustomerCompletionBlock) {
+        completion(nil, nil)
+    }
+    
+    @objc func selectDefaultCustomerSource(_ source: STPSource, completion: @escaping STPErrorBlock) {
+        completion(nil)
+    }
+    
+    @objc func attachSource(toCustomer source: STPSource, completion: @escaping STPErrorBlock) {
+        completion(nil)
+    }
+}
 
 class AddCardLocalizationTests: FBSnapshotTestCase {
+    
+    var paymentConfig: STPPaymentConfiguration!
     
     override func setUp() {
         super.setUp()
         
-    }
-    
-    override func tearDown() {
+        self.recordMode = true
+        
+        let config = STPPaymentConfiguration.init()
+        config.publishableKey = "test"
+        config.companyName = "Test Company"
+        config.requiredBillingAddressFields = .full
+        config.additionalPaymentMethods = .all
+        config.smsAutofillDisabled = false
+        
+        self.paymentConfig = config
         
     }
     
-    func testExample() {
+    func performSnapshotTestFor(language: String) {
+        STPLocalizationUtils.overrideLanguage(to: language)
         
+        let addCardVC = STPAddCardViewController.init(configuration: self.paymentConfig, 
+                                                  theme: STPTheme.default())
+        
+        let navController = UINavigationController.init(rootViewController: addCardVC)
+        navController.view.frame = CGRect(x: 0, y: 0, width: 320, height: 750)
+        
+        // TODO: dig into the controller's scroll view to dig out the actual height needed
+
+        FBSnapshotVerifyView(navController.view)
+        
+        STPLocalizationUtils.overrideLanguage(to: nil)
     }
     
-    func testPerformanceExample() {
-        
+    func testGerman() {
+        performSnapshotTestFor(language: "de")
     }
     
+    func testEnglish() {
+        performSnapshotTestFor(language: "en")
+    }
+    
+    func testSpanish() {
+        performSnapshotTestFor(language: "es")
+    }    
+    
+    func testFrench() {
+        performSnapshotTestFor(language: "fr")
+    }
+    
+    func testItalian() {
+        performSnapshotTestFor(language: "it")
+    }
+    
+    func testJapanese() {
+        performSnapshotTestFor(language: "ja")
+    }
+    
+    func testDutch() {
+        performSnapshotTestFor(language: "nl")
+    }
+    
+    func testChinese() {
+        performSnapshotTestFor(language: "zh-Hans")
+    }
 }
