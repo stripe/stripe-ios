6334e18ce2465ccd215c43792bb29e1e94dd79b4
Brian Dorfman <bdorfman@stripe.com>
2016-10-17 11:57:35 -0700

    Move snapshot tests to framework target

--- Example/Stripe iOS Example (Simple)/CheckoutViewController.swift
+++ Example/Stripe iOS Example (Simple)/CheckoutViewController.swift
@@ -13,13 +13,13 @@ class CheckoutViewController: UIViewController, STPPaymentContextDelegate {
 
     // 1) To get started with this demo, first head to https://dashboard.stripe.com/account/apikeys
     // and copy your "Test Publishable Key" (it looks like pk_test_abcdef) into the line below.
-    let stripePublishableKey = ""
+    let stripePublishableKey = "pk_test_09v6ikyygqCtZmooFRjZ34Pi"
     
     // 2) Next, optionally, to have this demo save your user's payment details, head to
     // https://github.com/stripe/example-ios-backend , click "Deploy to Heroku", and follow
     // the instructions (don't worry, it's free). Replace nil on the line below with your
     // Heroku URL (it looks like https://blazing-sunrise-1234.herokuapp.com ).
-    let backendBaseURL: String? = nil
+    let backendBaseURL: String? = "https://serene-wave-70571.herokuapp.com"
 
     // 3) Optionally, to enable Apple Pay, follow the instructions at https://stripe.com/docs/mobile/apple-pay
     // to create an Apple Merchant ID. Replace nil on the line below with it (it looks like merchant.com.yourappname).
@@ -57,6 +57,10 @@ class CheckoutViewController: UIViewController, STPPaymentContextDelegate {
     }
 
     init(product: String, price: Int, settings: Settings) {
+//        [[NSUserDefaults standardUserDefaults] setObject:[NSArray arrayWithObjects:@"de", @"en", @"fr", nil] forKey:@"AppleLanguages"];
+        
+//        UserDefaults.standard.set(["fr"], forKey: "AppleLanguages")
+        
         self.product = product
         self.productImage.text = product
         self.theme = settings.theme
