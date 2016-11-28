74c5846b04916bd5e0bd8e347556aef9c6dc71a7
Jack Flintermann <jack@stripe.com>
2016-11-28 16:42:12 -0500

    temporarily disable xcpretty

--- ci_scripts/run_tests.sh
+++ ci_scripts/run_tests.sh
@@ -3,6 +3,6 @@ set -euf -o pipefail
 carthage bootstrap --platform ios --configuration Release --no-use-binaries
 
 gem install xcpretty --no-ri --no-rdoc
-xcodebuild test -workspace Stripe.xcworkspace -scheme "StripeiOS Tests" -configuration Debug -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' | xcpretty -c
-xcodebuild build -workspace Stripe.xcworkspace -scheme "Stripe iOS Example (Simple)" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' | xcpretty -c
-xcodebuild build -workspace Stripe.xcworkspace -scheme "Stripe iOS Example (Custom)" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' | xcpretty -c
+xcodebuild test -workspace Stripe.xcworkspace -scheme "StripeiOS Tests" -configuration Debug -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' # | xcpretty -c
+xcodebuild build -workspace Stripe.xcworkspace -scheme "Stripe iOS Example (Simple)" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest'  # | xcpretty -c
+xcodebuild build -workspace Stripe.xcworkspace -scheme "Stripe iOS Example (Custom)" -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' # | xcpretty -c
