72ea4bb743e5a3c4333e0996b37e348ce13fba2c
Jack Flintermann <jack@stripe.com>
2016-12-01 02:20:28 -0500

    update resource checking

--- ci_scripts/check_resource_bundle.rb
+++ ci_scripts/check_resource_bundle.rb
@@ -10,12 +10,22 @@ end
 require 'xcodeproj'
 
 contents_of_resources_dir = Dir.glob("Stripe/Resources/Images/*.png").map { |h| File.basename(h) }.uniq.sort
-resource_bundle_target = Xcodeproj::Project.open('Stripe.xcodeproj').targets.select { |t| t.name == 'StripeiOSResources' }.first
-resources = resource_bundle_target.resources_build_phase.file_display_names.uniq.sort.select{ |n| !n.end_with? ".strings"}
+targets = ['StripeiOSResources']
+targets.each do |target|
 
-if contents_of_resources_dir != resources
-  likely_culprits = ((contents_of_resources_dir - resources) + (resources - contents_of_resources_dir)).uniq
-  abort("The contents of Stripe/Resources/Images do not match the contents of the StripeiOSResources target. Likely culprits: #{likely_culprits}.")
+  resource_bundle_target = Xcodeproj::Project.open('Stripe.xcodeproj').targets.select { |t| t.name == target }.first
+  resource_bundle_files = resource_bundle_target.resources_build_phase.file_display_names
+  duplicates = resource_bundle_files.select { |f| resource_bundle_files.count(f) > 1 }.uniq
+  if duplicates.any?
+    abort("Found some duplicate entries in the resources build phase for target #{target}:\n#{duplicates}")
+  end
+  resources = resource_bundle_files.uniq.sort.select{ |n| !n.end_with? ".strings"}
+
+  if contents_of_resources_dir != resources
+    likely_culprits = ((contents_of_resources_dir - resources) + (resources - contents_of_resources_dir)).uniq
+    abort("The contents of Stripe/Resources/Images do not match the contents of the #{target} target. Likely culprits: #{likely_culprits}.")
+  end
+
 end
 
-puts "Resource bundle looks good!"
+puts "Resources look good!"
