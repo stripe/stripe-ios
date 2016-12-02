0a525c0da390eb002b69435bd7fb86e3f1b34637
Brian Dorfman <bdorfman@stripe.com>
2016-12-02 11:30:15 -0800

    Force sdk to use same loc as main bundle.
    If the language support by the main app's bundle is different than the language supported by our bundle, switch to pulling sdk from the main bundle.
    This lets users of the sdk fill in their own sdk localizations for languages we don't support in their main strings files instead of having to fork us.
    It also prevents edge cases where the sdk could possibly use a different localization than the app

--- Stripe/STPBundleLocator.m
+++ Stripe/STPBundleLocator.m
@@ -28,22 +28,27 @@
      4. main bundle (for people dragging all our files into their project)
      **/
     
-    NSBundle *ourBundle = [NSBundle bundleWithPath:@"Stripe.bundle"];
+    static NSBundle *ourBundle;
     
-    if (ourBundle == nil) {
-        // This might be the same as the previous check if not using a dynamic framework
-        NSString *path = [[NSBundle bundleForClass:[STPBundleLocatorInternal class]] pathForResource:@"Stripe" ofType:@"bundle"];
-        ourBundle = [NSBundle bundleWithPath:path];
-    }
-    
-    if (ourBundle == nil) {
-        // This will be the same as mainBundle if not using a dynamic framwork
-        ourBundle = [NSBundle bundleForClass:[STPBundleLocatorInternal class]];
-    }
-    
-    if (ourBundle == nil) {
-        ourBundle = [NSBundle mainBundle];
-    }
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        ourBundle = [NSBundle bundleWithPath:@"Stripe.bundle"];
+        if (ourBundle == nil) {
+            // This might be the same as the previous check if not using a dynamic framework
+            NSString *path = [[NSBundle bundleForClass:[STPBundleLocatorInternal class]] pathForResource:@"Stripe" ofType:@"bundle"];
+            ourBundle = [NSBundle bundleWithPath:path];
+        }
+        
+        if (ourBundle == nil) {
+            // This will be the same as mainBundle if not using a dynamic framework
+            ourBundle = [NSBundle bundleForClass:[STPBundleLocatorInternal class]];
+        }
+        
+        if (ourBundle == nil) {
+            ourBundle = [NSBundle mainBundle];
+        }
+    });
+
     return ourBundle;
 }
 
