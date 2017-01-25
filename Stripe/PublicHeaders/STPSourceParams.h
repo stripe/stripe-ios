dd2cb86b1884111dee78abe113655730b0209220
Ben Guo <bg@stripe.com>
2017-01-25 14:05:55 -0500

    add STPSourceParams constructors

--- Stripe/PublicHeaders/STPSourceParams.h
+++ Stripe/PublicHeaders/STPSourceParams.h
@@ -9,6 +9,10 @@
 #import <Foundation/Foundation.h>
 #import "STPFormEncodable.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
+@class STPCardParams;
+
 /**
  *  An object representing parameters used to create a Source object. 
  *  @see https://stripe.com/docs/api#create_source
@@ -70,4 +74,128 @@
  */
 @property (nonatomic, copy, nullable) NSString *usage;
 
+/**
+ *  Creates params for a Bancontact source.
+ *  @see https://stripe.com/docs/bitcoin#creating-and-displaying-a-source-object
+ *
+ *  @param amount               The amount to charge the customer.
+ *  @param name                 The full name of the account holder.
+ *  @param returnURL            The URL the customer should be redirected to after they have successfully verified the payment.
+ *  @param statementDescriptor  (Optional) A custom statement descriptor for the payment.
+ *
+ *  @return an STPSourceParams object populated with the provided values.
+ */
++ (STPSourceParams *)bancontactParamsWithAmount:(NSUInteger)amount
+                                           name:(NSString *)name
+                                      returnURL:(NSString *)returnURL
+                            statementDescriptor:(nullable NSString *)statementDescriptor;
+
+/**
+ *  Creates params for a Bitcoin source.
+ *  @see https://stripe.com/docs/bitcoin#creating-and-displaying-a-source-object
+ *
+ *  @param amount      The amount to charge the customer.
+ *  @param currency    The currency the payment is being created in.
+ *  @param email       The customer's email address.
+ *
+ *  @return an STPSourceParams object populated with the provided values.
+ */
++ (STPSourceParams *)bitcoinParamsWithAmount:(NSUInteger)amount
+                                    currency:(NSString *)currency
+                                       email:(NSString *)email;
+
+/**
+ *  Creates params for a Card source.
+ *  @see https://stripe.com/docs/sources/cards#create-source
+ *
+ *  @param card        An object containing the user's card details
+ *
+ *  @return an STPSourceParams object populated with the provided card details.
+ */
++ (STPSourceParams *)cardParamsWithCard:(STPCardParams *)card;
+
+/**
+ *  Creates params for a Giropay source.
+ *  @see https://stripe.com/docs/sources/giropay#create-source
+ *
+ *  @param amount               The amount to charge the customer.
+ *  @param name                 The full name of the account holder.
+ *  @param returnURL            The URL the customer should be redirected to after they have successfully verified the payment.
+ *  @param statementDescriptor  (Optional) A custom statement descriptor for the payment.
+ *
+ *  @return an STPSourceParams object populated with the provided values.
+ */
++ (STPSourceParams *)giropayParamsWithAmount:(NSUInteger)amount
+                                        name:(NSString *)name
+                                   returnURL:(NSString *)returnURL
+                         statementDescriptor:(nullable NSString *)statementDescriptor;
+
+/**
+ *  Creates params for an iDEAL source.
+ *  @see https://stripe.com/docs/sources/ideal#create-source
+ *
+ *  @param amount               The amount to charge the customer.
+ *  @param name                 The full name of the account holder.
+ *  @param returnURL            The URL the customer should be redirected to after they have successfully verified the payment.
+ *  @param statementDescriptor  (Optional) A custom statement descriptor for the payment.
+ *  @param bank                 (Optional) The customer's bank.
+ *
+ *  @return an STPSourceParams object populated with the provided values.
+ */
++ (STPSourceParams *)idealParamsWithAmount:(NSUInteger)amount
+                                      name:(NSString *)name
+                                 returnURL:(NSString *)returnURL
+                       statementDescriptor:(nullable NSString *)statementDescriptor
+                                      bank:(nullable NSString *)bank;
+
+/**
+ *  Creates params for a SEPA Debit source.
+ *  @see https://stripe.com/docs/sources/sepa-debit#create-source
+ *
+ *  @param amount      The amount to charge the customer.
+ *  @param name        The full name of the account holder.
+ *  @param iban        The IBAN number for the bank account you wish to debit.
+ *  @param address     The bank account holder's full address.
+ *
+ *  @return an STPSourceParams object populated with the provided values.
+ */
++ (STPSourceParams *)sepaDebitParamsWithAmount:(NSUInteger)amount
+                                          name:(NSString *)name
+                                          iban:(NSString *)iban
+                                       address:(NSDictionary<NSString *,NSString *>*)address;
+
+/**
+ *  Creates params for a Sofort source.
+ *  @see https://stripe.com/docs/sources/sofort#create-source
+ *
+ *  @param amount               The amount to charge the customer.
+ *  @param returnURL            The URL the customer should be redirected to after they have successfully verified the payment.
+ *  @param country              The country code of the customer's bank.
+ *  @param statementDescriptor  (Optional) A custom statement descriptor for the payment.
+ *
+ *  @return an STPSourceParams object populated with the provided values.
+ */
++ (STPSourceParams *)sofortParamsWithAmount:(NSUInteger)amount
+                                  returnURL:(NSString *)returnURL
+                                    country:(NSString *)country
+                        statementDescriptor:(nullable NSString *)statementDescriptor;
+
+/**
+ *  Creates params for a 3DS source.
+ *  @see https://stripe.com/docs/sources/three-d-secure#create-3ds-source
+ *
+ *  @param amount      The amount to charge the customer.
+ *  @param currency    The currency the payment is being created in.
+ *  @param returnURL   The URL the customer should be redirected to after they have successfully verified the payment.
+ *  @param card        The ID of the card source.
+ *
+ *  @return an STPSourceParams object populated with the provided card details.
+ */
++ (STPSourceParams *)threeDSecureParamsWithAmount:(NSUInteger)amount
+                                         currency:(NSString *)currency
+                                        returnURL:(NSString *)returnURL
+                                             card:(NSString *)card;
+
 @end
+
+NS_ASSUME_NONNULL_END
--- Stripe/STPSourceParams.m
+++ Stripe/STPSourceParams.m
@@ -7,6 +7,8 @@
 //
 
 #import "STPSourceParams.h"
+#import "STPCardParams.h"
+#import "STPFormEncoder.h"
 
 @implementation STPSourceParams
 
@@ -18,6 +20,159 @@
         _additionalAPIParameters = @{};
     }
     return self;
+}
+
++ (STPSourceParams *)bancontactParamsWithAmount:(NSUInteger)amount
+                                           name:(NSString *)name
+                                      returnURL:(NSString *)returnURL
+                            statementDescriptor:(nullable NSString *)statementDescriptor {
+    STPSourceParams *params = [STPSourceParams new];
+    params.type = @"bancontact";
+    params.amount = @(amount);
+    params.currency = @"eur"; // Bancontact must always use eur
+    params.owner = @{ @"name": name };
+    params.redirect = @{ @"return_url": returnURL };
+    if (statementDescriptor != nil) {
+        params.additionalAPIParameters = @{
+                                           @"bancontact": @{
+                                                   @"statement_descriptor": statementDescriptor
+                                                   }
+                                           };
+    }
+    return params;
+}
+
++ (STPSourceParams *)bitcoinParamsWithAmount:(NSUInteger)amount
+                                    currency:(NSString *)currency
+                                       email:(NSString *)email {
+    STPSourceParams *params = [STPSourceParams new];
+    params.type = @"bitcoin";
+    params.amount = @(amount);
+    params.currency = currency;
+    params.owner = @{ @"email": email };
+    return params;
+}
+
++ (STPSourceParams *)cardParamsWithCard:(STPCardParams *)card {
+    STPSourceParams *params = [STPSourceParams new];
+    params.type = @"card";
+    NSDictionary *keyPairs = [STPFormEncoder keyPairDictionaryForObject:card];
+    NSMutableDictionary *cardDict = [NSMutableDictionary dictionary];
+    NSArray<NSString *>*cardKeys = @[@"number", @"cvc", @"exp_month", @"exp_year"];
+    for (NSString *key in cardKeys) {
+        cardDict[key] = keyPairs[key];
+    }
+    params.additionalAPIParameters = @{ @"card": cardDict };
+    NSMutableDictionary *addressDict = [NSMutableDictionary dictionary];
+    NSDictionary<NSString *,NSString *>*addressKeyMapping = @{
+                                                              @"address_line1": @"line1",
+                                                              @"address_line2": @"line2",
+                                                              @"address_city": @"city",
+                                                              @"address_state": @"state",
+                                                              @"address_zip": @"postal_code",
+                                                              @"address_country": @"country",
+                                                              };
+    for (NSString *key in [addressKeyMapping allKeys]) {
+        NSString *newKey = [addressKeyMapping objectForKey:key];
+        addressDict[newKey] = keyPairs[key];
+    }
+    params.owner = @{ @"address": addressDict };
+    return params;
+}
+
++ (STPSourceParams *)giropayParamsWithAmount:(NSUInteger)amount
+                                        name:(NSString *)name
+                                   returnURL:(NSString *)returnURL
+                         statementDescriptor:(nullable NSString *)statementDescriptor {
+    STPSourceParams *params = [STPSourceParams new];
+    params.type = @"giropay";
+    params.amount = @(amount);
+    params.currency = @"eur"; // Giropay must always use eur
+    params.owner = @{ @"name": name };
+    params.redirect = @{ @"return_url": returnURL };
+    if (statementDescriptor != nil) {
+        params.additionalAPIParameters = @{
+                                           @"giropay": @{
+                                                   @"statement_descriptor": statementDescriptor
+                                                   }
+                                           };
+    }
+    return params;
+}
+
++ (STPSourceParams *)idealParamsWithAmount:(NSUInteger)amount
+                                      name:(NSString *)name
+                                 returnURL:(NSString *)returnURL
+                       statementDescriptor:(nullable NSString *)statementDescriptor
+                                      bank:(nullable NSString *)bank {
+    STPSourceParams *params = [STPSourceParams new];
+    params.type = @"ideal";
+    params.amount = @(amount);
+    params.currency = @"eur"; // iDEAL must always use eur
+    params.owner = @{ @"name": name };
+    params.redirect = @{ @"return_url": returnURL };
+    if (statementDescriptor != nil || bank != nil) {
+        NSMutableDictionary *idealDict = [NSMutableDictionary dictionary];
+        idealDict[@"statement_descriptor"] = statementDescriptor;
+        idealDict[@"bank"] = bank;
+        params.additionalAPIParameters = @{ @"ideal": idealDict };
+    }
+    return params;
+}
+
++ (STPSourceParams *)sepaDebitParamsWithAmount:(NSUInteger)amount
+                                          name:(NSString *)name
+                                          iban:(NSString *)iban
+                                       address:(NSDictionary<NSString *,NSString *>*)address {
+    STPSourceParams *params = [STPSourceParams new];
+    params.type = @"sepa_debit";
+    params.amount = @(amount);
+    params.currency = @"eur"; // SEPA Debit must always use eur
+    params.owner = @{
+                     @"name": name,
+                     @"address": address
+                     };
+    params.additionalAPIParameters = @{
+                                       @"sepa_debit": @{
+                                               @"iban": iban
+                                               }
+                                       };
+    return params;
+}
+
++ (STPSourceParams *)sofortParamsWithAmount:(NSUInteger)amount
+                                  returnURL:(NSString *)returnURL
+                                    country:(NSString *)country
+                        statementDescriptor:(nullable NSString *)statementDescriptor {
+    STPSourceParams *params = [STPSourceParams new];
+    params.type = @"sofort";
+    params.amount = @(amount);
+    params.currency = @"eur"; // sofort must always use eur
+    params.redirect = @{ @"return_url": returnURL };
+    NSMutableDictionary *sofortDict = [NSMutableDictionary dictionary];
+    sofortDict[@"country"] = country;
+    if (statementDescriptor != nil) {
+        sofortDict[@"statement_descriptor"] = statementDescriptor;
+    }
+    params.additionalAPIParameters = @{ @"sofort": sofortDict };
+    return params;
+}
+
++ (STPSourceParams *)threeDSecureParamsWithAmount:(NSUInteger)amount
+                                         currency:(NSString *)currency
+                                        returnURL:(NSString *)returnURL
+                                             card:(NSString *)card {
+    STPSourceParams *params = [STPSourceParams new];
+    params.type = @"three_d_secure";
+    params.amount = @(amount);
+    params.currency = currency;
+    params.additionalAPIParameters = @{
+                                       @"three_d_secure": @{
+                                               @"card": card
+                                               }
+                                       };
+    params.redirect = @{ @"return_url": returnURL };
+    return params;
 }
 
 #pragma mark - STPFormEncodable
