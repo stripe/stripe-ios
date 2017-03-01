e8f59b94b16dbf5d713ec581890ee8a14da489d7
Ben Guo <bg@stripe.com>
2017-03-01 17:12:35 -0500

    update to use 10.1 sim

--- Tests/installation_tests/carthage/test.sh
+++ Tests/installation_tests/carthage/test.sh
@@ -21,4 +21,4 @@ echo "git \"$GIT_REPO\" \"$GIT_BRANCH\"" > "$TESTDIR/Cartfile"
 
 carthage bootstrap --platform ios --configuration Debug --no-use-binaries
 
-xcodebuild build -project "${TESTDIR}/CarthageTest.xcodeproj" -scheme CarthageTest -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' | xcpretty -c
+xcodebuild build -project "${TESTDIR}/CarthageTest.xcodeproj" -scheme CarthageTest -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=10.1' | xcpretty -c
