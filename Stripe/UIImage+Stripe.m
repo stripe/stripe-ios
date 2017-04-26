1d8c0da0888507a98ef92fc02ad9aa55d9386aba
Brian Dorfman <bdorfman@stripe.com>
2017-04-26 11:21:12 -0700

    Update resize algo and tests
    Improved the resizing algorithm with one that is a lot better and tweaked tests based on PR feedback.

--- Stripe/UIImage+Stripe.m
+++ Stripe/UIImage+Stripe.m
@@ -13,17 +13,32 @@
 - (NSData *)stp_jpegDataWithMaxFileSize:(NSUInteger)maxBytes {
     CGFloat scale = 1.0;
     NSData *imageData = UIImageJPEGRepresentation(self, 0.5);
-    while (imageData.length > maxBytes) {
-        scale = scale * (CGFloat) 0.8;
-        CGSize newImageSize = CGSizeMake(self.size.width * scale,
-                                         self.size.height *scale);
-        UIGraphicsBeginImageContextWithOptions(newImageSize, NO, self.scale);
-        [self drawInRect:CGRectMake(0, 0, newImageSize.width, newImageSize.height)];
-        UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        imageData = UIImageJPEGRepresentation(newImage, 0.5);
-    }
 
+    // Try something smarter first
+    if (imageData.length > maxBytes) {
+        // Assuming jpeg file size roughly scales linearly with area of the image
+        // which is ~correct (although breaks down at really small file sizes)
+        CGFloat percentSmallerNeeded = (CGFloat) maxBytes / imageData.length;
+
+        // Shrink to a little bit less than we need to try to ensure we're under
+        // (otherwise its likely our first pass will be over the limit due to
+        // compression variance and floating point rounding)
+        scale = scale * sqrt(percentSmallerNeeded - (percentSmallerNeeded * 0.05));
+
+        do {
+            CGSize newImageSize = CGSizeMake(floor(self.size.width * scale),
+                                             floor(self.size.height *scale));
+            UIGraphicsBeginImageContextWithOptions(newImageSize, NO, self.scale);
+            [self drawInRect:CGRectMake(0, 0, newImageSize.width, newImageSize.height)];
+            UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+            UIGraphicsEndImageContext();
+            imageData = UIImageJPEGRepresentation(newImage, 0.5);
+
+            // If the smart thing doesn't work, just start scaling down a bit on a loop until we get there
+            scale = scale * (CGFloat) 0.7;
+        } while (imageData.length > maxBytes);
+
+    }
     return imageData;
 }
 
--- Tests/Tests/UIImage+StripeTests.m
+++ Tests/Tests/UIImage+StripeTests.m
@@ -18,25 +18,43 @@
 @implementation UIImage_StripeTests
 
 - (void)testJpegImageResizing {
-    // Strategy is to grab an image from our bundle and resize it to
-    // both smaller and bigger size than it already is and make sure we get
-    // what we expect
+    // Strategy is to grab an image from our bundle and pass to the resizer
+    // with maximums both larger and smaller than it already is
+    // then make sure we get what we expect
 
     UIImage *testImage = [STPImageLibrary safeImageNamed:@"stp_shipping_form@3x.png"
                                      templateIfAvailable:NO];
 
-    NSData *data = nil;
-    data = [testImage stp_jpegDataWithMaxFileSize:50000];
-    XCTAssertNotNil(data);
-    XCTAssertTrue(data.length < 50000);
+    static const NSUInteger kBiggerSize = 50000;
+    static const NSUInteger kSmallerSize = 10000;
+    static const NSUInteger kMuchSmallerSize = 5000; // don't make this too low or test becomes somewhat meaningless, as jpegs can only get so small
 
-    data = [testImage stp_jpegDataWithMaxFileSize:10000];
-    XCTAssertNotNil(data);
-    XCTAssertTrue(data.length < 10000);
 
-    data = [testImage stp_jpegDataWithMaxFileSize:1000];
+    // Verify that before being passed to resizer it is within the
+    // correct size range for our tests to be meaningful
+    NSData *data = UIImageJPEGRepresentation(testImage, 0.5);
     XCTAssertNotNil(data);
-    XCTAssertTrue(data.length < 1000);
+    XCTAssertTrue(data.length < kBiggerSize);
+    XCTAssertTrue(data.length > kSmallerSize);
+    XCTAssertTrue(data.length > kMuchSmallerSize);
+
+    // Test passing in a maxBytes larger than original image
+    data = [testImage stp_jpegDataWithMaxFileSize:kBiggerSize];
+    XCTAssertNotNil(data);
+    XCTAssertTrue(data.length < kBiggerSize);
+    UIImage *resizedImage = [UIImage imageWithData:data scale:testImage.scale];
+    // Image shouldn't have been shrunk at all
+    XCTAssertTrue(CGSizeEqualToSize(resizedImage.size, testImage.size));
+
+    // Test passing in a maxBytes a bit smaller than the original image
+    data = [testImage stp_jpegDataWithMaxFileSize:kSmallerSize];
+    XCTAssertNotNil(data);
+    XCTAssertTrue(data.length < kSmallerSize);
+
+    // Test passing in a maxBytes a lot smaller than the original image
+    data = [testImage stp_jpegDataWithMaxFileSize:kMuchSmallerSize];
+    XCTAssertNotNil(data);
+    XCTAssertTrue(data.length < kMuchSmallerSize);
 }
 
 @end
