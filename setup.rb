f67b6c9ba107eab2ac42958268696f43e35f676a
Ben Guo <bg@stripe.com>
2017-05-19 13:02:42 -0400

    add build phase error if dependencies aren't installed

--- setup.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-# encoding: utf-8
-
-# This script installs dependencies necessary to run the Swift example app.
-puts '▸ Installing dependencies for Stripe iOS Example (Simple)'
-system('cd Example; carthage bootstrap --platform ios')
-puts '▸ Finished installing dependencies for Stripe iOS Example (Simple)'
