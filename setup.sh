cb2af9f29b66d7d6efc4103647ac61e1b87640ea
Joey Dong <joeydong@stripe.com>
2017-06-12 14:13:32 -0700

    Check that `carthage` available before bootstrapping in `setup.sh`

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 echo '▸ Installing dependencies for Stripe iOS Example (Simple)';
+
 cd Example;
 rm Cartfile.resolved;
+
+if ! command -v carthage > /dev/null; then
+  echo ''
+  echo 'ERROR: Please install carthage before running setup.sh:'
+  echo 'https://github.com/Carthage/Carthage#installing-carthage';
+  exit 1;
+fi
+
 carthage bootstrap --platform ios;
+
 echo '▸ Finished installing dependencies for Stripe iOS Example (Simple)';
