e110b59f9811d9abba13b9497707d910270e44ac
Joey Dong <joeydong@stripe.com>
2017-06-20 12:16:29 -0700

    Fix `run_analyzer.sh` to exit unsuccessfully on analyze warnings
    We previously used `scan-build` to detect analyze warnings but that
    broke. Switching to a simpler approach which can also break but works
    well for now.

--- ci_scripts/run_analyzer.sh
+++ ci_scripts/run_analyzer.sh
@@ -1,5 +1,42 @@
-gem install xcpretty --no-ri --no-rdoc
-export PYTHONUSERBASE=~/.local
-easy_install --user scan-build
-export PATH="${HOME}/.local/bin:${PATH}"
-set -o pipefail && scan-build --status-bugs --use-analyzer Xcode xcodebuild analyze -workspace Stripe.xcworkspace -scheme "StripeiOS" -configuration Debug -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO | xcpretty
+#!/bin/sh
+
+log_file="${TMPDIR}/xcodebuild_analyze.log"
+
+# Install xcpretty
+if ! command -v xcpretty > /dev/null; then
+  echo "Installing xcpretty..."
+  gem install xcpretty --no-ri --no-rdoc
+fi
+
+# Reset log file
+echo "Resetting log file..."
+rm -f "${log_file}"
+
+# Run static analyzer
+echo "Running static analyzer..."
+xcodebuild clean analyze \
+  -workspace "Stripe.xcworkspace" \
+  -scheme "StripeiOS" \
+  -configuration "Debug" \
+  -sdk "iphonesimulator" \
+  ONLY_ACTIVE_ARCH=NO \
+  | tee "${log_file}" \
+  | xcpretty
+
+exit_code="${PIPESTATUS[0]}"
+
+if [[ "${exit_code}" != 0 ]]; then
+  echo "ERROR: xcodebuild exited with non-zero status code: ${exit_code}"
+  exit 1
+fi
+
+# Search for warnings in log file
+echo "Searching for static analyzer warnings..."
+grep "warning:" "${log_file}" > "/dev/null"
+
+if [[ "$?" != 1 ]]; then
+  echo "ERROR: Found static analyzer warnings!"
+  exit 1
+fi
+
+echo "All good!"
