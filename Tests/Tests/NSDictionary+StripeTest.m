f9259a6eb92675a29223aea29739de27dadd7036
Ben Guo <bg@stripe.com>
2017-12-18 18:32:17 -0500

    add safe NSDictionary getters

--- Tests/Tests/NSDictionary+StripeTest.m
+++ Tests/Tests/NSDictionary+StripeTest.m
@@ -164,4 +164,82 @@
     XCTAssertFalse([result isKindOfClass:[NSMutableDictionary class]]);
 }
 
+#pragma mark - Getters
+
+- (void)testArrayForKey {
+    NSDictionary *dict = @{
+                           @"a": @[@"foo"],
+                           };
+
+    XCTAssertEqualObjects([dict stp_arrayForKey:@"a"], @[@"foo"]);
+    XCTAssertNil([dict stp_arrayForKey:@"b"]);
+}
+
+- (void)testBoolForKey {
+    NSDictionary *dict = @{
+                           @"a": @1,
+                           @"b": @0,
+                           @"c": @"true",
+                           @"d": @"false",
+                           @"e": @"1",
+                           @"f": @"foo",
+                           };
+
+    XCTAssertTrue([dict stp_boolForKey:@"a" or:NO]);
+    XCTAssertFalse([dict stp_boolForKey:@"b" or:YES]);
+    XCTAssertTrue([dict stp_boolForKey:@"c" or:NO]);
+    XCTAssertFalse([dict stp_boolForKey:@"d" or:YES]);
+    XCTAssertTrue([dict stp_boolForKey:@"e" or:NO]);
+    XCTAssertFalse([dict stp_boolForKey:@"f" or:NO]);
+}
+
+- (void)testIntForKey {
+    NSDictionary *dict = @{
+                           @"a": @1,
+                           @"b": @-1,
+                           @"c": @"1",
+                           @"d": @"-1",
+                           @"e": @"10.0",
+                           @"f": @"10.5",
+                           @"g": @(10.0),
+                           @"h": @(10.5),
+                           @"i": @"foo",
+                           };
+
+    XCTAssertEqual([dict stp_intForKey:@"a" or:0], 1);
+    XCTAssertEqual([dict stp_intForKey:@"b" or:0], -1);
+    XCTAssertEqual([dict stp_intForKey:@"c" or:0], 1);
+    XCTAssertEqual([dict stp_intForKey:@"e" or:0], 10);
+    XCTAssertEqual([dict stp_intForKey:@"f" or:0], 10);
+    XCTAssertEqual([dict stp_intForKey:@"g" or:0], 10);
+    XCTAssertEqual([dict stp_intForKey:@"h" or:0], 10);
+    XCTAssertEqual([dict stp_intForKey:@"i" or:0], 0);
+}
+
+- (void)testDateForKey {
+    NSDictionary *dict = @{
+                           @"a": @0,
+                           @"b": @"0",
+                           };
+    NSDate *expectedDate = [NSDate dateWithTimeIntervalSince1970:0];
+
+    XCTAssertEqualObjects([dict stp_dateForKey:@"a"], expectedDate);
+    XCTAssertEqualObjects([dict stp_dateForKey:@"b"], expectedDate);
+}
+
+- (void)testDictionaryForKey {
+    NSDictionary *dict = @{
+                           @"a": @{@"foo": @"bar"},
+                           };
+
+    XCTAssertEqualObjects([dict stp_dictionaryForKey:@"a"], @{@"foo": @"bar"});
+    XCTAssertNil([dict stp_dictionaryForKey:@"b"]);
+}
+
+- (void)testStringForKey {
+    NSDictionary *dict = @{@"a": @"foo"};
+    XCTAssertEqualObjects([dict stp_stringForKey:@"a"], @"foo");
+    XCTAssertNil([dict stp_stringForKey:@"b"]);
+}
+
 @end
