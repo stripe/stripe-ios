dd3f3da3ec014f41b67dde27be57a2e7fe8c55b3
Dan Jackson <danj@stripe.com>
2018-04-17 14:32:38 -0700

    Add basic STPSourceParams unit test for masterpass api parameters

--- Tests/Tests/STPSourceParamsTest.m
+++ Tests/Tests/STPSourceParamsTest.m
@@ -203,6 +203,19 @@
     XCTAssertEqualObjects(sourceAddress[@"country"], card.address.country);
 }
 
+- (void)testParamsWithMasterPass {
+    STPSourceParams *params = [STPSourceParams masterpassParamsWithCartId:@"12345678"
+                                                            transactionId:@"87654321"];
+
+    XCTAssertEqual(params.type, STPSourceTypeCard);
+    NSDictionary *sourceCard = params.additionalAPIParameters[@"card"];
+    XCTAssertNotNil(sourceCard);
+    NSDictionary *sourceMasterpass = sourceCard[@"masterpass"];
+    XCTAssertNotNil(sourceMasterpass);
+    XCTAssertEqualObjects(sourceMasterpass[@"cart_id"], @"12345678");
+    XCTAssertEqualObjects(sourceMasterpass[@"transaction_id"], @"87654321");
+}
+
 
 #pragma mark - Redirect Dictionary Tests
 
