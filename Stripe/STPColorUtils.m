83935099a2190b0dd7a115894391604d6b181b8a
Dan Jackson <danj@stripe.com>
2018-05-30 14:41:37 -0700

    Fixes crash in `STPColorUtils.perceivedBrightnessForColor` (#954)
    This is used for deciding whether to use light/dark scroll bars & status bar indicators,
    and choosing which color should be used for the switch in `STPSwitchTableViewCell`.
    
    This code assumed the color was always in the RGB color space, which is incorrect. It would
    reliably crash with Address Sanitization turned on, and give inconsistent answers with
    it turned off.
    
    Now using the `UIColor` `getRed:green:blue:alpha:` method to convert the color to RGB
    before calculating the luma value. Also added some links to documentation for why this
    calculation is interesting.
    
    Added tests for `STPColorUtils colorIsBright:`
    This also tests `perceivedBrightnessForColor:`, but does so by checking something that's
    more meaningful to me, rather than testing for specific luma numbers

--- Stripe/STPColorUtils.m
+++ Stripe/STPColorUtils.m
@@ -11,9 +11,19 @@
 @implementation STPColorUtils
 
 + (CGFloat)perceivedBrightnessForColor:(UIColor *)color {
-    const CGFloat *component = CGColorGetComponents(color.CGColor);
-    CGFloat brightness = ((component[0] * 299) + (component[1] * 587) + (component[2] * 114)) / 1000;
-    return brightness;
+    CGFloat red, green, blue;
+    if ([color getRed:&red green:&green blue:&blue alpha:nil]) {
+        // We're using the luma value from YIQ
+        // https://en.wikipedia.org/wiki/YIQ#From_RGB_to_YIQ
+        // recommended by https://www.w3.org/WAI/ER/WD-AERT/#color-contrast
+        return red * (CGFloat)0.299 + green * (CGFloat)0.587 + blue * (CGFloat)0.114;
+    } else {
+        // Couldn't get RGB for this color, device couldn't convert it from whatever
+        // colorspace it's in.
+        // Make it "bright", since most of the color space is (based on our current
+        // formula), but not very bright.
+        return (CGFloat)0.4;
+    }
 }
 
 + (UIColor *)brighterColor:(UIColor *)color1 color2:(UIColor *)color2 {
