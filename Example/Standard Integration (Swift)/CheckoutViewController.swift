a1df464c27ae731e853e7006c918d3fae8dd7b01
Joseph Gardi <joeyg@stripe.com>
2018-07-05 12:36:17 -0700

    made standard integration run

--- Example/Standard Integration (Swift)/CheckoutViewController.swift
+++ Example/Standard Integration (Swift)/CheckoutViewController.swift
@@ -13,17 +13,17 @@ class CheckoutViewController: UIViewController, STPPaymentContextDelegate {
 
     // 1) To get started with this demo, first head to https://dashboard.stripe.com/account/apikeys
     // and copy your "Test Publishable Key" (it looks like pk_test_abcdef) into the line below.
-    let stripePublishableKey = ""
+    let stripePublishableKey = "pk_test_vNfjx5fIqtH12V0PiJ5HTZr9"
 
     // 2) Next, optionally, to have this demo save your user's payment details, head to
     // https://github.com/stripe/example-ios-backend/tree/v13.0.3, click "Deploy to Heroku", and follow
     // the instructions (don't worry, it's free). Replace nil on the line below with your
     // Heroku URL (it looks like https://blazing-sunrise-1234.herokuapp.com ).
-    let backendBaseURL: String? = nil
+    let backendBaseURL: String? = "https://stripe-ios-example-backend.herokuapp.com/"
 
     // 3) Optionally, to enable Apple Pay, follow the instructions at https://stripe.com/docs/mobile/apple-pay
     // to create an Apple Merchant ID. Replace nil on the line below with it (it looks like merchant.com.yourappname).
-    let appleMerchantID: String? = nil
+    let appleMerchantID: String? = "merchant.com.stripe.ios.internal.test"
 
     // These values will be shown to the user when they purchase with Apple Pay.
     let companyName = "Emoji Apparel"
