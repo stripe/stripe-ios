2321d4975814cd6a85081f0762fe767ecaa4a7d0
Joey Dong <joeydong@stripe.com>
2018-07-09 16:32:48 -0700

    Combine 32-bit and 64-bit test steps by setting `ONLY_ACTIVE_ARCH=NO`

--- ci_scripts/run_tests.sh
+++ ci_scripts/run_tests.sh
@@ -46,10 +46,11 @@ if [[ "${exit_code}" != 0 ]]; then
   die "xcodebuild exited with non-zero status code: ${exit_code}"
 fi
 
-# Execute tests on legacy devices (iPhone 6 @ iOS 10.x, iPhone 6 @ iOS 9.x)
+# Execute tests on legacy devices (iPhone 6 @ iOS 10.x, iPhone 6 @ iOS 9.x, iPhone 4s @ iOS 9.x)
 # - Skips snapshot tests because they're recorded for the iPhone 6 on the newest iOS version only
 # - Not sure why tests STPImageLibraryTest fail on older iOS versions
-info "Executing tests on legacy devices (iPhone 6 @ iOS 10.x, iPhone 6 @ iOS 9.x)..."
+# - Set `ONLY_ACTIVE_ARCH=NO` to build both 32-bit and 64-bit products
+info "Executing tests on legacy devices (iPhone 6 @ iOS 10.x, iPhone 6 @ iOS 9.x, iPhone 4s @ iOS 9.x)..."
 
 xcodebuild clean test \
   -workspace "Stripe.xcworkspace" \
@@ -58,35 +59,13 @@ xcodebuild clean test \
   -sdk "iphonesimulator" \
   -destination "platform=iOS Simulator,name=iPhone 6,OS=10.3.1" \
   -destination "platform=iOS Simulator,name=iPhone 6,OS=9.3" \
-  -skip-testing:"StripeiOS Tests/STPAddCardViewControllerLocalizationTests" \
-  -skip-testing:"StripeiOS Tests/STPPaymentMethodsViewControllerLocalizationTests" \
-  -skip-testing:"StripeiOS Tests/STPShippingAddressViewControllerLocalizationTests" \
-  -skip-testing:"StripeiOS Tests/STPShippingMethodsViewControllerLocalizationTests" \
-  -skip-testing:"StripeiOS Tests/STPImageLibraryTest" \
-  | xcpretty
-
-exit_code="${PIPESTATUS[0]}"
-
-if [[ "${exit_code}" != 0 ]]; then
-  die "xcodebuild exited with non-zero status code: ${exit_code}"
-fi
-
-# Execute tests on 32-bit device (iPhone 4s @ iOS 9.x)
-# - Skips snapshot tests because they're recorded for the iPhone 6 on the newest iOS version only
-# - Not sure why tests STPImageLibraryTest fail on older iOS versions
-info "Executing tests on 32-bit device  (iPhone 4s @ iOS 9.x)..."
-
-xcodebuild clean test \
-  -workspace "Stripe.xcworkspace" \
-  -scheme "StripeiOS" \
-  -configuration "Debug" \
-  -sdk "iphonesimulator" \
   -destination "platform=iOS Simulator,name=iPhone 4s,OS=9.3" \
   -skip-testing:"StripeiOS Tests/STPAddCardViewControllerLocalizationTests" \
   -skip-testing:"StripeiOS Tests/STPPaymentMethodsViewControllerLocalizationTests" \
   -skip-testing:"StripeiOS Tests/STPShippingAddressViewControllerLocalizationTests" \
   -skip-testing:"StripeiOS Tests/STPShippingMethodsViewControllerLocalizationTests" \
   -skip-testing:"StripeiOS Tests/STPImageLibraryTest" \
+  ONLY_ACTIVE_ARCH=NO \
   | xcpretty
 
 exit_code="${PIPESTATUS[0]}"
