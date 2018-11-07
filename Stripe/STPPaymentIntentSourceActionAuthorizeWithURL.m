98a68e7b919a46a9cc4f26029c61cbbb1c5f4863
Dan Jackson <danj@stripe.com>
2018-11-07 11:07:35 -0800

    Fill out implementation of STPPaymentIntentSourceActionAuthorizeWithURL

--- Stripe/STPPaymentIntentSourceActionAuthorizeWithURL.m
+++ Stripe/STPPaymentIntentSourceActionAuthorizeWithURL.m
@@ -8,14 +8,50 @@
 
 #import "STPPaymentIntentSourceActionAuthorizeWithURL.h"
 
+#import "NSDictionary+Stripe.h"
+
+@interface STPPaymentIntentSourceActionAuthorizeWithURL()
+@property (nonatomic, strong, nonnull, readwrite) NSURL *url;
+@property (nonatomic, strong, nullable, readwrite) NSURL *returnURL;
+@property (nonatomic, readwrite, nonnull, copy) NSDictionary *allResponseFields;
+@end
+
 @implementation STPPaymentIntentSourceActionAuthorizeWithURL
 
 @synthesize allResponseFields;
 
+- (NSString *)description {
+    NSArray *props = @[
+                       // Object
+                       [NSString stringWithFormat:@"%@: %p", NSStringFromClass([self class]), self],
+
+                       // AuthorizeWithURL details (alphabetical)
+                       [NSString stringWithFormat:@"returnURL = %@", self.returnURL],
+                       [NSString stringWithFormat:@"url = %@", self.url],
+                       ];
+
+    return [NSString stringWithFormat:@"<%@>", [props componentsJoinedByString:@"; "]];
+}
+
 + (nullable instancetype)decodedObjectFromAPIResponse:(nullable NSDictionary *)response {
-    // TODO
-    NSLog(@"%@", response);
-    return nil;
+    NSDictionary *dict = [response stp_dictionaryByRemovingNulls];
+    if (!dict) {
+        return nil;
+    }
+
+    // required fields
+    NSURL *url = [response stp_urlForKey:@"url"];
+    if (!url) {
+        return nil;
+    }
+
+    STPPaymentIntentSourceActionAuthorizeWithURL *authorize = [self new];
+
+    authorize.url = url;
+    authorize.returnURL = [dict stp_urlForKey:@"return_url"];
+    authorize.allResponseFields = dict;
+
+    return authorize;
 }
 
 @end
