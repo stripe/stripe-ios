c3b3903c32850efae836ca10f3e192ab75d19876
Jack Flintermann <jack@stripe.com>
2018-11-24 23:57:22 -0800

    code comments

--- Tests/Tests/STPNetworkStubbingTestCase.h
+++ Tests/Tests/STPNetworkStubbingTestCase.h
@@ -10,7 +10,11 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ Test cases that subclass `STPNetworkStubbingTestCase` will automatically capture all network traffic when run with `recordingMode = YES` and save it to disk. When run with `recordingMode = NO`, they will use the persisted request/response pairs, and raise an exception if an unexpected HTTP request is made.
+ */
 @interface STPNetworkStubbingTestCase : XCTestCase
+// Set this to YES to record all traffic during this test. The test will then fail to remind you to set this back to NO before pushing.
 @property (nonatomic) BOOL recordingMode;
 @end
 
