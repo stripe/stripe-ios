4baf622291ea144f4c7e97b2169a739ea3638ea6
Yuki Tokuhiro <yuki@stripe.com>
2019-03-12 20:47:34 -0700

    STPPaymentMethod+Private should be a class extension, not a category

--- Stripe/STPPaymentMethod+Private.h
+++ Stripe/STPPaymentMethod+Private.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface STPPaymentMethod (Private)
+@interface STPPaymentMethod ()
 
 + (STPPaymentMethodType)typeFromString:(NSString *)string;
 + (nullable NSString *)stringFromType:(STPPaymentMethodType)type;
