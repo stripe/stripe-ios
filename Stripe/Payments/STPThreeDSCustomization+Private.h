563938a6de0a77a291c92deb43bfc6062373ff10
Yuki Tokuhiro <yuki@stripe.com>
2019-06-17 10:40:54 -0700

    ButtonCustomization

---
+++ Stripe/Payments/STPThreeDSCustomization+Private.h
@@ -0,0 +1,13 @@
+//
+//  STPThreeDSCustomization+Private.h
+//  StripeiOS
+//
+//  Created by Yuki Tokuhiro on 6/17/19.
+//  Copyright © 2019 Stripe, Inc. All rights reserved.
+//
+
+@class STDSButtonCustomization;
+
+@interface STPThreeDSButtonCustomization ()
+@property (nonatomic, strong) STDSButtonCustomization *buttonCustomization;
+@end
