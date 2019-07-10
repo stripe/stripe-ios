384a16c4ce3e29a35dc534bd5ee4854c9b88768a
Yuki Tokuhiro <yuki@stripe.com>
2019-07-10 16:34:51 -0700

    Revert "PR feedback"
    This reverts commit f9bc29cb393ffb3fcc7a921c7330b4961cddfa30.

--- Stripe/PublicHeaders/STPThreeDSCustomizationSettings.h
+++ Stripe/PublicHeaders/STPThreeDSCustomizationSettings.h
@@ -38,13 +38,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  `authenticationTimeout` is the total time allowed for a user to complete a 3DS2 authentication
- interaction. This value *must* be at least 5 minutes.
- 
- @note The value is rounded down to the nearest minute.
+ interaction, in minutes.  This value *must* be at least 5 minutes.
  
  Defaults to 5 minutes.
  */
-@property (nonatomic) NSTimeInterval authenticationTimeout;
+@property (nonatomic) NSInteger authenticationTimeout;
 
 @end
 
