06e8fd0dd0c3fdfae9c13a9fea9f3733615c25b0
Josh Avant <joshavant@gmail.com>
2019-07-29 17:09:01 -0500

    Call STPRedirectContext completion handler after SFSafariVC dismissal has completed (#1229)
    * add SafariVC Transitioning infrastructure
    
    * call completion in SFSVCDismissalDelegate method
    
    * remove broken unit tests
    
    * Apply conventions
    
    Co-Authored-By: Yuki Tokuhiro <yuki@stripe.com>
    
    * remove UIVCTransitioningDelegate middleman
    
    * Update unused variable decorations
    
    * Revert "remove broken unit tests"
    
    This reverts commit da0a083d08ea247322d2c8b7489ce65223fe25a1.
    
    * fix STPRedirectContext tests
    
    * pass nil instead of error on SFVCDidFinish

--- Stripe/STPRedirectContext+Private.h
+++ Stripe/STPRedirectContext+Private.h
@@ -7,10 +7,15 @@
 //
 
 #import "STPRedirectContext.h"
+#import <SafariServices/SafariServices.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface STPRedirectContext()
+@protocol STPSafariViewControllerDismissalDelegate <NSObject>
+- (void)safariViewControllerDidCompleteDismissal:(SFSafariViewController *)controller;
+@end
+
+@interface STPRedirectContext() <STPSafariViewControllerDismissalDelegate>
 
 /// Optional URL for a native app. This is passed directly to `UIApplication openURL:`, and if it fails this class falls back to `redirectURL`
 @property (nonatomic, nullable, copy) NSURL *nativeRedirectURL;
@@ -20,6 +25,8 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, copy) NSURL *returnURL;
 /// Completion block to execute when finished redirecting, with optional error parameter.
 @property (nonatomic, copy) STPErrorBlock completion;
+/// Error parameter for completion block.
+@property (nonatomic, nullable, copy) NSError *completionError;
 
 @end
 
