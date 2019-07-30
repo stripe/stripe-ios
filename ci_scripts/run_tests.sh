f694971987414762e7728925c4dac8af974571b6
David Estes <davidestes@stripe.com>
2019-07-29 20:18:31 -0700

    Don't run the legacy tests in the main test, that's the entire point of moving the tests!

--- ci_scripts/run_tests.sh
+++ ci_scripts/run_tests.sh
@@ -63,35 +63,4 @@ if [[ "${exit_code}" != 0 ]]; then
   die "xcodebuild exited with non-zero status code: ${exit_code}"
 fi
 
-# Execute tests on legacy devices (iPhone 6 @ iOS 10.x, iPhone 6 @ iOS 9.x, iPhone 4s @ iOS 9.x)
-# - Skips snapshot tests because they're recorded for the iPhone 6 on the newest iOS version only
-# - Not sure why tests STPImageLibraryTest fail on older iOS versions
-# - Set `ONLY_ACTIVE_ARCH=NO` to build both 32-bit and 64-bit products
-info "Executing tests on legacy devices (iPhone 6 @ iOS 10.x, iPhone 6 @ iOS 9.x, iPhone 4s @ iOS 9.x)..."
-
-# Workaround for iOS 9.3 erroring w/ “dyld: Library not loaded: /usr/lib/libauto.dylib” 
-# See https://developer.apple.com/documentation/xcode_release_notes/xcode_10_2_1_release_notes
-sudo mkdir '/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS 9.3.simruntime/Contents/Resources/RuntimeRoot/usr/lib/swift'
-
-xcodebuild clean test \
-  -workspace "Stripe.xcworkspace" \
-  -scheme "StripeiOS" \
-  -configuration "Debug" \
-  -sdk "iphonesimulator" \
-  -destination "platform=iOS Simulator,name=iPhone 6,OS=10.3.1" \
-  -destination "platform=iOS Simulator,name=iPhone 6,OS=9.3" \
-  -destination "platform=iOS Simulator,name=iPhone 4s,OS=9.3" \
-  -skip-testing:"StripeiOS Tests/STPAddCardViewControllerLocalizationTests" \
-  -skip-testing:"StripeiOS Tests/STPPaymentOptionsViewControllerLocalizationTests" \
-  -skip-testing:"StripeiOS Tests/STPShippingAddressViewControllerLocalizationTests" \
-  -skip-testing:"StripeiOS Tests/STPShippingMethodsViewControllerLocalizationTests" \
-  ONLY_ACTIVE_ARCH=NO \
-  | xcpretty
-
-exit_code="${PIPESTATUS[0]}"
-
-if [[ "${exit_code}" != 0 ]]; then
-  die "xcodebuild exited with non-zero status code: ${exit_code}"
-fi
-
 info "All good!"
