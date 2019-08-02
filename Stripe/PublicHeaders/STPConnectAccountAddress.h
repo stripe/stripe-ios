4d953a644f55da1dfe81b5d8f4ece4c0da57c4a8
Yuki Tokuhiro <yuki@stripe.com>
2019-08-02 13:42:48 -0700

    Fill out STPConnectAccountCompanyParams

--- Stripe/PublicHeaders/STPConnectAccountAddress.h
+++ Stripe/PublicHeaders/STPConnectAccountAddress.h
@@ -13,7 +13,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /**
- An address for use with `STPConnectAccountParams`.
+ An address to use with `STPConnectAccountParams`.
  */
 @interface STPConnectAccountAddress : NSObject <STPFormEncodable>
 
--- Stripe/STPConnectAccountCompanyParams.m
+++ Stripe/STPConnectAccountCompanyParams.m
@@ -10,4 +10,41 @@
 
 @implementation STPConnectAccountCompanyParams
 
+- (NSString *)description {
+    NSArray *props = @[
+                       // Object
+                       [NSString stringWithFormat:@"%@: %p", NSStringFromClass([self class]), self],
+                       
+                       // Properties omitted b/c they're PII
+                       ];
+    
+    return [NSString stringWithFormat:@"<%@>", [props componentsJoinedByString:@"; "]];
+}
+
+#pragma mark - STPFormEncodable
+
+@synthesize additionalAPIParameters;
+
++ (nonnull NSDictionary *)propertyNamesToFormFieldNamesMapping {
+    return @{
+             NSStringFromSelector(@selector(address)): @"address",
+             NSStringFromSelector(@selector(kanaAddress)): @"address_kana",
+             NSStringFromSelector(@selector(kanjiAddress)): @"address_kanji",
+             NSStringFromSelector(@selector(directorsProvided)): @"directorsProvided",
+             NSStringFromSelector(@selector(name)): @"name",
+             NSStringFromSelector(@selector(kanaName)): @"name_kana",
+             NSStringFromSelector(@selector(kanjiName)): @"name_kanji",
+             NSStringFromSelector(@selector(ownersProvided)): @"owners_provided",
+             NSStringFromSelector(@selector(phone)): @"phone",
+             NSStringFromSelector(@selector(taxID)): @"tax_id",
+             NSStringFromSelector(@selector(taxIDRegistrar)): @"tax_id_registrar",
+             NSStringFromSelector(@selector(vatID)): @"vat_id",
+             };
+}
+
++ (nullable NSString *)rootObjectName {
+    return nil;
+}
+
 @end
+
