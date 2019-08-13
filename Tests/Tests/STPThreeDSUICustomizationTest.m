df5768c9767ffbb061490baa86bb18c73575ce83
Yuki Tokuhiro <yuki@stripe.com>
2019-08-13 10:06:02 -0700

    Add preferredStatusBarStyle to STPThreeDSUICustomization

--- Tests/Tests/STPThreeDSUICustomizationTest.m
+++ Tests/Tests/STPThreeDSUICustomizationTest.m
@@ -48,6 +48,7 @@
     customization.backgroundColor = UIColor.redColor;
     customization.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhiteLarge;
     customization.blurStyle = UIBlurEffectStyleDark;
+    customization.preferredStatusBarStyle = UIStatusBarStyleLightContent;
     
     XCTAssertEqual(UIColor.redColor, customization.backgroundColor);
     XCTAssertEqual(customization.backgroundColor, customization.uiCustomization.backgroundColor);
@@ -57,6 +58,9 @@
     
     XCTAssertEqual(UIBlurEffectStyleDark, customization.blurStyle);
     XCTAssertEqual(customization.blurStyle, customization.uiCustomization.blurStyle);
+    
+    XCTAssertEqual(UIStatusBarStyleLightContent, customization.preferredStatusBarStyle);
+    XCTAssertEqual(customization.preferredStatusBarStyle, customization.uiCustomization.preferredStatusBarStyle);
 }
 
 @end
