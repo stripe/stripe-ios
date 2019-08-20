78307b60c673be2eed330e40f1cf68fa12d93fdd
Yuki Tokuhiro <yuki@stripe.com>
2019-08-20 10:53:10 -0700

    Fill in empty STPConnectAccountAddressTest

--- Tests/Tests/STPConnectAccountAddressTest.m
+++ Tests/Tests/STPConnectAccountAddressTest.m
@@ -8,30 +8,36 @@
 
 #import <XCTest/XCTest.h>
 
+#import "STPConnectAccountAddress.h"
+
 @interface STPConnectAccountAddressTest : XCTestCase
 
 @end
 
 @implementation STPConnectAccountAddressTest
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+#pragma mark STPFormEncodable Tests
+
+- (void)testRootObjectName {
+    XCTAssertNil([STPConnectAccountAddress rootObjectName]);
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testPropertyNamesToFormFieldNamesMapping {
+    STPConnectAccountAddress *address = [STPConnectAccountAddress new];
+    
+    NSDictionary *mapping = [STPConnectAccountAddress propertyNamesToFormFieldNamesMapping];
+    
+    for (NSString *propertyName in [mapping allKeys]) {
+        XCTAssertFalse([propertyName containsString:@":"]);
+        XCTAssert([address respondsToSelector:NSSelectorFromString(propertyName)]);
+    }
+    
+    for (NSString *formFieldName in [mapping allValues]) {
+        XCTAssert([formFieldName isKindOfClass:[NSString class]]);
+        XCTAssert([formFieldName length] > 0);
+    }
+    
+    XCTAssertEqual([[mapping allValues] count], [[NSSet setWithArray:[mapping allValues]] count]);
 }
 
 @end
