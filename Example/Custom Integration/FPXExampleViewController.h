482d732d7424374ab264c9027bbc6d24c507c9ff
David Estes <davidestes@stripe.com>
2019-08-26 16:37:49 -0700

    Fix copypasta

--- Example/Custom Integration/FPXExampleViewController.h
+++ Example/Custom Integration/FPXExampleViewController.h
@@ -2,8 +2,8 @@
 //  FPXExampleViewController.h
 //  Custom Integration
 //
-//  Created by Ben Guo on 2/22/17.
-//  Copyright © 2017 Stripe. All rights reserved.
+//  Created by David Estes on 8/26/19.
+//  Copyright © 2019 Stripe. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
