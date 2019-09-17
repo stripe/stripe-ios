0d47e77758dddc47ea28094e8b53c34c117e3c88
Yuki Tokuhiro <yuki@stripe.com>
2019-09-17 16:51:30 -0700

    [Remove] add test keys

--- Example/Custom Integration/Constants.m
+++ Example/Custom Integration/Constants.m
@@ -9,11 +9,11 @@
 #import "Constants.h"
 
 // This can be found at https://dashboard.stripe.com/account/apikeys
-NSString *const StripePublishableKey = nil; // TODO: replace nil with your own value
+NSString *const StripePublishableKey = @"pk_test_JBVAMwnBuzCdmsgN34jfxbU700LRiPqVit"; // TODO: replace nil with your own value
 
 // To set this up, check out https://github.com/stripe/example-ios-backend/tree/v17.0.0
 // This should be in the format https://my-shiny-backend.herokuapp.com
-NSString *const BackendBaseURL = nil; // TODO: replace nil with your own value
+NSString *const BackendBaseURL = @"https://yuki-test-15.herokuapp.com/"; // TODO: replace nil with your own value
 
 // To learn how to obtain an Apple Merchant ID, head to https://stripe.com/docs/mobile/apple-pay
-NSString *const AppleMerchantId = nil; // TODO: replace nil with your own value
+NSString *const AppleMerchantId = @"merchant.com.stripe.apple-pay-qualifications";
