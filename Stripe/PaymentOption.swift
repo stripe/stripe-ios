763ad29221c8a98bba1cf10f867bfc483fd24c07
Yuki Tokuhiro <yuki@stripe.com>
2019-11-12 13:37:39 -0800

    Add a swift file

---
+++ Stripe/PaymentOption.swift
@@ -0,0 +1,13 @@
+//
+//  PaymentOption.swift
+//  StripeiOS
+//
+//  Created by Yuki Tokuhiro on 11/12/19.
+//  Copyright © 2019 Stripe, Inc. All rights reserved.
+//
+
+import Foundation
+
+public struct PaymentOption {
+    let version = "1"
+}
