0fc7f34113261c7de73cc97774d61e1be86e3558
Cameron <36750494+csabol-stripe@users.noreply.github.com>
2020-03-24 15:10:19 -0700

    Fixes completion block type definition (#1526)

--- Stripe/STPAPIClient.m
+++ Stripe/STPAPIClient.m
@@ -459,7 +459,9 @@ static NSString * const APIEndpointPaymentIntents = @"payment_intents";
     }];
 }
 
-- (NSURLSessionDataTask *)retrieveSourceWithId:(NSString *)identifier clientSecret:(NSString *)secret responseCompletion:(STPAPIResponseBlock)completion {
+- (NSURLSessionDataTask *)retrieveSourceWithId:(NSString *)identifier
+                                  clientSecret:(NSString *)secret
+                            responseCompletion:(void (^)(STPSource * _Nullable, NSHTTPURLResponse * _Nullable, NSError * _Nullable))completion {
     NSString *endpoint = [NSString stringWithFormat:@"%@/%@", APIEndpointSources, identifier];
     NSDictionary *parameters = @{@"client_secret": secret};
     return [STPAPIRequest<STPSource *> getWithAPIClient:self
