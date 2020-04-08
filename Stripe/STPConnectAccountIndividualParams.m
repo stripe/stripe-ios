7e8aa0099c9a5265bde9f29259129fa88d05a7d9
Allen Hurst <avhurst@users.noreply.github.com>
2020-04-08 08:00:32 -0700

    Relax need for dob with full name connect account params

--- Stripe/STPConnectAccountIndividualParams.m
+++ Stripe/STPConnectAccountIndividualParams.m
@@ -44,6 +44,10 @@
 @synthesize additionalAPIParameters;
 
 - (STPDateOfBirth *)_dateOfBirth {
+    if (!self.dateOfBirth) {
+        return nil;
+    }
+
     STPDateOfBirth *dob = [STPDateOfBirth new];
     dob.day = self.dateOfBirth.day;
     dob.month = self.dateOfBirth.month;
