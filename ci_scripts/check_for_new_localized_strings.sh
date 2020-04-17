5b60e80729af4c6a7d5c768b3cea196041ede41e
Cameron <36750494+csabol-stripe@users.noreply.github.com>
2020-04-17 15:34:34 -0700

    Adds localization helper scripts (#1541)
    * Adds new scripts to push new localized strings to lokalise, pull translations from lokalise, detect missing translations, and detect newly added localized strings
    
    * Better git diff detection.
    
    * Use custom final status
    
    * Remove custom lang map (set in lokalise instead).
    
    * Request zh-Hant
    
    * Better newline handling

---
+++ ci_scripts/check_for_new_localized_strings.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+find Stripe -name \*.m -print0 | xargs -0 genstrings -s STPLocalizedString -o Stripe/Resources/Localizations/en.lproj
+
+if [[ $? -eq 0 ]]; then
+
+  if [[ -z $(which recode) ]]; then
+    if [[ -z $(which brew) ]]; then
+      echo "Please install homebrew or the recode command line tool"
+      exit 1
+    else
+      brew install recode
+    fi
+  fi
+
+  if [[ $? -eq 0 ]]; then
+
+    # Genstrings outputs in utf16 but we want to store in utf8
+    recode utf16..utf8 Stripe/Resources/Localizations/en.lproj/Localizable.strings
+
+  else
+    echo "Error recoding into utf8"
+    exit 1
+  fi
+else
+  echo "Error occurred generating english strings file."
+  exit 1
+fi
+
+git diff --quiet --exit-code -- Stripe/Resources/Localizations/en.lproj
+if [[ $? -ne 0 ]]; then
+    echo -e "\t\033[0;31mNew strings detected\033[0m"
+    exit 1
+else
+    exit 0
+fi
+
