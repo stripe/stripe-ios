08fc2eeafb8c5655df116d7bc490f6db60b1b6bf
Cameron <36750494+csabol-stripe@users.noreply.github.com>
2020-05-19 10:35:18 -0700

    Adds xcparse command to screenshot script (#1581)

--- ci_scripts/screenshot_localization_tester.sh
+++ ci_scripts/screenshot_localization_tester.sh
@@ -2,9 +2,21 @@
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+if ! command -v xcpretty > /dev/null; then
+
+  info "Installing xcpretty..."
+  gem install xcpretty --no-document || die "Executing \`gem install xcpretty\` failed"
+fi
+
+if [[ -z $(which xcparse) ]]; then
+    echo "Installing xcparse via homebrew..."
+    brew install chargepoint/xcparse/xcparse
+fi
+
 for i in en,US zh-HANS,CN de,DE es,ES it,IT ja,JP nl,NL fr,FR fi,FI nb,NO da,DK pt-BR,BR pt-PT,PT sv,SE es-AR,AR fr-CA,CA nn,NO, en,GB ko,ko ru,ru tr,tr; do
   #statements
   IFS=",";
   set -- $i;
-  xcodebuild clean test -workspace "${script_dir}/../Stripe.xcworkspace" -scheme "LocalizationTester" -configuration "Debug" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone 6s,OS=11.4" -resultBundlePath "${script_dir}/../build/loc_qa/$1_$2" -testLanguage $1 -testRegion $2
+  xcodebuild clean test -workspace "${script_dir}/../Stripe.xcworkspace" -scheme "LocalizationTester" -configuration "Debug" -sdk "iphonesimulator" -destination "platform=iOS Simulator,name=iPhone 6s,OS=11.4" -resultBundlePath "${script_dir}/../build/loc_qa/$1_$2" -testLanguage $1 -testRegion $2 | xcpretty
+  xcparse screenshots "${script_dir}/../build/loc_qa/$1_$2.xcresult" "${script_dir}/../build/loc_qa/$1_$2_screenshots"
 done
