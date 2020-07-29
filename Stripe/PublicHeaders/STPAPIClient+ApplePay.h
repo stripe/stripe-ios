7e74d50f22d93a3132433284a10522db64bf4b31
Yuki <yuki@stripe.com>
2020-07-29 11:12:14 -0700

    `pkPaymentErrorForStripeError` no longer returns PKPaymentUnknownErrors (#1583)

--- Stripe/PublicHeaders/STPAPIClient+ApplePay.h
+++ Stripe/PublicHeaders/STPAPIClient+ApplePay.h
@@ -45,15 +45,16 @@ NS_ASSUME_NONNULL_BEGIN
                             completion:(STPPaymentMethodCompletionBlock)completion;
 
 /**
- Converts Stripe errors into the appropriate Apple Pay error, for use in `PKPaymentAuthorizationResult`. The error is displayed in the Apple Pay sheet, and the user can try again.
+ Converts Stripe errors into the appropriate Apple Pay error, for use in `PKPaymentAuthorizationResult`.
  
- We can convert billing address related errors into a PKPaymentError that helpfully points to the billing address field in the Apple Pay sheet.
- All other errors map to PKPaymentUnknownError, resulting in a generic error message in the Apple Pay sheet.
+ If the error can be fixed by the customer within the Apple Pay sheet, we return an NSError that can be displayed in the Apple Pay sheet.
+ Otherwise, the original error is returned, resulting in the Apple Pay sheet being dismissed. You should display the error message to the customer afterwards.
  
- Apple Pay should prevent most card errors (e.g. invalid CVC) when you add a card to the wallet.
+ Currently, we convert billing address related errors into a PKPaymentError that helpfully points to the billing address field in the Apple Pay sheet.
+  
+ Note that Apple Pay should prevent most card errors (e.g. invalid CVC, expired cards) when you add a card to the wallet.
  
  @param stripeError   An error from the Stripe SDK.
- @see ApplePayExampleViewController for an example of how to use this method in your Apple Pay integration.
  */
 + (nullable NSError *)pkPaymentErrorForStripeError:(nullable NSError *)stripeError API_AVAILABLE(ios(11.0), watchos(4.0));
 
