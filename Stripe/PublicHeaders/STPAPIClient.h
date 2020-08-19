f3e07df141119b1238e12f985a7757baef4b8874
Yuki Tokuhiro <yuki@stripe.com>
2020-08-19 16:33:51 -0700

    Bump version

--- Stripe/PublicHeaders/STPAPIClient.h
+++ Stripe/PublicHeaders/STPAPIClient.h
@@ -18,7 +18,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  The current version of this library.
  */
-static NSString *const STPSDKVersion = @"19.4.0";
+static NSString *const STPSDKVersion = @"19.4.0-grabpay-preview";
 
 @class STPBankAccount, STPBankAccountParams, STPCard, STPCardParams, STPConnectAccountParams;
 @class STPPaymentConfiguration, STPPaymentIntentParams, STPSourceParams, STPToken, STPPaymentMethodParams;
