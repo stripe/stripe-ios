145d2119d2998f711bd041cccbc8a1fec37a0285
David Estes <davidestes@stripe.com>
2020-08-27 16:49:33 -0700

    Update Stripe3DS2 with compact version

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
   targets: [
     .target(
       name: "Stripe",
-      dependencies: ["Stripe3DS2", "ssl", "crypto"],
+      dependencies: ["Stripe3DS2"],
       path: "Stripe",
         resources: [
           .process("Resources/Images"),
@@ -30,16 +30,8 @@ let package = Package(
         ]
     ),
     .binaryTarget(
-      name: "ssl",
-      url: "https://github.com/davidme-stripe/stripe-3ds2-ios-releases/releases/download/0.0.9/ssl.xcframework.zip",
-      checksum: "cb01ea8fe3f53c5c3311608490d67c8ad15cf13f5447d537b6fb5420769bce54"),
-    .binaryTarget(
-      name: "crypto",
-      url: "https://github.com/davidme-stripe/stripe-3ds2-ios-releases/releases/download/0.0.9/crypto.xcframework.zip",
-      checksum: "60ca6e56c6b1768749ae5f877499adc3c11847dc7bc4626f6533850d362413ed"),
-    .binaryTarget(
       name: "Stripe3DS2",
-      url: "https://github.com/davidme-stripe/stripe-3ds2-ios-releases/releases/download/0.0.9/Stripe3DS2.xcframework.zip",
-      checksum: "15be05a4fc05b22eea351eb90aa135fec54059b174d4c0869713fcc04ad5b4af"),
+      url: "https://github.com/davidme-stripe/stripe-3ds2-ios-releases/releases/download/0.0.89/Stripe3DS2.xcframework.zip",
+      checksum: "e7c69c260d6d417406c3e5bfb77d1180c71ca8a99bd9b654d5c8f80b05173532"),
   ]
 )
