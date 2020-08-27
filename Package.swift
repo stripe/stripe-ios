a75349b151c87044ec096d5a3d66fa8cc327fca8
David Estes <davidestes@stripe.com>
2020-08-26 17:20:31 -0700

    Fix check_public_headers.rb

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
   name: "Stripe",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v10)
+      .iOS(.v10)
     ],
   products: [
     .library(
@@ -14,16 +14,14 @@ let package = Package(
       targets: ["Stripe"]
     ),
   ],
-  dependencies: [
-  ],
   targets: [
     .target(
       name: "Stripe",
       dependencies: ["Stripe3DS2"],
       path: "Stripe",
         resources: [
-            .process("Resources/Images"),
-            .process("ExternalResources/Stripe3DS2.bundle"),
+          .process("Resources/Images"),
+          .process("ExternalResources/Stripe3DS2.bundle"),
         ],
         publicHeadersPath: "PublicHeaders",
         cSettings: [
@@ -31,8 +29,9 @@ let package = Package(
           .headerSearchPath("PublicHeaders/Stripe"),
         ]
     ),
-    .binaryTarget( name: "Stripe3DS2",
-                   url: "https://github.com/davidme-stripe/stripe-3ds2-ios-releases/releases/download/0.0.222/Stripe3DS2.xcframework.zip",
-                   checksum: "adab590c5427c4c0c1adc224811730e7a8c15485d92772f45d7fc503c410154f"),
+    .binaryTarget(
+      name: "Stripe3DS2",
+      url: "https://github.com/davidme-stripe/stripe-3ds2-ios-releases/releases/download/0.0.222/Stripe3DS2.xcframework.zip",
+      checksum: "adab590c5427c4c0c1adc224811730e7a8c15485d92772f45d7fc503c410154f"),
   ]
 )
--- ci_scripts/check_public_headers.rb
+++ ci_scripts/check_public_headers.rb
@@ -11,16 +11,16 @@ require 'xcodeproj'
 
 puts "Checking headers..."
 
-contents_of_stripe_dot_h = (File.readlines("Stripe/PublicHeaders/Stripe.h").map do |line|
+contents_of_stripe_dot_h = (File.readlines("Stripe/PublicHeaders/Stripe/Stripe.h").map do |line|
 	/\#import \"(.*?)\"\n/.match(line)
 end.compact.map do |match|
 	match.captures.first
-end + File.readlines("Stripe/PublicHeaders/Stripe3DS2.h").map do |line|
+end + File.readlines("Stripe/PublicHeaders/Stripe/Stripe3DS2.h").map do |line|
 	/\#import \"(.*?)\"\n/.match(line)
 end.compact.map do |match|
 	match.captures.first
 end + ["Stripe.h"]).sort
-contents_of_public_headers_dir = Dir.glob("Stripe/PublicHeaders/*.h").map { |h| File.basename(h) }.sort
+contents_of_public_headers_dir = Dir.glob("Stripe/PublicHeaders/Stripe/*.h").map { |h| File.basename(h) }.sort
 
 duplicate_dot_h_imports = contents_of_stripe_dot_h.select{ |e| contents_of_stripe_dot_h.count(e) > 1 }.uniq
 
@@ -30,7 +30,7 @@ end
 
 if contents_of_public_headers_dir != contents_of_stripe_dot_h
 	likely_culprits = ([contents_of_stripe_dot_h - contents_of_public_headers_dir] + [contents_of_public_headers_dir - contents_of_stripe_dot_h]).uniq
-	abort("The contents of Stripe/PublicHeaders do not match what is #imported in Stripe/PublicHeaders/Stripe.h. Likely culprits: #{likely_culprits}.")
+	abort("The contents of Stripe/PublicHeaders do not match what is #imported in Stripe/PublicHeaders/Stripe/Stripe.h. Likely culprits: #{likely_culprits}.")
 end
 
 dynamic_framework_target = Xcodeproj::Project.open('Stripe.xcodeproj').targets.select { |t| t.name == 'StripeiOS' }.first
