b76c33ab3ec400336d217d4abaa56249c0986dbb
David Estes <davidestes@stripe.com>
2020-09-09 13:45:23 -0700

    Add SPM Test

---
+++ Tests/installation_tests/swift_package_manager/test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+function info {
+  echo "[$(basename "${0}")] [INFO] ${1}"
+}
+
+function die {
+  echo "[$(basename "${0}")] [ERROR] ${1}"
+  exit 1
+}
+
+# Verify xcpretty is installed
+if ! command -v xcpretty > /dev/null; then
+  if [[ "${CI}" != "true" ]]; then
+    die "Please install xcpretty: https://github.com/supermarin/xcpretty#installation"
+  fi
+
+  info "Installing xcpretty..."
+  gem install xcpretty --no-document || die "Executing \`gem install xcpretty\` failed"
+fi
+
+# Switch to script directory
+cd "${script_dir}" || die "Executing \`cd\` failed"
+
+# Execute xcodebuild
+info "Executing xcodebuild..."
+
+xcodebuild clean build \
+  -workspace "SPMTest.xcworkspace" \
+  -scheme "SPMTest" \
+  -sdk "iphonesimulator" \
+  -destination "platform=iOS Simulator,name=iPhone 6,OS=11.4" \
+  | xcpretty
+
+xcodebuild_exit_code="${PIPESTATUS[0]}"
+
+if [[ "${xcodebuild_exit_code}" != 0 ]]; then
+  die "Executing xcodebuild failed with status code: ${xcodebuild_exit_code}"
+fi
+
+info "All good!"
