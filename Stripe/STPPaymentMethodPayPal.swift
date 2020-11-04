663b2c81b3bb40d2196a57ea6fb644ad8431908c
davidme-stripe <52758633+davidme-stripe@users.noreply.github.com>
2020-11-04 15:41:55 -0800

    Swift conversion review: 20.1.0 improvements + missing files (#1690)
    * Add additional changes from 20.1.0, and a few missing files

---
+++ Stripe/STPPaymentMethodPayPal.swift
@@ -0,0 +1,40 @@
+//
+//  STPPaymentMethodPayPal.swift
+//  StripeiOS
+//
+//  Created by Cameron Sabol on 10/5/20.
+//  Copyright © 2020 Stripe, Inc. All rights reserved.
+//
+
+import Foundation
+
+/// A PayPal Payment Method. :nodoc:
+/// - seealso: https://stripe.com/docs/payments/paypal
+public class STPPaymentMethodPayPal: NSObject, STPAPIResponseDecodable {
+  @objc private(set) public var allResponseFields: [AnyHashable: Any] = [:]
+
+  // MARK: - Description
+  /// :nodoc:
+  @objc public override var description: String {
+    let props = [
+      // Object
+      String(format: "%@: %p", NSStringFromClass(STPPaymentMethodPayPal.self), self)
+    ]
+
+    return "<\(props.joined(separator: "; "))>"
+  }
+
+  // MARK: - STPAPIResponseDecodable
+  @objc
+  public class func decodedObject(fromAPIResponse response: [AnyHashable: Any]?) -> Self? {
+    guard let response = response else {
+      return nil
+    }
+    return self.init(dictionary: response)
+  }
+
+  required init(dictionary dict: [AnyHashable: Any]) {
+    super.init()
+    allResponseFields = dict
+  }
+}
