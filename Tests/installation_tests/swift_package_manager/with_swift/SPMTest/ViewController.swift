f0435cc29082fded5e17c9fc5db43445e202cfca
davidme-stripe <52758633+davidme-stripe@users.noreply.github.com>
2021-08-10 10:57:38 -0700

    Update ViewController.swift

--- Tests/installation_tests/swift_package_manager/with_swift/SPMTest/ViewController.swift
+++ Tests/installation_tests/swift_package_manager/with_swift/SPMTest/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         StripeAPI.defaultPublishableKey = "foo"
         let _ = IdentityVerificationSheet(verificationSessionClientSecret: "test")
-	// Initialize a card field to make sure we can load image resources
+        // Initialize a card field to make sure we can load image resources
         let cardField = STPPaymentCardTextField()
         cardField.frame = CGRect(x: 0, y: 0, width: 300, height: 100)
         self.view.addSubview(cardField)
