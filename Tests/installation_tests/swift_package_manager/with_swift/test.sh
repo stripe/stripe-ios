3d551c66423390fe308df78dc470d6cfa219251e
David Estes <davidestes@stripe.com>
2021-08-10 10:11:08 -0700

    Update tests to actually run the app

--- Tests/installation_tests/swift_package_manager/with_swift/test.sh
+++ Tests/installation_tests/swift_package_manager/with_swift/test.sh
@@ -17,12 +17,12 @@ cd "${script_dir}" || die "Executing \`cd\` failed"
 # Execute xcodebuild
 info "Executing xcodebuild..."
 
-xcodebuild clean build \
+xcodebuild clean test \
   -quiet \
   -workspace "SPMTest.xcworkspace" \
   -scheme "SPMTest" \
   -sdk "iphonesimulator" \
-  -destination "platform=iOS Simulator,name=iPhone 8,OS=13.7"
+  -destination "platform=iOS Simulator,name=iPhone 8,OS=latest"
 
 xcodebuild_exit_code="${PIPESTATUS[0]}"
 
