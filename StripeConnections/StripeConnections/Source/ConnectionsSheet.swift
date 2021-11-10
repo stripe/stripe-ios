a88c2d05d8004806cb2a205197c82de68171e2fa
Vardges Avetisyan <vardges@stripe.com>
2021-11-10 15:59:46 -0500

    Adds ConnectionsSDK installation tests

---
+++ StripeConnections/StripeConnections/Source/ConnectionsSheet.swift
@@ -0,0 +1,15 @@
+//
+//  ConnectionsSheet.swift
+//  StripeConnections
+//
+//  Created by Vardges Avetisyan on 11/10/21.
+//
+
+import UIKit
+
+// TODO(vav): Currently this is a placeholder class to make the installation tests work.
+//            This will be made into an actual class soon.
+final public class ConnectionsSheet {
+    public init() {
+    }
+}
