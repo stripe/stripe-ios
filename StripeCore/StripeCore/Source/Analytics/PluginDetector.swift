8e92cf1e9d2fec1f9b5eb9a6f79368ca04e51fe2
Nick Porter <88012362+porter-stripe@users.noreply.github.com>
2021-11-16 10:57:12 -0700

    Add PluginDector to detect cross-platform solutions  (#399)
    * Start on plugin detector
    
    * Add xamarin and unity
    
    * Add unity/ionic
    
    * Fix xamarin support
    
    * fix conditional chaning
    
    * Add comments
    
    * Use UnityFramework, fix link, cache value
    
    * Use CaseIterable in PluginType

---
+++ StripeCore/StripeCore/Source/Analytics/PluginDetector.swift
@@ -0,0 +1,45 @@
+//
+//  PluginDetector.swift
+//  StripeCore
+//
+//  Created by Nick Porter on 10/1/21.
+//
+
+import Foundation
+
+/// A class which can detect if the host app is using a known cross-platform solution
+class PluginDetector {
+    
+    /// Shared instance of the `PluginDetector` to enable caching of the `pluginType`
+    static let shared = PluginDetector()
+    
+    /// Represents all the known/tracked cross-platform solutions
+    enum PluginType: String, CaseIterable {
+        case cordova
+        case flutter
+        case ionic
+        case reactNative = "react-native"
+        case unity
+        case xamarin
+        
+        /// Represents a known class contained in each cross-platform environment
+        var className: String {
+            switch self {
+                case .cordova: return "CDVPlugin"
+                case .flutter: return "FlutterAppDelegate"
+                case .ionic: return "CAPPlugin"
+                case .reactNative: return "RCTBridge"
+                case .unity: return "UnityFramework"
+                case .xamarin: return "XamarinAssociatedObject"
+            }
+        }
+    }
+    
+    /// Determines if this app is running within a plugin environment
+    /// - Returns: returns the plugin type if found, otherwise nil
+    lazy var pluginType: PluginType? = {
+        PluginType.allCases.first { type in
+            NSClassFromString(type.className) != nil
+        }
+    }()
+}
