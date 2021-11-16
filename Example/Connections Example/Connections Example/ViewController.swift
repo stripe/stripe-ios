79e1b422b53b9a93ca6b06e0819a0577e583c67f
Vardges Avetisyan <vardges@stripe.com>
2021-11-16 15:24:36 -0500

    Adds connections SDK example App

---
+++ Example/Connections Example/Connections Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Connections Example
+//
+//  Created by Vardges Avetisyan on 11/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
