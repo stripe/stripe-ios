316bb9d81813d2df081eabd408f9ebc2578e049a
kingst-stripe <85941858+kingst-stripe@users.noreply.github.com>
2021-11-16 16:46:01 -0800

    Add an installation test for the existence of icon_chevron_down@3.png (#508)

--- StripeUICore/StripeUICore/Source/Image.swift
+++ StripeUICore/StripeUICore/Source/Image.swift
@@ -10,7 +10,7 @@ import Foundation
 /// The canonical set of all image files in the SDK.
 /// This helps us avoid duplicates and automatically test that all images load properly
 /// Raw value is the image file name. We use snake case to make long names easier to read.
-@_spi(STP) public enum Image: String, ImageMaker {
+@_spi(STP) public enum Image: String, ImageMaker, CaseIterable {
     public typealias BundleLocator = StripeUICoreBundleLocator
 
     case icon_chevron_down = "icon_chevron_down"
