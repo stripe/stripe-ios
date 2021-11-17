22e64ce106b386c7f6370c58cff87ad53a668cac
Vardges Avetisyan <vardges@stripe.com>
2021-11-17 12:55:55 -0500

    Add StripeConnections dependency

--- Example/Connections Example/Connections Example/ViewController.swift
+++ Example/Connections Example/Connections Example/ViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
+import StripeConnections
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
