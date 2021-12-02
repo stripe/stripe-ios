d0d098a62f2eebce924bebf49599f8f3b60131a4
Vardges Avetisyan <vardges@stripe.com>
2021-12-01 20:08:15 -0500

    Remove web view

---
+++ StripeConnections/StripeConnections/Source/View Controllers/ConnectionsHostViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ConnectionsHostViewController.swift
+//  StripeConnections
+//
+//  Created by Vardges Avetisyan on 12/1/21.
+//
+
+import UIKit
+
+final class ConnectionsHostViewController: UIViewController {
+
+    // MARK: - UIViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
