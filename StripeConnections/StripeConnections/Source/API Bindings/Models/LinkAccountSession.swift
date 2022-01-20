2f462a6f70abcca4e050bd582706ab9e4cca9683
Vardges Avetisyan <vardges@stripe.com>
2022-01-20 10:54:30 -0500

    Add PaymentAccount polymorphic field

--- StripeConnections/StripeConnections/Source/API Bindings/Models/LinkAccountSession.swift
+++ StripeConnections/StripeConnections/Source/API Bindings/Models/LinkAccountSession.swift
@@ -8,14 +8,51 @@
 import Foundation
 @_spi(STP) import StripeCore
 
-
 public extension StripeAPI {
 
     struct LinkAccountSession: StripeDecodable {
+
+        // MARK: - Types
+
+        public struct BankAccount: StripeDecodable {
+            public let bankName: String?
+            public let id: String
+            public let last4: String
+            public let routingNumber: String?
+            public var _allResponseFieldsStorage: NonEncodableParameters?
+        }
+
+        public enum PaymentAccount: Decodable {
+
+            case linkedAccount(StripeAPI.LinkedAccount)
+            case bankAccount(StripeAPI.LinkAccountSession.BankAccount)
+            case unparsable
+
+            // MARK: - Decodable
+
+            /**
+             Per API specification paymentAccount is a polymorphic field denoted by openAPI anyOf modifier.
+             We are translating it to an anum with associated types.
+             */
+            public init(from decoder: Decoder) throws {
+                let container = try decoder.singleValueContainer()
+                if let value = try? container.decode(LinkedAccount.self) {
+                    self = .linkedAccount(value)
+                } else if let value = try? container.decode(LinkAccountSession.BankAccount.self) {
+                    self = .bankAccount(value)
+                } else {
+                    self = .unparsable
+                }
+            }
+        }
+
+        // MARK: - Properties
+
         public let clientSecret: String
         public let id: String
         public let linkedAccounts: LinkedAccountList
         public let livemode: Bool
+        public let paymentAccount: PaymentAccount?
         public var _allResponseFieldsStorage: NonEncodableParameters?
     }
 }
