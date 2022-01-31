0106a1834ac0efa71b70ea85c8a1b5a27212b0be
Mel <78050250+mludowise-stripe@users.noreply.github.com>
2022-01-31 13:34:31 -0800

    IDPROD-2774 part 1: CameraSession (#670)
    Added `CameraSession` coordinator class to
    configure the camera feed and display it inside
    `CameraPreviewView`, replacing `VideoFeed`. 
    This will be used inside
    `DocumentCaptureViewController` to configure the
    camera and start scanning the camera frames for a
    document image.

---
+++ StripeCameraCore/StripeCameraCore/Source/Views/CameraPreviewView.swift
@@ -0,0 +1,72 @@
+//
+//  CameraPreviewView.swift
+//  StripeCameraCore
+//
+//  Created by Mel Ludowise on 12/1/21.
+//
+
+import UIKit
+import AVFoundation
+
+@_spi(STP) public class CameraPreviewView: UIView {
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+
+    /**
+     Camera session that configures the video feed for this view.
+
+     - Note:
+     A session can only be used on one `PreviewView` at a time. Setting the same
+     session on a different `PreviewView` will remove it from the previous one.
+     */
+    public weak var session: CameraSession? {
+        didSet {
+            guard oldValue !== session else {
+                return
+            }
+            oldValue?.previewView = nil
+            session?.previewView = self
+        }
+    }
+
+    // MARK: Initialization
+    
+    public init() {
+        super.init(frame: .zero)
+
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: UIView
+    
+    public override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+
+    // MARK: Internal
+
+    /**
+     Sets the video capture session in thread-safe way that doesn't block main
+     thread when configuring the session.
+
+     - Parameters:
+       - captureSession: The capture session to set on this view
+       - queue: Worker queue to configure the session
+     */
+    func setCaptureSession(_ captureSession: AVCaptureSession?, on queue: DispatchQueue) {
+        // Get reference to videoPreviewLayer on main queue then dispatch to
+        // worker queue to set session so it doesn't block main.
+
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            queue.async { [weak captureSession, weak videoPreviewLayer = self.videoPreviewLayer] in
+                videoPreviewLayer?.session = captureSession
+            }
+        }
+    }
+}
