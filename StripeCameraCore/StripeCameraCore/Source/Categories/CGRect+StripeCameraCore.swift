054faf5f88639fe4d4428bc37288c38655df0167
Mel <78050250+mludowise-stripe@users.noreply.github.com>
2022-02-03 17:19:23 -0800

    IDPROD-2482 part 1: Utils needed for ML IDDetector (#693)
    Adds utilities needed by the IDDetector ML model including:
    - CGRect helpers
    - MLMultiArray helper
    - A non-maximum suppression algorithm copied from https://github.com/hollance/CoreMLHelpers

--- StripeCameraCore/StripeCameraCore/Source/Categories/CGRect+StripeCameraCore.swift
+++ StripeCameraCore/StripeCameraCore/Source/Categories/CGRect+StripeCameraCore.swift
@@ -9,6 +9,10 @@ import Foundation
 import CoreGraphics
 
 @_spi(STP) public extension CGRect {
+
+    /// Represents the bounds of a normalized coordinate system with range from (0,0) to (1,1)
+    static let normalizedBounds = CGRect(x: 0, y: 0, width: 1, height: 1)
+
     /**
      - Returns: A `CGRect` that has its y-coordinates inverted between the
      upper-left corner and lower-left corner.
@@ -24,6 +28,56 @@ import CoreGraphics
             y: 1 - minY - height,
             width: width,
             height: height
+        )
+    }
+
+    /**
+     Converts a rectangle that's using a normalized coordinate system from a
+     center-crop coordinate system to an un-cropped coordinate system
+
+     Example, if the original size has a portrait aspect ratio, center-cropping
+     the rect will result in the square area:
+     ```
+     +---------+
+     |         |
+     |---------|
+     |         |
+     |         |
+     |         |
+     |---------|
+     |         |
+     +---------+
+     ```
+
+     This method converts the rect's coordinate relative to the center-cropped
+     area into coordinates relative to the original un-cropped area:
+     ```
+                     +---------+
+                     |         |
+     +---------+     |         |
+     |    +--+ |     |    +--+ |
+     |    |  | | --> |    |  | |
+     |    +--+ |     |    +--+ |
+     +---------+     |         |
+                     |         |
+                     +---------+
+     ```
+
+     - Parameters:
+       - size: The original size of the un-cropped area.
+     */
+    func convertFromNormalizedCenterCropSquare(
+        toOriginalSize originalSize: CGSize
+    ) -> CGRect {
+        let croppedWidth = min(originalSize.width, originalSize.height)
+        let scaleX = croppedWidth / originalSize.width
+        let scaleY = croppedWidth / originalSize.height
+
+        return CGRect(
+            x: (minX - 0.5) * scaleX + 0.5,
+            y: (minY - 0.5) * scaleY + 0.5,
+            width: width * scaleX,
+            height: height * scaleY
         )
     }
 }
