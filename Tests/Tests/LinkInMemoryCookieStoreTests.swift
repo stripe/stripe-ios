7aeec03440b56fb9eced26069d11f1f153fc8e12
ramont-stripe <88752322+ramont-stripe@users.noreply.github.com>
2022-03-02 15:24:32 -0500

    [Link] Update cookie logic to latest API (#820)
    * Update consumer session business logic
    
    * Rename method
    
    * Update test
    
    * Update tests
    
    * Cleanup
    
    * Cleanup API
    
    * Make test method names consistent
    
    * Document update cookie logic
    
    * Remove `ConsumerSessionCookiesTests`.
    
    Functionality already covered by `LinkSecureCookieStoreTests` and
    `ConsumerSessionTests` (functional tests).

--- Tests/Tests/LinkInMemoryCookieStoreTests.swift
+++ Tests/Tests/LinkInMemoryCookieStoreTests.swift
@@ -14,43 +14,60 @@ class LinkInMemoryCookieStoreTests: XCTestCase {
     static let testKey: String = "test-key"
 
     func testWrite() {
-        let cookieStore = LinkInMemoryCookieStore()
-        cookieStore.write(key: Self.testKey, value: "cookie_value")
+        let sut = makeSUT()
+        sut.write(key: Self.testKey, value: "cookie_value")
 
-        XCTAssertEqual(cookieStore.read(key: Self.testKey), "cookie_value")
+        XCTAssertEqual(sut.read(key: Self.testKey), "cookie_value")
     }
 
     func testWrite_overwriting() {
-        let cookieStore = LinkInMemoryCookieStore()
-        cookieStore.write(key: Self.testKey, value: "cookie_value")
-        cookieStore.write(key: Self.testKey, value: "new_cookie_value")
+        let sut = makeSUT()
+        sut.write(key: Self.testKey, value: "cookie_value")
+        sut.write(key: Self.testKey, value: "new_cookie_value")
 
-        XCTAssertEqual(cookieStore.read(key: Self.testKey), "new_cookie_value")
+        XCTAssertEqual(sut.read(key: Self.testKey), "new_cookie_value")
     }
 
     func testDelete() {
-        let cookieStore = LinkInMemoryCookieStore()
-        cookieStore.write(key: Self.testKey, value: "cookie_value")
-        cookieStore.delete(key: Self.testKey)
+        let sut = makeSUT()
+        sut.write(key: Self.testKey, value: "cookie_value")
+        sut.delete(key: Self.testKey)
 
-        XCTAssertNil(cookieStore.read(key: Self.testKey))
+        XCTAssertNil(sut.read(key: Self.testKey))
     }
 
-    func testDelete_withMatchingValue() {
-        let cookieStore = LinkInMemoryCookieStore()
-        cookieStore.write(key: Self.testKey, value: "cookie_value")
-        cookieStore.delete(key: Self.testKey, value: "cookie_value")
+    // MARK: Session cookies
 
-        XCTAssertNil(cookieStore.read(key: Self.testKey))
+    func testFormattedSessionCookies() {
+        let sut = makeSUT()
+
+        sut.write(key: sut.sessionCookieKey, value: "cookie_value")
+        XCTAssertEqual(sut.formattedSessionCookies(), [
+            "verification_session_client_secrets": ["cookie_value"]
+        ])
+
+        sut.delete(key: sut.sessionCookieKey)
+        XCTAssertNil(sut.formattedSessionCookies())
     }
 
-    func testDelete_withNonMatchingValue() {
-        let cookieStore = LinkInMemoryCookieStore()
-        cookieStore.write(key: Self.testKey, value: "cookie_value")
-        cookieStore.delete(key: Self.testKey, value: "different_cookie_value")
+    func testUpdateSessionCookie() {
+        let sut = makeSUT()
+        sut.updateSessionCookie(with: "top_secret")
+        XCTAssertEqual(sut.read(key: sut.sessionCookieKey), "top_secret")
 
-        XCTAssertEqual(cookieStore.read(key: Self.testKey), "cookie_value")
+        sut.updateSessionCookie(with: nil)
+        XCTAssertEqual(sut.read(key: sut.sessionCookieKey), "top_secret")
+
+        sut.updateSessionCookie(with: "")
+        XCTAssertNil(sut.read(key: sut.sessionCookieKey))
     }
 
 }
 
+extension LinkInMemoryCookieStoreTests {
+
+    func makeSUT() -> LinkInMemoryCookieStore {
+        return LinkInMemoryCookieStore()
+    }
+
+}
