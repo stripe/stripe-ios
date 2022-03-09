aed61748a3b431fdd34f3949024693045f61ca66
Nick Porter <88012362+porter-stripe@users.noreply.github.com>
2022-03-09 11:45:19 -0700

    Add dark mode tests for PaymentSheet snapshots (#841)

--- Example/PaymentSheet Example/PaymentSheet Example/PaymentSheet Example/SceneDelegate.swift
+++ Example/PaymentSheet Example/PaymentSheet Example/PaymentSheet Example/SceneDelegate.swift
@@ -38,6 +38,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        #if DEBUG
+        if CommandLine.arguments.contains("UITestingDarkModeEnabled") {
+            window?.overrideUserInterfaceStyle = .dark
+        }
+        #endif
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
