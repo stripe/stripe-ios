d871daefaa63d9ee7491440c22437136e15c7e37
John Woo <99628984+wooj-stripe@users.noreply.github.com>
2022-03-16 10:19:07 -0700

    Added bindings for link account session attach (#861)

--- Tests/Tests/STPAPIClientStubbedTest.swift
+++ Tests/Tests/STPAPIClientStubbedTest.swift
@@ -19,17 +19,17 @@ class STPAPIClientStubbedTest: APIStubbedTestCase {
     func testSetupIntent_LinkAccountSessionForUSBankAccount() {
         let sut = stubbedAPIClient()
         stub { urlRequest in
-            return urlRequest.url?.absoluteString.contains("/setup_intents/seti_12345/link_account_session") ?? false
+            return urlRequest.url?.absoluteString.contains("/setup_intents/seti_12345/link_account_sessions") ?? false
         } response: { urlRequest in
             guard let data = urlRequest.httpBodyOrBodyStream,
-                    let body = String(data: data, encoding: .utf8) else {
-                return HTTPStubsResponse(data: "".data(using: .utf8)!, statusCode: 400, headers: nil)
-            }
+                  let body = String(data: data, encoding: .utf8) else {
+                      return HTTPStubsResponse(data: "".data(using: .utf8)!, statusCode: 400, headers: nil)
+                  }
             XCTAssert(body.contains("client_secret=si_client_secret_123"))
             XCTAssert(body.contains("payment_method_data%5Bbilling_details%5D%5Bemail%5D=test%40example.com"))
             XCTAssert(body.contains("payment_method_data%5Bbilling_details%5D%5Bname%5D=Test%20Tester"))
             XCTAssert(body.contains("payment_method_data%5Btype%5D=us_bank_account"))
-
+            
             let jsonText = """
                  {
                    "id": "xxxxx",
@@ -47,12 +47,12 @@ class STPAPIClientStubbedTest: APIStubbedTestCase {
                  """
             return HTTPStubsResponse(data: jsonText.data(using: .utf8)!, statusCode: 200, headers: nil)
         }
-
+        
         let expectCallback = expectation(description: "bindings serialize/deserialize")
-        sut.linkAccountForUSBankAccount(setupIntentID: "seti_12345",
-                                        clientSecret: "si_client_secret_123",
-                                        customerName: "Test Tester",
-                                        customerEmailAddress: "test@example.com") { intent, error in
+        sut.createLinkAccountSession(setupIntentID: "seti_12345",
+                                     clientSecret: "si_client_secret_123",
+                                     customerName: "Test Tester",
+                                     customerEmailAddress: "test@example.com") { intent, error in
             guard let intent = intent else {
                 XCTFail("Intent was null")
                 return
@@ -60,24 +60,24 @@ class STPAPIClientStubbedTest: APIStubbedTestCase {
             XCTAssertEqual(intent.clientSecret, "las_client_secret_123456")
             expectCallback.fulfill()
         }
-
+        
         wait(for: [expectCallback], timeout: 2.0)
     }
-
+    
     func testPaymentIntent_LinkAccountSessionForUSBankAccount() {
         let sut = stubbedAPIClient()
         stub { urlRequest in
-            return urlRequest.url?.absoluteString.contains("/payment_intents/pi_12345/link_account_session") ?? false
+            return urlRequest.url?.absoluteString.contains("/payment_intents/pi_12345/link_account_sessions") ?? false
         } response: { urlRequest in
             guard let data = urlRequest.httpBodyOrBodyStream,
-                    let body = String(data: data, encoding: .utf8) else {
-                return HTTPStubsResponse(data: "".data(using: .utf8)!, statusCode: 400, headers: nil)
-            }
+                  let body = String(data: data, encoding: .utf8) else {
+                      return HTTPStubsResponse(data: "".data(using: .utf8)!, statusCode: 400, headers: nil)
+                  }
             XCTAssert(body.contains("client_secret=si_client_secret_123"))
             XCTAssert(body.contains("payment_method_data%5Bbilling_details%5D%5Bemail%5D=test%40example.com"))
             XCTAssert(body.contains("payment_method_data%5Bbilling_details%5D%5Bname%5D=Test%20Tester"))
             XCTAssert(body.contains("payment_method_data%5Btype%5D=us_bank_account"))
-
+            
             let jsonText = """
                  {
                    "id": "las_12345",
@@ -86,7 +86,7 @@ class STPAPIClientStubbedTest: APIStubbedTestCase {
                    "linked_accounts": {
                      "object": "list",
                      "data": [
-
+                 
                      ],
                      "has_more": false,
                      "total_count": 0,
@@ -97,17 +97,124 @@ class STPAPIClientStubbedTest: APIStubbedTestCase {
                  """
             return HTTPStubsResponse(data: jsonText.data(using: .utf8)!, statusCode: 200, headers: nil)
         }
-
+        
         let expectCallback = expectation(description: "bindings serialize/deserialize")
-        sut.linkAccountForUSBankAccount(paymentIntentID: "pi_12345",
-                                        clientSecret: "si_client_secret_123",
-                                        customerName: "Test Tester",
-                                        customerEmailAddress: "test@example.com") { intent, error in
+        sut.createLinkAccountSession(paymentIntentID: "pi_12345",
+                                     clientSecret: "si_client_secret_123",
+                                     customerName: "Test Tester",
+                                     customerEmailAddress: "test@example.com") { intent, error in
             guard let intent = intent else {
                 XCTFail("Intent was null")
                 return
             }
             XCTAssertEqual(intent.clientSecret, "las_client_secret_654321")
+            expectCallback.fulfill()
+        }
+        
+        wait(for: [expectCallback], timeout: 2.0)
+    }
+
+    func testSetupIntent_LinkAccountSessionAttach() {
+        let sut = stubbedAPIClient()
+        stub { urlRequest in
+            return urlRequest.url?.absoluteString.contains("/setup_intents/seti_12345/link_account_sessions/las_123456/attach") ?? false
+        } response: { urlRequest in
+            guard let data = urlRequest.httpBodyOrBodyStream,
+                  let body = String(data: data, encoding: .utf8) else {
+                      return HTTPStubsResponse(data: "".data(using: .utf8)!, statusCode: 400, headers: nil)
+                  }
+            XCTAssert(body.contains("client_secret=si_client_secret_123"))
+            
+            let jsonText = """
+                 {
+                   "id": "seti_12345",
+                   "object": "setup_intent",
+                   "cancellation_reason": null,
+                   "client_secret": "seti_abc_secret_def",
+                   "created": 1647000000,
+                   "description": null,
+                   "last_setup_error": null,
+                   "livemode": false,
+                   "next_action": null,
+                   "payment_method": "pm_abcdefg",
+                   "payment_method_options": {
+                     "us_bank_account": {
+                       "verification_method": "instant"
+                     }
+                   },
+                   "payment_method_types": [
+                     "us_bank_account"
+                   ],
+                   "status": "requires_confirmation",
+                   "usage": "off_session"
+                 }
+                 """
+            return HTTPStubsResponse(data: jsonText.data(using: .utf8)!, statusCode: 200, headers: nil)
+        }
+        
+        let expectCallback = expectation(description: "bindings serialize/deserialize")
+        sut.attachLinkAccountSession(setupIntentID: "seti_12345",
+                                     linkAccountSessionID: "las_123456",
+                                     clientSecret: "si_client_secret_123") { intent, error in
+            guard let intent = intent else {
+                XCTFail("Intent was null")
+                return
+            }
+            XCTAssertEqual(intent.paymentMethodID, "pm_abcdefg")
+            expectCallback.fulfill()
+        }
+        
+        wait(for: [expectCallback], timeout: 2.0)
+    }
+
+    func testPaymentIntent_LinkAccountSessionAttach() {
+        let sut = stubbedAPIClient()
+        stub { urlRequest in
+            return urlRequest.url?.absoluteString.contains("/payment_intents/pi_12345/link_account_sessions/las_123456/attach") ?? false
+        } response: { urlRequest in
+            guard let data = urlRequest.httpBodyOrBodyStream,
+                  let body = String(data: data, encoding: .utf8) else {
+                      return HTTPStubsResponse(data: "".data(using: .utf8)!, statusCode: 400, headers: nil)
+                  }
+            XCTAssert(body.contains("client_secret=pi_client_secret_123"))
+            
+            let jsonText = """
+                 {
+                   "id": "pi_12345",
+                   "object": "payment_intent",
+                   "amount": 100,
+                   "currency": "usd",
+                   "cancellation_reason": null,
+                   "client_secret": "seti_abc_secret_def",
+                   "created": 1647000000,
+                   "description": null,
+                   "last_setup_error": null,
+                   "livemode": false,
+                   "next_action": null,
+                   "payment_method": "pm_abcdefg",
+                   "payment_method_options": {
+                     "us_bank_account": {
+                       "verification_method": "instant"
+                     }
+                   },
+                   "payment_method_types": [
+                     "us_bank_account"
+                   ],
+                   "status": "requires_payment_method"
+                 }
+                 """
+            return HTTPStubsResponse(data: jsonText.data(using: .utf8)!, statusCode: 200, headers: nil)
+        }
+        
+        let expectCallback = expectation(description: "bindings serialize/deserialize")
+        sut.attachLinkAccountSession(paymentIntentID: "pi_12345",
+                                     linkAccountSessionID: "las_123456",
+                                     clientSecret: "pi_client_secret_123") { intent, error in
+            guard let intent = intent else {
+                XCTFail("Intent was null")
+                return
+            }
+            XCTAssertEqual(intent.paymentMethodId, "pm_abcdefg")
             expectCallback.fulfill()
         }
 
