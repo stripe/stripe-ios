a487f46a14f75da8de0e9150270aa25b7a8b5270
davidme-stripe <52758633+davidme-stripe@users.noreply.github.com>
2022-03-22 11:21:02 -0700

    De-flake UnionPay BIN tests (#891)

--- Tests/Tests/STPCardValidatorTest.swift
+++ Tests/Tests/STPCardValidatorTest.swift
@@ -199,7 +199,7 @@ class STPCardValidatorTest: XCTestCase {
             (.discover, Set([16])),
             (.dinersClub, Set([14, 16])),
             (.JCB, Set([16])),
-            (.unionPay, Set([16])),
+            (.unionPay, Set([16, 19])),
             (.unknown, Set([19])),
         ]
         for test in tests {
