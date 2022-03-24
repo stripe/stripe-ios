00cae9fec44580f6ffbe2c0827a81eb1318b4dc1
John Woo <99628984+wooj-stripe@users.noreply.github.com>
2022-03-24 09:48:19 -0700

    Update affirm copy to buy now, pay later (#906)

--- Stripe/AffirmCopyLabel.swift
+++ Stripe/AffirmCopyLabel.swift
@@ -18,7 +18,7 @@ class AffirmCopyLabel: UIView {
         self.init(frame: .zero)
         let affirmLabel = UILabel()
         
-        let message = NSMutableAttributedString(string: STPLocalizedString("Buy now or pay later with %@.", "Affirm buy now or pay later copy"))
+        let message = NSMutableAttributedString(string: STPLocalizedString("Buy now, pay later with %@.", "Affirm buy now, pay later copy"))
         logo.image = STPImageLibrary.affirmLogo()
         message.replaceOccurrences(of: "%@", with: logo)
         affirmLabel.attributedText = message
