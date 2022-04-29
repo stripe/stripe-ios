ce8488582e0c3a661135f0f9d62de00c570b78e9
Scott Grant <sgrant@stripe.com>
2022-04-29 12:47:22 -0700

    Update object that we parse server resposnes into to include new fields.

--- StripeCardScan/StripeCardScan/Source/CardVerify/Api/CardImageVerificationDetailsResponse.swift
+++ StripeCardScan/StripeCardScan/Source/CardVerify/Api/CardImageVerificationDetailsResponse.swift
@@ -13,6 +13,49 @@ struct CardImageVerificationExpectedCard: Decodable {
     let issuer: String
 }
 
+struct CardImageVerificationImageSettings: Decodable {
+    var compressionRatio: Double? = 0.8
+    var imageSize: [Double]? = [1080, 1920]
+}
+
+enum CardImageVerificationFormat: String, SafeEnumCodable {
+    case heic = "heic"
+    case jpeg = "jpeg"
+    case webp = "webp"
+    case unparsable
+}
+
+struct CardImageVerificationAcceptedImageConfigs: Decodable {
+    private let defaultSettings: CardImageVerificationImageSettings?
+    private let formatSettings: [CardImageVerificationFormat: CardImageVerificationImageSettings?]? /// Change name to formatSettings
+    let preferredFormats: [CardImageVerificationFormat]?
+
+    init() {
+        defaultSettings = CardImageVerificationImageSettings()
+        formatSettings = nil
+        preferredFormats = [.heic, .jpeg]
+    }
+}
+
 struct CardImageVerificationDetailsResponse: Decodable {
     let expectedCard: CardImageVerificationExpectedCard?
+    let acceptedImageConfigs: CardImageVerificationAcceptedImageConfigs?
+}
+
+extension CardImageVerificationAcceptedImageConfigs {
+    func imageSettings(format: CardImageVerificationFormat) -> CardImageVerificationImageSettings {
+        var result = defaultSettings ?? CardImageVerificationImageSettings()
+
+        if let formatSpecificSettings = formatSettings?[format] {
+            if let compressionRatio = formatSpecificSettings?.compressionRatio {
+                result.compressionRatio = compressionRatio
+            }
+
+            if let imageSize = formatSpecificSettings?.imageSize {
+                result.imageSize = imageSize
+            }
+        }
+
+        return result
+    }
 }
