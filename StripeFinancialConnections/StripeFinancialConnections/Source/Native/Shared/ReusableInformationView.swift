4073af707f386603151210d81f80dcd01967bf0b
Krisjanis Gaidis <kgaidis@stripe.com>
2022-07-25 15:29:59 -0400

    Financial Connections: Refactored ReusableInformationView for readability.

--- StripeFinancialConnections/StripeFinancialConnections/Source/Native/Shared/ReusableInformationView.swift
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Native/Shared/ReusableInformationView.swift
@@ -24,23 +24,6 @@ final class ReusableInformationView: UIView {
         super.init(frame: .zero)
         backgroundColor = .customBackgroundColor
         
-        let titleLabel = UILabel()
-        titleLabel.font = .stripeFont(forTextStyle: .subtitle)
-        titleLabel.textColor = .textPrimary
-        titleLabel.numberOfLines = 0
-        titleLabel.text = title
-        let subtitleLabel = UILabel()
-        subtitleLabel.font = .stripeFont(forTextStyle: .body)
-        subtitleLabel.textColor = .textSecondary
-        subtitleLabel.numberOfLines = 0
-        subtitleLabel.text = subtitle
-        let labelStackView = UIStackView(arrangedSubviews: [
-            titleLabel,
-            subtitleLabel,
-        ])
-        labelStackView.axis = .vertical
-        labelStackView.spacing = 8
-        
         let iconContainerView = UIView()
         iconContainerView.backgroundColor = .textBrand
         iconContainerView.layer.cornerRadius = 20 // TODO(kgaidis): fix temporary "icon" styling before we get loading icons
@@ -53,7 +36,7 @@ final class ReusableInformationView: UIView {
         
         let headerStackView = UIStackView(arrangedSubviews: [
             iconContainerView,
-            labelStackView,
+            CreateTitleAndSubtitleView(title: title, subtitle: subtitle),
         ])
         headerStackView.axis = .vertical
         headerStackView.spacing = 16
@@ -72,6 +55,26 @@ final class ReusableInformationView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+private func CreateTitleAndSubtitleView(title: String, subtitle: String) -> UIView {
+    let titleLabel = UILabel()
+    titleLabel.font = .stripeFont(forTextStyle: .subtitle)
+    titleLabel.textColor = .textPrimary
+    titleLabel.numberOfLines = 0
+    titleLabel.text = title
+    let subtitleLabel = UILabel()
+    subtitleLabel.font = .stripeFont(forTextStyle: .body)
+    subtitleLabel.textColor = .textSecondary
+    subtitleLabel.numberOfLines = 0
+    subtitleLabel.text = subtitle
+    let labelStackView = UIStackView(arrangedSubviews: [
+        titleLabel,
+        subtitleLabel,
+    ])
+    labelStackView.axis = .vertical
+    labelStackView.spacing = 8
+    return labelStackView
 }
 
 #if DEBUG
