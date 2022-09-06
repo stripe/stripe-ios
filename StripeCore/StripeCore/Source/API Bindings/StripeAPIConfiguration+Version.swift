4be9eed802b0a0aad7b0c23334bda287bd0eb4e1
David Estes <davidestes@stripe.com>
2022-09-06 10:01:13 -0700

    Update version to 22.8.0

--- StripeCore/StripeCore/Source/API Bindings/StripeAPIConfiguration+Version.swift
+++ StripeCore/StripeCore/Source/API Bindings/StripeAPIConfiguration+Version.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public extension StripeAPIConfiguration {
     /// The current version of this library.
-    static let STPSDKVersion = "22.7.1"
+    static let STPSDKVersion = "22.8.0"
 
     /*
      NOTE: `STPSDKVersion` must be a hard-coded static string instead of
