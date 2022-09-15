8bd2e01384f42a13f8b66902ca30a6d1da7f83b4
John Woo <99628984+wooj-stripe@users.noreply.github.com>
2022-09-15 11:26:42 -0700

    Update LUXE metrics for server side filtering of payment methods (#1435)

--- Stripe/AddPaymentMethodViewController.swift
+++ Stripe/AddPaymentMethodViewController.swift
@@ -47,8 +47,8 @@ class AddPaymentMethodViewController: UIViewController {
             )
         }
 
-        let serverFilteredPaymentMethods = PaymentSheet.PaymentMethodType.recommendedPaymentMethodTypes(from: intent).filter({$0 != .USBankAccount})
-        let paymentTypesFiltered = paymentTypes.filter({$0 != .USBankAccount})
+        let serverFilteredPaymentMethods = PaymentSheet.PaymentMethodType.recommendedPaymentMethodTypes(from: intent).filter({$0 != .USBankAccount && $0 != .link})
+        let paymentTypesFiltered = paymentTypes.filter({$0 != .USBankAccount && $0 != .link})
         if serverFilteredPaymentMethods != paymentTypesFiltered {
             let result = serverFilteredPaymentMethods.symmetricDifference(paymentTypes)
             STPAnalyticsClient.sharedClient.logClientFilteredPaymentMethods(clientFilteredPaymentMethods: result.stringList())
