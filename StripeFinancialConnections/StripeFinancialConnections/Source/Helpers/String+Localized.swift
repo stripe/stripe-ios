96cb2ae098511f91ab80505370bb913e40a6b17e
Krisjanis Gaidis <kgaidis@stripe.com>
2022-09-25 10:59:51 -0400

    Financial Connections: localized native AccountPickerAccountLoadErrorView strings.

--- StripeFinancialConnections/StripeFinancialConnections/Source/Helpers/String+Localized.swift
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Helpers/String+Localized.swift
@@ -15,4 +15,12 @@ extension String.Localized {
     static var learn_more: String {
         return STPLocalizedString("Learn more", "Represents the text of a button that can be clicked to learn more about some topic. Once clicked, a web-browser will be opened to give users more info.")
     }
+    
+    static var select_another_bank: String {
+        return STPLocalizedString("Select another bank", "The title of a button. The button presents the user an option to select another bank. For example, we may show this button after user failed to link their primary bank, but maybe the user can try to link their secondary bank!")
+    }
+    
+    static var enter_bank_details_manually: String {
+        return STPLocalizedString("Enter bank details manually", "The title of a button. The button presents the user an option to enter their bank details (account number, routing number) manually. For example, we may show this button after user failed to link their bank 'automatically' with Stripe, so we offer them the option manually link it.")
+    }
 }
--- StripeFinancialConnections/StripeFinancialConnections/Source/Native/AccountPicker/AccountPickerAccountLoadErrorView.swift
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Native/AccountPicker/AccountPickerAccountLoadErrorView.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+@_spi(STP) import StripeCore
 @_spi(STP) import StripeUICore
 
 final class AccountPickerAccountLoadErrorView: UIView {
@@ -18,41 +19,43 @@ final class AccountPickerAccountLoadErrorView: UIView {
         didSelectEnterBankDetailsManually: (() -> Void)? // if nil, don't show button
     ) {
         super.init(frame: .zero)
+        
         let subtitle: String
         let secondaryButtonConfiguration: ReusableInformationView.ButtonConfiguration?
         let primaryButtonConfiguration: ReusableInformationView.ButtonConfiguration
+        
         if let didSelectTryAgain = didSelectTryAgain {
-            subtitle = "Please select another bank or try again."
+            subtitle = STPLocalizedString("Please select another bank or try again.", "The subtitle/description of a screen that shows an error. The error appears after we failed to load users bank accounts. Here we instruct the user to select another bank or to try loading their bank accounts again.")
             secondaryButtonConfiguration = ReusableInformationView.ButtonConfiguration(
-                title: "Select another bank",
+                title: String.Localized.select_another_bank,
                 action: didSelectAnotherBank
             )
             primaryButtonConfiguration = ReusableInformationView.ButtonConfiguration(
-                title: "Try again",
+                title: "Try again", // TODO: once we localize, pull in the string from StripeCore `String.Localized.tryAgain`
                 action: didSelectTryAgain
             )
         } else if let didSelectEnterBankDetailsManually = didSelectEnterBankDetailsManually {
-            subtitle = "Please enter your bank details manually or select another bank."
+            subtitle = STPLocalizedString("Please enter your bank details manually or select another bank.", "The subtitle/description of a screen that shows an error. The error appears after we failed to load users bank accounts. Here we instruct the user to enter their bank details manually or to try selecting another bank.")
             secondaryButtonConfiguration = ReusableInformationView.ButtonConfiguration(
-                title: "Enter bank details manually",
+                title: String.Localized.enter_bank_details_manually,
                 action: didSelectEnterBankDetailsManually
             )
             primaryButtonConfiguration = ReusableInformationView.ButtonConfiguration(
-                title: "Select another bank",
+                title: String.Localized.select_another_bank,
                 action: didSelectAnotherBank
             )
         } else {
-            subtitle = "Please select another bank."
+            subtitle = STPLocalizedString("Please select another bank.", "The subtitle/description of a screen that shows an error. The error appears after we failed to load users bank accounts. Here we instruct the user to try selecting another bank.")
             secondaryButtonConfiguration = nil
             primaryButtonConfiguration = ReusableInformationView.ButtonConfiguration(
-                title: "Select another bank",
+                title: String.Localized.select_another_bank,
                 action: didSelectAnotherBank
             )
         }
         
         let reusableInformationView = ReusableInformationView(
             iconType: .icon, // TODO(kgaidis): set institution image with exclamation error
-            title: "There was a problem accessing your \(institution.name) account",
+            title: String(format: STPLocalizedString("There was a problem accessing your %@ account", "The title of a screen that shows an error. The error appears after we failed to load users bank accounts. Here we describe to the user that we had issues with the bank. '%@' gets replaced by the name of the bank."), institution.name),
             subtitle: subtitle,
             primaryButtonConfiguration: primaryButtonConfiguration,
             secondaryButtonConfiguration: secondaryButtonConfiguration
