13fc2df56ca49cceb1855ac8e0ced5b7eae2b1b1
Chen Cen <ccen@stripe.com>
2022-10-05 16:12:51 -0700

    [Identity] don't use barcode scanner for now

--- StripeIdentity/StripeIdentity/Source/NativeComponents/Coordinators/ImageScanner/DocumentScanner/DocumentScannerOutput.swift
+++ StripeIdentity/StripeIdentity/Source/NativeComponents/Coordinators/ImageScanner/DocumentScanner/DocumentScannerOutput.swift
@@ -30,13 +30,11 @@ struct DocumentScannerOutput: Equatable {
         matchingDocumentType type: DocumentType,
         side: DocumentSide
     ) -> Bool {
-        // If the barcode is clear enough to decode, then that's good enough and
-        // it doesn't matter if the MotionBlurDetector believes there's motion blur
-        let hasNoBlur = barcode?.hasBarcode == true
-        || (!motionBlur.hasMotionBlur && barcode == nil || barcode?.isTimedOut == true)
-
+        // Don't check barcode result as it would end up returning fast and collecting a blury image
+        // that fails to decode on backend
+        // TODO(ccen|IDPROD-4697): Implement better heuristic to decode the back of ID.
         return idDetectorOutput.classification.matchesDocument(type: type, side: side)
         && cameraProperties?.isAdjustingFocus != true
-        && hasNoBlur
+        && !motionBlur.hasMotionBlur
     }
 }
