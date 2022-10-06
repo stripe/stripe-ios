78e35a36883a5d5394dacf99abd5e10d7fb66200
Sergio Campamá <kaipi@copilot.money>
2022-10-06 16:55:36 -0300

    Use iPad's form sheet presentation for macCatalyst

--- Stripe/UIViewController+BottomSheet.swift
+++ Stripe/UIViewController+BottomSheet.swift
@@ -13,9 +13,28 @@ extension UIViewController {
     func presentAsBottomSheet(
         _ viewControllerToPresent: BottomSheetPresentable,
         appearance: PaymentSheet.Appearance,
-        completion: (() -> ())? = nil
+        completion: (() -> Void)? = nil
     ) {
-        if UIDevice.current.userInterfaceIdiom == .pad {
+        // Whether to present the bottomsheet as a form sheet on larger devices, or as bottom sheet
+        // on smaller devices.
+        let presentAsForm: Bool
+        if #available(iOS 14.0, macCatalyst 14.0, *) {
+            if UIDevice.current.userInterfaceIdiom == .pad
+                || UIDevice.current.userInterfaceIdiom == .mac
+            {
+                presentAsForm = true
+            } else {
+                presentAsForm = false
+            }
+        } else {
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                presentAsForm = true
+            } else {
+                presentAsForm = false
+            }
+        }
+
+        if presentAsForm {
             viewControllerToPresent.modalPresentationStyle = .formSheet
             if let vc = viewControllerToPresent as? BottomSheetViewController {
                 viewControllerToPresent.presentationController?.delegate = vc
@@ -26,7 +45,7 @@ extension UIViewController {
             BottomSheetTransitioningDelegate.appearance = appearance
             viewControllerToPresent.transitioningDelegate = BottomSheetTransitioningDelegate.default
         }
-        
+
         present(viewControllerToPresent, animated: true, completion: completion)
     }
 }
