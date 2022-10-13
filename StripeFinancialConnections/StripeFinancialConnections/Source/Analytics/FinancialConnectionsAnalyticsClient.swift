24f11155521b1b34f49c8a6e2696395e29d061cd
Krisjanis Gaidis <kgaidis@stripe.com>
2022-10-13 16:43:45 -0400

    Financial Connections: created a v0 of AnalyticsClient and added it to AuthFlowDataManager.

---
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Analytics/FinancialConnectionsAnalyticsClient.swift
@@ -0,0 +1,59 @@
+//
+//  FinancialConnectionsAnalyticsClient.swift
+//  StripeFinancialConnections
+//
+//  Created by Krisjanis Gaidis on 10/13/22.
+//
+
+import Foundation
+@_spi(STP) import StripeCore
+
+final class FinancialConnectionsAnalyticsClient {
+    
+    private let analyticsClient: AnalyticsClientV2
+    private var additionalParameters: [String:Any] = [:]
+    
+    init(analyticsClient: AnalyticsClientV2 = AnalyticsClientV2(clientId: "", origin: "")) {
+        self.analyticsClient = analyticsClient
+    }
+    
+    public func log(eventName: String, parameters: [String: Any]) {
+        let parameters = parameters.merging(
+            additionalParameters,
+            uniquingKeysWith: { eventParameter, _ in
+                // prioritize event `parameters` over `additionalParameters`
+                return eventParameter
+            }
+        )
+        
+        // TODO(kgaidis): uncomment when we are ready to fire events
+        print("^ logging event: ", eventName, parameters)
+        // analyticsClient.log(eventName: eventName, parameters: parameters)
+    }
+}
+
+extension FinancialConnectionsAnalyticsClient {
+    
+    func setAdditionalParameters(fromManifest manifest: FinancialConnectionsSessionManifest) {
+        // TODO(kgaidis): discuss with others on the need for the other events
+        
+//        additionalParameters["hostname"] = document.location.hostname
+//        additionalParameters["las_client_secret"] = this.linkAccountSessionClientSecret
+//        additionalParameters["las_creator_client_secret"] = this.linkAccountSessionCreatorClientSecret
+//        additionalParameters["las_creator_type"] = this.linkAccountSessionCreatorType
+//        additionalParameters["las_creator_id"] = this.linkAccountSessionCreatorId
+
+//        additionalParameters["key"] = this.apiKey
+//        additionalParameters["stripe_account"] = this.stripeAccount
+//        additionalParameters["logger_id"] = this.getLoggerId(),
+//        additionalParameters["navigator_language"] = navigatorLanguage(),
+//        additionalParameters["is_webview"] = isWebView(window.navigator.userAgent)
+        
+        additionalParameters["livemode"] = manifest.livemode
+        additionalParameters["product"] = manifest.product
+        additionalParameters["is_stripe_direct"] = manifest.isStripeDirect
+        additionalParameters["single_account"] = manifest.singleAccount
+        additionalParameters["allow_manual_entry"] = manifest.singleAccount
+        additionalParameters["account_holder_id"] = manifest.accountholderToken
+    }
+}
