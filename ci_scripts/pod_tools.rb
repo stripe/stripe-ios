94f47771ed69a4d0fe10775e0404ad8584dc2d3e
ramont-stripe <88752322+ramont-stripe@users.noreply.github.com>
2022-10-25 15:27:47 -0400

    Optimize pod tools (#1542)
    * Reformat
    
    * Optimize and add retry logic
    
    * Cleanup
    
    * Highlight pod name
    
    * Cleanup
    
    * Cleanup usage text
    
    * Cleanup

--- ci_scripts/pod_tools.rb
+++ ci_scripts/pod_tools.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# frozen_string_literal: true
+
 require 'cocoapods'
 require 'colorize'
 require 'mail'
@@ -7,44 +9,42 @@ require 'open3'
 require 'yaml'
 require 'json'
 
-PODSPECS = YAML.load_file("modules.yaml")['pod_push_order']
+PODSPECS = YAML.load_file('modules.yaml')['pod_push_order']
 
-USAGE =
-"#{"Usage:".underline}\n"\
-"\n"\
-"\t$ #{"#{__FILE__} COMMAND".green}\n"\
-"\n"\
-"#{"Commands:".underline}\n"\
-"\n"\
-"\t#{"push".green}\n"\
-"\n"\
-"\t\tPushes all podspecs to `trunk` in order by dependencies.\n"\
-"\n"\
-"\n"\
-"\t#{"add-all-owners".green} [#{"POD".magenta}]\n"\
-"\n"\
-"\t\tAdds all the owners of the `Stripe` pod as owners of #{"`POD`".magenta}.\n"\
-"\t\tIf no pod is specified, adds all owners to all pods other than `Stripe`.\n"\
-"\n"\
-"\n"\
-"\t#{"add-owner".green} #{"OWNER-EMAIL".magenta}\n"\
-"\n"\
-"\t\tAdds the registered user specified with #{"`OWNER-EMAIL`".magenta} as an\n"\
-"\t\towner of all pods in this repo.\n"\
-"\n"\
-"\n"\
-"\t#{"remove-owner".green} #{"OWNER-EMAIL".magenta}\n"\
-"\n"\
-"\t\tRemoves the user specified with #{"`OWNER-EMAIL`".magenta} from being\n"\
-"\t\towner of all pods in this repo.\n"
+USAGE = <<~TEXT
+  #{'Usage:'.underline}
+
+    #{"#{__FILE__} COMMAND".green}
+
+  #{'Commands:'.underline}
+
+    #{'push'.green}
+
+      Pushes all podspecs to `trunk` in order by dependencies.
+
+    #{'add-all-owners'.green} [#{'POD'.magenta}]
+
+      Adds all the owners of the `Stripe` pod as owners of #{'`POD`'.magenta}.
+      If no pod is specified, adds all owners to all pods other than `Stripe`.
+
+    #{'add-owner'.green} #{'OWNER-EMAIL'.magenta}
+
+      Adds the registered user specified with #{'`OWNER-EMAIL`'.magenta} as an
+      owner of all pods in this repo.
+
+    #{'remove-owner'.green} #{'OWNER-EMAIL'.magenta}
+
+      Removes the user specified with #{'`OWNER-EMAIL`'.magenta} from being
+      owner of all pods in this repo.
+TEXT
 
 # Pushes all podspecs to `trunk` in order by dependencies.
 def push
   puts "If this is your first time running this command, it's going to take a while."
-  puts "Go for a 🚶 or take a ☕️ break."
-  puts ""
-  puts "Pushing the following podspecs to #{"`trunk`".bold.magenta}: "\
-  "#{PODSPECS.map { |s| s.underline }.join(" ")}"
+  puts 'Go for a 🚶 or take a ☕️ break.'
+  puts ''
+  puts "Pushing the following podspecs to #{'`trunk`'.bold.magenta}: "\
+  "#{PODSPECS.map { |s| s.underline }.join(' ')}"
 
   PODSPECS.each do |podspec|
     stdout, stderr, status = Open3.capture3("pod spec cat #{podspec}")
@@ -58,80 +58,81 @@ def push
       system "pod trunk push #{podspec} --synchronous"
       unless $?.success?
         abort "Unable to push pod #{podspec}.\n"\
-              "If the spec failed to validate due to not finding a compatible version of a pod that was just pushed, wait a few minutes and try again."
+              'If the spec failed to validate due to not finding a compatible version of a pod that was just pushed, wait a few minutes and try again.'
       end
     end
   end
 end
 
-# Adds all the owners of the `Stripe` pod as owners of the given pod.
-def add_all_owners(pod)
-  if pod == "Stripe"
-    abort("Specify a different pod other than #{"`Stripe`".magenta}.".red)
-  end
-
-  puts "Looking up owners of `Stripe` pod..."
-
-  # Get a list of all the owners for the `Stripe` pod
-  stdout, stderr, status = Open3.capture3("pod trunk info Stripe")
-
-  unless status.success?
-    abort(stderr)
-  end
-
-  # Parse output and extract emails
-  yaml = YAML.load(stdout.lines[2..-1].join)
-  owners = yaml[1]["Owners"]
-  owner_emails = owners.map { |owner| Mail::Address.new(owner).address }
-
-  # Add owners for all pods in repo
-  updated_owners=""
-  owner_emails.each do |ownerEmail|
-    puts "Adding #{ownerEmail.blue} to #{pod.magenta} owners."
-    updated_owners=`pod trunk add-owner #{pod} #{ownerEmail}`
-
-    unless $?.success?
-      raise StandardError.new updated_owners
+def run_retrying(cmd, max_retries = 10)
+  retries = 0
+  loop do
+    stdout, stderr, status = Open3.capture3(cmd)
+    if status.success?
+      return stdout
+    elsif retries <= max_retries
+      retries += 1
+      delay = 2**retries
+      puts "Something went wrong. Trying again in #{delay} seconds..."
+      sleep(delay)
+      puts 'Retrying...'
+    else
+      abort(stderr.red)
     end
   end
+end
 
-  puts "Done updating owners:"
-  puts updated_owners
+def get_owners(pod)
+  puts "Looking up owners of #{pod.magenta} pod..."
+  stdout = run_retrying("pod trunk info #{pod}")
+
+  # Parse output and extract emails
+  yaml = YAML.safe_load(stdout.lines[2..-1].join)
+  owners = yaml[1]['Owners']
+
+  owners.map { |owner| Mail::Address.new(owner).address }
+end
+
+def add_owners(pod, owners)
+  abort("Specify a different pod other than #{'Stripe'.magenta}.".red) if pod == 'Stripe'
+
+  pod_owners = get_owners(pod)
+  owners_to_be_added = owners - pod_owners
+
+  changes = pod_owners.map { |email| "  #{email}" }
+
+  owners_to_be_added.each do |owner|
+    puts "Adding #{owner.blue} to #{pod.magenta} owners."
+    run_retrying("pod trunk add-owner #{pod} #{owner}")
+    changes << "+ #{owner}".green
+  end
+
+  puts 'Done updating owners:'
+  puts changes.join("\n")
 end
 
 # Adds all the owners of the `Stripe` pod as owners of the given pod.
-def add_all_owners_all_pods()
-  pods = all_pods_in_repo - ["Stripe"]
-  failed_pods = []
+def add_all_owners_all_pods
+  pods = all_pods_in_repo - ['Stripe']
+  owners = get_owners('Stripe')
   pods.each do |pod|
-    begin
-      add_all_owners(pod)
-    rescue => e
-      puts e.message.red
-      failed_pods.append(pod)
-    end
-  end
-
-  unless failed_pods.empty?
-    abort "Unable to add owners to pods: #{failed_pods.join(", ")}\n".red +
-          "Please run the following command for each of the above listed pods:\n" +
-          "\tbundle exec ruby #{__FILE__} add-all-owners POD"
+    add_owners(pod, owners)
   end
 end
 
 # Adds the specified registered user as an owner of all pods in this repo.
-def add_owner(ownerEmail)
+def add_owner(owner_email)
   all_pods_in_repo.each do |pod|
-    puts "Adding #{ownerEmail.magenta} to #{pod.magenta} owners."
-    system "pod trunk add-owner #{pod} #{ownerEmail}"
+    puts "Adding #{owner_email.magenta} to #{pod.magenta} owners."
+    run_retrying("pod trunk add-owner #{pod} #{owner_email}")
   end
 end
 
 # Removes the specified user from being an owner of all pods in this repo.
-def remove_owner(ownerEmail)
+def remove_owner(owner_email)
   all_pods_in_repo.each do |pod|
-    puts "Removing #{ownerEmail.magenta} from #{pod.magenta} owners."
-    system "pod trunk remove-owner #{pod} #{ownerEmail}"
+    puts "Removing #{owner_email.magenta} from #{pod.magenta} owners."
+    run_retrying("pod trunk remove-owner #{pod} #{owner_email}")
   end
 end
 
@@ -139,30 +140,30 @@ end
 
 # Returns a list of all the pods in this repo
 def all_pods_in_repo
-  return Dir.glob("./*.podspec").map { |file| Pod::Specification.from_file(file).root.name }
+  Dir.glob('./*.podspec').map { |file| Pod::Specification.from_file(file).root.name }
 end
 
 # -- Command Line
 
 if ARGV.empty?
-  abort("#{"Please specify a command.".red}\n\n#{USAGE}")
-elsif ARGV[0] == "push"
+  abort("#{'Please specify a command.'.red}\n\n#{USAGE}")
+elsif ARGV[0] == 'push'
   push
-elsif ARGV[0] == "add-all-owners"
+elsif ARGV[0] == 'add-all-owners'
   if ARGV.length < 2
-    add_all_owners_all_pods()
+    add_all_owners_all_pods
   else
-    add_all_owners(ARGV[1])
+    add_owners(ARGV[1], get_owners('Stripe'))
   end
-elsif ARGV[0] == "add-owner"
+elsif ARGV[0] == 'add-owner'
   if ARGV.length < 2
-    abort("#{"Please specify an owner email to add.".red}\n\n#{USAGE}")
+    abort("#{'Please specify an owner email to add.'.red}\n\n#{USAGE}")
   else
     add_owner(ARGV[1])
   end
-elsif ARGV[0] == "remove-owner"
+elsif ARGV[0] == 'remove-owner'
   if ARGV.length < 2
-    abort("#{"Please specify an owner email to remove.".red}\n\n#{USAGE}")
+    abort("#{'Please specify an owner email to remove.'.red}\n\n#{USAGE}")
   else
     remove_owner(ARGV[1])
   end
