f558c686d1574fc7723cddeda09bc475675fbc95
Ramon Torres <ramont@stripe.com>
2022-10-26 20:31:44 -0400

    Add alias

---
+++ StripeUICore/StripeUICore/Source/Helpers/CompatibleColor.swift
@@ -0,0 +1,14 @@
+//
+//  CompatibleColor.swift
+//  StripeUICore
+//
+//  Created by Ramon Torres on 10/26/22.
+//  Copyright © 2022 Stripe, Inc. All rights reserved.
+//
+
+import UIKit
+
+// TODO(ramont): Remove.
+
+/// Alias to prevent active feature branches from breaking.
+@_spi(STP) public typealias CompatibleColor = UIColor
