bcd034d7ec72b68efb77e33c1236273363b4de3b
ramont-stripe <88752322+ramont-stripe@users.noreply.github.com>
2022-11-01 19:37:34 -0400

    Phone metadata v2 (#2039)
    * Add LZFSE file loader
    
    * Add metadata v2
    
    * Remove hardcoded metadata
    
    * Cleanup
    
    * Update to latest metadata format
    
    * Add fallback data
    
    * Update metadata
    
    * Update fallback metadata
    
    * Remove force unwrap
    
    * Remove redundant concatenation
    
    * Cleanup
    
    * Fix tests
    
    * Cleanup
    
    * Handle trunk prefixes
    
    * Format numbers with trunk prefix
    
    * Update snapshot tests
    
    * Cleanup
    
    * Dynamically generate national templates, if not provided
    
    * Measure size impact of the fallback metadata
    
    * Ensure that file size is already read as little endian
    
    * Restore fallback metadata
    
    * Improve formatting
    
    * Add scripts
    
    * Add missing dependency
    
    * Do not format numbers for places with complex formatting rules
    
    * Remove data that can be created at runtime
    
    * Cleanup
    
    * Fix matching
    
    * Cleanup
    
    * Update fallback metadata
    
    * Add documentation
    
    * E.164 - Do not drop leading 0s other than trunk prefix
    
    * Move class to its own file
    
    * Make metadata inmutable
    
    * Update fallback metadata
    
    * Fix PhoneNumber equatable conformance
    
    * Remove unsupported territories
    
    * Update fallback metadata
    
    * Cleanup
    
    * Cleanup scripts
    
    * Add script for easy updating
    
    * Check that national format contains national prefix
    
    Fix national formatting for BR, CO, CL, GA
    
    * Redo format expansion
    
    * Lint
    
    * Add EditorConfig
    
    * Fail early
    
    * Expand curl options
    
    * Fix typo
    
    * Error on truncated files
    
    * Cleanup
    
    * Update editorconfig
    
    * Update metadata
    
    * Optimize memory usage
    
    * Add analytics
    
    * Fix header
    
    * Optimize fallback metadata
    
    * Correct E.164 length
    
    * Remove tuple label
    
    * Update cache settings
    
    * Rename event
    
    * Fix typo
    
    Co-authored-by: eurias-stripe <114543737+eurias-stripe@users.noreply.github.com>
    
    * Fix typo
    
    Co-authored-by: eurias-stripe <114543737+eurias-stripe@users.noreply.github.com>
    
    * Remove EditorConfig
    
    Co-authored-by: eurias-stripe <114543737+eurias-stripe@users.noreply.github.com>

--- StripeUICore/StripeUICore/Source/Elements/PhoneNumber/PhoneNumberElement.swift
+++ StripeUICore/StripeUICore/Source/Elements/PhoneNumber/PhoneNumberElement.swift
@@ -62,7 +62,7 @@ import UIKit
         theme: ElementsUITheme = .default
     ) {
         let defaults = Self.deriveDefaults(countryCode: defaultCountryCode, phoneNumber: defaultPhoneNumber)
-        let allowedCountryCodes = allowedCountryCodes ?? PhoneNumber.Metadata.allMetadata.map { $0.regionCode }
+        let allowedCountryCodes = allowedCountryCodes ?? PhoneMetadataProvider.shared.metadata.map { $0.region }
         let countryDropdownElement = DropdownFieldElement.makeCountryCode(
             countryCodes: allowedCountryCodes,
             defaultCountry: defaults.countryCode,
@@ -94,7 +94,7 @@ import UIKit
             // Note: We only validate against the currently selected country code, as an autofilled number _without_ a country code can trigger false positives, e.g. "2481234567" could be either "(248) 123-4567" (a phone number from Michigan, USA with no country code) or "+248 1 234 567" (a phone number from Seychelles with a country code). We can assume that generally, a user's autofilled phone number will match their phone's region setting.
             // Autofilled numbers can include the + prefix indicating a country code, but we can't tell if they do here, as by the time we get here the input has already been sanitized and the "+" has been removed.
             let countryCode = countryDropdownElement.selectedItem.rawData
-            if let prefix = PhoneNumber.Metadata.metadata(for: countryCode)?.prefix.dropFirst(), textFieldElement.text.hasPrefix(prefix) {
+            if let prefix = PhoneMetadataProvider.shared.metadata(for: countryCode)?.code.dropFirst(), textFieldElement.text.hasPrefix(prefix) {
                 let unprefixedNumber = String(textFieldElement.text.dropFirst(prefix.count))
                 // Double check that we actually have a valid phone number without the prefix.
                 if let phoneNumber = PhoneNumber(number: unprefixedNumber, countryCode: countryCode), phoneNumber.isComplete {
@@ -139,7 +139,7 @@ extension DropdownFieldElement {
         let countryDisplayStrings: [DropdownFieldElement.DropdownItem] = countryCodes.map {
             let flagEmoji = String.countryFlagEmoji(for: $0) ?? ""              // 🇺🇸
             let name = locale.localizedString(forRegionCode: $0) ?? $0          // United States
-            let prefix = PhoneNumber.Metadata.metadata(for: $0)?.prefix ?? ""   // +1
+            let prefix = PhoneMetadataProvider.shared.metadata(for: $0)?.code ?? ""   // +1
             return .init(
                 pickerDisplayName: "\(flagEmoji) \(name) \(prefix)",            // 🇺🇸 United States +1
                 labelDisplayName: "\(flagEmoji) \(prefix)",
---
+++ StripeUICore/StripeUICore/Source/PhoneNumber/PhoneNumber.swift
@@ -0,0 +1,175 @@
+//
+//  PhoneNumber.swift
+//  StripeUICore
+//
+//  Created by Cameron Sabol on 9/22/21.
+//  Copyright © 2021 Stripe, Inc. All rights reserved.
+//
+
+import Foundation
+
+@_spi(STP) import StripeCore
+
+@_spi(STP) public struct PhoneNumber {
+    struct Constants {
+        /// Maximum number of digits for E.164.
+        ///
+        /// The standard recommends a max length of 15 digits, but according to libphonenumber this hasn't been followed by all countries.
+        ///
+        /// <https://github.com/google/libphonenumber/blob/master/FAQ.md#what-is-the-maximum-and-minimum-length-of-a-phone-number>
+        static let e164MaxDigits = 17
+    }
+
+    typealias Metadata = PhoneMetadataProvider.Metadata
+
+    public enum Format {
+        /// Formatted according to e164 standard, e.g. +15555555555
+        case e164
+        /// Formatted for display as non-international number, e.g. (555) 555-555
+        case national
+        /// Formatted for display as an international number with country code, e.g. +1 (555) 555-5555
+        case international
+    }
+
+    /// The phone number without the country prefix and containing only digits
+    public let number: String
+    private let metadata: Metadata
+
+    /// The country that matches this phone number, e.g. "US"
+    public var countryCode: String {
+        return metadata.region
+    }
+
+    /// The phone number prefix for the country of this phone number, e.g. "+1"
+    public var prefix: String {
+        return metadata.code
+    }
+
+    /// Whether this represents a complete phone number
+    public var isComplete: Bool {
+        let normalizedNumber = metadata.removeTrunkPrefixIfNeeded(number)
+        return (
+            metadata.lengths.contains(normalizedNumber.count) ||
+            normalizedNumber.count > metadata.maxLength
+        )
+    }
+
+    public init?(number: String, countryCode: String) {
+        guard let metadata = PhoneMetadataProvider.shared.metadata(for: countryCode) else {
+            return nil
+        }
+
+        self.number = number.stp_stringByRemovingCharacters(from: .stp_invertedAsciiDigit)
+        self.metadata = metadata
+    }
+
+    init(number: String, metadata: Metadata) {
+        self.number = number
+        self.metadata = metadata
+    }
+
+}
+
+// MARK: - Parsing
+
+extension PhoneNumber {
+
+    /// Parses phone numbers in (*globalized*) E.164 format.
+    ///
+    /// - Note: Our metadata lacks national destination code (area code) ranges, because of this we fallback to
+    ///         the device's locale to disambiguate when a number can possibly belong to multiple regions.
+    ///
+    /// - Parameters:
+    ///   - number: Phone number to parse.
+    ///   - locale: User's locale.
+    /// - Returns: `PhoneNumber`, or `nil` if the number is not parsable.
+    public static func fromE164(_ number: String, locale: Locale = .current) -> PhoneNumber? {
+        let characters: [Character] = .init(number)
+
+        // Matching regex: ^\+[1-9]\d{2,14}$
+        guard
+            characters.count > 4,
+            characters.count <= Constants.e164MaxDigits + 1,
+            characters[0] == "+",
+            characters[1] != "0",
+            characters[1...].allSatisfy({
+                $0.unicodeScalars.allSatisfy(CharacterSet.stp_asciiDigit.contains(_:))
+            })
+        else {
+            return nil
+        }
+
+        let makePhoneNumber: (Metadata) -> PhoneNumber = { metadata in
+            return PhoneNumber(
+                number: String(characters[metadata.code.count...]),
+                metadata: metadata
+            )
+        }
+
+        // This filter should narrow down the metadata list to just 1 candidate in most cases,
+        // as very few countries share country calling codes. Country calling codes are also
+        // *Prefix codes*, which means that two codes will never overlap.
+        let candidates = PhoneMetadataProvider.shared.metadata.filter({ number.hasPrefix($0.code) })
+        if candidates.count == 1 {
+            return candidates.first.flatMap(makePhoneNumber)
+        }
+
+        // This second filter uses the device's locale to pick a winner out of N candidates.
+        if let winner = candidates.first(where: { $0.region == locale.regionCode }) {
+            return makePhoneNumber(winner)
+        }
+
+        // If no winner, we simply return the first candidate. Our metadata is sorted in a way that the
+        // main country of a prefix is always first.
+        return candidates.first.flatMap(makePhoneNumber)
+    }
+
+}
+
+// MARK: - Formatting
+
+extension PhoneNumber {
+
+    public func string(as format: Format) -> String {
+        guard !number.isEmpty else {
+            return ""
+        }
+
+        switch format {
+        case .e164:
+            var result = metadata.removeTrunkPrefixIfNeeded(number)
+
+            let countryCodeLength = metadata.code.count - 1
+            let maxNationalNumberLength = Constants.e164MaxDigits - countryCodeLength
+
+            // E.164 doesn't accept more than 15 digits
+            if result.count > maxNationalNumberLength {
+                result = String(result.prefix(maxNationalNumberLength))
+            }
+
+            return metadata.code + result
+        case .national:
+            guard let pattern = metadata.bestFormat(for: number),
+                  let formatter = TextFieldFormatter(format: pattern) else {
+                return number
+            }
+
+            let result = formatter.applyFormat(to: number, shouldAppendRemaining: true)
+            return result.count > 0 ? result : number
+        case .international:
+            return "\(metadata.code) \(string(as: .national))"
+        }
+    }
+
+}
+
+// MARK: - Equatable
+
+extension PhoneNumber: Equatable {
+    public static func == (lhs: PhoneNumber, rhs: PhoneNumber) -> Bool {
+        return (
+            lhs.number == rhs.number &&
+            lhs.metadata.region == rhs.metadata.region
+        )
+    }
+}
