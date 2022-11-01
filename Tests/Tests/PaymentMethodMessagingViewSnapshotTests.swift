483113724a8c9689526b14be8c3ee2f8e091efb5
Yuki Tokuhiro <yuki@stripe.com>
2022-11-01 12:13:04 -0700

    Update snapshot tests

--- Tests/Tests/PaymentMethodMessagingViewSnapshotTests.swift
+++ Tests/Tests/PaymentMethodMessagingViewSnapshotTests.swift
@@ -39,26 +39,6 @@ class PaymentMethodMessagingViewSnapshotTests: FBSnapshotTestCase {
         verify(view)
     }
     
-    let mockHTMLWithWhiteImages =
-    """
-    <img src=\"https://cdn.glitch.global/2d9cc690-78ea-45e5-9a44-bb3f3d2128a0/afterpay_logo_white.png?v=1666389081830\"><img src=\"https://cdn.glitch.global/2d9cc690-78ea-45e5-9a44-bb3f3d2128a0/klarna_logo_white.png?v=1666389081830\">
-    <br/>
-    As low as 4 <i>interest-free</i> payments of <b> $24.75 </b> 🎉
-    """
-    @available(iOS 13.0, *)
-    func testDarkMode() async {
-        guard let mockAttributedString = try? await PaymentMethodMessagingView.makeAttributedString(from: mockHTMLWithWhiteImages, font: configuration.font) else {
-            XCTFail()
-            return
-        }
-        let window = UIWindow(frame: CGRect(x: 0, y: 0, width: 375, height: 1026))
-        window.isHidden = false
-        window.overrideUserInterfaceStyle = .dark
-        let view = PaymentMethodMessagingView(attributedString: mockAttributedString, modalURL: "https://stripe.com", configuration: configuration)
-        window.addSubview(view)
-        verify(view)
-    }
-
     func testCustomFontAndCustomTextColor() async {
         var configuration = configuration
         configuration.font = UIFont(name: "AmericanTypewriter", size: 10)!
@@ -71,21 +51,22 @@ class PaymentMethodMessagingViewSnapshotTests: FBSnapshotTestCase {
         verify(view)
     }
     
-    func testReal() {
-        // Uncomment to snapshot a real view
-//        let apiClient = STPAPIClient(publishableKey: STPTestingDefaultPublishableKey)
-//        let config = PaymentMethodMessagingView.Configuration(apiClient: apiClient, paymentMethods: PaymentMethodMessagingView.Configuration.PaymentMethod.allCases, currency: "USD", amount: 1099)
-//        let createViewExpectation = expectation(description: "")
-//        PaymentMethodMessagingView.create(configuration: config) { [weak self] result in
-//            switch result {
-//            case .failure(let error):
-//                XCTFail(error.localizedDescription)
-//            case .success(let view):
-//                self?.verify(view)
-//            }
-//            createViewExpectation.fulfill()
-//        }
-//        waitForExpectations(timeout: 10)
+    // Remove _ to snapshot a view using the production endpoint
+    // We can't test this, even if we stub the network response, because the response contains image URLs 
+    func _testReal() {
+        let apiClient = STPAPIClient(publishableKey: STPTestingDefaultPublishableKey)
+        let config = PaymentMethodMessagingView.Configuration(apiClient: apiClient, paymentMethods: PaymentMethodMessagingView.Configuration.PaymentMethod.allCases, currency: "USD", amount: 1099)
+        let createViewExpectation = expectation(description: "")
+        PaymentMethodMessagingView.create(configuration: config) { [weak self] result in
+            switch result {
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            case .success(let view):
+                self?.verify(view)
+            }
+            createViewExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 10)
     }
     
     // MARK: - Helpers
