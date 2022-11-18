029bed198964bbb305629e8d51b4d6aff91785c4
Krisjanis Gaidis <kgaidis@stripe.com>
2022-11-17 22:37:01 -0500

    t puFinancial Connections: reverted Account Picker text logic to some of the previous state.

--- StripeFinancialConnections/StripeFinancialConnections/Source/Native/AccountPicker/AccountPickerFooterView.swift
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Native/AccountPicker/AccountPickerFooterView.swift
@@ -12,11 +12,12 @@ import UIKit
 @available(iOSApplicationExtension, unavailable)
 final class AccountPickerFooterView: UIView {
     
+    private let singleAccount: Bool
+    private let institutionHasAccountPicker: Bool
     private let didSelectLinkAccounts: () -> Void
     
     private lazy var linkAccountsButton: Button = {
         let linkAccountsButton = Button(configuration: .financialConnectionsPrimary)
-        linkAccountsButton.title = STPLocalizedString("Confirm", "A button that allows users to confirm the process of saving their bank accounts for future payments. This button appears in a screen that allows users to select which bank accounts they want to use to pay for something.")
         linkAccountsButton.addTarget(self, action: #selector(didSelectLinkAccountsButton), for: .touchUpInside)
         linkAccountsButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -29,9 +30,13 @@ final class AccountPickerFooterView: UIView {
         isStripeDirect: Bool,
         businessName: String?,
         permissions: [StripeAPI.FinancialConnectionsAccount.Permissions],
+        singleAccount: Bool,
+        institutionHasAccountPicker: Bool,
         didSelectLinkAccounts: @escaping () -> Void,
         didSelectMerchantDataAccessLearnMore: @escaping () -> Void
     ) {
+        self.singleAccount = singleAccount
+        self.institutionHasAccountPicker = institutionHasAccountPicker
         self.didSelectLinkAccounts = didSelectLinkAccounts
         super.init(frame: .zero)
         
@@ -64,6 +69,25 @@ final class AccountPickerFooterView: UIView {
     
     func didSelectAccounts(count numberOfAccountsSelected: Int) {
         linkAccountsButton.isEnabled = (numberOfAccountsSelected > 0)
+        
+        if institutionHasAccountPicker {
+            linkAccountsButton.title = STPLocalizedString("Confirm", "A button that allows users to confirm the process of saving their bank accounts for future payments. This button appears in a screen that allows users to select which bank accounts they want to use to pay for something.")
+        } else {
+            let singleAccountButtonTitle = STPLocalizedString("Link account", "A button that allows users to confirm the process of saving their bank account for future payments. This button appears in a screen that allows users to select which bank accounts they want to use to pay for something.")
+            let multipleAccountButtonTitle = STPLocalizedString("Link accounts", "A button that allows users to confirm the process of saving their bank accounts for future payments. This button appears in a screen that allows users to select which bank accounts they want to use to pay for something.")
+            
+            if numberOfAccountsSelected == 0 {
+                if singleAccount {
+                    linkAccountsButton.title = singleAccountButtonTitle
+                } else {
+                    linkAccountsButton.title = multipleAccountButtonTitle
+                }
+            } else if numberOfAccountsSelected == 1 {
+                linkAccountsButton.title = singleAccountButtonTitle
+            } else { // numberOfAccountsSelected > 1
+                linkAccountsButton.title = multipleAccountButtonTitle
+            }
+        }
     }
 }
 
