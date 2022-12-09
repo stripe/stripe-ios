3c68936760652e9f7272ab2fccc1ed30ca04b24a
Nick Porter <porter@stripe.com>
2022-12-09 13:57:26 -0700

    Make decimal formatting more generic

--- Tests/Tests/NSDecimalNumber+StripeTest.swift
+++ Tests/Tests/NSDecimalNumber+StripeTest.swift
@@ -14,17 +14,59 @@
 
 class NSDecimalNumberStripeTest: XCTestCase {
     func testDecimalAmount_hasDecimal() {
-        let decimalNumber = NSDecimalNumber.stp_decimalNumber(withAmount: 1000, currency: "usd")
-        XCTAssertEqual(decimalNumber, NSDecimalNumber(string: "10.00"))
+        // an incomplete list of 2 decimal point currencies
+        let twoDecimalPointCurrencies = [
+            "usd",
+            "dkk",
+            "eur",
+            "aud",
+            "sek",
+            "sgd"
+        ]
+        
+        for twoDecimalPointCurrency in twoDecimalPointCurrencies {
+            let decimalNumber = NSDecimalNumber.stp_decimalNumber(withAmount: 1000, currency: twoDecimalPointCurrency)
+            XCTAssertEqual(decimalNumber, NSDecimalNumber(string: "10.00"))
+        }
     }
 
     func testDecimalAmount_noDecimal() {
-        let decimalNumber = NSDecimalNumber.stp_decimalNumber(withAmount: 1000, currency: "jpy")
-        XCTAssertEqual(decimalNumber, NSDecimalNumber(string: "1000"))
+        let noDecimalPointCurrencies = [
+            "bif",
+            "clp",
+            "djf",
+            "gnf",
+            "jpy",
+            "kmf",
+            "krw",
+            "mga",
+            "pyg",
+            "rwf",
+            "vnd",
+            "vuv",
+            "xaf",
+            "xof",
+            "xpf"
+        ]
+
+        for currency in noDecimalPointCurrencies {
+            let decimalNumber = NSDecimalNumber.stp_decimalNumber(withAmount: 1000, currency: currency)
+            XCTAssertEqual(decimalNumber, NSDecimalNumber(string: "1000"))
+        }
     }
     
     func testDecimalAmount_threeDecimal() {
-        let decimalNumber = NSDecimalNumber.stp_decimalNumber(withAmount: 92000, currency: "kwd")
-        XCTAssertEqual(decimalNumber, NSDecimalNumber(string: "92"))
+        let threeDecimalCurrencies = [
+            "bhd",
+            "jod",
+            "kwd",
+            "omr",
+            "tnd"
+        ]
+        
+        for currency in threeDecimalCurrencies {
+            let decimalNumber = NSDecimalNumber.stp_decimalNumber(withAmount: 92000, currency: currency)
+            XCTAssertEqual(decimalNumber, NSDecimalNumber(string: "92"))
+        }
     }
 }
