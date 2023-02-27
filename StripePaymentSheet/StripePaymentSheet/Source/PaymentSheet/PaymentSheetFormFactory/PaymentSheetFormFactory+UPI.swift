542ed9c5276c53e9c4a8b5c33d83a47797a1b55b
eurias-stripe <eurias@stripe.com>
2023-02-27 12:08:13 -0800

    Enable BDCC for UPI

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheetFormFactory/PaymentSheetFormFactory+UPI.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheetFormFactory/PaymentSheetFormFactory+UPI.swift
@@ -14,8 +14,49 @@ import UIKit
 
 extension PaymentSheetFormFactory {
 
-    func makeUPI() -> FormElement {
-        return FormElement(autoSectioningElements: [makeUPIHeader(), makeVPAField()], theme: theme)
+    func makeUPI() -> PaymentMethodElement {
+        let contactInformationElement = makeContactInformation(
+            includeName: configuration.billingDetailsCollectionConfiguration.name == .always,
+            includeEmail: configuration.billingDetailsCollectionConfiguration.email == .always,
+            includePhone: configuration.billingDetailsCollectionConfiguration.phone == .always)
+        let billingAddressElement = configuration.billingDetailsCollectionConfiguration.address == .full
+            ? makeBillingAddressSection(countries: nil)  // Should we restrict to India?
+            : nil
+
+        let allElements: [Element?] = [
+            makeUPIHeader(),
+            makeVPAField(),
+            contactInformationElement,
+            billingAddressElement,
+        ]
+        let autoSectioningElements = allElements.compactMap { $0 }
+        let formElement = FormElement(autoSectioningElements: autoSectioningElements, theme: theme)
+        return PaymentMethodElementWrapper(
+            formElement,
+            defaultsApplier: { [configuration] _, params in
+                // Only apply defaults when the flag is on.
+                guard configuration.billingDetailsCollectionConfiguration.attachDefaultsToPaymentMethod else {
+                    return params
+                }
+
+                if let name = configuration.defaultBillingDetails.name {
+                    params.paymentMethodParams.nonnil_billingDetails.name = name
+                }
+                if let phone = configuration.defaultBillingDetails.phone {
+                    params.paymentMethodParams.nonnil_billingDetails.phone = phone
+                }
+                if let email = configuration.defaultBillingDetails.email {
+                    params.paymentMethodParams.nonnil_billingDetails.email = email
+                }
+                if configuration.defaultBillingDetails.address != .init() {
+                    params.paymentMethodParams.nonnil_billingDetails.address =
+                        STPPaymentMethodAddress(address: configuration.defaultBillingDetails.address)
+                }
+                return params
+            },
+            paramsUpdater: { element, params in
+                return element.updateParams(params: params)
+            })
     }
 
     private func makeUPIHeader() -> StaticElement {
