690570b79c763725e09f8dd93af67054ecba2cd5
kgaidis-stripe <105514761+kgaidis-stripe@users.noreply.github.com>
2023-03-02 17:14:00 -0500

    FC Networking: Polished NetworkingVerificationPane with a new OTP text field (#2330)
    * FC Networking: added a polished OneTimeCodeTextField into the NetworkingLinkVerification pane and started cleaning up the logic.
    
    * FC Networking: split up lookup/startVerification calls in NetworkingLinkVerification.
    
    * FC Networking: other small clean-up in NetworkingLinkVerification pane.
    
    * FC Networking: added support for expected OTP errors in NetworkingLinkVerification.

--- StripeFinancialConnections/StripeFinancialConnections/Source/Native/NetworkingLinkVerification/NetworkingLinkVerificationViewController.swift
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Native/NetworkingLinkVerification/NetworkingLinkVerificationViewController.swift
@@ -15,7 +15,7 @@ protocol NetworkingLinkVerificationViewControllerDelegate: AnyObject {
     func networkingLinkVerificationViewController(
         _ viewController: NetworkingLinkVerificationViewController,
         didRequestNextPane nextPane: FinancialConnectionsSessionManifest.NextPane,
-        consumerSession: ConsumerSessionData
+        consumerSession: ConsumerSessionData?
     )
     func networkingLinkVerificationViewController(
         _ viewController: NetworkingLinkVerificationViewController,
@@ -55,23 +55,61 @@ final class NetworkingLinkVerificationViewController: UIViewController {
         view.backgroundColor = .customBackgroundColor
 
         showLoadingView(true)
-        dataSource.startVerificationSession()
+        dataSource.lookupConsumerSession()
             .observe { [weak self] result in
                 guard let self = self else { return }
-                self.showLoadingView(false)
                 switch result {
-                case .success(let consumerSessionResponse):
-                    self.showContent(redactedPhoneNumber: consumerSessionResponse.consumerSession.redactedPhoneNumber)
+                case .success(let lookupConsumerSessionResponse):
+                    if lookupConsumerSessionResponse.exists {
+                        self.dataSource.startVerificationSession()
+                            .observe { [weak self] result in
+                                guard let self = self else { return }
+                                self.showLoadingView(false)
+                                switch result {
+                                case .success(let consumerSessionResponse):
+                                    self.showContent(redactedPhoneNumber: consumerSessionResponse.consumerSession.redactedPhoneNumber)
+                                case .failure(let error):
+                                    self.dataSource.analyticsClient.logUnexpectedError(
+                                        error,
+                                        errorName: "StartVerificationSessionError",
+                                        pane: .networkingLinkVerification
+                                    )
+                                    self.dataSource.analyticsClient.log(
+                                        eventName: "networking.verification.error",
+                                        parameters: [
+                                            "error": "StartVerificationSession"
+                                        ],
+                                        pane: .networkingLinkVerification
+                                    )
+                                    self.delegate?.networkingLinkVerificationViewController(self, didReceiveTerminalError: error)
+                                }
+                            }
+                    } else {
+                        self.dataSource.analyticsClient.log(
+                            eventName: "networking.verification.error",
+                            parameters: [
+                                "error": "ConsumerNotFoundError"
+                            ],
+                            pane: .networkingLinkVerification
+                        )
+                        self.delegate?.networkingLinkVerificationViewController(self, didRequestNextPane: .institutionPicker, consumerSession: nil)
+                        self.showLoadingView(false)
+                    }
                 case .failure(let error):
+                    self.dataSource.analyticsClient.logUnexpectedError(
+                        error,
+                        errorName: "LookupConsumerSessionError",
+                        pane: .networkingLinkVerification
+                    )
                     self.dataSource.analyticsClient.log(
                         eventName: "networking.verification.error",
                         parameters: [
-                            // TODO(kgaidis): figure out a proper way to log this error
-                            "error": "here"
+                            "error": "LookupConsumerSession"
                         ],
                         pane: .networkingLinkVerification
                     )
                     self.delegate?.networkingLinkVerificationViewController(self, didReceiveTerminalError: error)
+                    self.showLoadingView(false)
                 }
             }
     }
@@ -83,13 +121,15 @@ final class NetworkingLinkVerificationViewController: UIViewController {
                 "The title of a screen where users are informed that they can sign-in-to Link."
             ),
             subtitle: STPLocalizedString(
-                "Enter the code sent to \(redactedPhoneNumber)",
+                "Enter the code sent to \(redactedPhoneNumber)", // TODO(kgaidis): polish how the redacted phone numnber is displayed
                 "The subtitle/description of a screen where users are informed that they have received a One-Type-Password (OTP) to their phone."
             ),
             contentView: bodyView,
             footerView: nil
         )
         pane.addTo(view: view)
+
+        bodyView.otpTextField.becomeFirstResponder()
     }
 
     private func showLoadingView(_ show: Bool) {
@@ -127,25 +167,22 @@ final class NetworkingLinkVerificationViewController: UIViewController {
 extension NetworkingLinkVerificationViewController: NetworkingLinkVerificationBodyViewDelegate {
 
     func networkingLinkVerificationBodyView(
-        _ view: NetworkingLinkVerificationBodyView,
+        _ bodyView: NetworkingLinkVerificationBodyView,
         didEnterValidOTPCode otpCode: String
     ) {
-        view.otpTextField.text = "CONFIRMING OTP..."
+        bodyView.otpTextField.resignFirstResponder()
+        // TODO(kgaidis): consider implementing a loading/grayed-out state for `otpTextField`
 
         dataSource.confirmVerificationSession(otpCode: otpCode)
             .observe { [weak self] result in
                 guard let self = self else { return }
                 switch result {
                 case .success:
-                    view.otpTextField.text = "SUCCESS! CALLING markLinkVerified..."
-
                     self.dataSource.markLinkVerified()
                         .observe { [weak self] result in
                             guard let self = self else { return }
                             switch result {
                             case .success(let manifest):
-                                view.otpTextField.text = "markLinkVerified SUCCESS! Wait on accounts..."
-
                                 self.dataSource.fetchNetworkedAccounts()
                                     .observe { [weak self] result in
                                         guard let self = self else { return }
@@ -166,7 +203,13 @@ extension NetworkingLinkVerificationViewController: NetworkingLinkVerificationBo
                                                 self.requestNextPane(.linkAccountPicker)
                                             }
                                         case .failure(let error):
-                                            // TODO(kgaidis): log the error using the standard error logging too
+                                            self.dataSource
+                                                .analyticsClient
+                                                .logUnexpectedError(
+                                                    error,
+                                                    errorName: "FetchNetworkedAccountsError",
+                                                    pane: .networkingLinkVerification
+                                                )
                                             self.dataSource
                                                 .analyticsClient
                                                 .log(
@@ -176,21 +219,43 @@ extension NetworkingLinkVerificationViewController: NetworkingLinkVerificationBo
                                                     ],
                                                     pane: .networkingLinkVerification
                                                 )
-                                            print(error) // TODO(kgaidis): remove print
                                             self.requestNextPane(manifest.nextPane)
                                         }
                                     }
                             case .failure(let error):
-                                print(error) // TODO(kgaidis): remove print
-                                view.otpTextField.text = "markLinkVerified FAILURE: \(error.localizedDescription)"
-                                // TODO(kgaidis): go to terminal error but double-check
+                                self.dataSource
+                                    .analyticsClient
+                                    .logUnexpectedError(
+                                        error,
+                                        errorName: "MarkLinkVerifiedError",
+                                        pane: .networkingLinkVerification
+                                    )
                                 self.delegate?.networkingLinkVerificationViewController(self, didReceiveTerminalError: error)
                             }
                         }
                 case .failure(let error):
-                    print(error) // TODO(kgaidis): remove print
-                    view.otpTextField.text = "FAILURE...\(error.localizedDescription)"
-                    // TODO(kgaidis): display various known errors, or if unknown error, show terminal error
+                    if let errorMessage = AuthFlowHelpers.networkingOTPErrorMessage(fromError: error, otpType: "SMS") {
+                        self.dataSource
+                            .analyticsClient
+                            .logExpectedError(
+                                error,
+                                errorName: "ConfirmVerificationSessionError",
+                                pane: .networkingLinkVerification
+                            )
+
+                        bodyView.otpTextField.performInvalidCodeAnimation(shouldClearValue: false)
+                        bodyView.showErrorText(errorMessage)
+                    } else {
+                        self.dataSource
+                            .analyticsClient
+                            .logUnexpectedError(
+                                error,
+                                errorName: "ConfirmVerificationSessionError",
+                                pane: .networkingLinkVerification
+                            )
+
+                        self.delegate?.networkingLinkVerificationViewController(self, didReceiveTerminalError: error)
+                    }
                 }
             }
     }
