f3fbe14d19b548555b14aecd9e72650d62de3037
Chen Cen <ccen@stripe.com>
2023-03-01 20:22:00 -0800

    [Identity] UX tweak for id/addr

--- StripeIdentity/StripeIdentity/Source/NativeComponents/Views/IdentityFlowView.swift
+++ StripeIdentity/StripeIdentity/Source/NativeComponents/Views/IdentityFlowView.swift
@@ -43,14 +43,6 @@ class IdentityFlowView: UIView {
             config.font = buttonFont
             return config
         }
-
-        static let buttonBackgroundBlurStyle: UIBlurEffect.Style = {
-            if #available(iOS 13.0, *) {
-                return .systemUltraThinMaterial
-            } else {
-                return .regular
-            }
-        }()
     }
 
     struct ViewModel {
@@ -113,11 +105,11 @@ class IdentityFlowView: UIView {
         return stackView
     }()
 
-    private let buttonBackgroundBlurView = UIVisualEffectView(
-        effect: UIBlurEffect(
-            style: Style.buttonBackgroundBlurStyle
-        )
-    )
+    private let buttonBackgroundView: UIView = {
+        let buttonBackgroundView = UIView()
+        buttonBackgroundView.backgroundColor = .systemBackground
+        return buttonBackgroundView
+    }()
 
     private var flowViewDelegate: IdentityFlowViewDelegate?
 
@@ -125,6 +117,7 @@ class IdentityFlowView: UIView {
     private var contentViewModel: ContentViewModel?
     private var buttons: [Button] = []
     private var buttonTapActions: [() -> Void] = []
+    private var initialScrollViewBottomInsect: CGFloat?
 
     // MARK: - Init
 
@@ -163,7 +156,7 @@ class IdentityFlowView: UIView {
 
         // Adjust bottom inset to make space for keyboard
         let bottomInset =
-            isKeyboardHidden ? 0 : (endFrame.height - frame.height + scrollView.frame.maxY)
+            isKeyboardHidden ? initialScrollViewBottomInsect! : (endFrame.height - frame.height + scrollView.frame.maxY)
         scrollView.contentInset.bottom = bottomInset
         scrollView.verticalScrollIndicatorInsets.bottom = bottomInset
         scrollView.horizontalScrollIndicatorInsets.bottom = bottomInset
@@ -175,13 +168,15 @@ class IdentityFlowView: UIView {
         // Update the scrollView's inset based on the height of the button
         // container so that the content displays above the container plus
         // buttonSpacing when scrolled all the way to the bottom
-        let bottomInset = buttonBackgroundBlurView.frame.height + Style.buttonSpacing
+        let bottomInset = buttonBackgroundView.frame.height + Style.buttonSpacing
         scrollView.verticalScrollIndicatorInsets.bottom = bottomInset
         scrollView.contentInset.bottom = bottomInset
 
         if scrollView.contentSize.height > 0 {
             flowViewDelegate?.scrollViewFullyLaiedOut(scrollView)
         }
+
+        initialScrollViewBottomInsect = scrollView.contentInset.bottom
     }
 }
 
@@ -196,21 +191,21 @@ extension IdentityFlowView {
 
         // Arrange container stack view: scroll + button
         addAndPinSubview(scrollView)
-        addSubview(buttonBackgroundBlurView)
-        buttonBackgroundBlurView.contentView.addAndPinSubviewToSafeArea(
+        addSubview(buttonBackgroundView)
+        buttonBackgroundView.addAndPinSubviewToSafeArea(
             buttonStackView,
             insets: Style.buttonInsets
         )
     }
 
     fileprivate func installConstraints() {
-        buttonBackgroundBlurView.translatesAutoresizingMaskIntoConstraints = false
+        buttonBackgroundView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             // Constrain buttons to bottom
-            buttonBackgroundBlurView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            buttonBackgroundBlurView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            buttonBackgroundBlurView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            buttonBackgroundView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            buttonBackgroundView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            buttonBackgroundView.bottomAnchor.constraint(equalTo: bottomAnchor),
 
             // Make scroll view's content full-width
             scrollView.contentLayoutGuide.leadingAnchor.constraint(
@@ -233,10 +228,10 @@ extension IdentityFlowView {
     fileprivate func configureButtons(with buttonViewModels: [ViewModel.Button]) {
         // If there are no buttons to display, hide the container view
         guard buttonViewModels.count > 0 else {
-            buttonBackgroundBlurView.isHidden = true
+            buttonBackgroundView.isHidden = true
             return
         }
-        buttonBackgroundBlurView.isHidden = false
+        buttonBackgroundView.isHidden = false
 
         // Configure buttons and tapActions based from models after we ensure
         // there are the right number of buttons
