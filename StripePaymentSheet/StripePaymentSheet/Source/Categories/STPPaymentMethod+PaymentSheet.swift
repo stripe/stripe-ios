1ee095f17f9887cc695a27f4b3302536018a0a69
Bill Meltsner <bmelts@stripe.com>
2023-03-02 10:05:35 -0800

    Remove accessibilityLabel override from STPPaymentMethod extension

--- StripePaymentSheet/StripePaymentSheet/Source/Categories/STPPaymentMethod+PaymentSheet.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/Categories/STPPaymentMethod+PaymentSheet.swift
@@ -25,29 +25,25 @@ extension STPPaymentMethod {
         }
     }
 
-    public override var accessibilityLabel: String? {
-        get {
-            switch type {
-            case .card:
-                guard let card = self.card else {
-                    return nil
-                }
-                let brand = STPCardBrandUtilities.stringFrom(card.brand) ?? ""
-                let last4 = card.last4 ?? ""
-                let last4Spaced = last4.map { String($0) }.joined(separator: " ")
-                let localized = String.Localized.card_brand_ending_in_last_4
-                return String(format: localized, brand, last4Spaced)
-            case .USBankAccount:
-                guard let usBankAccount = self.usBankAccount else {
-                    return nil
-                }
-                return String(format: String.Localized.bank_name_account_ending_in_last_4, usBankAccount.bankName, usBankAccount.last4)
-            default:
+    var paymentSheetAccessibilityLabel: String? {
+        switch type {
+        case .card:
+            guard let card = self.card else {
                 return nil
             }
+            let brand = STPCardBrandUtilities.stringFrom(card.brand) ?? ""
+            let last4 = card.last4 ?? ""
+            let last4Spaced = last4.map { String($0) }.joined(separator: " ")
+            let localized = String.Localized.card_brand_ending_in_last_4
+            return String(format: localized, brand, last4Spaced)
+        case .USBankAccount:
+            guard let usBankAccount = self.usBankAccount else {
+                return nil
+            }
+            return String(format: String.Localized.bank_name_account_ending_in_last_4, usBankAccount.bankName, usBankAccount.last4)
+        default:
+            return nil
         }
-        set {
-            // no-op
-        }
+
     }
 }
