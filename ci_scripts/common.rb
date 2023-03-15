40c4c5956c231113c58141bc378668aeb27be1e7
eurias-stripe <114543737+eurias-stripe@users.noreply.github.com>
2023-03-15 10:27:16 -0700

    Verify xcode version before proposing release (#2355)

--- ci_scripts/common.rb
+++ ci_scripts/common.rb
@@ -3,6 +3,21 @@
 require 'colorize'
 require 'English'
 
+# This should generally be the minimum Xcode version supported by the App Store, as the
+# compiled XCFrameworks won't be usable on older versions.
+# We sometimes bump this if an Xcode bug or deprecation forces us to upgrade early.
+MIN_SUPPORTED_XCODE_VERSION = '13.2.1'.freeze
+
+def verify_xcode_version
+  # Verify that xcode-select -p returns the correct version for building Stripe.xcframework.
+  unless `xcodebuild -version`.include?("Xcode #{MIN_SUPPORTED_XCODE_VERSION}")
+    rputs "Xcode #{MIN_SUPPORTED_XCODE_VERSION} is required to build Stripe.xcframework."
+    rputs 'Use `xcode-select -s` to select the correct version, or download it from https://developer.apple.com/download/more/.'
+    rputs "If you believe this is no longer the correct version, update `MIN_SUPPORTED_XCODE_VERSION` in `#{__FILE__}`."
+    abort
+  end
+end
+
 def rputs(string)
   puts string.red
 end
