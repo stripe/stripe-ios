5a8425801cbbed67cea90b88f3eb0ec968e5619c
Till Hellmund <tillh@stripe.com>
2023-03-20 15:22:44 -0400

    Remove reference to script in `create_release.rb`

--- ci_scripts/create_release.rb
+++ ci_scripts/create_release.rb
@@ -118,7 +118,7 @@ def propose_release
     new_dir_file = File.join_if_safe(temp_dir, "new_directories_#{@version}.txt")
     File.open(new_dir_file, 'w') { |file| file.puts new_dirs }
 
-    rputs "Complete the pull request checklist at #{pr.html_url} and the above docs PR, then run `bundle exec ruby ci_scripts/propose_release.rb`"
+    rputs "Complete the pull request checklist at #{pr.html_url} and the above docs PR"
     rputs "For a list of new directories since tag #{prev_release_tag}, `cat #{new_dir_file}`"
     notify_user
   end
