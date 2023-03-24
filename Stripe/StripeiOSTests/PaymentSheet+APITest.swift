c3f4e57c8a76705c75e3e821e7f8c8db0d29cb3b
Nick Porter <porter@stripe.com>
2023-03-24 10:57:04 -0600

    Fix test

--- Stripe/StripeiOSTests/PaymentSheet+APITest.swift
+++ Stripe/StripeiOSTests/PaymentSheet+APITest.swift
@@ -455,17 +455,19 @@ class PaymentSheetAPITest: XCTestCase {
             // These tests don't confirm, so this is unused
         }
         let expectation = expectation(description: "Updates")
+        var flowController: PaymentSheet.FlowController!
         PaymentSheet.FlowController.create(intentConfig: intentConfig, configuration: configuration) { result in
             switch result {
             case .success(let sut):
+                flowController = sut
                 // This update should be ingored in favor of the second update
                 intentConfig.mode = .setup(currency: nil, setupFutureUsage: .offSession)
-                sut.update(intentConfiguration: intentConfig) { _ in
+                flowController.update(intentConfiguration: intentConfig) { _ in
                    XCTFail("This update call should be ignored in favor of the second update call")
                 }
 
                 intentConfig.mode = .setup(currency: nil, setupFutureUsage: .onSession)
-                sut.update(intentConfiguration: intentConfig) { error in
+                flowController.update(intentConfiguration: intentConfig) { error in
                     XCTAssertNil(error)
                     expectation.fulfill()
                 }
