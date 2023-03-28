acbe9fa4327807a93147e822df94f1908c6d3970
John Woo <wooj@stripe.com>
2023-03-28 15:52:59 -0700

    Adjust placement of close button on iOS payment sheet

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/SheetNavigationBar.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/SheetNavigationBar.swift
@@ -21,7 +21,16 @@ protocol SheetNavigationBarDelegate: AnyObject {
 class SheetNavigationBar: UIView {
     static let height: CGFloat = 48
     weak var delegate: SheetNavigationBarDelegate?
-    fileprivate lazy var closeButton: UIButton = {
+    fileprivate lazy var closeButtonLeft: UIButton = {
+        let button = SheetNavigationButton(type: .custom)
+        button.setImage(Image.icon_x_standalone.makeImage(template: true), for: .normal)
+        button.tintColor = appearance.colors.icon
+        button.accessibilityLabel = String.Localized.close
+        button.accessibilityIdentifier = "UIButton.Close"
+        return button
+    }()
+
+    fileprivate lazy var closeButtonRight: UIButton = {
         let button = SheetNavigationButton(type: .custom)
         button.setImage(Image.icon_x_standalone.makeImage(template: true), for: .normal)
         button.tintColor = appearance.colors.icon
@@ -54,7 +63,8 @@ class SheetNavigationBar: UIView {
     override var isUserInteractionEnabled: Bool {
         didSet {
             // Explicitly disable buttons to update their appearance
-            closeButton.isEnabled = isUserInteractionEnabled
+            closeButtonLeft.isEnabled = isUserInteractionEnabled
+            closeButtonRight.isEnabled = isUserInteractionEnabled
             backButton.isEnabled = isUserInteractionEnabled
             additionalButton.isEnabled = isUserInteractionEnabled
         }
@@ -64,15 +74,15 @@ class SheetNavigationBar: UIView {
         self.appearance = appearance
         super.init(frame: .zero)
         backgroundColor = appearance.colors.background.withAlphaComponent(0.9)
-        [closeButton, backButton, additionalButton].forEach {
+        [closeButtonLeft, closeButtonRight, backButton, additionalButton].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             addSubview($0)
         }
 
         NSLayoutConstraint.activate([
-            closeButton.leadingAnchor.constraint(
+            closeButtonLeft.leadingAnchor.constraint(
                 equalTo: leadingAnchor, constant: PaymentSheetUI.defaultPadding),
-            closeButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            closeButtonLeft.centerYAnchor.constraint(equalTo: centerYAnchor),
 
             backButton.leadingAnchor.constraint(
                 equalTo: leadingAnchor, constant: PaymentSheetUI.defaultPadding),
@@ -81,6 +91,10 @@ class SheetNavigationBar: UIView {
             additionalButton.trailingAnchor.constraint(
                 equalTo: trailingAnchor, constant: -PaymentSheetUI.defaultPadding),
             additionalButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+            closeButtonRight.trailingAnchor.constraint(
+                equalTo: trailingAnchor, constant: -PaymentSheetUI.defaultPadding),
+            closeButtonRight.centerYAnchor.constraint(equalTo: centerYAnchor)
         ])
 
         if isTestMode {
@@ -89,14 +103,15 @@ class SheetNavigationBar: UIView {
 
             NSLayoutConstraint.activate([
                 testModeView.leadingAnchor.constraint(
-                    equalTo: closeButton.trailingAnchor, constant: PaymentSheetUI.defaultPadding),
+                    equalTo: closeButtonLeft.trailingAnchor, constant: PaymentSheetUI.defaultPadding),
                 testModeView.centerYAnchor.constraint(equalTo: centerYAnchor),
                 testModeView.widthAnchor.constraint(equalToConstant: 82),
                 testModeView.heightAnchor.constraint(equalToConstant: 23),
             ])
         }
 
-        closeButton.addTarget(self, action: #selector(didTapCloseButton), for: .touchUpInside)
+        closeButtonLeft.addTarget(self, action: #selector(didTapCloseButton), for: .touchUpInside)
+        closeButtonRight.addTarget(self, action: #selector(didTapCloseButton), for: .touchUpInside)
         backButton.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
 
         setStyle(.close(showAdditionalButton: false))
@@ -130,19 +145,22 @@ class SheetNavigationBar: UIView {
     func setStyle(_ style: Style) {
         switch style {
         case .back:
-            closeButton.isHidden = true
+            closeButtonLeft.isHidden = true
+            closeButtonRight.isHidden = true
             additionalButton.isHidden = true
             backButton.isHidden = false
             bringSubviewToFront(backButton)
         case .close(let showAdditionalButton):
-            closeButton.isHidden = false
+            closeButtonLeft.isHidden = !showAdditionalButton
+            closeButtonRight.isHidden = showAdditionalButton
             additionalButton.isHidden = !showAdditionalButton
             if showAdditionalButton {
                 bringSubviewToFront(additionalButton)
             }
             backButton.isHidden = true
         case .none:
-            closeButton.isHidden = true
+            closeButtonLeft.isHidden = true
+            closeButtonRight.isHidden = true
             additionalButton.isHidden = true
             backButton.isHidden = true
         }
