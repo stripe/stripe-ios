793c1af99683ce2a017dc8c63ce7e2280853d9cf
David Estes <davidestes@stripe.com>
2023-06-14 16:32:16 -0700

    Update tests, add real params

--- StripePaymentSheet/StripePaymentSheet/Source/Internal/Link/Utils/LinkURLGenerator.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/Internal/Link/Utils/LinkURLGenerator.swift
@@ -4,19 +4,21 @@
 //
 
 import Foundation
+@_spi(STP) import StripePayments
+@_spi(STP) import StripeCore
 
 struct LinkURLParams: Encodable {
     struct MerchantInfo: Encodable {
-        var businessName: String?
-        var country: String?
+        var businessName: String
+        var country: String
     }
     struct CustomerInfo: Encodable {
-        var country: String?
+        var country: String
         var email: String?
     }
     struct PaymentInfo: Encodable {
-        var currency: String?
-        var amount: Int?
+        var currency: String
+        var amount: Int
     }
     enum LinkMode: String, Encodable {
         case pm
@@ -26,17 +28,56 @@ struct LinkURLParams: Encodable {
     var integrationType = "mobile"
     var linkMode: LinkMode
     var publishableKey: String
+    var stripeAccount: String?
+    var paymentUserAgent: String
     var merchantInfo: MerchantInfo
     var customerInfo: CustomerInfo
-    var paymentInfo: PaymentInfo
-    var returnUrl: URL
-    var experiments: [String]
-    var flags: [String]
-    var loggerMetadata: [String]
+    var paymentInfo: PaymentInfo?
+    var experiments: [String: Bool]
+    var flags: [String: Bool]
+    var loggerMetadata: [String: Bool]
     var locale: String
 }
 
 class LinkURLGenerator {
+    static func linkParams(configuration: PaymentSheet.Configuration, intent: Intent) async throws -> LinkURLParams {
+        guard let publishableKey = configuration.apiClient.publishableKey ?? STPAPIClient.shared.publishableKey else {
+            throw LinkURLGeneratorError.noPublishableKey
+        }
+        guard let merchantCountryCode = intent.countryCode else {
+            throw LinkURLGeneratorError.noCountryCode
+        }
+        
+        // US isn't the best default, but we only expect regionCode to be nil in the nil region on a simulator, and we don't want to crash.
+        let customerCountryCode = configuration.defaultBillingDetails.address.country ?? Locale.current.regionCode ?? "US"
+        
+        // Get email from the billing details, or the Customer object if the billing details is empty
+        var customerEmail = configuration.defaultBillingDetails.email
+        if customerEmail == nil,
+           let customerID = configuration.customer?.id,
+           let ephemeralKey = configuration.customer?.ephemeralKeySecret,
+           let customer = try? await configuration.apiClient.retrieveCustomer(customerID, using: ephemeralKey)
+        {
+            customerEmail = customer.email
+        }
+        
+        let paymentInfo: LinkURLParams.PaymentInfo? = {
+            if let currency = intent.currency, let amount = intent.amount {
+                return LinkURLParams.PaymentInfo(currency: currency, amount: amount)
+            }
+            return nil
+        }()
+        
+        return LinkURLParams(linkMode: .pm,
+                             publishableKey: publishableKey,
+                             paymentUserAgent: PaymentsSDKVariant.paymentUserAgent,
+                             merchantInfo: LinkURLParams.MerchantInfo(businessName: configuration.merchantDisplayName, country: merchantCountryCode),
+                             customerInfo: LinkURLParams.CustomerInfo(country: customerCountryCode, email: customerEmail),
+                             paymentInfo: paymentInfo,
+                             experiments: [:], flags: [:], loggerMetadata: [:],
+                             locale: Locale.current.toLanguageTag())
+    }
+    
     static func url(params: LinkURLParams) throws -> URL {
         var components = URLComponents(string: "https://checkout.link.com/")!
         components.fragment = try params.toURLEncodedBase64()
@@ -44,6 +85,11 @@ class LinkURLGenerator {
             throw LinkURLGeneratorError.urlCreationFailed
         }
         return url
+    }
+    
+    static func url(configuration: PaymentSheet.Configuration, intent: Intent) async throws -> URL {
+        let params = try await Self.linkParams(configuration: configuration, intent: intent)
+        return try url(params: params)
     }
 }
 
@@ -56,4 +102,6 @@ extension LinkURLParams {
 
 enum LinkURLGeneratorError: Error {
     case urlCreationFailed
+    case noPublishableKey
+    case noCountryCode
 }
--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/Link/LinkURLGeneratorTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/Link/LinkURLGeneratorTests.swift
@@ -7,7 +7,7 @@ import Foundation
 
 @_spi(STP) @testable import StripeCore
 @_spi(STP) @testable import StripePayments
-@testable import StripePaymentSheet
+@_spi(STP) @_spi(ExperimentalPaymentSheetDecouplingAPI) @testable import StripePaymentSheet
 
 import OHHTTPStubs
 import OHHTTPStubsSwift
@@ -16,36 +16,81 @@ import XCTest
 
 class LinkURLGeneratorTests: XCTestCase {
     let testParams = LinkURLParams(linkMode: .pm,
-                                  publishableKey: "pk_test_123",
-                                  merchantInfo: LinkURLParams.MerchantInfo(businessName: "Test test", country: "US"),
-                                  customerInfo: LinkURLParams.CustomerInfo(country: "US", email: "test@example.com"),
-                                  paymentInfo: LinkURLParams.PaymentInfo(currency: "USD", amount: 100),
-                                  returnUrl: URL(string: "stripesdk://")!,
-                                  experiments: [],
-                                  flags: [],
-                                  loggerMetadata: [],
-                                  locale: Locale.init(identifier: "en_US").toLanguageTag())
-    
+                                   publishableKey: "pk_test_123",
+                                   paymentUserAgent: "test",
+                                   merchantInfo: LinkURLParams.MerchantInfo(businessName: "Test test", country: "US"),
+                                   customerInfo: LinkURLParams.CustomerInfo(country: "US", email: "test@example.com"),
+                                   paymentInfo: LinkURLParams.PaymentInfo(currency: "USD", amount: 100),
+                                   experiments: [:],
+                                   flags: [:],
+                                   loggerMetadata: [:],
+                                   locale: Locale.init(identifier: "en_US").toLanguageTag())
+
     func testURLCreation() {
         let url = try! LinkURLGenerator.url(params: testParams)
-        XCTAssertEqual(url.absoluteString, "https://checkout.link.com/#eyJsb2dnZXJNZXRhZGF0YSI6W10sImN1c3RvbWVySW5mbyI6eyJjb3VudHJ5IjoiVVMiLCJlbWFpbCI6InRlc3RAZXhhbXBsZS5jb20ifSwiZXhwZXJpbWVudHMiOltdLCJwYXltZW50SW5mbyI6eyJjdXJyZW5jeSI6IlVTRCIsImFtb3VudCI6MTAwfSwibG9jYWxlIjoiZW4tVVMiLCJwYXRoIjoibW9iaWxlX3BheSIsIm1lcmNoYW50SW5mbyI6eyJjb3VudHJ5IjoiVVMiLCJidXNpbmVzc05hbWUiOiJUZXN0IHRlc3QifSwicHVibGlzaGFibGVLZXkiOiJwa190ZXN0XzEyMyIsImxpbmtNb2RlIjoicG0iLCJyZXR1cm5VcmwiOiJzdHJpcGVzZGs6XC9cLyIsImZsYWdzIjpbXSwiaW50ZWdyYXRpb25UeXBlIjoibW9iaWxlIn0=")
+        XCTAssertEqual(url.absoluteString, "https://checkout.link.com/#eyJsb2dnZXJNZXRhZGF0YSI6e30sImN1c3RvbWVySW5mbyI6eyJjb3VudHJ5IjoiVVMiLCJlbWFpbCI6InRlc3RAZXhhbXBsZS5jb20ifSwiZXhwZXJpbWVudHMiOnt9LCJwYXltZW50VXNlckFnZW50IjoidGVzdCIsImxvY2FsZSI6ImVuLVVTIiwicGF0aCI6Im1vYmlsZV9wYXkiLCJwYXltZW50SW5mbyI6eyJjdXJyZW5jeSI6IlVTRCIsImFtb3VudCI6MTAwfSwibWVyY2hhbnRJbmZvIjp7ImNvdW50cnkiOiJVUyIsImJ1c2luZXNzTmFtZSI6IlRlc3QgdGVzdCJ9LCJwdWJsaXNoYWJsZUtleSI6InBrX3Rlc3RfMTIzIiwibGlua01vZGUiOiJwbSIsImZsYWdzIjp7fSwiaW50ZWdyYXRpb25UeXBlIjoibW9iaWxlIn0=")
     }
-    
+
     func testURLCreationRegularUnicode() {
         var params = testParams
         params.customerInfo.email = "유니코드"
         let url = try! LinkURLGenerator.url(params: params)
-        XCTAssertEqual(url.absoluteString, "https://checkout.link.com/#eyJsb2dnZXJNZXRhZGF0YSI6W10sImN1c3RvbWVySW5mbyI6eyJjb3VudHJ5IjoiVVMiLCJlbWFpbCI6IuycoOuLiOy9lOuTnCJ9LCJleHBlcmltZW50cyI6W10sInBheW1lbnRJbmZvIjp7ImN1cnJlbmN5IjoiVVNEIiwiYW1vdW50IjoxMDB9LCJsb2NhbGUiOiJlbi1VUyIsInBhdGgiOiJtb2JpbGVfcGF5IiwibWVyY2hhbnRJbmZvIjp7ImNvdW50cnkiOiJVUyIsImJ1c2luZXNzTmFtZSI6IlRlc3QgdGVzdCJ9LCJwdWJsaXNoYWJsZUtleSI6InBrX3Rlc3RfMTIzIiwibGlua01vZGUiOiJwbSIsInJldHVyblVybCI6InN0cmlwZXNkazpcL1wvIiwiZmxhZ3MiOltdLCJpbnRlZ3JhdGlvblR5cGUiOiJtb2JpbGUifQ==")
+        XCTAssertEqual(url.absoluteString, "https://checkout.link.com/#eyJsb2dnZXJNZXRhZGF0YSI6e30sImN1c3RvbWVySW5mbyI6eyJjb3VudHJ5IjoiVVMiLCJlbWFpbCI6IuycoOuLiOy9lOuTnCJ9LCJleHBlcmltZW50cyI6e30sInBheW1lbnRVc2VyQWdlbnQiOiJ0ZXN0IiwibG9jYWxlIjoiZW4tVVMiLCJwYXRoIjoibW9iaWxlX3BheSIsInBheW1lbnRJbmZvIjp7ImN1cnJlbmN5IjoiVVNEIiwiYW1vdW50IjoxMDB9LCJtZXJjaGFudEluZm8iOnsiY291bnRyeSI6IlVTIiwiYnVzaW5lc3NOYW1lIjoiVGVzdCB0ZXN0In0sInB1Ymxpc2hhYmxlS2V5IjoicGtfdGVzdF8xMjMiLCJsaW5rTW9kZSI6InBtIiwiZmxhZ3MiOnt9LCJpbnRlZ3JhdGlvblR5cGUiOiJtb2JpbGUifQ==")
     }
-    
+
     func testURLCreationHorribleUnicode() {
         var params = testParams
         params.customerInfo.email = String(bytes: [0xD8, 0x00] as [UInt8], encoding: .utf16BigEndian)! // Unpaired UTF-16 surrogates
         do {
-            let _ = try LinkURLGenerator.url(params: params)
+            _ = try LinkURLGenerator.url(params: params)
             XCTFail("Encoding should fail for invalid data")
         } catch {
             XCTAssertTrue(error as? EncodingError != nil)
         }
+    }
+    
+    func testURLParamsFromConfig() async {
+        let config = PaymentSheet.Configuration()
+        let intentConfig = PaymentSheet.IntentConfiguration(mode: .payment(amount: 100, currency: "USD")) { _, _, _ in
+            // Nothing
+        }
+        config.apiClient.publishableKey = "pk_123"
+        let intent = Intent.deferredIntent(elementsSession: STPElementsSession.emptyElementsSession, intentConfig: intentConfig)
+        let params = try! await LinkURLGenerator.linkParams(configuration: config, intent: intent)
+
+        let expectedParams = LinkURLParams(linkMode: .pm,
+                                           publishableKey: config.apiClient.publishableKey!,
+                                           paymentUserAgent: PaymentsSDKVariant.paymentUserAgent,
+                                           merchantInfo: LinkURLParams.MerchantInfo(businessName: "xctest", country: "US"),
+                                           customerInfo: LinkURLParams.CustomerInfo(country: "US", email: nil),
+                                           paymentInfo: LinkURLParams.PaymentInfo(currency: "USD", amount: 100),
+                                           experiments: [:],
+                                           flags: [:],
+                                           loggerMetadata: [:],
+                                           locale: Locale.init(identifier: "en_US").toLanguageTag())
+
+        XCTAssertEqual(params, expectedParams)
+    }
+}
+
+extension STPElementsSession {
+    // Should use the one from StripeiOSTests, but we don't have good infrastructure to share these
+    // and we're not using any details from it.
+    static var emptyElementsSession: STPElementsSession {
+        let apiResponse: [String: Any] = ["payment_method_preference": ["ordered_payment_method_types": ["123"],
+                                                                        "country_code": "US"] as [String : Any],
+                                          "session_id": "123"
+        ]
+        return STPElementsSession.decodedObject(fromAPIResponse: apiResponse)!
+    }
+}
+
+// Just for the purposes of this test. No need to add the (tiny) overhead of Equatable to the published binary
+extension LinkURLParams: Equatable {
+    public static func == (lhs: StripePaymentSheet.LinkURLParams, rhs: StripePaymentSheet.LinkURLParams) -> Bool {
+        let encoder = JSONEncoder()
+        // deterministic plz
+        encoder.outputFormatting = .sortedKeys
+        return try! encoder.encode(lhs) == encoder.encode(rhs)
     }
 }
