c9804be1741ca726d82b4a39b7d67be8d3bca5c7
Fionn Barrett <fionnbarrett@stripe.com>
2023-07-10 12:50:46 +0100

    run "swiftlint lint --fix"

--- StripeCardScan/StripeCardScan/Source/CardScan/MLRuntime/DetectedAllOcrBoxes.swift
+++ StripeCardScan/StripeCardScan/Source/CardScan/MLRuntime/DetectedAllOcrBoxes.swift
@@ -10,8 +10,6 @@ import Foundation
 struct DetectedAllOcrBoxes {
     var allBoxes: [DetectedSSDOcrBox] = []
 
-    init() {}
-
     func toArray() -> [[String: Any]] {
         let frameArray = self.allBoxes.map { $0.toDict() }
         return frameArray
