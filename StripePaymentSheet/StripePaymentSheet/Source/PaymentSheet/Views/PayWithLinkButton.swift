9d30daca9d46a07627fb69aa0941a242d11ddb07
David Estes <davidestes@stripe.com>
2023-07-17 12:18:57 -0700

    Don't show account info in button

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/PayWithLinkButton.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/PayWithLinkButton.swift
@@ -228,7 +228,7 @@ final class PayWithLinkButton: UIControl {
     //        // Stop listening for account changes
     //        LinkAccountContext.shared.removeObserver(self)
     //    }
-        
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
