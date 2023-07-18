4d875b8aafd425dcd748646cb005a3abdfc6a5e4
John Woo <wooj@stripe.com>
2023-07-18 10:49:49 -0700

    Revert "Fix build by temporarily introducing fileprivate struct (#2748)"
    This reverts commit 0e8873df47d8a5e21758592b5c4827d777301b13.

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/PayWithLinkButton.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/PayWithLinkButton.swift
@@ -29,14 +29,6 @@ final class PayWithLinkButton: UIControl {
         static let arrowInsets: UIEdgeInsets = .init(top: 1, left: 0, bottom: 0, right: 0)
     }
 
-    fileprivate struct LinkAccountStub: PaymentSheetLinkAccountInfoProtocol {
-        let email: String
-        let redactedPhoneNumber: String?
-        let lastPM: LinkPMDisplayDetails?
-        let isRegistered: Bool
-        let isLoggedIn: Bool
-    }
-
     /// Link account of the current user.
     var linkAccount: PaymentSheetLinkAccountInfoProtocol? = LinkAccountStub(email: "", redactedPhoneNumber: nil, lastPM: nil, isRegistered: false, isLoggedIn: false) {
         didSet {
@@ -486,8 +478,16 @@ struct UIViewPreview<View: UIView>: UIViewRepresentable {
     }
 }
 
-private func makeAccountStub(email: String, isRegistered: Bool, lastPM: LinkPMDisplayDetails?) -> PayWithLinkButton.LinkAccountStub {
-    return PayWithLinkButton.LinkAccountStub(
+private struct LinkAccountStub: PaymentSheetLinkAccountInfoProtocol {
+    let email: String
+    let redactedPhoneNumber: String?
+    let lastPM: LinkPMDisplayDetails?
+    let isRegistered: Bool
+    let isLoggedIn: Bool
+}
+
+private func makeAccountStub(email: String, isRegistered: Bool, lastPM: LinkPMDisplayDetails?) -> LinkAccountStub {
+    return LinkAccountStub(
         email: email,
         redactedPhoneNumber: "+1********55",
         lastPM: lastPM,
