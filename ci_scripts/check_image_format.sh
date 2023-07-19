1f363b6621d657828c642eb8c435b655c84604b3
John Woo <wooj@stripe.com>
2023-07-19 10:13:29 -0700

    updating per styleguide of shellcheck

--- ci_scripts/check_image_format.sh
+++ ci_scripts/check_image_format.sh
@@ -3,27 +3,26 @@
 ORIGINAL_IFS="$IFS"
 IFS=$'\n'
 
-script_dir=`dirname $0`
-pushd "${script_dir}/../" > /dev/null 2>&1
-stripe_ios_dir="`pwd`"
-popd >/dev/null > /dev/null 2>&1
+script_dir="$(dirname "$0")"
+pushd "${script_dir}/../" > /dev/null 2>&1 || exit 1
+stripe_ios_dir="$(pwd)"
+popd >/dev/null > /dev/null 2>&1 || exit 1
 
 SHOULD_FAIL=0
-for subdir in `ls ${stripe_ios_dir}`; do
-    TEMPNODE="${stripe_ios_dir}/${subdir}"
-    if [[ -d "${TEMPNODE}" ]] && [[ x"${subdir}" != x"build" ]]; then
-	for png in `find "${TEMPNODE}" -type f -path '*Resources*.png'`; do
-	    OUTPUT=`file "${png}"`
-
-	    echo ${OUTPUT} | grep -e "-bit"
-	    if [ $? -eq 0 ]; then
-		echo ${OUTPUT} | grep -e "8-bit" > /dev/null 2>&1
-		if [ $? -ne 0 ]; then    
+subdirs=$(find "${stripe_ios_dir}" -type d -maxdepth 1 -mindepth 1)
+for subdir in $subdirs; do
+    dirname=$(basename "$subdir")
+    if [[ -d "${subdir}" ]] && [[ x"${dirname}" != x"build" ]]; then
+	while IFS= read -r -d '' png
+	do
+	    OUTPUT=$(file "${png}")
+	    if echo "${OUTPUT}" | grep -e "-bit"; then
+		if ! echo "${OUTPUT}" | grep -e "8-bit" > /dev/null 2>&1; then
 		    echo "[ERROR] Image found as non 8-bit PNG image: ${png}"
 		    SHOULD_FAIL=1
 		fi		
 	    fi
-	done
+	done <  <(find "${subdir}" -type f -path '*Resources*.png' -print0)
     fi
 done
 
