efb5aed7f33a027bbdbcf8ed17d3afdf4ae87730
Yuki Tokuhiro <yuki@stripe.com>
2023-07-21 10:58:52 -0700

    Move Elements helper methods out of test into dedicated file

--- Stripe/StripeiOSTests/PaymentSheetFormFactoryTest.swift
+++ Stripe/StripeiOSTests/PaymentSheetFormFactoryTest.swift
@@ -14,7 +14,6 @@ import XCTest
 @testable@_spi(STP) import StripePaymentSheet
 @testable@_spi(STP) import StripePaymentsUI
 @testable@_spi(STP) import StripeUICore
-@testable@_spi(STP) import StripeUICore
 
 class MockElement: Element {
     var paramsUpdater: (IntentConfirmParams) -> IntentConfirmParams?
@@ -1882,51 +1881,5 @@ class PaymentSheetFormFactoryTest: XCTestCase {
             return nil
         }
         return wrapper.element
-    }
-}
-
-extension Element {
-
-    /// A convenience method that overwrites the one defined in Element.swift that unwraps any Elements wrapped in `PaymentMethodElementWrapper`
-    /// and returns all Elements underneath this Element, including this Element.
-    public func getAllUnwrappedSubElements() -> [Element] {
-        switch self {
-        case let container as ContainerElement:
-            return [container] + container.elements.flatMap { $0.getAllUnwrappedSubElements() }
-        case let wrappedElement as PaymentMethodElementWrapper<FormElement>:
-            return wrappedElement.element.getAllUnwrappedSubElements()
-        case let wrappedElement as PaymentMethodElementWrapper<CheckboxElement>:
-            return wrappedElement.element.getAllUnwrappedSubElements()
-        case let wrappedElement as PaymentMethodElementWrapper<TextFieldElement>:
-            return wrappedElement.element.getAllUnwrappedSubElements()
-        case let wrappedElement as PaymentMethodElementWrapper<AddressSectionElement>:
-            return wrappedElement.element.getAllUnwrappedSubElements()
-        default:
-            return [self]
-        }
-    }
-
-    func getTextFieldElement(_ label: String) -> TextFieldElement? {
-        return getAllUnwrappedSubElements()
-            .compactMap { $0 as? TextFieldElement }
-            .first { $0.configuration.label == label }
-    }
-
-    func getDropdownFieldElement(_ label: String) -> DropdownFieldElement? {
-        return getAllUnwrappedSubElements()
-            .compactMap { $0 as? DropdownFieldElement }
-            .first { $0.label == label }
-    }
-
-    func getMandateElement() -> SimpleMandateElement? {
-        return getAllUnwrappedSubElements()
-            .compactMap { $0 as? SimpleMandateElement }
-            .first
-    }
-}
-
-extension TextFieldElement: CustomDebugStringConvertible {
-    public var debugDescription: String {
-        return "TextFieldElement (\"\(configuration.label)\") (\(validationState))"
     }
 }
