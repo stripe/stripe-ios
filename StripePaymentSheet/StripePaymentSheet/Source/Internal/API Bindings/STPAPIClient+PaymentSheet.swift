3ecb73a2e75256b6e955271962f88a299a973c17
John Woo <wooj@stripe.com>
2023-07-24 18:41:10 -0700

    lint

--- StripePaymentSheet/StripePaymentSheet/Source/Internal/API Bindings/STPAPIClient+PaymentSheet.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/Internal/API Bindings/STPAPIClient+PaymentSheet.swift
@@ -48,11 +48,11 @@ extension STPAPIClient {
         var parameters: [String: Any] = [:]
         parameters["type"] = "deferred_intent"
         parameters["locale"] = Locale.current.toLanguageTag()
-        
+
         var deferredIntent = [String: Any]()
         deferredIntent["mode"] = "setup"
         parameters["deferred_intent"] = deferredIntent
-        
+
         return try await APIRequest<STPElementsSession>.getWith(
             self,
             endpoint: APIEndpointIntentWithPreferences,
