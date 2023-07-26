0400a13dabe9678a1aaa366fcf9ab311ded7a8db
Fionn Barrett <fionnbarrett@stripe.com>
2023-07-26 18:31:34 +0100

    Provider Source

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/ViewControllers/PollingViewControllerDataSourceProvider.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/ViewControllers/PollingViewControllerDataSourceProvider.swift
@@ -13,3 +13,57 @@ protocol PollingViewControllerDataSourceProvider: AnyObject {
     var instructionLabelAttributedText: NSAttributedString { get }
     func updateTimerCallback(callback: (TimeInterval) -> Void)
 }
+
+class PaymentMethodDataSourceProvider: PollingViewControllerDataSourceProvider {
+    var deadline: Date
+    var instructionLabelAttributedText: NSAttributedString {
+        let timeRemaining = dateFormatter.string(from: timeRemaining) ?? ""
+        let attrText = NSMutableAttributedString(string: String(format: .Localized.open_upi_app, timeRemaining))
+        attrText.addAttributes([.foregroundColor: appearance.colors.primary], range: NSString(string: attrText.string).range(of: timeRemaining))
+        return attrText
+    }
+    let appearance: PaymentSheet.Appearance
+
+    // MARK: public
+
+    public func updateTimerCallback(callback: (TimeInterval) -> Void){
+        callback(timeRemaining)
+    }
+
+    init(appearance: PaymentSheet.Appearance, deadline: Date) {
+        self.appearance = appearance
+        self.deadline = deadline
+    }
+
+    // MARK: private
+
+    var timeRemaining: TimeInterval {
+        return Date().compatibleDistance(to: deadline)
+    }
+
+    var dateFormatter: DateComponentsFormatter {
+        let formatter = DateComponentsFormatter()
+        if timeRemaining > 60 {
+            formatter.zeroFormattingBehavior = .dropLeading
+        } else {
+            formatter.zeroFormattingBehavior = .pad
+        }
+        formatter.allowedUnits = [.minute, .second]
+        return formatter
+    }
+}
+
+class UPIPaymentMethodDataSourceProvider: PaymentMethodDataSourceProvider{
+
+    override var instructionLabelAttributedText: NSAttributedString {
+        let timeRemaining = dateFormatter.string(from: timeRemaining) ?? ""
+        let attrText = NSMutableAttributedString(string: String(format: .Localized.open_upi_app, timeRemaining))
+        attrText.addAttributes([.foregroundColor: appearance.colors.primary], range: NSString(string: attrText.string).range(of: timeRemaining))
+        return attrText
+    }
+
+    init(appearance: PaymentSheet.Appearance) {
+        let deadline = Date().addingTimeInterval(60 * 5)
+        super.init(appearance: appearance, deadline: deadline)
+    }
+}
