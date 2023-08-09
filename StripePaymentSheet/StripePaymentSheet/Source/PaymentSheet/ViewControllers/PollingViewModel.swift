d52ac893b52d604f8d38b1799373fb18dabafffc
Fionn Barrett <fionnbarrett@stripe.com>
2023-08-09 10:37:37 +0100

    use computed lazy

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/ViewControllers/PollingViewModel.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/ViewControllers/PollingViewModel.swift
@@ -15,30 +15,24 @@ import UIKit
 class PollingViewModel {
 
     let paymentMethodType: STPPaymentMethodType
-    var CTA: String
-    var deadline: Date
-
-    init(paymentMethodType: STPPaymentMethodType) {
-        self.paymentMethodType = paymentMethodType
-        self.CTA = PollingViewModel.computeCallToAction(paymentMethodType: paymentMethodType)
-        self.deadline = PollingViewModel.computeDeadline(paymentMethodType: paymentMethodType)
-    }
-
-    private static func computeCallToAction(paymentMethodType: STPPaymentMethodType) -> String {
+    lazy var CTA: String = {
         switch paymentMethodType {
         case .UPI:
             return .Localized.open_upi_app
         default:
             fatalError("Polling CTA has not been implemented for \(paymentMethodType)")
         }
-    }
-
-    private static func computeDeadline(paymentMethodType: STPPaymentMethodType) -> Date {
+    }()
+    lazy var deadline: Date = {
         switch paymentMethodType {
         case .UPI:
             return Date().addingTimeInterval(60 * 5) // 5 minutes
         default:
             fatalError("Polling deadline has not been implemented for \(paymentMethodType)")
         }
+    }()
+
+    init(paymentMethodType: STPPaymentMethodType) {
+        self.paymentMethodType = paymentMethodType
     }
 }
