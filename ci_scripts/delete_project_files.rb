7120c07ca316ca26fc471a7c1ba94418503dac9d
davidme-stripe <52758633+davidme-stripe@users.noreply.github.com>
2023-08-24 13:34:23 -0700

    Stop deleting Package.resolved (#2876)
    ## Summary
    * If we run the project cleanup script while Xcode has the project open,
    Xcode will delete the Package.resolved. To prevent this, we'll kill
    Xcode before cleaning up the project.
    * Add a comment to the .gitignore to explain why we commit the
    Package.resolved.
    
    ## Testing
    Ran script locally

--- ci_scripts/delete_project_files.rb
+++ ci_scripts/delete_project_files.rb
@@ -2,6 +2,8 @@
 
 require_relative 'common'
 
+# If we delete the project while Xcode is running, it will corrupt the Package.resolved
+run_command('killall -9 Xcode', false)
 run_command('rm -f .package.resolved', false)
 run_command('find Stripe.xcworkspace -type f ! -name "Package.resolved" -exec rm {} +', false)
 run_command('find Stripe* -type d -name "*.xcodeproj" -exec rm -r {} +', false)
