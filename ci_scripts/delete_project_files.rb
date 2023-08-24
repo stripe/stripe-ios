50dc123ce2051318da24f51440f03811c5761586
David Estes <davidestes@stripe.com>
2023-08-24 09:49:00 -0700

    commit

--- ci_scripts/delete_project_files.rb
+++ ci_scripts/delete_project_files.rb
@@ -3,7 +3,7 @@
 require_relative 'common'
 
 run_command('rm -f .package.resolved', false)
-run_command('find Stripe.xcworkspace -type f ! -name "Package.resolved" -exec rm {} +', false)
-run_command('find Stripe* -type d -name "*.xcodeproj" -exec rm -r {} +', false)
-run_command('find Example -type d -name "*.xcodeproj" -exec rm -r {} +', false)
-run_command('find Testers -type d -name "*.xcodeproj" -exec rm -r {} +', false)
+run_command('find Stripe.xcworkspace -type f ! -name "Package.resolved"', false)
+run_command('find Stripe* -type d -name "*.xcodeproj"', false)
+run_command('find Example -type d -name "*.xcodeproj"', false)
+run_command('find Testers -type d -name "*.xcodeproj"', false)
