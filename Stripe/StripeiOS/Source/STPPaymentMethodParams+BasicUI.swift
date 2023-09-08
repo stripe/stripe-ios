e078babf57ca3ab38ed870daef15de543c9413b8
Yuki Tokuhiro <yuki@stripe.com>
2023-09-08 14:42:33 -0700

    Add PayNow to STPPaymentMethodType enum

--- Stripe/StripeiOS/Source/STPPaymentMethodParams+BasicUI.swift
+++ Stripe/StripeiOS/Source/STPPaymentMethodParams+BasicUI.swift
@@ -38,7 +38,7 @@ extension STPPaymentMethodParams: STPPaymentOption {
             return true
         case .alipay, .AUBECSDebit, .bacsDebit, .SEPADebit, .iDEAL, .FPX, .cardPresent, .giropay,
             .grabPay, .EPS, .przelewy24, .bancontact, .netBanking, .OXXO, .payPal, .sofort, .UPI,
-            .afterpayClearpay, .blik, .weChatPay, .boleto, .klarna, .linkInstantDebit, .affirm, .cashApp,
+            .afterpayClearpay, .blik, .weChatPay, .boleto, .klarna, .linkInstantDebit, .affirm, .cashApp, .paynow,
             .unknown:
             return false
         @unknown default:
