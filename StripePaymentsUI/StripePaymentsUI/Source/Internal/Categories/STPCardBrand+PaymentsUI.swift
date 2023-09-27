aef54ae8831f2638d0210b53da0d7c0fedd0fd2b
David Estes <davidestes@stripe.com>
2023-09-27 12:04:01 -0700

    Fix

--- StripePaymentsUI/StripePaymentsUI/Source/Internal/Categories/STPCardBrand+PaymentsUI.swift
+++ StripePaymentsUI/StripePaymentsUI/Source/Internal/Categories/STPCardBrand+PaymentsUI.swift
@@ -18,10 +18,13 @@ extension STPCardBrand: Comparable {
         let brandImageAttachment = NSTextAttachment()
         let image: UIImage = self == .unknown ? STPImageLibrary.cardBrandChoiceImage() : STPImageLibrary.cardBrandImage(for: self)
         brandImageAttachment.image = image
+        // TODO: -3 is a hack for proper vertical alignment, investigate this
+        let hackyVerticalInset: CGFloat = -3
         if let maxWidth = maxWidth {
             let widthRatio = maxWidth / image.size.width
-            // TODO: -3 is a hack for proper vertical alignment, investigate this
-            brandImageAttachment.bounds = .init(x: 0, y: -3, width: maxWidth, height: image.size.height * widthRatio)
+            brandImageAttachment.bounds = .init(x: 0, y: hackyVerticalInset, width: maxWidth, height: image.size.height * widthRatio)
+        } else {
+            brandImageAttachment.bounds = .init(x: 0, y: hackyVerticalInset, width: image.size.width, height: image.size.height)
         }
 
         return NSAttributedString(attachment: brandImageAttachment)
