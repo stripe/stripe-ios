29fdf8d40b37a8cb8bd6f453d70ad5a8e333344b
Chen Cen <79880926+ccen-stripe@users.noreply.github.com>
2023-10-09 15:33:26 -0700

    [Identity] support consent page for Butter 2.0 (#2979)

--- StripeIdentity/StripeIdentity/Source/API Bindings/IdentityAPIClient.swift
+++ StripeIdentity/StripeIdentity/Source/API Bindings/IdentityAPIClient.swift
@@ -47,7 +47,7 @@ final class IdentityAPIClientImpl: IdentityAPIClient {
     /// SDK is capable of using.
     ///
     /// - Note: Update this value when a new API version is ready for use in production.
-    static let productionApiVersion: Int = 4
+    static let productionApiVersion: Int = 5
 
     var betas: Set<String> {
         return ["identity_client_api=v\(apiVersion)"]
