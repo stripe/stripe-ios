29a879e0bcb6a43c3de9754c76c44954371a57a1
David Estes <davidestes@stripe.com>
2023-10-17 11:56:48 -0700

    Update scripts

--- ci_scripts/common.rb
+++ ci_scripts/common.rb
@@ -3,11 +3,18 @@
 require 'colorize'
 require 'English'
 require 'net/ssh'
+require 'git'
 
 # This should generally be the minimum Xcode version supported by the App Store, as the
 # compiled XCFrameworks won't be usable on older versions.
 # We sometimes bump this if an Xcode bug or deprecation forces us to upgrade early.
 MIN_SUPPORTED_XCODE_VERSION = '14.1'.freeze
+
+# The name of the VM for the Xcode version
+# This should generally be the minimum Xcode version supported by the App Store, as the
+# compiled XCFrameworks won't be usable on older versions.
+# We sometimes bump this if an Xcode bug or deprecation forces us to upgrade early.
+VM_NAME = 'ventura-xcode:14.1'.freeze
 
 def verify_xcode_version
   # Verify that xcode-select -p returns the correct version for building Stripe.xcframework.
@@ -33,19 +40,12 @@ def run_command(command, raise_on_failure = true)
 end
 
 def run_command_vm(command, raise_on_failure = true)
-  tart_pid = spawn("tart run my-ventura-xcode --dir=stripe-ios:/Users/davidestes/stripe/stripe-ios-private")
+  tart_pid = spawn("tart run my-ventura-xcode --dir=stripe-ios:/Users/davidestes/stripe/stripe-ios")
   Process.detach(tart_pid)
   puts "tart> #{command}".blue
 
   Net::SSH.start(`tart ip my-ventura-xcode`.strip, 'admin', :password => "admin") do |ssh|
-    # capture all stderr and stdout output from a remote process
-    output = ssh.exec!("mkdir -p /tmp/build/stripe-ios")
-    puts output
-    output = ssh.exec!("rsync -a --delete --exclude '.git' \"/Volumes/My Shared Files/stripe-ios\" /tmp/build/")
-    puts output
     output = ssh.exec!("cd \"/tmp/build/stripe-ios\" && #{command}")
-    puts output
-    output = ssh.exec!("rsync -aO --exclude '.git' /tmp/build/stripe-ios/ \"/Volumes/My Shared Files/stripe-ios/\"")
     puts output
   end
 
@@ -55,10 +55,59 @@ def run_command_vm(command, raise_on_failure = true)
   # rputs "Command failed: #{command} \a"
   # raise if raise_on_failure
 end
-# run_command_vm('sudo gem install bundler:2.1.2')
-# run_command_vm('bundle install')
-# run_command_vm('source ~/.zprofile && brew install tuist')
-# run_command_vm('source ~/.zprofile && tuist generate -n')
-# run_command_vm('source ~/.zprofile && bundle install')
-# run_command_vm('source ~/.zprofile && bundle exec ./ci_scripts/export_builds.rb')
-run_command_vm('source ~/.zprofile')
+
+def bring_up_vm_and_wait_for_boot
+  # Try to stop/delete the VM, ignoring failures:
+  `tart stop builder-vm`
+  `tart delete builder-vm`
+
+  # Create a new VM
+  run_command("tart clone #{VM_NAME} builder-vm")
+  tart_pid = spawn("tart run my-ventura-xcode --dir=stripe-ios:/Users/davidestes/stripe/stripe-ios")
+  Process.detach(tart_pid)
+  # Retry a basic command every ten seconds until the VM boots, catch error and retry
+  booted = false
+  until booted
+    begin
+      run_command_vm('echo hello world')  
+      booted = true
+    rescue => exception
+      puts 'Not online yet, retrying...'
+      sleep(10)
+    end
+  end
+  # Copy files to VM
+  run_command_vm("mkdir -p /tmp/build/stripe-ios")
+  run_command_vm("rsync -a --delete --exclude '.git' \"/Volumes/My Shared Files/stripe-ios\" /tmp/build/")
+end
+
+def setup_vm_requirements
+  # Clone the repo:
+  g = Git.clone('org-13141669@github.com:stripe-ios/ios-deploy-vm.git', path: '~/stripe/ios-deploy-vm')
+  g = Git.open('~/stripe/ios-deploy-vm')
+  g.checkout("origin/#{VM_NAME.sub(':', '_')}")
+end
+
+def finish_vm
+  # Copy files from VM
+  run_command_vm("rsync -aO --exclude '.git' /tmp/build/stripe-ios/ \"/Volumes/My Shared Files/stripe-ios/\"")
+  
+end
+
+def need_to_build_vm?
+  `tart list`.include? VM_NAME
+end
+
+def build_vm
+  rputs 'Building VM images! This will take a while, grab some coffee and keep your Mac awake...'
+  setup_vm_requirements
+  Dir.chdir(git_publish_dir) do
+    run_command('packer build -var-file="variables.pkrvars.hcl" templates/base.pkr.hcl')
+    run_command('packer build -var-file="variables.pkrvars.hcl" templates/xcode.pkr.hcl')
+  end
+end
+
+if need_to_build_vm?
+  build_vm
+end
+run_command_vm('source ~/.zprofile && sudo gem install bundler:2.1.2 && bundle install && tuist generate -n && bundle exec ./ci_scripts/export_builds.rb')
