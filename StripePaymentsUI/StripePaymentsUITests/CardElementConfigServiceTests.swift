46c541191bc3b2fc4f18722f42d81652bd5b2d9b
David Estes <davidestes@stripe.com>
2023-10-19 09:47:16 -0700

    Update images

--- StripePaymentsUI/StripePaymentsUITests/CardElementConfigServiceTests.swift
+++ StripePaymentsUI/StripePaymentsUITests/CardElementConfigServiceTests.swift
@@ -41,7 +41,7 @@ class CardElementConfigServiceTests: APIStubbedTestCase {
         }
         waitForExpectations(timeout: 1.0)
     }
-    
+
     func testNothingBadHappensOnInvalidData() throws {
         let exp = expectation(description: "fetched config")
         let cecs = CardElementConfigService()
