150ee7bdc77760b331ee035b7a69897c75f59dae
Yuki Tokuhiro <yuki@stripe.com>
2023-11-13 12:40:32 -0800

    Add analytics - turns out there's no tmuch to do

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/STPAnalyticsClient+PaymentSheet.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/STPAnalyticsClient+PaymentSheet.swift
@@ -336,7 +336,6 @@ extension PaymentSheet.PaymentOption {
         case .applePay:
             return .applePay
         case .new, .external:
-            // TODO(yuki|EPMs): Add analytics for EPMs - probably new AnalyticsPaymentMethodType .external(String) case
             return .newPM
         case .saved:
             return .savedPM
@@ -367,7 +366,8 @@ extension PaymentSheet.Configuration {
         payload["save_payment_method_opt_in_behavior"] = savePaymentMethodOptInBehavior.description
         payload["appearance"] = appearance.analyticPayload
         payload["billing_details_collection_configuration"] = billingDetailsCollectionConfiguration.analyticPayload
-
+        // Only grab the first 10 as a safeguard, since `externalPaymentMethods` is configured by the merchant and theoretically ~unbounded
+        payload["external_payment_methods"] = externalPaymentMethodConfiguration?.externalPaymentMethods.prefix(10)
         return payload
     }
 }
