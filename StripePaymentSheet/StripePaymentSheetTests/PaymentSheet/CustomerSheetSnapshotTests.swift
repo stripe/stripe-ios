96db642376f8e612d0a2efdcfa04ddd5e77da18f
John Woo <wooj@stripe.com>
2023-11-13 14:27:32 -0800

    removing new sepa debit bdcc tests

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/CustomerSheetSnapshotTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/CustomerSheetSnapshotTests.swift
@@ -410,6 +410,7 @@ class CustomerSheetSnapshotTests: STPSnapshotTestCase {
         verify(cs.bottomSheetViewController.view!)
     }
 */
+    /*
     func testSEPADebit_bdcc_0200() {
         stubSessions(paymentMethods: "\"sepa_debit\"")
         let bdcc = billingDetailsCollectionConfiguration(name: .automatic,
@@ -463,7 +464,7 @@ class CustomerSheetSnapshotTests: STPSnapshotTestCase {
         presentCS(darkMode: false)
         verify(cs.bottomSheetViewController.view!)
     }
-
+*/
     func stubbedPaymentMethod() -> STPPaymentMethod {
         return STPPaymentMethod.decodedObject(fromAPIResponse: [
             "id": "pm_123card",
