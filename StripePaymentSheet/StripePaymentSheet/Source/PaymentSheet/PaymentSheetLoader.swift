cc8adfe0f3170469ea840cf8b5cd0d5de420950d
John Woo <wooj@stripe.com>
2024-01-03 16:53:23 -0800

    Disable link on usage of billingDetailsCollectionConfig

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheetLoader.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheetLoader.swift
@@ -67,14 +67,14 @@ final class PaymentSheetLoader {
                             intent: intent
                         )
                     }
-
+                let isLinkEnabled = isLinkEnabled(intent: intent, configuration: configuration)
                 analyticsClient.logPaymentSheetEvent(event: .paymentSheetLoadSucceeded,
                                                                      duration: Date().timeIntervalSince(loadingStartDate))
                 completion(
                     .success(
                         intent: intent,
                         savedPaymentMethods: filteredSavedPaymentMethods,
-                        isLinkEnabled: intent.supportsLink
+                        isLinkEnabled: isLinkEnabled
                     )
                 )
             } catch {
@@ -137,6 +137,16 @@ final class PaymentSheetLoader {
         } else {
             return nil
         }
+    }
+
+    static func isLinkEnabled(intent: Intent, configuration: PaymentSheet.Configuration) -> Bool {
+        guard intent.supportsLink else {
+            return false
+        }
+        return configuration.billingDetailsCollectionConfiguration.name == .automatic
+        && configuration.billingDetailsCollectionConfiguration.phone == .automatic
+        && configuration.billingDetailsCollectionConfiguration.email == .automatic
+        && configuration.billingDetailsCollectionConfiguration.address == .automatic
     }
 
     static func fetchIntent(mode: PaymentSheet.InitializationMode, configuration: PaymentSheet.Configuration, analyticsClient: STPAnalyticsClient) async throws -> Intent {
