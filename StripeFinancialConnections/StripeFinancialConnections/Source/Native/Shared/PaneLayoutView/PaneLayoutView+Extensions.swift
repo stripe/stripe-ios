601aca6bd876e07920d0f92ab0ebe06bcfa1242d
kgaidis-stripe <105514761+kgaidis-stripe@users.noreply.github.com>
2024-01-08 14:24:25 -0800

    Financial Connections: for v3, added support for showing a v3 Data Access Notice + Legal Details Notice (#3150)
    ## Summary
    
    This PR:
    - Adds data and legal notices sheets that were removed [in another
    pr](https://github.com/stripe/stripe-ios/pull/3145)
    - Fixes more SheetViewController bugs
    
    Details to understand before reviewing PR:
    - This is one PR, out of expected ~50+, to change the design of
    Financial Connections from a "V2" design to a "V3" design. [Here is a
    link to the
    designs](https://www.figma.com/file/wXQ8NJApqSJiLmxxANDRTs/%E2%9C%A8-Connections-3.0?type=design&node-id=2524-217093&mode=design&t=kXaBiAlgVsC7YcH1-0).
    - The code here is not the finished version. It's just an iteration
    towards the final version. This code will be re-visited later. The goal
    is to go screen-by-screen and make easy changes to learn about all the
    difficulties in each screen. Later, all the learnings will be combined
    to do a "final" polish of all screens.
    - The above also applies to design. The design here is not the finished
    version.
    - This PR is merged into a feature branch `fc-v3`. It is not merged into
    `master`.
    - There might be TODO's scattered throughout the code. That's expected
    and they will be fixed later.
    
    [Data Access Notice
    Model](https://livegrep.corp.stripe.com/view/stripe-internal/pay-server/lib/bank_connections/text/data/data_access_notice_v3.rb#L11)
    
    [Legal Details
    Model](https://livegrep.corp.stripe.com/view/stripe-internal/pay-server/lib/bank_connections/text/data/legal_details_notice_v3.rb)
    
    ## Testing
    
    ### Data Access Notice
    
    ![Simulator Screenshot - iPhone 15 Pro - 2024-01-04 at 15 51
    02](https://github.com/stripe/stripe-ios/assets/105514761/2e3fd603-f684-4bca-995b-df63699721dc)
    
    ### Terms And Privacy Policy Notice
    
    ![Simulator Screenshot - iPhone 15 Pro - 2024-01-04 at 15 51
    08](https://github.com/stripe/stripe-ios/assets/105514761/43e29652-f990-4d77-bcd0-57d6a7c390e0)
    
    ### Video Of Links In Sheets Working (and can see animations)
    
    
    https://github.com/stripe/stripe-ios/assets/105514761/ef5c1f41-495d-46b7-b9f6-160d8420834b

--- StripeFinancialConnections/StripeFinancialConnections/Source/Native/Shared/PaneLayoutView/PaneLayoutView+Extensions.swift
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Native/Shared/PaneLayoutView/PaneLayoutView+Extensions.swift
@@ -87,6 +87,7 @@ extension PaneLayoutView {
         contentView: UIView?
     ) -> UIView {
         let paddingStackView = HitTestStackView()
+        paddingStackView.axis = .vertical
         paddingStackView.spacing = 16
         paddingStackView.isLayoutMarginsRelativeArrangement = true
         paddingStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(
@@ -122,7 +123,9 @@ extension PaneLayoutView {
     @available(iOSApplicationExtension, unavailable)
     static func createFooterView(
         primaryButtonConfiguration: PaneLayoutView.ButtonConfiguration?,
-        secondaryButtonConfiguration: PaneLayoutView.ButtonConfiguration?
+        secondaryButtonConfiguration: PaneLayoutView.ButtonConfiguration?,
+        topText: String? = nil,
+        didSelectURL: ((URL) -> Void)? = nil
     ) -> UIView? {
         guard
             primaryButtonConfiguration != nil || secondaryButtonConfiguration != nil
@@ -135,6 +138,23 @@ extension PaneLayoutView {
         )
         footerStackView.axis = .vertical
         footerStackView.spacing = 8
+
+        if let topText = topText {
+            let topTextLabel = AttributedTextView(
+                font: .label(.small),
+                boldFont: .label(.smallEmphasized),
+                linkFont: .label(.small),
+                textColor: .textDefault,
+                alignCenter: true
+            )
+            topTextLabel.setText(
+                topText,
+                action: didSelectURL ?? { _ in }
+            )
+            footerStackView.addArrangedSubview(topTextLabel)
+            footerStackView.setCustomSpacing(16, after: topTextLabel)
+        }
+
         if let primaryButtonConfiguration = primaryButtonConfiguration {
             let primaryButton = Button(configuration: FinancialConnectionsPrimaryButtonConfiguration())
             primaryButton.title = primaryButtonConfiguration.title
@@ -149,6 +169,7 @@ extension PaneLayoutView {
             ])
             footerStackView.addArrangedSubview(primaryButton)
         }
+
         if let secondaryButtonConfiguration = secondaryButtonConfiguration {
             let secondaryButton = Button(configuration: FinancialConnectionsSecondaryButtonConfiguration())
             secondaryButton.title = secondaryButtonConfiguration.title
