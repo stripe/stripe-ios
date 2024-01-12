70516d16e0248d92a0548cd280687e7e2c1f8f83
kgaidis-stripe <105514761+kgaidis-stripe@users.noreply.github.com>
2024-01-12 14:38:54 -0800

    Finacial Connections: initial V3 success pane (#3170)
    ## Summary
    
    This PR:
    - Modifies Success Pane design to be V3
    
    Details to understand before reviewing PR:
    - This is one PR, out of expected ~50+, to change the design of
    Financial Connections from a "V2" design to a "V3" design. [Here is a
    link to the
    designs](https://www.figma.com/file/wXQ8NJApqSJiLmxxANDRTs/%E2%9C%A8-Connections-3.0?type=design&node-id=2524-217093&mode=design&t=kXaBiAlgVsC7YcH1-0).
    - The code here is not the finished version. It's just an iteration
    towards the final version. This code will be re-visited later. The goal
    is to go screen-by-screen and make easy changes to learn about all the
    difficulties in each screen. Later, all the learnings will be combined
    to do a "final" polish of all screens.
    - The above also applies to design. The design here is not the finished
    version.
    - This PR is merged into a feature branch `fc-v3`. It is not merged into
    `master`.
    - There might be TODO's scattered throughout the code. That's expected
    and they will be fixed later.
    
    ## Testing
    
    | Before | After |
    | --- | --- |
    | ![Simulator Screenshot - iPhone 15 Pro - 2024-01-12 at 14 30
    44](https://github.com/stripe/stripe-ios/assets/105514761/87ac26ce-239f-4b3e-8dc6-8703c099417c)
    | ![Simulator Screenshot - iPhone 15 Pro - 2024-01-11 at 10 01
    47](https://github.com/stripe/stripe-ios/assets/105514761/1cae9a5a-a421-419c-a0fb-8298e963b819)
    |

--- StripeFinancialConnections/StripeFinancialConnections/Source/Native/Success/SuccessFooterView.swift
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Native/Success/SuccessFooterView.swift
@@ -27,25 +27,22 @@ final class SuccessFooterView: UIView {
     }()
 
     init(
-        showFailedToLinkNotice: Bool,
-        businessName: String?,
         didSelectDone: @escaping (SuccessFooterView) -> Void
     ) {
         self.didSelectDone = didSelectDone
         super.init(frame: .zero)
 
-        let footerStackView = UIStackView()
-        footerStackView.axis = .vertical
-        footerStackView.spacing = 24
-
-        if showFailedToLinkNotice {
-            let saveToLinkFailedNoticeView = CreateSaveToLinkFailedNoticeView(
-                businessName: businessName
-            )
-            footerStackView.addArrangedSubview(saveToLinkFailedNoticeView)
-        }
-        footerStackView.addArrangedSubview(doneButton)
-        addAndPinSubviewToSafeArea(footerStackView)
+        let paddingStackView = UIStackView()
+        paddingStackView.axis = .vertical
+        paddingStackView.addArrangedSubview(doneButton)
+        paddingStackView.isLayoutMarginsRelativeArrangement = true
+        paddingStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(
+            top: 16,
+            leading: 24,
+            bottom: 16,
+            trailing: 24
+        )
+        addAndPinSubviewToSafeArea(paddingStackView)
     }
 
     required init?(coder: NSCoder) {
@@ -59,61 +56,4 @@ final class SuccessFooterView: UIView {
     func setIsLoading(_ isLoading: Bool) {
         doneButton.isLoading = isLoading
     }
-}
-
-private func CreateSaveToLinkFailedNoticeView(
-    businessName: String?
-) -> UIView {
-    let errorLabelFont = FinancialConnectionsFont.label(.smallEmphasized)
-    let warningIconWidthAndHeight: CGFloat = 12
-    let warningIconInsets = errorLabelFont.topPadding
-    let warningIconImageView = UIImageView()
-    warningIconImageView.image = Image.warning_triangle.makeImage()
-        .withTintColor(.textCritical)
-        // Align the icon to the center of the first line.
-        //
-        // UIStackView does not do a great job of doing this
-        // automatically.
-        .withAlignmentRectInsets(
-            UIEdgeInsets(top: -warningIconInsets, left: 0, bottom: warningIconInsets, right: 0)
-        )
-    warningIconImageView.translatesAutoresizingMaskIntoConstraints = false
-    NSLayoutConstraint.activate([
-        warningIconImageView.widthAnchor.constraint(equalToConstant: warningIconWidthAndHeight),
-        warningIconImageView.heightAnchor.constraint(equalToConstant: warningIconWidthAndHeight),
-    ])
-
-    let errorLabel = AttributedLabel(
-        font: .label(.smallEmphasized),
-        textColor: .textPrimary
-    )
-    errorLabel.numberOfLines = 0
-    errorLabel.text = {
-        if let businessName = businessName {
-            return String(format: STPLocalizedString("Your account was connected to %@ but could not be saved to Link at this time.", "A warning message that explains the user that their bank account was successfully connected for payments, but it was not connected to Stripe's Link network. '%@' will be replaced by the business name, ex. Cola Cola Inc."), businessName)
-        } else {
-            return STPLocalizedString("Your account was connected but could not be saved to Link at this time.", "A warning message that explains the user that their bank account was successfully connected for payments, but it was not connected to Stripe's Link network.")
-        }
-    }()
-    errorLabel.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
-
-    let horizontalStackView = HitTestStackView(
-        arrangedSubviews: [
-            warningIconImageView,
-            errorLabel,
-        ]
-    )
-    horizontalStackView.axis = .horizontal
-    horizontalStackView.alignment = .top
-    horizontalStackView.spacing = 8
-    horizontalStackView.isLayoutMarginsRelativeArrangement = true
-    horizontalStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(
-        top: 10,
-        leading: 12,
-        bottom: 10,
-        trailing: 12
-    )
-    horizontalStackView.backgroundColor = .attention50
-    horizontalStackView.layer.cornerRadius = 8
-    return horizontalStackView
 }
