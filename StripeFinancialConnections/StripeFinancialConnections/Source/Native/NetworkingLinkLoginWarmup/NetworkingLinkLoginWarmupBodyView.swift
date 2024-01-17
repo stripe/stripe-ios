435a6097fc0529efb7f4775bc926ac9d7434fab5
Krisjanis Gaidis <kgaidis@stripe.com>
2024-01-17 11:43:41 -0800

    Financial Connections: for v3, warmup pane, changed design to v3.

--- StripeFinancialConnections/StripeFinancialConnections/Source/Native/NetworkingLinkLoginWarmup/NetworkingLinkLoginWarmupBodyView.swift
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Native/NetworkingLinkLoginWarmup/NetworkingLinkLoginWarmupBodyView.swift
@@ -12,48 +12,31 @@ import UIKit
 
 final class NetworkingLinkLoginWarmupBodyView: HitTestView {
 
-    private let didSelectContinue: () -> Void
-
-    init(
-        email: String,
-        didSelectContinue: @escaping (() -> Void),
-        didSelectSkip: @escaping (() -> Void)
-    ) {
-        self.didSelectContinue = didSelectContinue
+    init(email: String) {
         super.init(frame: .zero)
-        let verticalStackView = HitTestStackView(
-            arrangedSubviews: [
-                CreateContinueButton(
-                    email: email,
-                    didSelectContinue: didSelectContinue,
-                    target: self
-                ),
-                CreateSkipButton(didSelectSkip: didSelectSkip),
-            ]
-        )
-        verticalStackView.axis = .vertical
-        verticalStackView.spacing = 20
-        addAndPinSubview(verticalStackView)
+        let emailView = CreateEmailView(email: email)
+        addAndPinSubview(emailView)
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    @objc fileprivate func didSelectContinueButton() {
-        didSelectContinue()
-    }
 }
 
-private func CreateContinueButton(
-    email: String,
-    didSelectContinue: @escaping () -> Void,
-    target: UIView
+private func CreateEmailView(
+    email: String
 ) -> UIView {
+
+    let emailLabel = AttributedLabel(
+        font: .body(.small),
+        textColor: .textDefault
+    )
+    emailLabel.setText(email)
+
     let horizontalStack = UIStackView(
         arrangedSubviews: [
-            CreateContinueButtonLabelView(email: email),
-            CreateArrowIconView(),
+            CreateAvatarView(email: email),
+            emailLabel,
         ]
     )
     horizontalStack.axis = .horizontal
@@ -66,81 +49,37 @@ private func CreateContinueButton(
         bottom: 12,
         trailing: 16
     )
-    horizontalStack.layer.borderColor = UIColor.borderNeutral.cgColor
+    horizontalStack.layer.borderColor = UIColor.borderDefault.cgColor
     horizontalStack.layer.borderWidth = 1
-    horizontalStack.layer.cornerRadius = 4
-
-    let tapGestureRecognizer = UITapGestureRecognizer(
-        target: target,
-        action: #selector(NetworkingLinkLoginWarmupBodyView.didSelectContinueButton)
-    )
-    horizontalStack.addGestureRecognizer(tapGestureRecognizer)
-
+    horizontalStack.layer.cornerRadius = 12
     return horizontalStack
 }
 
-private func CreateContinueButtonLabelView(email: String) -> UIView {
-    let continueLabel = AttributedLabel(
-        font: .label(.small),
-        textColor: .textSecondary
-    )
-    continueLabel.text = STPLocalizedString(
-        "Continue as",
-        "Leading text that comes before an e-mail. For example, it might say 'Continue as username@gmail.com'. This text will be combined together to form a button which, when pressed, will automatically log-in the user with their e-mail."
-    )
+private func CreateAvatarView(email: String) -> UIView {
+    let diameter: CGFloat = 36
 
-    let emailLabel = AttributedLabel(
-        font: .label(.largeEmphasized),
-        textColor: .textPrimary
-    )
-    emailLabel.text = email
-
-    let verticalStackView = UIStackView(
-        arrangedSubviews: [
-            continueLabel,
-            emailLabel,
-        ]
-    )
-    verticalStackView.axis = .vertical
-    verticalStackView.spacing = 0
-    return verticalStackView
-}
-
-private func CreateArrowIconView() -> UIView {
-    let imageView = UIImageView(image: Image.arrow_right.makeImage(template: true))
-    imageView.tintColor = .textBrand
-    imageView.translatesAutoresizingMaskIntoConstraints = false
+    let circleView = UIView()
+    circleView.backgroundColor = .linkBrand200
+    circleView.layer.cornerRadius = diameter / 2
+    circleView.translatesAutoresizingMaskIntoConstraints = false
     NSLayoutConstraint.activate([
-        imageView.widthAnchor.constraint(equalToConstant: 16),
-        imageView.heightAnchor.constraint(equalToConstant: 16),
+        circleView.heightAnchor.constraint(equalToConstant: diameter),
+        circleView.widthAnchor.constraint(equalToConstant: diameter),
     ])
-    return imageView
-}
 
-private func CreateSkipButton(
-    didSelectSkip: @escaping () -> Void
-) -> UIView {
-    let leadingText = STPLocalizedString(
-        "Not you?",
-        "Leading text that comes before a button. For example, it will say 'Not you? Continue without signing in'. Pressing 'Continue without signing in' will allow the user to continue through the Bank Authentication Flow."
+    let letterLabel = AttributedLabel(
+        font: .body(.small),
+        textColor: .linkBrand600
     )
-    let continueButtonText = STPLocalizedString(
-        "Continue without signing in",
-        "Text for a butoon. Pressing it will allow the user to continue through the Bank Authentication Flow."
-    )
-    let skipLabel = AttributedTextView(
-        font: .label(.medium),
-        boldFont: .label(.mediumEmphasized),
-        linkFont: .label(.mediumEmphasized),
-        textColor: .textSecondary
-    )
-    skipLabel.setText(
-        "\(leadingText) [\(continueButtonText)](stripe://no-url-action-handler-will-be-used)",
-        action: { _ in
-            didSelectSkip()
-        }
-    )
-    return skipLabel
+    letterLabel.setText(String(email.uppercased().first ?? "E"))
+    circleView.addSubview(letterLabel)
+    letterLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+        letterLabel.centerXAnchor.constraint(equalTo: circleView.centerXAnchor),
+        letterLabel.centerYAnchor.constraint(equalTo: circleView.centerYAnchor),
+    ])
+
+    return circleView
 }
 
 #if DEBUG
@@ -150,11 +89,7 @@ import SwiftUI
 private struct NetworkingLinkLoginWarmupBodyViewUIViewRepresentable: UIViewRepresentable {
 
     func makeUIView(context: Context) -> NetworkingLinkLoginWarmupBodyView {
-        NetworkingLinkLoginWarmupBodyView(
-            email: "test@stripe.com",
-            didSelectContinue: {},
-            didSelectSkip: {}
-        )
+        NetworkingLinkLoginWarmupBodyView(email: "test@stripe.com")
     }
 
     func updateUIView(_ uiView: NetworkingLinkLoginWarmupBodyView, context: Context) {}
