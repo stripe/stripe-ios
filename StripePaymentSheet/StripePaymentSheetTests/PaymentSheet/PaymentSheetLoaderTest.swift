b183c7a72f2b2d8c196a518b92d1315a633d773d
Yuki <yuki@stripe.com>
2024-02-01 13:35:49 -0800

    Fix sending EPM load failed analytic when `external_payment_method_da… (#3238)
    …ta` is NSNull in the v1/elements/sessions response.
    
    Follow-up to #3232
    
    
    ## Motivation
    Turns out the server can also send `NSNull` as a value, so don't send an
    error in that case either!
    
    ## Testing
    Add a unit test
    
    ## Changelog
    Not user facing

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentSheetLoaderTest.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentSheetLoaderTest.swift
@@ -21,8 +21,9 @@ final class PaymentSheetLoaderTest: XCTestCase {
         return config
     }()
 
-    override func setUp() async throws {
-        try await super.setUp()
+    override func tearDown() {
+        STPAnalyticsClient.sharedClient._testLogHistory = []
+        super.tearDown()
     }
 
     func testPaymentSheetLoadWithPaymentIntent() async throws {
