cdadb869a14a4e0b3e348e8d0b3210c571f42198
John Woo <wooj@stripe.com>
2024-02-14 12:54:00 -0800

    Fix PaymentSheet scan card broken UI

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Elements/CardSectionWithScanner/CardSectionWithScannerView.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Elements/CardSectionWithScanner/CardSectionWithScannerView.swift
@@ -69,7 +69,7 @@ final class CardSectionWithScannerView: UIView {
     private func setCardScanVisible(_ isCardScanVisible: Bool) {
         UIView.animate(withDuration: PaymentSheetUI.defaultAnimationDuration) {
             self.cardScanButton.alpha = isCardScanVisible ? 0 : 1
-            self.cardScanningView.isHidden = !isCardScanVisible
+            self.cardScanningView.setHiddenIfNecessary(!isCardScanVisible)
             self.cardScanningView.alpha = isCardScanVisible ? 1 : 0
         }
     }
