2ce33f02f73ad8e3a28c34f1449897c6b3161af3
Krisjanis Gaidis <kgaidis@stripe.com>
2024-02-21 13:27:37 -0800

    Financial Connections: for v3, improved partner auth app to app state transitions.

--- StripeFinancialConnections/StripeFinancialConnections/Source/Web/FinancialConnectionsWebFlowViewController.swift
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Web/FinancialConnectionsWebFlowViewController.swift
@@ -35,20 +35,21 @@ final class FinancialConnectionsWebFlowViewController: UIViewController {
     // MARK: - Waiting state view
 
     private lazy var continueStateView: UIView = {
-        return PaneLayoutView(
-            contentView: ContinueStateViews.createContentView(
-                institutionImageUrl: nil
-            ),
-            footerView: ContinueStateViews.createFooterView(
-                didSelectContinue: { [weak self] in
-                    guard let self else { return }
-                    if let url = self.lastOpenedNativeURL {
-                        self.redirect(to: url)
-                    } else {
-                        self.startAuthenticationSession(manifest: manifest)
-                    }
+        let continueStateViews = ContinueStateViews(
+            institutionImageUrl: nil,
+            didSelectContinue: { [weak self] in
+                guard let self else { return }
+                if let url = self.lastOpenedNativeURL {
+                    self.redirect(to: url)
+                } else {
+                    self.startAuthenticationSession(manifest: manifest)
                 }
-            )
+            },
+            didSelectCancel: nil
+        )
+        return PaneLayoutView(
+            contentView: continueStateViews.contentView,
+            footerView: continueStateViews.footerView
         ).createView()
     }()
 
