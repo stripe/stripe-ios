8225162a30fda048e12bb5ffd6785102b8c0a965
kgaidis-stripe <105514761+kgaidis-stripe@users.noreply.github.com>
2024-03-01 12:17:13 -0800

    Financial Connections: changed verification screen logic to match v3 (#3345)
    ## Summary
    
    ^ this modifies logic of "Verification" screens to match that of new web
    code
    
    ## Testing
    
    ### NetworkingSaveToLinkVerificationViewController
    
    
    https://github.com/stripe/stripe-ios/assets/105514761/0fa0958a-bb59-413f-aac0-8444748a925b
    
    ### NetworkingLinkVerificationViewController
    
    
    https://github.com/stripe/stripe-ios/assets/105514761/343ec92a-dc0b-4dc3-85ea-13960dedff51

--- StripeFinancialConnections/StripeFinancialConnections/Source/Native/NativeFlowController.swift
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Native/NativeFlowController.swift
@@ -925,8 +925,7 @@ extension NativeFlowController: LinkAccountPickerViewControllerDelegate {
 extension NativeFlowController: NetworkingSaveToLinkVerificationViewControllerDelegate {
     func networkingSaveToLinkVerificationViewControllerDidFinish(
         _ viewController: NetworkingSaveToLinkVerificationViewController,
-        saveToLinkWithStripeSucceeded: Bool?,
-        error: Error?
+        saveToLinkWithStripeSucceeded: Bool?
     ) {
         if saveToLinkWithStripeSucceeded != nil {
             dataManager.saveToLinkWithStripeSucceeded = saveToLinkWithStripeSucceeded
