73b2cd9a47cbdbb85cd65a2cd2de4479116166c7
Nick Porter <porter@stripe.com>
2024-03-15 13:37:01 -0600

    Join by comma

--- ci_scripts/check_for_untranslated_strings.rb
+++ ci_scripts/check_for_untranslated_strings.rb
@@ -67,5 +67,5 @@ new_strings_added = get_added_strings($ROOT_DIR)
 missing_translations = check_lokalise_translations(ENV['LOKALISE_API_KEY'], '747824695e51bc2f4aa912.89576472', new_strings_added)
 puts(missing_translations)
 if missing_translations.any?
-  File.open("missing_translations.txt", 'w') { |f| f.write missing_translations.join("\n") }
+  File.open("missing_translations.txt", 'w') { |f| f.write missing_translations.join(", ") }
 end
