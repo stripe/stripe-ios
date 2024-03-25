788142ba76847174a8db59c8b971af9755e6d2a3
Yuki Tokuhiro <yuki@stripe.com>
2024-03-25 15:58:25 -0700

    Whoops, add file to project. Add stpAssert too

--- StripeCore/StripeCore/Source/Helpers/STPAssert.swift
+++ StripeCore/StripeCore/Source/Helpers/STPAssert.swift
@@ -16,3 +16,13 @@ import Foundation
     print("⚠️ STPAssertionFailure: \(message()) in \(file) on line \(line)")
     #endif
 }
+
+/// A wrapper that only calls `assert` when the `ENABLE_STPASSERTIONFAILURE` compiler flag is set.
+/// Use this for assertions that should not trigger in merchant apps.
+@inlinable @_spi(STP) public func stpAssert(_ condition: @autoclosure () -> Bool, _ message: @autoclosure () -> String = String(), file: StaticString = #file, line: UInt = #line) {
+    #if ENABLE_STPASSERTIONFAILURE
+    assert(condition(), message(), file: file, line: line)
+    #else
+    print("⚠️ STPAssertionFailure: \(message()) in \(file) on line \(line)")
+    #endif
+}
