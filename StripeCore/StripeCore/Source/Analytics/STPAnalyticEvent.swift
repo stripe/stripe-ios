a4bc6c4ca37440a61d6687b618c8c3feca960724
John Woo <99628984+wooj-stripe@users.noreply.github.com>
2024-04-04 12:27:53 -0700

    AddPaymentMethodViewController assert->events (#3465)

--- StripeCore/StripeCore/Source/Analytics/STPAnalyticEvent.swift
+++ StripeCore/StripeCore/Source/Analytics/STPAnalyticEvent.swift
@@ -234,6 +234,7 @@ import Foundation
     // MARK: - Unexpected errors
     // These errors should _never happen_ and indicate a problem with the SDK or the Stripe backend.
     case unexpectedPaymentSheetFormFactoryError = "unexpected_error.paymentsheet.formfactory"
+    case unexpectedPaymentSheetError = "unexpected_error.paymentsheet"
     case unexpectedPaymentSheetViewControllerError = "unexpected_error.paymentsheet.paymentsheetviewcontroller"
     case unexpectedFlowControllerViewControllerError = "unexpected_error.paymentsheet.flowcontrollerviewcontroller"
     case unexpectedPaymentHandlerError = "unexpected_error.paymenthandler"
