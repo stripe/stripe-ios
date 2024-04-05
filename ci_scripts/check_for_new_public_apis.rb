e30e081d71fca246fe0945bb744bdd8cf6b48b59
Nick Porter <porter@stripe.com>
2024-04-05 17:26:46 -0600

    Fix ruby script

--- ci_scripts/check_for_new_public_apis.rb
+++ ci_scripts/check_for_new_public_apis.rb
@@ -1,14 +1,14 @@
 def get_added_swift_code(current_dir)
-    added_lines = `git diff --name-only origin/master...`.split("\n").select do |file|
+    swift_files = `git diff --name-only origin/master...`.split("\n").select do |file|
       file.downcase.end_with?('.swift') && !file.downcase.include?("test")
-    end  
-   
-    code_diff = added_lines.flat_map do |file|
-      `git diff origin/master... -- #{file}`.split("\n").select do |line|
+    end
+  
+    code_diff = swift_files.flat_map do |file|
+      git_diff = `git diff origin/master... -- "#{file}"`
+      git_diff.split("\n").select do |line|
         line.start_with?('+') && !line.start_with?('+++')
       end
     end
-  
     code_diff
   end
   
@@ -18,7 +18,7 @@ def get_added_swift_code(current_dir)
     added_lines.each do |line|
       line = line.strip.delete_prefix('+')
       if line =~ /(public\s+)?func\s+\w+\s*\(/ and not line =~ /@_spi/
-        public_functions << line
+        public_functions << line.lstrip.chop
       end
     end
     public_functions
