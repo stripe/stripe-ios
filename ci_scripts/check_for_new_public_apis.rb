bf1c28197dc918d7e1b7e6091db1c4d486161829
Nick Porter <porter@stripe.com>
2024-04-05 17:08:46 -0600

    Ruby script to detect new public APIs

---
+++ ci_scripts/check_for_new_public_apis.rb
@@ -0,0 +1,32 @@
+def get_added_swift_code(current_dir)
+    added_lines = `git diff --name-only origin/master...`.split("\n").select do |file|
+      file.downcase.end_with?('.swift') && !file.downcase.include?("test")
+    end  
+   
+    code_diff = added_lines.flat_map do |file|
+      `git diff origin/master... -- #{file}`.split("\n").select do |line|
+        line.start_with?('+') && !line.start_with?('+++')
+      end
+    end
+  
+    code_diff
+  end
+  
+  def check_spi_protection(added_lines)
+    public_functions = []
+  
+    added_lines.each do |line|
+      line = line.strip.delete_prefix('+')
+      if line =~ /(public\s+)?func\s+\w+\s*\(/ and not line =~ /@_spi/
+        public_functions << line
+      end
+    end
+    public_functions
+  end
+  
+  added_swift_code = get_added_swift_code($ROOT_DIR)
+  new_public_apis = check_spi_protection(added_swift_code)
+  puts(new_public_apis)
+  if new_public_apis.any?
+    File.open("new_public_apis.txt", 'w') { |f| f.write new_public_apis.join(", ") }
+  end
