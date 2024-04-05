972d6556f997e0330e888c1c719488e4f4117926
David Estes <davidestes@stripe.com>
2024-04-05 15:08:02 -0700

    Fix size test

--- Tests/installation_tests/size_test/size_report.rb
+++ Tests/installation_tests/size_test/size_report.rb
@@ -117,7 +117,6 @@ def build(dir, target_name = 'SPMTest')
         'CODE_SIGN_IDENTITY="-" ' +
         'CODE_SIGNING_REQUIRED="NO" ' +
         'CODE_SIGN_ENTITLEMENTS="" ' +
-        'LD_GENERATE_MAP_FILE="YES" ' +
         "LD_MAP_FILE_PATH=\"build/#{target_name}-LinkMap.txt\" " +
         'CODE_SIGNING_ALLOWED="NO"').to_s)
 
