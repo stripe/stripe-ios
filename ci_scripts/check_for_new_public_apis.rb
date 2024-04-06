40fecb4655fb6cb69573e9bf583a8402340c43da
Nick Porter <porter@stripe.com>
2024-04-06 12:06:20 -0600

    Handle more complex SPI cases

--- ci_scripts/check_for_new_public_apis.rb
+++ ci_scripts/check_for_new_public_apis.rb
@@ -1,33 +1,47 @@
-def get_added_swift_code(current_dir)
-  swift_files = `git diff --name-only origin/master...`.split("\n").select do |file|
-    file.downcase.end_with?('.swift') && !file.downcase.split('/').any? { |path_part| path_part.include?("test") }
+def get_modified_swift_files(current_dir)
+  files = `git diff --name-only origin/master...`.split("\n").select { |f| f.downcase.end_with?(".swift") && !f.downcase.include?("test") }
+  files.select { |f| File.exist?(f) }
+end
+
+def get_added_lines_change(file)
+  `git diff origin/master... -- '#{file}'`.split("\n").select do |line|
+    line.start_with?('+') && !line.start_with?('+++')
+  end.map { |line| line.delete_prefix('+').strip }
+end
+
+def check_spi_protection(file)
+  in_spi_protected_block = false
+  block_depth = 0
+  public_items = []
+  added_lines = get_added_lines_change(file)
+  previous_line_spi_annotated = false
+
+
+  File.foreach(file) do |line|
+    line = line.strip
+    opening_braces = line.count('{')
+    closing_braces = line.count('}')
+    
+    block_depth += opening_braces - closing_braces
+    if line =~ /@_spi\(\w+\)\s+(public\s+)?(class|struct|enum|protocol)\s+\w+/ && opening_braces > 0
+      in_spi_protected_block = true
+    elsif line =~ /(public\s+)?(class|struct|enum|protocol)\s+\w+/ && opening_braces > 0
+      in_spi_protected_block = false
+    elsif added_lines.include?(line) && line =~ /(public\s+|open\s+)[a-z]+\s+\w+/ && !in_spi_protected_block && !previous_line_spi_annotated
+      public_items << (File.basename(file) + ": " + line.lstrip.chomp('{').rstrip)
+    end
+
+    previous_line_spi_annotated = line.include?("@_spi(")
+    in_spi_protected_block = false if block_depth == 0
   end
 
-  
-    code_diff = swift_files.flat_map do |file|
-      git_diff = `git diff origin/master... -- "#{file}"`
-      git_diff.split("\n").select do |line|
-        line.start_with?('+') && !line.start_with?('+++')
-      end
-    end
-    code_diff
-  end
-  
-  def check_spi_protection(added_lines)
-    public_functions = []
-  
-    added_lines.each do |line|
-      line = line.strip.delete_prefix('+')
-      if line =~ /(public\s+|open\s+)[a-z]+\s+\w+/ && !line.include?('@_spi')
-        public_functions << line.lstrip.chop
-      end
-    end
-    public_functions
-  end
-  
-  added_swift_code = get_added_swift_code($ROOT_DIR)
-  new_public_apis = check_spi_protection(added_swift_code)
-  puts(new_public_apis)
-  if new_public_apis.any?
-    File.open("new_public_apis.txt", 'w') { |f| f.write new_public_apis.join(", ") }
-  end
+  public_items
+end
+
+files = get_modified_swift_files($ROOT_DIR)
+new_public_items = files.flat_map { |file| check_spi_protection(file) }
+
+if new_public_items.any?
+  puts new_public_items
+  File.open("new_public_items.txt", 'w') { |f| f.write new_public_items.join("\n") }
+end
