8bada11e054a9d782992777c01328a0980f49f00
Nick Porter <porter@stripe.com>
2024-04-15 11:34:23 -0600

    Add some logs

--- ci_scripts/api_diff/diff_public_interface.rb
+++ ci_scripts/api_diff/diff_public_interface.rb
@@ -25,10 +25,12 @@ for framework_name in GetFrameworks.framework_names("./modules.yaml")
 	# Add the framework headers and the diff to the final diff string
 	unless module_diff.empty?
 		final_diff_string += "\n### #{framework_name}\n```diff\n#{module_diff}\n```\n"
+    put final_diff_string
 	end
 end
 
 # Write the final diff string to a file
 if not final_diff_string.empty?
+  puts 'Wrote diff_result.txt'
 	File.open("diff_result.txt", 'w') { |f| f.write final_diff_string }
 end
