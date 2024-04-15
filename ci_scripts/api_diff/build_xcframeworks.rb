aab3e41d278aed3ae6e63dae33cbc3d4ed670ce9
Nick Porter <porter@stripe.com>
2024-04-15 10:27:13 -0600

    Build all frameworks XCFramework

--- ci_scripts/api_diff/build_xcframeworks.rb
+++ ci_scripts/api_diff/build_xcframeworks.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'open3'
+require_relative 'get_frameworks' # adjust the path if required
 
 # function to checkout git branch, build and generate API JSON
 def checkout_build_generate(branch, archive_name)
@@ -7,33 +8,27 @@ def checkout_build_generate(branch, archive_name)
   puts "Building and generating public interface from #{branch}..."
   system("git checkout #{branch}")
 
-#   system("xcodebuild clean archive \
-#   -quiet \
-#   -workspace 'Stripe.xcworkspace' \
-#   -scheme 'AllStripeFrameworks' \
-#   -destination 'generic/platform=iOS Simulator' \
-#   -configuration 'Release' \
-#   -archivePath './StripeFrameworks-sim-#{archive_name}.xcarchive' \
-#   -sdk iphonesimulator \
-#   SUPPORTS_MACCATALYST=NO \
-#   BUILD_LIBRARIES_FOR_DISTRIBUTION=YES \
-#   SWIFT_ACTIVE_COMPILATION_CONDITIONS=STRIPE_BUILD_PACKAGE \
-#   SKIP_INSTALL=NO")
+  system("xcodebuild clean archive \
+  -quiet \
+  -workspace 'Stripe.xcworkspace' \
+  -scheme 'AllStripeFrameworks' \
+  -destination 'generic/platform=iOS Simulator' \
+  -configuration 'Release' \
+  -archivePath './StripeFrameworks-sim-#{archive_name}.xcarchive' \
+  -sdk iphonesimulator \
+  SUPPORTS_MACCATALYST=NO \
+  BUILD_LIBRARIES_FOR_DISTRIBUTION=YES \
+  SWIFT_ACTIVE_COMPILATION_CONDITIONS=STRIPE_BUILD_PACKAGE \
+  SKIP_INSTALL=NO")
 
-  Zip::File.open(File.join_if_safe(build_dir, 'Stripe.xcframework.zip'), create: true) do |zipfile|
-    # Add module framework directories to zip
-    modules.each do |m|
-      framework_name = m['framework_name']
-      Dir.glob("#{build_dir}/#{framework_name}.xcframework/**/*").each do |file|
-        file_name = Pathname.new(file).relative_path_from(Pathname.new(build_dir))
-        system("xcodebuild -create-xcframework \
-        -framework './StripeFrameworks-sim-#{archive_name}.xcarchive/Products/Library/Frameworks/StripePaymentSheet.framework' \
-        -output './StripePaymentSheet-#{archive_name}.xcframework'")
-      end
-    end
+  for framework_name in GetFrameworks.framework_names(file_path)
+    system("xcodebuild -create-xcframework \
+        -framework './StripeFrameworks-sim-#{archive_name}.xcarchive/Products/Library/Frameworks/#{framework_name}.framework' \
+        -output './#{framework_name}-#{archive_name}.xcframework'")
   end
+
 end
 
 # Run function for master and head_ref
-# checkout_build_generate("master", "master")
+checkout_build_generate("master", "master")
 checkout_build_generate(ENV['GITHUB_HEAD_REF'], "new")
