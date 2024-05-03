708cb33245fb32a3030f9e63626dee61f0f7a858
Nick Porter <porter@stripe.com>
2024-05-03 12:30:16 -0600

    Remove log

--- ci_scripts/api_diff/build_xcframeworks.rb
+++ ci_scripts/api_diff/build_xcframeworks.rb
@@ -8,8 +8,8 @@ def checkout_build_generate(branch, archive_name)
   puts "Building and generating public interface from #{branch}..."
   if archive_name == "master"
     head_ref = ENV['GITHUB_HEAD_REF']
-    puts "git checkout $(git merge-base #{branch}) #{head_ref}"
-    system("git checkout $(git merge-base #{branch}) #{head_ref}")
+    commit_sha = system("git merge-base #{branch} #{head_ref}")
+    system("git checkout #{commit_sha}")
   else 
     system("git checkout #{branch}")
   end
