b7d3f9da141df9f91bebdb9c6b30634418ece89a
Nick Porter <porter@stripe.com>
2024-05-07 12:01:28 -0600

    Update card view controller copy

--- StripePaymentSheet/StripePaymentSheet/Source/Categories/String+Localized.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/Categories/String+Localized.swift
@@ -73,12 +73,20 @@ extension String.Localized {
         STPLocalizedString("Back", "Text for back button")
     }
 
-    static var update_card: String {
+    static var update_card_brand: String {
         STPLocalizedString(
-            "Update card",
+            "Update card brand",
             """
-            Title for a button that when tapped, presents a screen for updating a card. Also
-            the heading the screen itself.
+            Title for a screen for updating a card brand.
+            """
+        )
+    }
+    
+    static var update: String {
+        STPLocalizedString(
+            "Update",
+            """
+            Title for a button that when tapped, updates a card brand.
             """
         )
     }
