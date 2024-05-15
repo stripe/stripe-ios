3a6bf25cb0e8f854eb07379f113182b6e203bbc4
John Woo <99628984+wooj-stripe@users.noreply.github.com>
2024-05-15 09:14:13 -0700

    Updated support for MobilePay bindings (#3581)
    ## Summary
    We had only partial support (params), for mobile pay bindings. This adds
    deserialization support and associated tests
    
    ## Motivation
    Missed the first time around. need to add support.
    
    ## Testing
    Added tests to deserialize PI w/ client secret
    
    ## Changelog
    Updated.

---
+++ Stripe/StripeiOSTests/STPPaymentMethodMobilePayParamsTests.swift
@@ -0,0 +1,42 @@
+//
+//  STPPaymentMethodMobilePayParamsTests.swift
+//  StripeiOSTests
+//
+
+import Foundation
+import StripeCoreTestUtils
+
+@testable@_spi(STP) import Stripe
+@testable@_spi(STP) import StripeCore
+@testable@_spi(STP) import StripePayments
+@testable@_spi(STP) import StripePaymentSheet
+@testable@_spi(STP) import StripePaymentsUI
+
+class STPPaymentMethodMobilePayParamsTests: XCTestCase {
+
+    func testCreateMobilePayPaymentMethod() throws {
+        let mobilePayParams = STPPaymentMethodMobilePayParams()
+
+        let params = STPPaymentMethodParams(
+            mobilePay: mobilePayParams,
+            billingDetails: nil,
+            metadata: nil
+        )
+
+        let exp = expectation(description: "Payment Method MobilePay create")
+
+        let client = STPAPIClient(publishableKey: STPTestingDefaultPublishableKey)
+        client.createPaymentMethod(with: params) {
+            (paymentMethod: STPPaymentMethod?, error: Error?) in
+            exp.fulfill()
+
+            XCTAssertNil(error)
+            XCTAssertNotNil(paymentMethod, "Payment method should be populated")
+            XCTAssertEqual(paymentMethod?.type, .mobilePay, "Incorrect PaymentMethod type")
+            XCTAssertNotNil(paymentMethod?.mobilePay, "The `mobilepay` property must be populated")
+        }
+
+        self.waitForExpectations(timeout: STPTestingNetworkRequestTimeout)
+    }
+
+}
