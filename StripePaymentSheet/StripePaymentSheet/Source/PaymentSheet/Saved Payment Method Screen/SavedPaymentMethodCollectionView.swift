f2594c7eb37efb3648a55bf7145a1e7323be9572
John Woo <wooj@stripe.com>
2024-05-17 10:58:11 -0700

    lint

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Saved Payment Method Screen/SavedPaymentMethodCollectionView.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Saved Payment Method Screen/SavedPaymentMethodCollectionView.swift
@@ -189,10 +189,6 @@ extension SavedPaymentMethodCollectionView {
             ])
         }
 
-        override func layoutSubviews() {
-            super.layoutSubviews()
-        }
-
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
