8b412876c3d51fb0a5bd009dbd2c0d399c4b1b11
Nick Porter <porter@stripe.com>
2024-05-30 07:31:53 -0600

    Lint

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Vertical Main Screen/RightAccessoryButton.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Vertical Main Screen/RightAccessoryButton.swift
@@ -12,11 +12,11 @@ import UIKit
 
 extension RowButton {
     final class RightAccessoryButton: UIButton {
-        
+
         enum AccessoryType {
             case edit
             case viewMore
-            
+
             var text: String? {
                 switch self {
                 case .edit:
@@ -25,7 +25,7 @@ extension RowButton {
                     return .Localized.view_more
                 }
             }
-            
+
             var accessoryImage: UIImage? {
                 switch self {
                 case .edit:
@@ -34,7 +34,7 @@ extension RowButton {
                     return Image.icon_chevron_right.makeImage(template: true)
                 }
             }
-            
+
             var imageEdgeInsets: UIEdgeInsets {
                 switch self {
                 case .edit:
@@ -44,11 +44,11 @@ extension RowButton {
                 }
             }
         }
-        
+
         // Overridden so auto layout properly accounts for the image offset (if any) and positions the button correctly
         override var intrinsicContentSize: CGSize {
             let size = super.intrinsicContentSize
-            
+
             // TODO(porter) Figure out how to handle this for VisionOS, the alternative (`UIButton.Configuration) was introduced in iOS 15 and we currently support below iOS 15.
             #if !canImport(CompositorServices)
             return CGSize(width: size.width + imageEdgeInsets.left + imageEdgeInsets.right,
@@ -57,7 +57,7 @@ extension RowButton {
             return size
             #endif
         }
-        
+
         init(accessoryType: AccessoryType, appearance: PaymentSheet.Appearance) {
             super.init(frame: .zero)
             setTitle(accessoryType.text, for: .normal)
@@ -70,12 +70,12 @@ extension RowButton {
             #if !canImport(CompositorServices)
             imageEdgeInsets = accessoryType.imageEdgeInsets
             #endif
-            
+
             accessibilityLabel = accessoryType.text
             accessibilityIdentifier = accessoryType.text
-            
+
         }
-        
+
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
