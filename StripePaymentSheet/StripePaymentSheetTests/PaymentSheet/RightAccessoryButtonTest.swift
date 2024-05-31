edb9aecb9aab32428a837bb6d567d47154363b05
Nick Porter <porter@stripe.com>
2024-05-31 13:06:55 -0600

    record snapshots

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/RightAccessoryButtonTest.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/RightAccessoryButtonTest.swift
@@ -6,16 +6,63 @@
 //
 
 import Foundation
-import XCTest
 @_spi(STP) @testable import StripePaymentSheet
+import XCTest
 
 final class RightAccessoryButtonTest: XCTestCase {
-    
+
+    // MARK: No saved payment methods
+    func testNil_Returned_When_NoSavedPaymentMethods() {
+        // Regardless of any flags, if we have zero we should return nil
+
+        // Iterating over all combinations of isFirstCardCoBranded, isCBCEligible, allowsRemovalOfLastSavedPaymentMethod, and paymentMethodRemove
+        let booleans = [false, true]
+        for isFirstCardCoBranded in booleans {
+            for isCBCEligible in booleans {
+                for allowsRemovalOfLastSavedPaymentMethod in booleans {
+                    for paymentMethodRemove in booleans {
+                        let result = RowButton.RightAccessoryButton.getAccessoryButtonType(savedPaymentMethodsCount: 0,
+                                                                                           isFirstCardCoBranded: isFirstCardCoBranded,
+                                                                                           isCBCEligible: isCBCEligible,
+                                                                                           allowsRemovalOfLastSavedPaymentMethod: allowsRemovalOfLastSavedPaymentMethod,
+                                                                                           paymentMethodRemove: paymentMethodRemove)
+                        XCTAssertNil(result)
+                    }
+                }
+            }
+        }
+    }
+
+    // MARK: 1 card
+
+    func testSingleCard_Returns_CorrectValue() {
+        // Iterating over all combinations of isFirstCardCoBranded, isCBCEligible, allowsRemovalOfLastSavedPaymentMethod, and paymentMethodRemove
+        let booleans = [false, true]
+        for isFirstCardCoBranded in booleans {
+            for isCBCEligible in booleans {
+                for allowsRemovalOfLastSavedPaymentMethod in booleans {
+                    for paymentMethodRemove in booleans {
+
+                        // We should only show the edit button if it is update-able or removable
+                        let canEdit = (isFirstCardCoBranded && isCBCEligible) || (allowsRemovalOfLastSavedPaymentMethod && paymentMethodRemove)
+                        let expected: RowButton.RightAccessoryButton.AccessoryType? = canEdit ? .edit : nil
+                        let result = RowButton.RightAccessoryButton.getAccessoryButtonType(savedPaymentMethodsCount: 1,
+                                                                                           isFirstCardCoBranded: isFirstCardCoBranded,
+                                                                                           isCBCEligible: isCBCEligible,
+                                                                                           allowsRemovalOfLastSavedPaymentMethod: allowsRemovalOfLastSavedPaymentMethod,
+                                                                                           paymentMethodRemove: paymentMethodRemove)
+                        XCTAssertEqual(result, expected)
+                    }
+                }
+            }
+        }
+    }
+
     // MARK: More than 1 saved payment method
-    
+
     func testViewMore_Returned_When_SavedPaymentsGreaterThanOneRegardlessOfOtherValues() {
         // Regardless of any flags, if we have more than 1 payment method we should show the view more button
-        
+
         // Iterating over all combinations of isFirstCardCoBranded, isCBCEligible, allowsRemovalOfLastSavedPaymentMethod, and paymentMethodRemove
         let booleans = [false, true]
         for isFirstCardCoBranded in booleans {
@@ -28,30 +75,6 @@ final class RightAccessoryButtonTest: XCTestCase {
                                                                                            allowsRemovalOfLastSavedPaymentMethod: allowsRemovalOfLastSavedPaymentMethod,
                                                                                            paymentMethodRemove: paymentMethodRemove)
                         XCTAssertEqual(result, .viewMore)
-                    }
-                }
-            }
-        }
-    }
-
-    // MARK: 1 card
-    
-    func testSingleCard_Returns_CorrectValue() {
-        // Iterating over all combinations of isFirstCardCoBranded, isCBCEligible, allowsRemovalOfLastSavedPaymentMethod, and paymentMethodRemove
-        let booleans = [false, true]
-        for isFirstCardCoBranded in booleans {
-            for isCBCEligible in booleans {
-                for allowsRemovalOfLastSavedPaymentMethod in booleans {
-                    for paymentMethodRemove in booleans {
-                        
-                        let canEdit = (isFirstCardCoBranded && isCBCEligible) || (allowsRemovalOfLastSavedPaymentMethod && paymentMethodRemove)
-                        let expected: RowButton.RightAccessoryButton.AccessoryType? = canEdit ? .edit : nil
-                        let result = RowButton.RightAccessoryButton.getAccessoryButtonType(savedPaymentMethodsCount: 1,
-                                                                                           isFirstCardCoBranded: isFirstCardCoBranded,
-                                                                                           isCBCEligible: isCBCEligible,
-                                                                                           allowsRemovalOfLastSavedPaymentMethod: allowsRemovalOfLastSavedPaymentMethod,
-                                                                                           paymentMethodRemove: paymentMethodRemove)
-                        XCTAssertEqual(result, expected)
                     }
                 }
             }
