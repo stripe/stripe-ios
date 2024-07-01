ad3d5339b5260e62a8838c095945c849431e4e45
Nick Porter <porter@stripe.com>
2024-07-01 15:03:13 -0600

    Bump height to 48

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Vertical Main Screen/RowButton.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Vertical Main Screen/RowButton.swift
@@ -88,9 +88,9 @@ class RowButton: UIView {
         }
 
         // Resolve ambiguous height warning by setting these constraints w/ low priority
-        let imageViewTopConstraint = imageView.topAnchor.constraint(equalTo: topAnchor, constant: 12)
+        let imageViewTopConstraint = imageView.topAnchor.constraint(equalTo: topAnchor, constant: 14)
         imageViewTopConstraint.priority = .defaultLow
-        let imageViewBottomConstraint = imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12)
+        let imageViewBottomConstraint = imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -14)
         imageViewBottomConstraint.priority = .defaultLow
 
         // To make all RowButtons the same height, set our height to the tallest variant (a RowButton w/ text and subtext)
@@ -103,8 +103,8 @@ class RowButton: UIView {
         NSLayoutConstraint.activate([
             imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
             imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            imageView.topAnchor.constraint(greaterThanOrEqualTo: topAnchor, constant: 12),
-            imageView.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor, constant: -12),
+            imageView.topAnchor.constraint(greaterThanOrEqualTo: topAnchor, constant: 14),
+            imageView.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor, constant: -14),
             imageView.heightAnchor.constraint(equalToConstant: 20),
             imageView.widthAnchor.constraint(equalToConstant: 24),
 
