99bcaa3238c913ded063df610fa68ed6166e043a
kgaidis-stripe <105514761+kgaidis-stripe@users.noreply.github.com>
2024-07-05 11:20:43 -0700

    Financial Connections: for networking manual entry, added a drawer for disabled accounts in LinkAccountPicker (#3745)
    This PR:
    - This PR adds a new "drawer/sheet" to LinkAccount Picker for the
    networking manual entry project.
    - We try to replicate the [Stripe.js PR
    here](https://git.corp.stripe.com/stripe-internal/stripe-js-v3/pull/21998).
    
    <img width="331" alt="Screenshot 2024-07-03 at 10 05 44 AM"
    src="https://github.com/stripe/stripe-ios/assets/105514761/c53963ed-f69f-47af-9681-8d2949addcd0">
    
    [JIRA](https://jira.corp.stripe.com/browse/BANKCON-11261)
    
    Context:
    - Financial Connections is implementing support for "networking manual
    entry." This feature allows a user to enter their manually entered
    account _once_, and then we will allow them to reuse it later through
    Link.
    - This is just one PR of many where each PR will be merged to a feature
    branch `fc-networkingmanualentry`.
    - It's expected that this PR may have some gaps or TODO's because its
    going to a feature branch. That being said, we still want to make sure
    there's no glaring logic issues/bugs.
    
    https://github.com/stripe/stripe-ios/assets/105514761/66de13ed-631f-4d6d-b891-6e2b26e5ff65
    
    <img width="275" alt="Screenshot 2024-07-03 at 10 06 58 AM"
    src="https://github.com/stripe/stripe-ios/assets/105514761/1e84cb18-6bed-4edb-b7ee-1249b7c1620c">
    
    Ran `bitrise run financial-connections-stability-tests` and passed:
    
    ```
    FinancialConnectionsNetworkingUITests
        ✓ testNativeNetworkingTestMode (142.097 seconds)
        ✓ testNativeNetworkingTestModeSignUpWithMultiSelectAndPrefilledEmail (32.753 seconds)
        ✓ testNativeNetworkingTestModeSignUpWithPrefilledEmailAndPhone (24.318 seconds)
    FinancialConnectionsUITests
        ✓ testDataLiveModeOAuthNativeAuthFlow (29.027 seconds)
        ✓ testDataLiveModeOAuthWebAuthFlow (20.233 seconds)
        ✓ testDataTestModeOAuthNativeAuthFlow (20.803 seconds)
        ✓ testPaymentSearchInLiveModeNativeAuthFlow (25.867 seconds)
        ✓ testPaymentTestModeLegacyNativeAuthFlow (19.842 seconds)
        ✓ testPaymentTestModeManualEntryAutofill (14.907 seconds)
        ✓ testPaymentTestModeManualEntryNativeAuthFlow (24.661 seconds)
    
    	 Executed 10 tests, with 0 failures (0 unexpected) in 354.508 (354.521) seconds
    ```

---
+++ StripeFinancialConnections/StripeFinancialConnections/Source/API Bindings/Models/FinancialConnectionsGenericInfoScreen.swift
@@ -0,0 +1,133 @@
+//
+//  FinancialConnectionsGenericInfoScreen.swift
+//  StripeFinancialConnections
+//
+//  Created by Krisjanis Gaidis on 6/20/24.
+//
+
+import Foundation
+
+struct FinancialConnectionsGenericInfoScreen: Decodable {
+
+    let id: String
+    let header: Header?
+    let body: Body?
+    let footer: Footer?
+    let options: Options?
+
+    struct Header: Decodable {
+        let title: String?
+        let subtitle: String?
+        let icon: FinancialConnectionsImage?
+        let alignment: Alignment?
+
+        enum Alignment: String, Decodable {
+            case left = "left"
+            case center = "center"
+            case right = "right"
+        }
+    }
+
+    struct Body: Decodable {
+
+        let entries: [BodyEntry]
+
+        enum BodyEntry: Decodable {
+            case text(TextBodyEntry)
+            case image(ImageBodyEntry)
+            case unparasable
+
+            public init(from decoder: Decoder) throws {
+                let type = try? decoder
+                    .container(keyedBy: TypeDecodingContainer.CodingKeys.self)
+                    .decode(TypeDecodingContainer.self, forKey: .type)
+                    .type
+                let container = try decoder.singleValueContainer()
+                if type == .text, let value = try? container.decode(TextBodyEntry.self) {
+                    self = .text(value)
+                } else if type == .image, let value = try? container.decode(ImageBodyEntry.self) {
+                    self = .image(value)
+                } else {
+                    self = .unparasable
+                }
+            }
+
+            // this struct is here
+            private struct TypeDecodingContainer: Codable {
+                let type: BodyEntryType
+
+                enum BodyEntryType: String, Codable {
+                    case text = "text"
+                    case image = "image"
+                }
+
+                enum CodingKeys: String, CodingKey {
+                    case type
+                }
+            }
+        }
+
+        struct TextBodyEntry: Decodable {
+
+            let id: String
+            let text: String
+            let alignment: Alignment?
+            let size: Size?
+
+            enum Alignment: String, Decodable {
+                case left = "left"
+                case center = "center"
+                case right = "right"
+            }
+
+            enum Size: String, Decodable {
+                case xsmall = "x-small"
+                case small = "small"
+                case medium = "medium"
+            }
+        }
+
+        struct ImageBodyEntry: Decodable {
+            let id: String
+            let image: FinancialConnectionsImage
+            let alt: String
+        }
+
+        struct BulletsBodyEntry: Decodable {
+            let id: String
+            let bullets: [GenericBulletPoint]
+
+            struct GenericBulletPoint: Decodable {
+                let id: String
+                let icon: FinancialConnectionsImage?
+                let title: String?
+                let content: String?
+            }
+        }
+    }
+
+    struct Footer: Decodable {
+        let disclaimer: String?
+        let primaryCta: GenericInfoAction?
+        let secondaryCta: GenericInfoAction?
+        let belowCta: String?
+
+        struct GenericInfoAction: Decodable {
+            let id: String
+            let label: String
+            let icon: FinancialConnectionsImage?
+            let action: String?
+            let testId: String?
+        }
+    }
+
+    struct Options: Decodable {
+        let fullWidthContent: Bool?
+        let verticalAlignment: VerticalAlignment?
+
+        enum VerticalAlignment: String, Decodable {
+            case `default` = "default"
+            case centered = "centered"
+        }
+    }
+}
