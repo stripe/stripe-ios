6b610a1b7e8fbeaa37914680f77ed17c454c4929
kgaidis-stripe <105514761+kgaidis-stripe@users.noreply.github.com>
2024-07-12 16:34:45 -0700

    Financial Connections: more improvements to GenericInfoScreen by adding TextBodyEntry (#3767)
    ## Summary
    
    This PR:
    - This PR added support for `TextBodyEntry` server-driven-ux coming for
    `GenericInfoScreen` model.
    
    Context:
    - Financial Connections is implementing support for "networking manual
    entry." This feature allows a user to enter their manually entered
    account _once_, and then we will allow them to reuse it later through
    Link.
    - This is just one PR of many where each PR will be merged to a feature
    branch `fc-networkingmanualentry`.
    - It's expected that this PR may have some gaps or TODO's because its
    going to a feature branch. That being said, we still want to make sure
    there's no glaring logic issues/bugs.
    
    ## Testing
    
    SwiftUI preview:
    
    <img width="587" alt="image"
    src="https://github.com/user-attachments/assets/0edb482c-4b31-41bd-9542-63e93413af35">
    
    Also made sure the code that gets this model does not
    crash/is-not-effected (note that it does not utilize the body at all,
    unfortunately):
    
    <img width="405" alt="image"
    src="https://github.com/user-attachments/assets/4cee411a-e037-4623-94c2-d628f7527b61">

--- StripeFinancialConnections/StripeFinancialConnections/Source/Native/Success/SuccessViewController.swift
+++ StripeFinancialConnections/StripeFinancialConnections/Source/Native/Success/SuccessViewController.swift
@@ -135,7 +135,7 @@ private func CreateBodyView(subtitle: String?) -> UIView {
             boldFont: .body(.mediumEmphasized),
             linkFont: .body(.medium),
             textColor: .textDefault,
-            alignCenter: true
+            alignment: .center
         )
         subtitleLabel.setText(subtitle)
         labelVerticalStackView.addArrangedSubview(subtitleLabel)
