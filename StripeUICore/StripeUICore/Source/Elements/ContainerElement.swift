2200e56c8febb7d482f7381c5817a28cb5bb0a65
David Estes <davidestes@stripe.com>
2024-07-25 09:44:06 -0700

    lint

--- StripeUICore/StripeUICore/Source/Elements/ContainerElement.swift
+++ StripeUICore/StripeUICore/Source/Elements/ContainerElement.swift
@@ -59,7 +59,7 @@ extension ContainerElement {
             .dropFirst() // Drop `element` too
         for next in remainingElements {
             // Don't auto select hidden elements
-            if !(next is SectionElement.HiddenElement), 
+            if !(next is SectionElement.HiddenElement),
                 !next.view.isHidden,
                 next.beginEditing() {
                 UIAccessibility.post(notification: .screenChanged, argument: next.view)
