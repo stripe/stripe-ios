6acec33c79012166cddfc499e48601261770b90f
Nick Porter <porter@stripe.com>
2024-07-31 15:17:54 -0600

    Different approach

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/Appearance+FontScaling.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/Appearance+FontScaling.swift
@@ -15,17 +15,26 @@ extension PaymentSheet.Appearance {
     ///   - font: The font to be scaled
     ///   - style: The style for your text
     ///   - maximumPointSize: The maximum font size to be scaled to
-    ///   - fontSize: Optional font size to override the default font size provided by the `UIFont.TextStyle` before scaling
     /// - Returns: A font scaled to be used in PaymentSheet
     /// - Note: To prevent the font from being scaled down, set `minimumContentSizeCategory = .large` on the label.
-    func scaledFont(for font: UIFont, style: UIFont.TextStyle, maximumPointSize: CGFloat, fontSize: CGFloat? = nil) -> UIFont {
+    func scaledFont(for font: UIFont, style: UIFont.TextStyle, maximumPointSize: CGFloat) -> UIFont {
         let defaultTraitCollection = UITraitCollection(preferredContentSizeCategory: .large) // large is the default content size category
-        var fontDescriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style, compatibleWith: defaultTraitCollection)
-        if let fontSize {
-            // If present override the font size on the descriptor before scaling for dynamic type happens below
-            fontDescriptor = fontDescriptor.withSize(fontSize)
-        }
+        let fontDescriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style, compatibleWith: defaultTraitCollection)
         let customFont = font.withSize(fontDescriptor.pointSize * self.font.sizeScaleFactor)
+
+        // scale the custom font for dynamic type
+        return UIFontMetrics.default.scaledFont(for: customFont, maximumPointSize: maximumPointSize)
+    }
+
+    /// Computes a font scaled to be used in PaymentSheet
+    /// - Parameters:
+    ///   - font: The font to be scaled
+    ///   - size: The size of the font
+    ///   - maximumPointSize: The maximum font size to be scaled to
+    /// - Returns: A font scaled to be used in PaymentSheet
+    /// - Note: To prevent the font from being scaled down, set `minimumContentSizeCategory = .large` on the label.
+    func scaledFont(for font: UIFont, size: CGFloat, maximumPointSize: CGFloat) -> UIFont {
+        let customFont = font.withSize(size * self.font.sizeScaleFactor)
 
         // scale the custom font for dynamic type
         return UIFontMetrics.default.scaledFont(for: customFont, maximumPointSize: maximumPointSize)
