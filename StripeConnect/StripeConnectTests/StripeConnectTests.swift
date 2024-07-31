2527d49b10d174753f5b700f6bcc6321dd0e5362
nschris-stripe <nschris@stripe.com>
2024-07-31 09:51:13 -0400

    Create stripe connect SDK.

---
+++ StripeConnect/StripeConnectTests/StripeConnectTests.swift
@@ -0,0 +1,11 @@
+//
+//  StripeConnectTests.swift
+//  StripeConnectTests
+//
+//  Created by Chris Mays on 7/31/24.
+//
+
+import XCTest
+@testable import StripeConnect
+
+final class StripeConnectTests: XCTestCase { }
