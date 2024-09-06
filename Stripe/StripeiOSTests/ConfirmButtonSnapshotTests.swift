5e6ccc27adc02d1befeb58d5c1b95d0cc67871e5
Joyce Qin <joyceqin@stripe.com>
2024-09-06 10:46:52 -0700

    added more snapshot tests

--- Stripe/StripeiOSTests/ConfirmButtonSnapshotTests.swift
+++ Stripe/StripeiOSTests/ConfirmButtonSnapshotTests.swift
@@ -81,6 +81,68 @@ class ConfirmButtonSnapshotTests: STPSnapshotTestCase {
 
         verify(confirmButton)
     }
+    
+    // Tests that `primaryButton` disabled color matches the primary color when no background color or diabled color set
+    func testConfirmButtonDisabledColorWhenSetPrimaryColorAndNoSetBackgroundColorOrDisabledColor() {
+        var appearance = PaymentSheet.Appearance.default
+        var button = PaymentSheet.Appearance.PrimaryButton()
+        button.disabledTextColor = .green.withAlphaComponent(0.2)
+        appearance.primaryButton = button
+        appearance.colors.primary = .yellow
+        
+
+        let confirmButton = ConfirmButton(
+            state: .disabled,
+            style: .stripe,
+            callToAction: .setup,
+            appearance: appearance,
+            didTap: {}
+        )
+
+        verify(confirmButton)
+    }
+    
+    // Tests that `primaryButton` disabled color matches the background color when background color is set but disabled color is not
+    func testConfirmButtonDisabledColorWhenSetBackgroundColorAndNoSetDisabledColor() {
+        var appearance = PaymentSheet.Appearance.default
+        var button = PaymentSheet.Appearance.PrimaryButton()
+        button.backgroundColor = .yellow
+        button.disabledTextColor = .green.withAlphaComponent(0.2)
+        appearance.primaryButton = button
+        
+
+        let confirmButton = ConfirmButton(
+            state: .disabled,
+            style: .stripe,
+            callToAction: .setup,
+            appearance: appearance,
+            didTap: {}
+        )
+
+        verify(confirmButton)
+    }
+    
+    // Tests that `primaryButton` disabled color matches the disabled color when disabled color, background color, and primary color are set
+    func testConfirmButtonDisabledColorWhenSetDisabledBackgroundAndPrimaryColors() {
+        var appearance = PaymentSheet.Appearance.default
+        var button = PaymentSheet.Appearance.PrimaryButton()
+        button.backgroundColor = .red
+        button.disabledBackgroundColor = .black
+        button.disabledTextColor = .green.withAlphaComponent(0.2)
+        appearance.primaryButton = button
+        appearance.colors.primary = .yellow
+        
+
+        let confirmButton = ConfirmButton(
+            state: .disabled,
+            style: .stripe,
+            callToAction: .setup,
+            appearance: appearance,
+            didTap: {}
+        )
+
+        verify(confirmButton)
+    }
 
     // Tests that `primaryButton` disabled color is updated properly
     func testConfirmButtonDisabledColor() {
