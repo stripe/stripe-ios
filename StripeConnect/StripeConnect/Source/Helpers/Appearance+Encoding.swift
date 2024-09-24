75bdab9d40498441287aeb24ebcacd6cb9f749c7
Mel <78050250+mludowise-stripe@users.noreply.github.com>
2024-09-23 20:12:04 -0700

    [Connect] Add support for Dashboard app (#4034)
    ## Summary
    <!-- Simple summary of what was changed. -->
    
    Adds Dashboard app support for the StripeConnect SDK.
    
    -  **Adds Dashboard app setting override keys**
    If the API client's key is a `uk_` key, then `apiKeyOverride`,
    `merchantIdOverride`, `platformIdOverride`, and `livemodeOverride` URL
    parameters are configured and sent to the connect webview, enabling it
    for use with direct accounts.
    
    - **Fixes the `StripeConnect` package to compile as an SPM package**
    Removes `StripeConnectBundleLocator` and localization files. This file
    wasn't compiling because all the files in the package's `Resource`
    folder were empty so `Resource.bundle` wasn't being generated. Since we
    don't have localized strings currently, we can delete these helpers for
    now.
    
    Annotates all public types with `@available(iOS 15, *)`. The
    Package.swift file has a min supported iOS version of 13 but
    StripeConnect uses types only available on 15+.
    
    - **Adds the payment-details component for use in the Dashboard**
    Because payment-details will not be included in the beta release, these
    interfaces are marked with `@_spi(DashboardOnly)` to only make them
    available to Dashboard.
    
    - **Enables dynamic font scaling**
    Automatically scales appearance font sizes when the component's size
    class changes and updates the doc strings appropriately.
    
    ## Motivation
    <!-- Why are you making this change? If it's for fixing a bug, if
    possible, please include a code snippet or example project that
    demonstrates the issue. -->
    
    https://jira.corp.stripe.com/browse/MXMOBILE-2502
    
    ## Testing
    <!-- How was the code tested? Be as specific as possible. -->
    Unit tests:
    - AppearanceTests.testFontSizesChangeBasedOnTraitCollection
    - ConnectJSURLParamsTests
    - PaymentDetailsViewControllersTests
    
    Manually testing dynamic font sizing and localization:
    
    
    https://github.com/user-attachments/assets/6f492031-0ca3-4cac-bc09-226f7e9714b1

--- StripeConnect/StripeConnect/Source/Helpers/Appearance+Encoding.swift
+++ StripeConnect/StripeConnect/Source/Helpers/Appearance+Encoding.swift
@@ -8,14 +8,16 @@
 @_spi(STP) import StripeCore
 import UIKit
 
+@available(iOS 15, *)
 typealias Appearance = EmbeddedComponentManager.Appearance
 
+@available(iOS 15, *)
 extension Appearance {
     func asDictionary(traitCollection: UITraitCollection) -> [String: String] {
         var dict: [String: String] = [:]
-        
+
         dict.mergeAssertingOnOverwrites(colors.asDictionary(traitCollection: traitCollection))
-        dict.mergeAssertingOnOverwrites(typography.asDictionary())
+        dict.mergeAssertingOnOverwrites(typography.asDictionary(traitCollection: traitCollection))
         dict.mergeAssertingOnOverwrites(buttonPrimary.asDictionary(keyPrefix: "buttonPrimary"))
         dict.mergeAssertingOnOverwrites(buttonSecondary.asDictionary(keyPrefix: "buttonSecondary"))
         dict.mergeAssertingOnOverwrites(badgeNeutral.asDictionary(keyPrefix: "badgeNeutral"))
@@ -23,13 +25,14 @@ extension Appearance {
         dict.mergeAssertingOnOverwrites(badgeWarning.asDictionary(keyPrefix: "badgeWarning"))
         dict.mergeAssertingOnOverwrites(badgeDanger.asDictionary(keyPrefix: "badgeDanger"))
         dict.mergeAssertingOnOverwrites(cornerRadius.asDictionary())
-        
+
         dict["spacingUnit"] = spacingUnit?.pxString
-        
+
         return dict
     }
 }
 
+@available(iOS 15, *)
 extension Appearance.CornerRadius {
     private var mappings: [String: KeyPath<Self, CGFloat?>] {
         [
@@ -40,41 +43,44 @@ extension Appearance.CornerRadius {
             "overlayBorderRadius": \.overlay,
         ]
     }
-    
+
     func asDictionary() -> [String: String] {
         mappings.compactMapValues { self[keyPath: $0]?.pxString }
     }
 }
 
+@available(iOS 15, *)
 extension Appearance.Badge {
     private func mappings(keyPrefix: String) -> [String: KeyPath<Self, UIColor?>] {
         [
             "\(keyPrefix)ColorBackground": \.colorBackground,
             "\(keyPrefix)ColorText": \.colorText,
-            "\(keyPrefix)ColorBorder": \.colorBorder
+            "\(keyPrefix)ColorBorder": \.colorBorder,
         ]
     }
-    
+
     func asDictionary(keyPrefix: String) -> [String: String] {
         mappings(keyPrefix: keyPrefix).compactMapValues { self[keyPath: $0]?.cssValue(includeAlpha: false) }
     }
 }
 
+@available(iOS 15, *)
 extension Appearance.Button {
-    
+
     private func mappings(keyPrefix: String) -> [String: KeyPath<Self, UIColor?>] {
         [
             "\(keyPrefix)ColorBackground": \.colorBackground,
             "\(keyPrefix)ColorText": \.colorText,
-            "\(keyPrefix)ColorBorder": \.colorBorder
+            "\(keyPrefix)ColorBorder": \.colorBorder,
         ]
     }
-    
+
     func asDictionary(keyPrefix: String) -> [String: String] {
         mappings(keyPrefix: keyPrefix).compactMapValues { self[keyPath: $0]?.cssValue(includeAlpha: false) }
     }
 }
 
+@available(iOS 15, *)
 extension Appearance.Colors {
     var mappings: [String: KeyPath<Self, UIColor?>] {
         [
@@ -89,45 +95,63 @@ extension Appearance.Colors {
             "formBackgroundColor": \.formBackground,
             "colorSecondaryText": \.secondaryText,
             "colorBorder": \.border,
-            "formHighlightColorBorder": \.formHighlightBorder
+            "formHighlightColorBorder": \.formHighlightBorder,
         ]
     }
-    
+
     func asDictionary(traitCollection: UITraitCollection) -> [String: String] {
         mappings.compactMapValues { self[keyPath: $0]?.resolvedColor(with: traitCollection).cssRgbValue }
     }
 }
 
+@available(iOS 15, *)
 extension Appearance.Typography {
-    func asDictionary() -> [String: String] {
+    var mappings: [String: KeyPath<Self, Style>] {
+        [
+            "bodySm": \.bodySm,
+            "bodyMd": \.bodyMd,
+            "headingXs": \.headingXs,
+            "headingSm": \.headingSm,
+            "headingMd": \.headingMd,
+            "headingLg": \.headingLg,
+            "headingXl": \.headingXl,
+            "labelMd": \.labelMd,
+            "labelSm": \.labelSm,
+        ]
+    }
+
+    func asDictionary(traitCollection: UITraitCollection) -> [String: String] {
         var dict: [String: String] = [:]
-        
+
         // Default font to "-apple-system" to use the system default,
         // otherwise the webView will use Times
         dict["fontFamily"] = font?.familyName ?? "-apple-system"
-        
-        dict["fontSizeBase"] = fontSizeBase?.pxString
-        
-        dict.mergeAssertingOnOverwrites(bodySm.asDictionary(keyPrefix: "bodySm"))
-        dict.mergeAssertingOnOverwrites(bodyMd.asDictionary(keyPrefix: "bodyMd"))
-        
-        dict.mergeAssertingOnOverwrites(headingXs.asDictionary(keyPrefix: "headingXs"))
-        dict.mergeAssertingOnOverwrites(headingSm.asDictionary(keyPrefix: "headingSm"))
-        dict.mergeAssertingOnOverwrites(headingMd.asDictionary(keyPrefix: "headingMd"))
-        dict.mergeAssertingOnOverwrites(headingLg.asDictionary(keyPrefix: "headingLg"))
-        dict.mergeAssertingOnOverwrites(headingXl.asDictionary(keyPrefix: "headingXl"))
-        
-        dict.mergeAssertingOnOverwrites(labelMd.asDictionary(keyPrefix: "labelMd"))
-        dict.mergeAssertingOnOverwrites(labelSm.asDictionary(keyPrefix: "labelSm"))
-        
+
+        if let fontSizeBase {
+            dict["fontSizeBase"] = UIFontMetrics.default.scaledValue(for: fontSizeBase, compatibleWith: traitCollection).pxString
+        }
+
+        mappings.forEach { (key, keyPath) in
+            dict.mergeAssertingOnOverwrites(self[keyPath: keyPath].asDictionary(
+                keyPrefix: key,
+                using: traitCollection))
+        }
+
         return dict
     }
 }
 
+@available(iOS 15, *)
 extension Appearance.Typography.Style {
-    func asDictionary(keyPrefix: String) -> [String: String] {
+    func asDictionary(
+        keyPrefix: String,
+        using traitCollection: UITraitCollection
+    ) -> [String: String] {
         var dict: [String: String] = [:]
-        dict["\(keyPrefix)FontSize"] = fontSize?.pxString
+
+        if let fontSize {
+            dict["\(keyPrefix)FontSize"] = UIFontMetrics.default.scaledValue(for: fontSize, compatibleWith: traitCollection).pxString
+        }
         dict["\(keyPrefix)FontWeight"] = weight?.cssValue
         dict["\(keyPrefix)TextTransform"] = textTransform?.rawValue
         return dict
--- StripeConnect/StripeConnect/Source/Internal/Webview/CustomFontSourceWrapper.swift
+++ StripeConnect/StripeConnect/Source/Internal/Webview/CustomFontSourceWrapper.swift
@@ -7,15 +7,16 @@
 
 import Foundation
 
+@available(iOS 15, *)
 struct CustomFontSourceWrapper: Encodable {
     let customFontSource: EmbeddedComponentManager.CustomFontSource
     enum CodingKeys: String, CodingKey {
         case family, style, weight, src
     }
-    
+
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        
+
         try container.encode(customFontSource.family, forKey: .family)
         try container.encodeIfPresent(customFontSource.style, forKey: .style)
         try container.encodeIfPresent(customFontSource.weight, forKey: .weight)
