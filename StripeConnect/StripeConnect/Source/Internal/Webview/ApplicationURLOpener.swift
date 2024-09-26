636a8fb52626a4c9d35aea6dc97a684303d7259d
Mel Ludowise <mludowise@stripe.com>
2024-09-26 15:41:48 -0700

    Fix StripeConnect to compile in Xcode 16

--- StripeConnect/StripeConnect/Source/Internal/Webview/ApplicationURLOpener.swift
+++ StripeConnect/StripeConnect/Source/Internal/Webview/ApplicationURLOpener.swift
@@ -10,10 +10,22 @@ import UIKit
 /// Protocol used to dependency inject `UIApplication.open` for use in tests
 protocol ApplicationURLOpener {
     func canOpenURL(_ url: URL) -> Bool
-    func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey: Any], completionHandler completion: ((Bool) -> Void)?)
+    func open(
+        _ url: URL,
+        options: [UIApplication.OpenExternalURLOptionsKey : Any],
+        completionHandler completion: OpenCompletionHandler?
+    )
 }
 
 extension ApplicationURLOpener {
+    // The signature of the `completionHandler` in UIApplication.open changed
+    // in Xcode 16
+    #if compiler(>=6.0)
+    typealias OpenCompletionHandler = @MainActor @Sendable (Bool) -> Void
+    #else
+    typealias OpenCompletionHandler = (Bool) -> Void
+    #endif
+
     func openIfPossible(_ url: URL) {
         guard canOpenURL(url) else {
             // TODO: MXMOBILE-2491 Log as analytics when url can't be opened.
