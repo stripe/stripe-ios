f55f15768bd95a67b18742fd5658352e7560a5c9
Mel <78050250+mludowise-stripe@users.noreply.github.com>
2024-10-04 15:51:06 -0700

    [Connect] Add Dashboard-only access to NotificationBanner (#4061)
    ## Summary
    Adds `NotificationBannerViewController` equivalent to the [Notification
    banner](https://docs.stripe.com/connect/supported-embedded-components/notification-banner)
    embedded component with DashboardOnly access.
    
    This component has a delegate callback, `didChangeWithTotal:
    andActionRequired` which contains:
    - The total number of notifications
    - The number of notifications that require action
    
    ## Motivation
    https://jira.corp.stripe.com/browse/MXMOBILE-2503
    
    ## Testing
    Unit tests

---
+++ StripeConnect/StripeConnect/Source/Components/NotificationBannerViewController.swift
@@ -0,0 +1,92 @@
+//
+//  NotificationBannerViewController.swift
+//  StripeConnectTests
+//
+//  Created by Mel Ludowise on 9/25/24.
+//
+
+import UIKit
+
+@_spi(DashboardOnly)
+@available(iOS 15, *)
+public class NotificationBannerViewController: UIViewController {
+
+    struct Props: Encodable {
+        let collectionOptions: AccountCollectionOptions
+
+        enum CodingKeys: String, CodingKey {
+            case collectionOptions = "setCollectionOptions"
+        }
+    }
+
+    let webView: ConnectComponentWebView
+
+    public weak var delegate: NotificationBannerViewControllerDelegate?
+
+    init(componentManager: EmbeddedComponentManager,
+         collectionOptions: AccountCollectionOptions) {
+        webView = ConnectComponentWebView(
+            componentManager: componentManager,
+            componentType: .notificationBanner
+        ) {
+            Props(collectionOptions: collectionOptions)
+        }
+        super.init(nibName: nil, bundle: nil)
+
+        webView.addMessageHandler(OnLoadErrorMessageHandler { [weak self] value in
+            guard let self else { return }
+            self.delegate?.notificationBanner(self, didFailLoadWithError: value.error.connectEmbedError)
+        })
+        webView.addMessageHandler(OnNotificationsChangeHandler { [weak self] value in
+            guard let self else { return }
+            self.delegate?.notificationBanner(self, didChangeWithTotal: value.total, andActionRequired: value.actionRequired)
+        })
+        webView.presentPopup = { [weak self] vc in
+            self?.present(vc, animated: true)
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func loadView() {
+        view = webView
+    }
+}
+
+@_spi(DashboardOnly)
+@available(iOS 15, *)
+public protocol NotificationBannerViewControllerDelegate: AnyObject {
+    /**
+     Triggered when an error occurs loading the notification banner component
+     - Parameters:
+       - notificationBanner: The notification banner component that errored when loading
+       - error: The error that occurred when loading the component
+     */
+    func notificationBanner(_ notificationBanner: NotificationBannerViewController,
+                            didFailLoadWithError error: Error)
+
+    /**
+     Triggered when the total number of notifications or notifications with required actions updates
+     - Parameters:
+       - notificationBanner: The notification banner component that changed
+       - total: The total number of notifications in the banner
+       - actionRequired: The number of notifications that require user action
+     */
+    func notificationBanner(_ notificationBanner: NotificationBannerViewController,
+                            didChangeWithTotal total: Int,
+                            andActionRequired actionRequired: Int)
+}
+
+@_spi(DashboardOnly)
+@available(iOS 15, *)
+public extension NotificationBannerViewControllerDelegate {
+    // Default implementation to make optional
+    func notificationBanner(_ notificationBanner: NotificationBannerViewController,
+                            didFailLoadWithError error: Error) { }
+
+    func notificationBanner(_ notificationBanner: NotificationBannerViewController,
+                            didChangeWithTotal total: Int,
+                            andActionRequired actionRequired: Int) { }
+}
