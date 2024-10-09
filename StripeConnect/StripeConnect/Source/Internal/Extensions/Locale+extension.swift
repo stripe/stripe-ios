f73d6983176c24041ae8471d850d8e253fc868e7
Mel Ludowise <mludowise@stripe.com>
2024-10-09 11:50:26 -0400

    Locale fixes

--- StripeConnect/StripeConnect/Source/Internal/Extensions/Locale+extension.swift
+++ StripeConnect/StripeConnect/Source/Internal/Extensions/Locale+extension.swift
@@ -9,12 +9,47 @@ import Foundation
 @_spi(STP) import StripeCore
 
 extension Locale {
-    /// iOS uses underscores for locale (`en_US`) but web uses hyphens (`en-US`)
-    var webIdentifier: String {
-        guard let region = stp_regionCode,
-              let language = stp_languageCode else {
-            return ""
+    /// Transforms the locale into a web locale
+    var webIdentifier: String? {
+        guard let languageCode else { return nil }
+
+        return [languageCode, adjustedScriptCode, languageRegionCode]
+            .compactMap { $0 }
+            .joined(separator: "-")
+    }
+
+    private var languageRegionCode: String? {
+        // On iOS 16+, the language can have a different region from the device
+        if #available(iOS 16, *),
+           let languageRegion = language.region {
+            // Use language region
+            return languageRegion.identifier
         }
-        return "\(language)-\(region)"
+
+        // Fallback to device region
+        return regionCode
+    }
+
+    private var adjustedScriptCode: String? {
+        // Use the system script if there is one
+        if let scriptCode { return scriptCode }
+
+        /*
+         iOS drops the language script when using:
+         - language=zh-Hans and region=CN
+         - language=zh-Hant and region=TW or HK
+
+         If the Chinese language script is not specified to web, it will default
+         to using zh-Hans, so we need to explicitly specify one for these cases.
+         */
+        switch (languageCode, languageRegionCode) {
+        case ("zh", "CN"):
+            return "Hans"
+        case ("zh", "TW"),
+             ("zh", "HK"):
+            return "Hant"
+        default:
+            return nil
+        }
     }
 }
