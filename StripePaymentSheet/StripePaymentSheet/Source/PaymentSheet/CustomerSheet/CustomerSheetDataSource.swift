9c3d941418a6cd80b37a3487323611922998872e
Nick Porter <porter@stripe.com>
2024-10-11 14:51:28 -0600

    Filter when using CustomerAdapter

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/CustomerSheet/CustomerSheetDataSource.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/CustomerSheet/CustomerSheetDataSource.swift
@@ -70,7 +70,10 @@ class CustomerSheetDataSource {
                 // Ensure local specs are loaded prior to the ones from elementSession
                 await loadFormSpecs()
 
-                let (paymentMethods, selectedPaymentMethod, elementSession) = try await (paymentMethodsResult, selectedPaymentMethodResult, elementsSessionResult)
+                let (paymentMethods, selectedPaymentMethod, elementSession) = try await (paymentMethodsResult.filter({ paymentMethod in
+                    guard let card = paymentMethod.card else { return true }
+                    return configuration.cardBrandFilter.isAccepted(cardBrand: card.preferredDisplayBrand)
+                }), selectedPaymentMethodResult, elementsSessionResult)
 
                 // Override with specs from elementSession
                 _ = FormSpecProvider.shared.loadFrom(elementSession.paymentMethodSpecs as Any)
