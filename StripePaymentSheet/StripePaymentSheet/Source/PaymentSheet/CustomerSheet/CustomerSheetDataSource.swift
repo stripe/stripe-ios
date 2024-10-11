0f7a10a3af8d5deaa9f98d272c2cf3b0f9a04fcd
Nick Porter <porter@stripe.com>
2024-10-11 14:46:54 -0600

    Filter out saved PMs in CustomerSheet with CustomerSessions

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/CustomerSheet/CustomerSheetDataSource.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/CustomerSheet/CustomerSheetDataSource.swift
@@ -47,7 +47,10 @@ class CustomerSheetDataSource {
                 // Override with specs from elementSession
                 _ = FormSpecProvider.shared.loadFrom(elementSession.paymentMethodSpecs as Any)
 
-                let savedPaymentMethods = elementSession.customer?.paymentMethods ?? []
+                let savedPaymentMethods = elementSession.customer?.paymentMethods.filter({ paymentMethod in
+                    guard let card = paymentMethod.card else { return true }
+                    return configuration.cardBrandFilter.isAccepted(cardBrand: card.preferredDisplayBrand)
+                }) ?? []
                 return completion(.success((savedPaymentMethods, paymentOption, elementSession)))
             } catch {
                 return completion(.failure(error))
