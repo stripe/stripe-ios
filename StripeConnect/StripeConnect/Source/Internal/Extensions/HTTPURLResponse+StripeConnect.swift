5be8e65f46abc160a97cdbe8394850a2b3594797
Mel Ludowise <mludowise@stripe.com>
2024-10-14 21:36:54 -0700

    comment

--- StripeConnect/StripeConnect/Source/Internal/Extensions/HTTPURLResponse+StripeConnect.swift
+++ StripeConnect/StripeConnect/Source/Internal/Extensions/HTTPURLResponse+StripeConnect.swift
@@ -11,13 +11,7 @@ extension HTTPURLResponse {
     /// Returns true when the status code of the response is considered an error for a web view
     var hasErrorStatus: Bool {
         /*
-         1xx - Informational response
-         2xx - Success
-         3xx - Redirect
-         4xx - Client error
-         5xx - Server error
-
-         Note: 100–399 status codes are not considered errors as the web view
+         100–399 status codes are not considered errors as the web view
          will gracefully handle these. Only 400+ are cases where the component
          cannot be loaded.
          */
