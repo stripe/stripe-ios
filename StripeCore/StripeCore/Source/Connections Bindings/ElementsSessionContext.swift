a702a6a3ccd40062d52f43f0c2251e25fd312b6c
Till Hellmund <tillh@stripe.com>
2024-10-17 10:46:22 -0400

    Enable native flow in `LinkPaymentController`

--- StripeCore/StripeCore/Source/Connections Bindings/ElementsSessionContext.swift
+++ StripeCore/StripeCore/Source/Connections Bindings/ElementsSessionContext.swift
@@ -18,16 +18,25 @@ import Foundation
     @_spi(STP) public let currency: String?
     @_spi(STP) public let intentId: IntentID?
     @_spi(STP) public let linkMode: LinkMode?
+    @_spi(STP) public let email: String?
 
     @_spi(STP) public init(
         amount: Int?,
         currency: String?,
         intentId: IntentID?,
-        linkMode: LinkMode?
+        linkMode: LinkMode?,
+        email: String? = nil
     ) {
         self.amount = amount
         self.currency = currency
         self.intentId = intentId
         self.linkMode = linkMode
+        self.email = email
+    }
+}
+
+@_spi(STP) public extension ElementsSessionContext {
+    init(email: String?) {
+        self.init(amount: nil, currency: nil, intentId: nil, linkMode: nil, email: email)
     }
 }
