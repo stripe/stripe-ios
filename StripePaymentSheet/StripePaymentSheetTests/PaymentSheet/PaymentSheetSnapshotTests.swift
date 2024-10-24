d7e91704a01161bba1a0cb7725d352c41d3f0598
Yuki <yuki@stripe.com>
2024-10-24 11:26:06 -0700

    Fix tests

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentSheetSnapshotTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentSheetSnapshotTests.swift
@@ -1113,6 +1113,7 @@ class PaymentSheetSnapshotTests: STPSnapshotTestCase {
                 config.customer = .init(id: customerId, ephemeralKeySecret: customerEphemeralKeySecret)
             }
             config.appearance = appearance
+            config.paymentMethodLayout = .horizontal
 
             if !applePayEnabled {
                 config.applePay = nil
