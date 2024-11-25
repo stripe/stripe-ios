8cdadbf12b76ac61a930f93c785147822ce975af
joyceqin-stripe <joyceqin@stripe.com>
2024-11-25 10:51:26 -0800

    Extracted form creation to separate files SavedPaymentMethodFormFactory (#4298)
    ## Summary
    <!-- Simple summary of what was changed. -->
    Separate files SavedPaymentMethodFormFactory+PAYMENT_METHOD for card, US
    bank account, SEPA debit
    ## Motivation
    <!-- Why are you making this change? If it's for fixing a bug, if
    possible, please include a code snippet or example project that
    demonstrates the issue. -->
    
    ## Testing
    <!-- How was the code tested? Be as specific as possible. -->
    
    ## Changelog
    <!-- Is this a notable change that affects users? If so, add a line to
    `CHANGELOG.md` and prefix the line with one of the following:
        - [Added] for new features.
        - [Changed] for changes in existing functionality.
        - [Deprecated] for soon-to-be removed features.
        - [Removed] for now removed features.
        - [Fixed] for any bug fixes.
        - [Security] in case of vulnerabilities.
    -->

---
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/SavedPaymentMethodFormFactory/SavedPaymentMethodFormFactory+USBankAccount.swift
@@ -0,0 +1,32 @@
+//
+//  SavedPaymentMethodFormFactory+USBankAccount.swift
+//  StripePaymentSheet
+//
+//  Created by Joyce Qin on 11/22/24.
+//
+
+import Foundation
+@_spi(STP) import StripePayments
+@_spi(STP) import StripePaymentsUI
+@_spi(STP) import StripeUICore
+import UIKit
+
+extension SavedPaymentMethodFormFactory {
+    func makeUSBankAccount() -> UIView {
+        let nameElement: SectionElement = {
+            return SectionElement(elements: [TextFieldElement.NameConfiguration(defaultValue: viewModel.paymentMethod.billingDetails?.name, isEditable: false).makeElement(theme: viewModel.appearance.asElementsTheme)])
+        }()
+        let emailElement: SectionElement = {
+            return SectionElement(elements: [TextFieldElement.EmailConfiguration(defaultValue: viewModel.paymentMethod.billingDetails?.email, isEditable: false).makeElement(theme: viewModel.appearance.asElementsTheme)])
+        }()
+        let bankAccountElement: SectionElement = {
+            return SectionElement(elements: [TextFieldElement.USBankNumberConfiguration(bankName: viewModel.paymentMethod.usBankAccount?.bankName ?? "Bank name", lastFour: viewModel.paymentMethod.usBankAccount?.last4 ?? "").makeElement(theme: viewModel.appearance.asElementsTheme)])
+        }()
+        let stackView = UIStackView(arrangedSubviews: [nameElement.view, emailElement.view, bankAccountElement.view])
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.axis = .vertical
+        stackView.setCustomSpacing(8, after: nameElement.view) // custom spacing from figma
+        stackView.setCustomSpacing(8, after: emailElement.view) // custom spacing from figma
+        return stackView
+    }
+}
