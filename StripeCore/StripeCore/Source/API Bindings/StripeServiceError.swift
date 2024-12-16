4e195c688f3621445fc640f2ddbb48551c26d177
Mel <78050250+mludowise-stripe@users.noreply.github.com>
2024-12-16 11:22:53 -0800

    [Connect] Update SPI shape for returning from FinancialConnections (#4359)
    ## Summary
    
    The StripeConnect web layer requires the complete json object for the
    `BankToken` and `FinancialConnectionsSession` accounts. This change
    encodes these API objects to be compatible with their equivalent
    Stripe.js types and includes them in the webview JS message payload. To
    achieve this:
    - Some additional properties were added to the FinancialConnections
    types that are required by Stripe.js
    - The Stripe.js types use snake_case encoding while the Connect JS
    MessageSender uses camelCase, so custom key-encoding overrides were
    added to `MessageSender`
    - The `returnedFromFinancialConnections` message sender is replaced with
    a `setCollectMobileFinancialConnectionsResult` setter.
    
    Example message payload:
    ```
    {
        "setter": "setCollectMobileFinancialConnectionsResult",
        "value": {
            "financialConnectionsSession": {  // camelCase
                "accounts": [
                    {
                        "balance_refresh": ... // snake_case
                    }
                ]
            },
            token: {
                "account_holder_name": ... // snake_case
            }
        }
    }
    ```
    
    The payload of the messenger matches the following shape where
    [FinancialConnectionsSession.Account](https://github.com/stripe/stripe-js/blob/d212300e2796954faa7f3b4f916847f31b78be10/types/api/financial-connections.d.ts#L125),
    [BankAccountToken](https://github.com/stripe/stripe-js/blob/d212300e2796954faa7f3b4f916847f31b78be10/types/api/tokens.d.ts#L50-L52),
    and
    [StripeError](https://github.com/stripe/stripe-js/blob/d212300e2796954faa7f3b4f916847f31b78be10/types/stripe-js/stripe.d.ts#L1477)
    match the shapes defined in Stripe.js.
    ```
    {
        financialConnectionsSession?: {
            accounts: FinancialConnectionsSession.Account[];
        };
        token?: BankAccountToken;
        error?: StripeError
    }
    ```
    
    The payload contains the following values depending on the result of the
    FinanicalConnectionsSheet:
    - **Success:** 
      - `financialConnectionsSession.accounts`:  non-empty
      - `token`: non-null
      - `error`: null
    - **Canceled:**
      - `financialConnectionsSession.accounts`:  empty
      - `token`: null
      - `error`: null
    - **Error:**
      - `financialConnectionsSession.accounts`:  null
      - `token`: null
    - `error`: If the error is an API error, its value will be returned
    here. If it was a client-side error, the value returned will be null but
    the error will be logged to analytics.
    
    ## Motivation
    https://jira.corp.stripe.com/browse/MXMOBILE-2526
    
    ## Testing
    Unit tests
    
    Manual testing from @mxl-stripe against WIP web changes:
    
    
    https://github.com/user-attachments/assets/b7b5cc0d-a6c6-4bce-85ef-6457f75a982b
    
    
    ## Changelog
    n/a
    
    ---------
    
    Co-authored-by: Max Heller <mxl@stripe.com>

--- StripeCore/StripeCore/Source/API Bindings/StripeServiceError.swift
+++ StripeCore/StripeCore/Source/API Bindings/StripeServiceError.swift
@@ -11,7 +11,7 @@ import Foundation
 /// An error returned from the Stripe API.
 ///
 /// https://stripe.com/docs/api/errors
-@_spi(STP) public struct StripeAPIError: UnknownFieldsDecodable {
+@_spi(STP) public struct StripeAPIError: UnknownFieldsDecodable, Encodable, Equatable {
     /// The type of error returned.
     @_spi(STP) public var type: ErrorType
     /// For some errors that could be handled programmatically,
