734345ef251bff1adbd40c1df7c53a47f4954d21
Joyce Qin <joyceqin@stripe.com>
2024-12-17 15:32:14 -0800

    update snapshots

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/SavedPaymentOptionsViewControllerSnapshotTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/SavedPaymentOptionsViewControllerSnapshotTests.swift
@@ -43,7 +43,7 @@ final class SavedPaymentOptionsViewControllerSnapshotTests: STPSnapshotTestCase 
                                                     paymentSheetConfiguration: PaymentSheet.Configuration(),
                                                     intent: intent,
                                                     appearance: appearance,
-                                                    elementsSession: showDefaultPMBadge ? ._testDefaultCardValue(defaultPaymentMethod: paymentMethods.first?.stripeId ?? STPPaymentMethod._testCard().stripeId) : .emptyElementsSession,
+                                                    elementsSession: showDefaultPMBadge ? ._testDefaultCardValue(defaultPaymentMethod: paymentMethods.first?.stripeId ?? STPPaymentMethod._testCard().stripeId, paymentMethods: [testCardJSON, testUSBankAccountJSON, testSEPAJSON]) : .emptyElementsSession,
                                                     analyticsHelper: ._testValue())
         let testWindow = UIWindow()
         testWindow.isHidden = false
@@ -54,13 +54,59 @@ final class SavedPaymentOptionsViewControllerSnapshotTests: STPSnapshotTestCase 
         // Adding sut.view as the subview should be implied by the above line, but Autolayout can't lay out the view correctly on this pass of the runloop unless we explicitly addSubview. Maybe there are side effects that happen one turn of the runloop after setting the rootViewController.
         testWindow.addSubview(sut.view)
         sut.view.autosizeHeight(width: 1000)
+        if showDefaultPMBadge {
+            sut.isRemovingPaymentMethods = true
+        }
         NSLayoutConstraint.activate([
             sut.view.topAnchor.constraint(equalTo: testWindow.topAnchor),
             sut.view.leftAnchor.constraint(equalTo: testWindow.leftAnchor),
         ])
-        if showDefaultPMBadge {
-            sut.isRemovingPaymentMethods = true
-        }
         STPSnapshotVerifyView(sut.view)
     }
+
+    private let testCardJSON = [
+        "id": "pm_123card",
+        "type": "card",
+        "card": [
+            "last4": "4242",
+            "brand": "visa",
+            "fingerprint": "B8XXs2y2JsVBtB9f",
+            "networks": ["available": ["visa"]],
+            "exp_month": "01",
+            "exp_year": Calendar.current.component(.year, from: Date()) + 1
+        ]
+    ] as [AnyHashable : Any]
+    private let testUSBankAccountJSON = [
+        "id": "pm_123bank",
+        "type": "us_bank_account",
+        "us_bank_account": [
+            "account_holder_type": "individual",
+            "account_type": "checking",
+            "bank_name": "STRIPE TEST BANK",
+            "fingerprint": "ickfX9sbxIyAlbuh",
+            "last4": "6789",
+            "networks": [
+              "preferred": "ach",
+              "supported": [
+                "ach",
+              ],
+            ] as [String: Any],
+            "routing_number": "110000000",
+        ] as [String: Any],
+        "billing_details": [
+            "name": "Sam Stripe",
+            "email": "sam@stripe.com",
+        ] as [String: Any],
+    ] as [AnyHashable : Any]
+    private let testSEPAJSON = [
+        "id": "pm_123sepa",
+        "type": "sepa_debit",
+        "sepa_debit": [
+            "last4": "1234",
+        ],
+        "billing_details": [
+            "name": "Sam Stripe",
+            "email": "sam@stripe.com",
+        ] as [String: Any],
+    ] as [AnyHashable : Any]
 }
