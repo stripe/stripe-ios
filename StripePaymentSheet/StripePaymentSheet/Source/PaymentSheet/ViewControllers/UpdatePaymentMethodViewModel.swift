19b6115d3c22f7abe78642fd2b81cdd619884759
Joyce Qin <joyceqin@stripe.com>
2024-12-20 09:29:15 -0800

    moved computing disabled background color to UIColor extension

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/ViewControllers/UpdatePaymentMethodViewModel.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/ViewControllers/UpdatePaymentMethodViewModel.swift
@@ -12,43 +12,6 @@ import Foundation
 @_spi(STP) import StripeUICore
 import UIKit
 
-// calculate the resulting color of the mask overlaying the component background
-private func disabledBackgroundColor(componentBackground: UIColor) -> UIColor {
-    let alpha: CGFloat = 0.075
-    let colorMaskForLight = UIColor.black.withAlphaComponent(alpha)
-    let colorMaskForDark = UIColor.white.withAlphaComponent(alpha)
-
-    let lightModeComponentBackground = componentBackground.resolvedColor(with: UITraitCollection(userInterfaceStyle: .light))
-    let darkModeComponentBackground = componentBackground.resolvedColor(with: UITraitCollection(userInterfaceStyle: .dark))
-    return componentBackground.isBright
-        ? UIColor.dynamic(light: overlayColor(overlayColor: colorMaskForLight, baseColor: lightModeComponentBackground),
-                          dark: overlayColor(overlayColor: colorMaskForDark, baseColor: darkModeComponentBackground))
-        : UIColor.dynamic(light: overlayColor(overlayColor: colorMaskForDark, baseColor: darkModeComponentBackground),
-                          dark: overlayColor(overlayColor: colorMaskForLight, baseColor: lightModeComponentBackground))
-}
-
-private func overlayColor(overlayColor: UIColor, baseColor: UIColor) -> UIColor {
-    var r1: CGFloat = 0
-    var g1: CGFloat = 0
-    var b1: CGFloat = 0
-    var alpha1: CGFloat = 0
-    overlayColor.getRed(&r1, green: &g1, blue: &b1, alpha: &alpha1)
-
-    var r2: CGFloat = 0
-    var g2: CGFloat = 0
-    var b2: CGFloat = 0
-    var alpha2: CGFloat = 0
-    baseColor.getRed(&r2, green: &g2, blue: &b2, alpha: &alpha2)
-
-    let alphaResult = alpha1 + alpha2 * (1 - alpha1)
-
-    let rResult = (r1 * alpha1 + r2 * alpha2 * (1 - alpha1)) / alphaResult
-    let gResult = (g1 * alpha1 + g2 * alpha2 * (1 - alpha1)) / alphaResult
-    let bResult = (b1 * alpha1 + b2 * alpha2 * (1 - alpha1)) / alphaResult
-
-    return UIColor(red: rResult, green: gResult, blue: bResult, alpha: alphaResult)
-}
-
 class UpdatePaymentMethodViewModel {
     static let supportedPaymentMethods: [STPPaymentMethodType] = [.card, .USBankAccount, .SEPADebit]
 
@@ -94,7 +57,7 @@ class UpdatePaymentMethodViewModel {
         }
         self.paymentMethod = paymentMethod
         var disabledAppearance = appearance
-        disabledAppearance.colors.componentBackground = disabledBackgroundColor(componentBackground:    disabledAppearance.colors.componentBackground)
+        disabledAppearance.colors.componentBackground = disabledAppearance.colors.componentBackground.disabledBackgroundColor
         self.appearance = disabledAppearance
         self.hostedSurface = hostedSurface
         self.cardBrandFilter = cardBrandFilter
--- StripeUICore/StripeUICore/Source/Categories/UIColor+StripeUICore.swift
+++ StripeUICore/StripeUICore/Source/Categories/UIColor+StripeUICore.swift
@@ -139,6 +139,21 @@ import UIKit
         return self.withAlphaComponent(alpha * 0.4)
     }
 
+    /// Returns this color in a "disabled" state by overlaying a translucent mask
+    var disabledBackgroundColor: UIColor {
+        let alpha: CGFloat = 0.075
+        let colorMaskForLight = UIColor.black.withAlphaComponent(alpha)
+        let colorMaskForDark = UIColor.white.withAlphaComponent(alpha)
+
+        let lightModeComponentBackground = self.resolvedColor(with: UITraitCollection(userInterfaceStyle: .light))
+        let darkModeComponentBackground = self.resolvedColor(with: UITraitCollection(userInterfaceStyle: .dark))
+        return isBright
+            ? UIColor.dynamic(light: overlayColor(overlayColor: colorMaskForLight, baseColor: lightModeComponentBackground),
+                              dark: overlayColor(overlayColor: colorMaskForDark, baseColor: darkModeComponentBackground))
+            : UIColor.dynamic(light: overlayColor(overlayColor: colorMaskForDark, baseColor: darkModeComponentBackground),
+                              dark: overlayColor(overlayColor: colorMaskForLight, baseColor: lightModeComponentBackground))
+    }
+
     /// Returns this color in a "disabled" state by reducing the alpha by 40% if `isDisabled` is `true`,
     /// or the original color if `false`.
     func disabled(_ isDisabled: Bool = true) -> UIColor {
@@ -213,6 +228,28 @@ private extension UIColor {
                 alpha: alpha
             )
         })
+    }
+
+    /// Applies a mask over a base color
+    ///
+    /// - Parameters:
+    ///   - overlayColor: The color to overlay
+    ///   - baseColor: The base color
+    /// - Returns: The computed color of the overlay color on the base color.
+    func overlayColor(overlayColor: UIColor, baseColor: UIColor) -> UIColor {
+        var r1: CGFloat = 0, g1: CGFloat = 0, b1: CGFloat = 0, alpha1: CGFloat = 0
+        overlayColor.getRed(&r1, green: &g1, blue: &b1, alpha: &alpha1)
+
+        var r2: CGFloat = 0, g2: CGFloat = 0, b2: CGFloat = 0, alpha2: CGFloat = 0
+        baseColor.getRed(&r2, green: &g2, blue: &b2, alpha: &alpha2)
+
+        let alphaResult = alpha1 + alpha2 * (1 - alpha1)
+
+        let rResult = (r1 * alpha1 + r2 * alpha2 * (1 - alpha1)) / alphaResult
+        let gResult = (g1 * alpha1 + g2 * alpha2 * (1 - alpha1)) / alphaResult
+        let bResult = (b1 * alpha1 + b2 * alpha2 * (1 - alpha1)) / alphaResult
+
+        return UIColor(red: rResult, green: gResult, blue: bResult, alpha: alphaResult)
     }
 
 }
