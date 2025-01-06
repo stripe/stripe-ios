bc8df251515ddee2af3ab68ab1a5dab037f32795
David Estes <davidestes@stripe.com>
2025-01-06 13:45:57 -0800

    Remove unused code

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheet+Link.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheet+Link.swift
@@ -30,7 +30,7 @@ extension PaymentSheet: PayWithLinkWebControllerDelegate {
         }
     }
 
-    func payWithLinkWebControllerDidCancel(_ payWithLinkWebController: PayWithLinkWebController) {
+    func payWithLinkWebControllerDidCancel() {
     }
 }
 
