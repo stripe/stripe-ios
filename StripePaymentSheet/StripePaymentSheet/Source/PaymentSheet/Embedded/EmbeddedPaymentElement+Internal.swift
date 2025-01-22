eb7ac903833a60af141a2ac847135f2aeb675887
Yuki <yuki@stripe.com>
2025-01-22 15:23:14 -0800

    Don't assert presentingViewController/delegate are non-nil in Embeddded APIs that don't require them

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentElement+Internal.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentElement+Internal.swift
@@ -189,18 +189,6 @@ extension EmbeddedPaymentElement: EmbeddedPaymentMethodsViewDelegate {
         let bottomSheetVC = bottomSheetController(with: verticalSavedPaymentMethodsViewController)
         presentingViewController?.presentAsBottomSheet(bottomSheetVC, appearance: configuration.appearance)
     }
-
-    func verifyIntegration() {
-        guard delegate != nil else {
-            stpAssertionFailure("Delegate not set. Please set EmbeddedPaymentElement.delegate.")
-            return
-        }
-
-        guard presentingViewController != nil else {
-            stpAssertionFailure("Presenting view controller not found. Please set EmbeddedPaymentElement.presentingViewController.")
-            return
-        }
-    }
 }
 
 // MARK: UpdatePaymentMethodViewControllerDelegate
@@ -342,8 +330,6 @@ extension EmbeddedPaymentElement: EmbeddedFormViewControllerDelegate {
 extension EmbeddedPaymentElement {
 
     func _confirm() async -> (result: PaymentSheetResult, deferredIntentConfirmationType: STPAnalyticsClient.DeferredIntentConfirmationType?) {
-        verifyIntegration()
-
         guard !hasConfirmedIntent else {
             return (.failed(error: PaymentSheetError.embeddedPaymentElementAlreadyConfirmedIntent), STPAnalyticsClient.DeferredIntentConfirmationType.none)
         }
