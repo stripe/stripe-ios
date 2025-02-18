stripe/stripe-ios

commit b7b11b784ea7fa1fa158d1e561e18f4d7c36dffa

Set 'Change >' and sublabel to selected embedded form rows (#4538)
## Summary
Adds logic to set and unset the "Change >" + sublabel.

## Motivation
https://jira.corp.stripe.com/browse/MOBILESDK-3057

## Testing
See new tests

## Changelog
See changelog

StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentMethodsView.swift
@@ -38,19 +38,33 @@ class EmbeddedPaymentMethodsView: UIView {
     private let customer: PaymentSheet.CustomerConfiguration?
     private var previousSelectedRowButton: RowButton? {
         didSet {
-            previousSelectedRowButton?.isSelected = false
+            guard let previousSelectedRowButton, selectedRowButton?.type != previousSelectedRowButton.type else {
+                return
+            }
+            previousSelectedRowButton.isSelected = false
+            // Clear out the 'Change >' button and any sublabel (eg 4242) we set for new PM rows
+            switch previousSelectedRowButton.type {
+            case .new(paymentMethodType: let paymentMethodType):
+                let isCardOrUSBankAccount = paymentMethodType == .stripe(.card) || paymentMethodType == .stripe(.USBankAccount)
+                previousSelectedRowButton.removeChangeButton(shouldClearSublabel: isCardOrUSBankAccount)
+            default:
+               break
+            }
         }
     }
     private(set) var selectedRowButton: RowButton? {
         didSet {
             previousSelectedRowButton = oldValue
-            selectedRowButton?.isSelected = true
-            updateMandate()
             if oldValue?.type != selectedRowButton?.type {
                 delegate?.embeddedPaymentMethodsViewDidUpdateSelection()
             }
+            if let selectedRowButton {
+                selectedRowButton.isSelected = true
+            }
+            updateMandate()
         }
     }
+    
     private let mandateProvider: MandateTextProvider
     private let shouldShowMandate: Bool
     private let analyticsHelper: PaymentSheetAnalyticsHelper
@@ -376,7 +390,7 @@ class EmbeddedPaymentMethodsView: UIView {
     func makePaymentMethodRowButton(paymentMethodType: PaymentSheet.PaymentMethodType, savedPaymentMethods: [STPPaymentMethod]) -> RowButton {
         // We always add a hidden accessory button ("Change >") so we can show/hide it easily
         let accessoryButton = RowButton.RightAccessoryButton(
-            accessoryType: appearance.embeddedPaymentElement.row.style == .flatWithCheckmark ? .changeWithChevron : .change,
+            accessoryType: appearance.embeddedPaymentElement.row.style == .flatWithCheckmark ? .change : .changeWithChevron,
             appearance: appearance,
             didTap: { [weak self] in
                 guard let self, let selectedRowButton else { return }
@@ -430,5 +444,24 @@ extension PaymentSheet.Appearance.EmbeddedPaymentElement.Row.Style {
 extension Array where Element == STPPaymentMethod {
     var hasSavedCard: Bool {
         return !self.filter { $0.type == .card }.isEmpty
+    }
+}
+
+extension RowButton {
+    func addChangeButton(sublabel: String?) {
+        rightAccessoryView?.isHidden = false
+        if let sublabel {
+            self.sublabel.text = sublabel
+            self.sublabel.isHidden = sublabel.isEmpty
+        }
+        makeSameHeightAsOtherRowButtonsIfNecessary()
+    }
+    
+    func removeChangeButton(shouldClearSublabel: Bool) {
+        rightAccessoryView?.isHidden = true
+        if shouldClearSublabel {
+            sublabel.text = nil
+            sublabel.isHidden = true
+        }
     }
 }

StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/EmbeddedPaymentElementTest.swift
@@ -6,6 +6,7 @@
 //
 
 @testable@_spi(STP) import StripeCore
+@testable@_spi(STP) import StripeUICore
 import StripeCoreTestUtils
 @testable@_spi(STP) import StripePaymentsTestUtils
 import XCTest
@@ -458,6 +459,50 @@ class EmbeddedPaymentElementTest: XCTestCase {
         // Inject the test payment option and assert the label
         sut._test_paymentOption = .saved(paymentMethod: ._testCard(), confirmParams: confirmParams)
         XCTAssertEqual(sut.paymentOption?.label, "••••6789")
+    }
+    
+    func testChangeButtonStateRespectsCardBrandChoice() async throws {
+        // Given an EmbeddedPaymentElement w/ CBC enabled...
+        let intentConfig = PaymentSheet.IntentConfiguration(mode: .payment(amount: 1000, currency: "USD")) { _, _, _ in }
+        let elementsSession = STPElementsSession._testValue(cardBrandChoice: ._testValue())
+        let intent = Intent.deferredIntent(intentConfig: intentConfig)
+        let loadResult = PaymentSheetLoader.LoadResult(
+            intent: intent,
+            elementsSession: elementsSession,
+            savedPaymentMethods: [],
+            paymentMethodTypes: [.stripe(.card)]
+        )
+        let sut = EmbeddedPaymentElement(
+            configuration: configuration,
+            loadResult: loadResult,
+            analyticsHelper: ._testValue()
+        )
+        sut.delegate = self
+        sut.presentingViewController = UIViewController()
+        sut.view.autosizeHeight(width: 320)
+        await AddressSpecProvider.shared.loadAddressSpecs()
+        await FormSpecProvider.shared.load()
+        // ...with card selected...
+        let cardRowButton = sut.embeddedPaymentMethodsView.getRowButton(accessibilityIdentifier: "Card")
+        sut.embeddedPaymentMethodsView.didTap(rowButton: cardRowButton)
+        // ...and filling out the form using a CBC-enabled card...
+        let cardForm = sut.formCache[.stripe(.card)]!
+        cardForm.getTextFieldElement("Card number").setText("4000002500001001")
+        cardForm.getTextFieldElement("MM / YY").setText("1232")
+        cardForm.getTextFieldElement("CVC").setText("123")
+        cardForm.getTextFieldElement("ZIP").setText("65432")
+        sut.selectedFormViewController?.didTapPrimaryButton()
+        // ...the change button state label (the label that appears on the selected row) should read ****1001 w/o a network (b/c no network was selected)...
+        var changeButtonState = sut.getChangeButtonState(for: .new(paymentMethodType: .stripe(.card)))
+        XCTAssertEqual(changeButtonState.sublabel, "•••• 1001")
+        // ...and setting a preferred network (ie what happens if you select a brand from the dropdown)...
+        // Hack: Since the dropdown field isn't properly hooked up to the Element hierarchy, we can't access it via `cardForm.getDropdownFieldElement`
+        // TODO(https://jira.corp.stripe.com/browse/MOBILESDK-3088): Make the CBC dropdown field participate in the Element hierarchy correctly!
+        let cbcDropdown = (cardForm.getTextFieldElement("Card number").configuration as! TextFieldElement.PANConfiguration).cardBrandDropDown
+        cbcDropdown?.selectedIndex = 1
+        // ...the label should read "Cartes Bancaire ****1001"
+        changeButtonState = sut.getChangeButtonState(for: .new(paymentMethodType: .stripe(.card)))
+        XCTAssertEqual(changeButtonState.sublabel, "Cartes Bancaires •••• 1001")
     }
 }
 

StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/EmbeddedPaymentMethodsViewTests.swift
@@ -50,13 +50,13 @@ final class EmbeddedPaymentMethodsViewTests: XCTestCase {
 
         // Simulate tapping Cash App and verify delegate is called
         embeddedView.didTap(rowButton: embeddedView.getRowButton(accessibilityIdentifier: "Cash App Pay"))
-        XCTAssertEqual(mockDelegate.calls, [.didUpdateHeight, .didUpdateSelection, .didTapPaymentMethodRow])
+        XCTAssertEqual(mockDelegate.calls, [.didUpdateSelection, .didUpdateHeight, .didTapPaymentMethodRow])
         XCTAssertEqual(embeddedView.selectedRowButton?.type, .new(paymentMethodType: .stripe(.cashApp)), "Cash App Pay should be the current selection")
         mockDelegate.calls = []
 
         // Simulate tapping Klarna and verify delegate is called
         embeddedView.didTap(rowButton: embeddedView.getRowButton(accessibilityIdentifier: "Klarna"))
-        XCTAssertEqual(mockDelegate.calls, [.didUpdateHeight, .didUpdateSelection, .didTapPaymentMethodRow])
+        XCTAssertEqual(mockDelegate.calls, [.didUpdateSelection, .didUpdateHeight, .didTapPaymentMethodRow])
         XCTAssertEqual(embeddedView.selectedRowButton?.type, .new(paymentMethodType: .stripe(.klarna)), "Klarna should be the current selection")
         mockDelegate.calls = []
 
@@ -65,7 +65,7 @@ final class EmbeddedPaymentMethodsViewTests: XCTestCase {
         // ...should go back to Cash App
         XCTAssertEqual(embeddedView.selectedRowButton?.type, .new(paymentMethodType: .stripe(.cashApp)))
         // ...and call/not call the various delegate methods
-        XCTAssertEqual(mockDelegate.calls, [.didUpdateHeight, .didUpdateSelection])
+        XCTAssertEqual(mockDelegate.calls, [.didUpdateSelection, .didUpdateHeight])
         mockDelegate.calls = []
 
         // Resetting...
@@ -73,12 +73,12 @@ final class EmbeddedPaymentMethodsViewTests: XCTestCase {
         // ...should go to nil
         XCTAssertNil(embeddedView.selectedRowButton)
         // ...and call/not call the various delegate methods
-        XCTAssertEqual(mockDelegate.calls, [.didUpdateHeight, .didUpdateSelection])
+        XCTAssertEqual(mockDelegate.calls, [.didUpdateSelection, .didUpdateHeight])
         mockDelegate.calls = []
 
         // Simulate tapping PayPal and verify delegate is called
         embeddedView.didTap(rowButton: embeddedView.getRowButton(accessibilityIdentifier: "PayPal"))
-        XCTAssertEqual(mockDelegate.calls, [.didUpdateHeight, .didUpdateSelection, .didTapPaymentMethodRow])
+        XCTAssertEqual(mockDelegate.calls, [.didUpdateSelection, .didUpdateHeight, .didTapPaymentMethodRow])
         XCTAssertEqual(embeddedView.selectedRowButton?.type, .new(paymentMethodType: .stripe(.payPal)), "PayPal should be the current selection")
         mockDelegate.calls = []
 

StripePaymentSheet/StripePaymentSheet/Source/Internal/API Bindings/v1-elements-sessions/STPCardBrandChoice.swift
@@ -36,7 +36,7 @@ class STPCardBrandChoice: NSObject {
         return "<\(props.joined(separator: "; "))>"
     }
 
-    private init(
+   required init(
         eligible: Bool,
         preferredNetworks: [String],
         supportedCobrandedNetworks: [String: Bool],

StripePaymentSheet/StripePaymentSheet/Source/Internal/API Bindings/v1-elements-sessions/STPElementsSession.swift
@@ -53,7 +53,7 @@ final class STPElementsSession: NSObject {
 
     let allResponseFields: [AnyHashable: Any]
 
-    private init(
+    internal init(
         allResponseFields: [AnyHashable: Any],
         sessionID: String,
         orderedPaymentMethodTypes: [STPPaymentMethodType],

StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentElement+Internal.swift
@@ -331,13 +331,59 @@ extension EmbeddedPaymentElement: EmbeddedFormViewControllerDelegate {
         // If the formViewController was populated with a previous payment option don't reset
         if embeddedFormViewController.previousPaymentOption == nil {
             embeddedPaymentMethodsView.resetSelectionToLastSelection()
+            // Show change button if the newly selected row needs it
+            if let newSelectedType = embeddedPaymentMethodsView.selectedRowButton?.type {
+                let changeButtonState = getChangeButtonState(for: newSelectedType)
+                if changeButtonState.shouldShowChangeButton {
+                    embeddedPaymentMethodsView.selectedRowButton?.addChangeButton(sublabel: changeButtonState.sublabel)
+                }
+            }
         }
         embeddedFormViewController.dismiss(animated: true)
     }
 
     func embeddedFormViewControllerDidContinue(_ embeddedFormViewController: EmbeddedFormViewController) {
+        // Show change button if the selected row needs it
+        if let newSelectedType = embeddedPaymentMethodsView.selectedRowButton?.type {
+            let changeButtonState = getChangeButtonState(for: newSelectedType)
+            if changeButtonState.shouldShowChangeButton {
+                embeddedPaymentMethodsView.selectedRowButton?.addChangeButton(sublabel: changeButtonState.sublabel)
+            }
+        }
         embeddedFormViewController.dismiss(animated: true)
         informDelegateIfPaymentOptionUpdated()
+    }
+    
+    func getChangeButtonState(for type: RowButtonType) -> (shouldShowChangeButton: Bool, sublabel: String?) {
+        guard let _paymentOption, let displayData = paymentOption else {
+            return (false, nil)
+        }
+        // Show change button for new PMs that have a valid form
+        let shouldShowChangeButton: Bool = {
+            if case .new = type, selectedFormViewController != nil {
+               return true
+            }
+            return false
+        }()
+        
+        // Add a sublabel to the selected row for cards and us bank account like "Visa 4242"
+        let sublabel: String? = {
+            switch type.paymentMethodType {
+            case .stripe(.card):
+                guard case .new(confirmParams: let params) = _paymentOption else {
+                    return nil
+                }
+                let brand = STPCardValidator.brand(for: params.paymentMethodParams.card)
+                let brandString = brand == .unknown ? nil : STPCardBrandUtilities.stringFrom(brand)
+                return [brandString, displayData.label].compactMap({ $0 }).joined(separator: " ")
+            case .stripe(.USBankAccount):
+                return displayData.label
+            default:
+                return nil
+            }
+        }()
+        
+        return (shouldShowChangeButton: shouldShowChangeButton, sublabel: sublabel)
     }
 }
 

StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentElement.swift
@@ -311,7 +311,7 @@ public final class EmbeddedPaymentElement {
 
     internal private(set) lazy var paymentHandler: STPPaymentHandler = STPPaymentHandler(apiClient: configuration.apiClient)
 
-    private init(
+    internal init(
         configuration: Configuration,
         loadResult: PaymentSheetLoader.LoadResult,
         analyticsHelper: PaymentSheetAnalyticsHelper

StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentOption+Images.swift
@@ -129,16 +129,7 @@ extension STPPaymentMethod {
     func makeIcon(updateHandler: DownloadManager.UpdateImageHandler?) -> UIImage {
         switch type {
         case .card:
-            guard let card = card, let number = card.number else {
-                return STPImageLibrary.unknownCardCardImage()
-            }
-
-            var brand = STPCardValidator.brand(forNumber: number)
-            // Handle co-banded cards for flow controller
-            if let networks = card.networks {
-                brand = networks.preferred?.toCardBrand ?? .unknown
-            }
-
+            let brand = STPCardValidator.brand(for: card)
             return STPImageLibrary.cardBrandImage(for: brand)
         default:
             // If there's no image specific to this PaymentMethod (eg card network logo, bank logo), default to the PaymentMethod type's icon

commit b85ffc6937368b54e107242c7281a996c34cc78e

Update mandate before notifying delegate (#4573)
## Summary
- Fixes a bug introduced in
https://github.com/stripe/stripe-ios/pull/4538 where we update the
mandate after notifying the delegate the payment option has changed
- This bug was only evident in our playground example app when you would
have the merchant display the mandate not us
- This resulted in a nil mandateText being provided on our payment
option to merchants

## Motivation
- 🐛 

## Testing
- Manual
- New test

## Changelog
N/A

StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentMethodsView.swift
@@ -56,6 +56,7 @@ class EmbeddedPaymentMethodsView: UIView {
         didSet {
             previousSelectedRowButton = oldValue
             let selectedRowButtonTypeDidChange = oldValue?.type != selectedRowButton?.type
+            updateMandate()
             if selectedRowButtonTypeDidChange {
                 selectedRowChangeButtonState = nil
                 delegate?.embeddedPaymentMethodsViewDidUpdateSelection()
@@ -63,7 +64,6 @@ class EmbeddedPaymentMethodsView: UIView {
             if let selectedRowButton {
                 selectedRowButton.isSelected = true
             }
-            updateMandate()
         }
     }
 

StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/EmbeddedPaymentElementTest.swift
@@ -16,6 +16,8 @@ import XCTest
 @MainActor
 // https://jira.corp.stripe.com/browse/MOBILESDK-2607 Make these STPNetworkStubbingTestCase; blocked on getting them to record image requests
 class EmbeddedPaymentElementTest: XCTestCase {
+    var delegatePaymentOption: EmbeddedPaymentElement.PaymentOptionDisplayData?
+
     lazy var configuration: EmbeddedPaymentElement.Configuration = {
         var config = EmbeddedPaymentElement.Configuration._testValue_MostPermissive(isApplePayEnabled: false)
         config.apiClient = STPAPIClient(publishableKey: STPTestingDefaultPublishableKey)
@@ -40,7 +42,7 @@ class EmbeddedPaymentElementTest: XCTestCase {
     let paymentIntentConfig2 = EmbeddedPaymentElement.IntentConfiguration(mode: .payment(amount: 999, currency: "USD"), paymentMethodTypes: ["card", "cashapp"]) { _, _, _ in
         // These tests don't confirm, so this is unused
     }
-    let setupIntentConfig = EmbeddedPaymentElement.IntentConfiguration(mode: .setup(setupFutureUsage: .offSession), paymentMethodTypes: ["card", "cashapp"]) { _, _, _ in
+    let setupIntentConfig = EmbeddedPaymentElement.IntentConfiguration(mode: .setup(setupFutureUsage: .offSession), paymentMethodTypes: ["card", "cashapp", "amazon_pay"]) { _, _, _ in
         // These tests don't confirm, so this is unused
     }
     var delegateDidUpdatePaymentOptionCalled = false
@@ -297,6 +299,31 @@ class EmbeddedPaymentElementTest: XCTestCase {
         }
     }
 
+    func testPaymentOptionDisplayData() async throws {
+        // Given a EmbeddedPaymentElement instance...
+        let sut = try await EmbeddedPaymentElement.create(intentConfiguration: setupIntentConfig, configuration: configuration)
+        sut.delegate = self
+        sut.presentingViewController = UIViewController()
+        sut.view.autosizeHeight(width: 320)
+
+        // Initially, no paymentOption should be selected
+        XCTAssertNil(sut.paymentOption)
+
+        // Select the "Cash App Pay" payment method
+        sut.embeddedPaymentMethodsView.didTap(rowButton: sut.embeddedPaymentMethodsView.getRowButton(accessibilityIdentifier: "Cash App Pay"))
+        // The delegate should have been notified with proper data
+        XCTAssertEqual(delegatePaymentOption?.label, "Cash App Pay")
+        XCTAssertEqual(delegatePaymentOption?.paymentMethodType, "cashapp")
+        XCTAssertTrue(delegatePaymentOption?.mandateText?.string.contains("Cash App") ?? false)
+
+        // Tap another row, payment option data should be updated
+        sut.embeddedPaymentMethodsView.didTap(rowButton: sut.embeddedPaymentMethodsView.getRowButton(accessibilityIdentifier: "Amazon Pay"))
+        // The delegate should have been notified with proper data
+        XCTAssertEqual(delegatePaymentOption?.label, "Amazon Pay")
+        XCTAssertEqual(delegatePaymentOption?.paymentMethodType, "amazon_pay")
+        XCTAssertTrue(delegatePaymentOption?.mandateText?.string.contains("Amazon Pay") ?? false)
+    }
+
     func testClearPaymentOptionAfterSelection() async throws {
         // Given a EmbeddedPaymentElement instance...
         let sut = try await EmbeddedPaymentElement.create(intentConfiguration: paymentIntentConfig, configuration: configuration)
@@ -512,6 +539,7 @@ extension EmbeddedPaymentElementTest: EmbeddedPaymentElementDelegate {
     }
 
     func embeddedPaymentElementDidUpdatePaymentOption(embeddedPaymentElement: StripePaymentSheet.EmbeddedPaymentElement) {
+        delegatePaymentOption = embeddedPaymentElement.paymentOption
         delegateDidUpdatePaymentOptionCalled = true
     }
 

StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/EmbeddedPaymentMethodsViewTests.swift
@@ -50,13 +50,13 @@ final class EmbeddedPaymentMethodsViewTests: XCTestCase {
 
         // Simulate tapping Cash App and verify delegate is called
         embeddedView.didTap(rowButton: embeddedView.getRowButton(accessibilityIdentifier: "Cash App Pay"))
-        XCTAssertEqual(mockDelegate.calls, [.didUpdateSelection, .didUpdateHeight, .didTapPaymentMethodRow])
+        XCTAssertEqual(mockDelegate.calls, [.didUpdateHeight, .didUpdateSelection, .didTapPaymentMethodRow])
         XCTAssertEqual(embeddedView.selectedRowButton?.type, .new(paymentMethodType: .stripe(.cashApp)), "Cash App Pay should be the current selection")
         mockDelegate.calls = []
 
         // Simulate tapping Klarna and verify delegate is called
         embeddedView.didTap(rowButton: embeddedView.getRowButton(accessibilityIdentifier: "Klarna"))
-        XCTAssertEqual(mockDelegate.calls, [.didUpdateSelection, .didUpdateHeight, .didTapPaymentMethodRow])
+        XCTAssertEqual(mockDelegate.calls, [.didUpdateHeight, .didUpdateSelection, .didTapPaymentMethodRow])
         XCTAssertEqual(embeddedView.selectedRowButton?.type, .new(paymentMethodType: .stripe(.klarna)), "Klarna should be the current selection")
         mockDelegate.calls = []
 
@@ -65,7 +65,7 @@ final class EmbeddedPaymentMethodsViewTests: XCTestCase {
         // ...should go back to Cash App
         XCTAssertEqual(embeddedView.selectedRowButton?.type, .new(paymentMethodType: .stripe(.cashApp)))
         // ...and call/not call the various delegate methods
-        XCTAssertEqual(mockDelegate.calls, [.didUpdateSelection, .didUpdateHeight])
+        XCTAssertEqual(mockDelegate.calls, [.didUpdateHeight, .didUpdateSelection])
         mockDelegate.calls = []
 
         // Resetting...
@@ -73,12 +73,12 @@ final class EmbeddedPaymentMethodsViewTests: XCTestCase {
         // ...should go to nil
         XCTAssertNil(embeddedView.selectedRowButton)
         // ...and call/not call the various delegate methods
-        XCTAssertEqual(mockDelegate.calls, [.didUpdateSelection, .didUpdateHeight])
+        XCTAssertEqual(mockDelegate.calls, [.didUpdateHeight, .didUpdateSelection, ])
         mockDelegate.calls = []
 
         // Simulate tapping PayPal and verify delegate is called
         embeddedView.didTap(rowButton: embeddedView.getRowButton(accessibilityIdentifier: "PayPal"))
-        XCTAssertEqual(mockDelegate.calls, [.didUpdateSelection, .didUpdateHeight, .didTapPaymentMethodRow])
+        XCTAssertEqual(mockDelegate.calls, [.didUpdateHeight, .didUpdateSelection, .didTapPaymentMethodRow])
         XCTAssertEqual(embeddedView.selectedRowButton?.type, .new(paymentMethodType: .stripe(.payPal)), "PayPal should be the current selection")
         mockDelegate.calls = []
 