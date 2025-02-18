7664dcc0a20ba6ff816efbb69511d3613a72b990
Mat Schmid <mats@stripe.com>
2025-02-18 11:00:07 -0500

    Update FC Configuration to being immutable and public (#4561)
    Reference:
    https://docs.google.com/document/d/1pjaQA4LM3NVjC2PFr7_rgGleymHHbE_RTudNEgih5jw/edit?tab=t.0
    
    ## Summary
    
    This updates the `Configuration` property on `FinancialConnectionsSheet`
    from being mutable to immutable. We don't want the configuration to
    change mid flow, so it should be passed in on initialization and kept
    unchanged afterwards.
    
    ## Motivation
    
    Strive for immutability
    
    ## Testing
    
    Manually tested
    
    ## Changelog
    
    Same changelog message as https://github.com/stripe/stripe-ios/pull/4545

--- Example/FinancialConnections Example/FinancialConnections Example/Playground/PlaygroundViewModel.swift
+++ Example/FinancialConnections Example/FinancialConnections Example/Playground/PlaygroundViewModel.swift
@@ -155,7 +155,7 @@ final class PlaygroundViewModel: ObservableObject {
             }
         )
     }
-    
+
     var customerId: Binding<String> {
         Binding(
             get: {
@@ -167,7 +167,7 @@ final class PlaygroundViewModel: ObservableObject {
             }
         )
     }
-    
+
     var relinkAuthorization: Binding<String> {
         Binding(
             get: {
@@ -561,16 +561,16 @@ private func PresentFinancialConnectionsSheet(
     STPAPIClient.shared.publishableKey = publishableKey
 
     let isUITest = (ProcessInfo.processInfo.environment["UITesting"] != nil)
+    var configuration = FinancialConnectionsSheet.Configuration()
+    configuration.style = style.configurationValue
     let financialConnectionsSheet = FinancialConnectionsSheet(
         financialConnectionsSessionClientSecret: clientSecret,
         // disable app-to-app for UI tests
-        returnURL: isUITest ? nil : "financial-connections-example://redirect"
+        returnURL: isUITest ? nil : "financial-connections-example://redirect",
+        configuration: configuration
     )
     financialConnectionsSheet.apiClient.stripeAccount = stripeAccount
     financialConnectionsSheet.onEvent = onEvent
-    var configuration = FinancialConnectionsSheet.Configuration()
-    configuration.style = style.configurationValue
-    financialConnectionsSheet.configuration = configuration
     let topMostViewController = UIViewController.topMostViewController()!
     if useCase == .token {
         financialConnectionsSheet.presentForToken(
