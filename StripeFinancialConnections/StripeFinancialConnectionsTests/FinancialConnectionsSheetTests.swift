5f2884e2f12b5c8762f855dea2968b6f311495f5
Mat Schmid <mats@stripe.com>
2025-03-04 13:16:27 -0500

    Add async version of FinancialConnectionsSheet `present` and `presentForToken` (#4621)
    Part of
    https://docs.google.com/document/d/1_McNEkaB4HvXnl0eksjdgzcszUjK3y1Tsh-A8s_Rey0/edit?usp=sharing
    
    ## Summary
    
    Adds async versions of FC's entry points `present(from:)` and
    `presentForToken(from:)`. These are marked as `@MainActor`.
    
    ## Motivation
    
    Async Everywhere
    
    ## Testing
    
    Unit tests added, and manual testing in the example app for the token
    use case:
    
    
    https://github.com/user-attachments/assets/820355bf-75b0-4429-96c8-f0ea5982c20a
    
    ## Changelog
    
    Included as part of v25 changelog: 
    
    ```
    ### Financial Connections
    * [Added] Added an async versions of `present(from:)` and `presentForToken(from:)`.
    ```

--- StripeFinancialConnections/StripeFinancialConnectionsTests/FinancialConnectionsSheetTests.swift
+++ StripeFinancialConnections/StripeFinancialConnectionsTests/FinancialConnectionsSheetTests.swift
@@ -7,7 +7,7 @@
 
 @_spi(STP) import StripeCore
 @_spi(STP) import StripeCoreTestUtils
-@testable @_spi(STP) import StripeFinancialConnections
+@testable @_spi(STP) @_spi(v25) import StripeFinancialConnections
 import XCTest
 
 class EmptySessionFetcher: FinancialConnectionsSessionFetcher {
@@ -27,6 +27,82 @@ class FinancialConnectionsSheetTests: XCTestCase {
     override func setUpWithError() throws {
         try super.setUpWithError()
         mockAnalyticsClient.reset()
+    }
+
+    func testPresentCompletion() {
+        let sheet = FinancialConnectionsSheet(
+            financialConnectionsSessionClientSecret: mockClientSecret,
+            returnURL: nil,
+            configuration: .init(),
+            analyticsClient: mockAnalyticsClient
+        )
+
+        let expectation = expectation(description: "presentation completed")
+        sheet.present(from: mockViewController) { (result: FinancialConnectionsSheet.Result) in
+            guard case .canceled = result else {
+                XCTFail("Unexpected result: \(result)")
+                return
+            }
+            expectation.fulfill()
+        }
+
+        // Mock that financialConnections is completed
+        let host = HostController(
+            apiClient: mockApiClient,
+            analyticsClientV1: mockAnalyticsClient,
+            clientSecret: "test",
+            returnURL: nil,
+            configuration: .init(),
+            elementsSessionContext: nil,
+            publishableKey: "test",
+            stripeAccount: nil
+        )
+        sheet.hostController(
+            host,
+            viewController: mockViewController,
+            didFinish: .canceled,
+            linkAccountSessionId: "fcsess_123"
+        )
+
+        wait(for: [expectation], timeout: 5.0)
+    }
+
+    func testAsyncPresentCompletion() async {
+        let sheet = FinancialConnectionsSheet(
+            financialConnectionsSessionClientSecret: mockClientSecret,
+            returnURL: nil,
+            configuration: .init(),
+            analyticsClient: mockAnalyticsClient
+        )
+
+        async let result = sheet.present(from: mockViewController)
+
+        // Mock that financialConnections is completed
+        let host = HostController(
+            apiClient: mockApiClient,
+            analyticsClientV1: mockAnalyticsClient,
+            clientSecret: "test",
+            returnURL: nil,
+            configuration: .init(),
+            elementsSessionContext: nil,
+            publishableKey: "test",
+            stripeAccount: nil
+        )
+
+        // Ensure this is called on the main thread
+        await MainActor.run {
+            sheet.hostController(
+                host,
+                viewController: mockViewController,
+                didFinish: .canceled,
+                linkAccountSessionId: "fcsess_123"
+            )
+        }
+
+        guard case .canceled = await result else {
+            XCTFail("Unexpected result: \(await result)")
+            return
+        }
     }
 
     func testAnalytics() {
