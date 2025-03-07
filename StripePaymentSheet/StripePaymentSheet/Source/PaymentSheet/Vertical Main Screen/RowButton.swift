1a56df901111c85c222cb309107ab373000a7efe
Nick Porter <porter@stripe.com>
2025-03-07 13:07:40 -0700

    Rename componentText to contrastMatchingColor

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Vertical Main Screen/RowButton.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Vertical Main Screen/RowButton.swift
@@ -402,7 +402,7 @@ extension RowButton {
         isEmbedded: Bool = false,
         didTap: @escaping DidTapClosure
     ) -> RowButton {
-        let imageView = PaymentMethodTypeImageView(paymentMethodType: paymentMethodType, componentText: appearance.colors.componentText)
+        let imageView = PaymentMethodTypeImageView(paymentMethodType: paymentMethodType, contrastMatchingColor: appearance.colors.componentText)
         imageView.contentMode = .scaleAspectFit
 
         // Special case "New card" vs "Card" title
