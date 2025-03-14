5afc868839a306859814a40d77e9b187ae8793a6
Joyce Qin <joyceqin@stripe.com>
2025-03-14 14:54:17 -0700

    test

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/SavedPaymentOptionsViewControllerTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/SavedPaymentOptionsViewControllerTests.swift
@@ -293,8 +293,18 @@ class SavedPaymentOptionsViewControllerTests: XCTestCase {
         XCTAssertTrue(controller.canEditPaymentMethods)
     }
 
+    func testCanEditPaymentMethods_OnePM_remove0_default1_notCBCEligible() {
+        let configuration = savedPaymentOptionsConfig(allowsRemovalOfLastSavedPaymentMethod: false,
+                                                      allowsRemovalOfPaymentMethods: false,
+                                                      allowsSetAsDefaultPaymentMethod: true)
+        let controller = savedPaymentOptionsController(configuration,
+                                                       savedPaymentMethods: [STPPaymentMethod._testCard()],
+                                                       cbcEligible: false)
+        XCTAssertTrue(controller.canEditPaymentMethods)
+    }
+
     // MARK: Helpers
-    func savedPaymentOptionsConfig(allowsRemovalOfLastSavedPaymentMethod: Bool, allowsRemovalOfPaymentMethods: Bool) -> SavedPaymentOptionsViewController.Configuration {
+    func savedPaymentOptionsConfig(allowsRemovalOfLastSavedPaymentMethod: Bool, allowsRemovalOfPaymentMethods: Bool, allowsSetAsDefaultPaymentMethod: Bool = false) -> SavedPaymentOptionsViewController.Configuration {
         return SavedPaymentOptionsViewController.Configuration(customerID: "cus_123",
                                                                showApplePay: true,
                                                                showLink: true,
@@ -304,7 +314,7 @@ class SavedPaymentOptionsViewControllerTests: XCTestCase {
                                                                isTestMode: true,
                                                                allowsRemovalOfLastSavedPaymentMethod: allowsRemovalOfLastSavedPaymentMethod,
                                                                allowsRemovalOfPaymentMethods: allowsRemovalOfPaymentMethods,
-                                                               allowsSetAsDefaultPM: false)
+                                                               allowsSetAsDefaultPM: allowsSetAsDefaultPaymentMethod)
     }
 
     func savedPaymentOptionsController(_ configuration: SavedPaymentOptionsViewController.Configuration,
