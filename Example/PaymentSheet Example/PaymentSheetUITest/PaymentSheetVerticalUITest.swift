2802d94cfcfae7868e6bc566cbc112e58303717f
Nick Porter <88012362+porter-stripe@users.noreply.github.com>
2025-03-17 17:25:31 -0600

    Remove shipping required for Afterpay (#4669)
    ## Summary
    - Afterpay no longer requires shipping details to be confirmed
    
    ## Motivation
    - https://jira.corp.stripe.com/browse/RUN_MOBILESDK-3945
    
    ## Testing
    - Manually tested without shipping details
    - Added a new unit test
    
    ## Changelog
    N/A

--- Example/PaymentSheet Example/PaymentSheetUITest/PaymentSheetVerticalUITest.swift
+++ Example/PaymentSheet Example/PaymentSheetUITest/PaymentSheetVerticalUITest.swift
@@ -451,6 +451,7 @@ class PaymentSheetVerticalUITests: PaymentSheetUITestCase {
         app.buttons["Payment method"].waitForExistenceAndTap()
         app.buttons["Back"].waitForExistenceAndTap()
         app.buttons["Alipay"].waitForExistenceAndTap()
+        app.swipeUp() // scroll to see the Continue button
         app.buttons["Continue"].waitForExistenceAndTap()
         XCTAssertEqual(app.buttons["Payment method"].label, "Alipay, alipay")
         // ...and *updating* to a SetupIntent...
