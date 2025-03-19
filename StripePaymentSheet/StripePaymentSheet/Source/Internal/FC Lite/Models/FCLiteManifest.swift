dc871bb148904f7b9351f8622e3d27057bbd4a6b
Mat Schmid <mats@stripe.com>
2025-03-19 14:27:03 -0400

    Add web auth flow view controller

--- StripePaymentSheet/StripePaymentSheet/Source/Internal/FC Lite/Models/FCLiteManifest.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/Internal/FC Lite/Models/FCLiteManifest.swift
@@ -24,6 +24,20 @@ struct LinkAccountSessionManifest: Decodable {
         product == "instant_debits"
     }
 
+    init(
+        id: String,
+        hostedAuthURL: URL,
+        successURL: URL,
+        cancelURL: URL,
+        product: String
+    ) {
+        self.id = id
+        self.hostedAuthURL = hostedAuthURL
+        self.successURL = successURL
+        self.cancelURL = cancelURL
+        self.product = product
+    }
+
     enum CodingKeys: String, CodingKey {
         case id
         case hostedAuthURL = "hosted_auth_url"
