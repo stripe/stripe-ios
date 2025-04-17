a5bcb35e7122f5a5a44ea2b4ee0d157ccaebab69
Joyce Qin <joyceqin@stripe.com>
2025-04-17 14:57:07 -0700

    import for test?

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentSheetDeferredValidatorTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentSheetDeferredValidatorTests.swift
@@ -6,6 +6,7 @@
 //
 
 @testable@_spi(STP) import StripeCore
+@testable@_spi(STP) import StripePayments
 @testable@_spi(STP) import StripePaymentSheet
 @testable@_spi(STP) import StripePaymentsTestUtils
 import XCTest
