625519c85f97fe0b8270c6749d5bc4b66b5bacfe
Joyce Qin <joyceqin@stripe.com>
2025-04-21 00:25:46 -0700

    revert one more lint file change

--- Stripe/StripeiOSTests/STPSetupIntentFunctionalTest.swift
+++ Stripe/StripeiOSTests/STPSetupIntentFunctionalTest.swift
@@ -12,6 +12,7 @@ import StripePaymentsTestUtils
 import XCTest
 
 @testable@_spi(STP) import Stripe
+@testable@_spi(STP) import StripeCore
 @testable@_spi(STP) import StripePayments
 @testable@_spi(STP) import StripePaymentSheet
 @testable@_spi(STP) import StripePaymentsUI
