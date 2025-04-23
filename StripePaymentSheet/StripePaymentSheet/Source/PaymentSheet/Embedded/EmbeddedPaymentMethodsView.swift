d402275c272725da7584f0bb625281d14f9ee280
Nick Porter <porter@stripe.com>
2025-04-22 19:12:18 -0600

    Fix mandate vend

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentMethodsView.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentMethodsView.swift
@@ -381,6 +381,8 @@ class EmbeddedPaymentMethodsView: UIView {
                 self.layoutIfNeeded()
             }
         case (true, false): // Hidden -> Hidden
+            // Update mandate text on the view so the payment option we vend to the merchant has the correct mandate
+            self.mandateView.attributedText = mandateText
             break
         }
     }
