187b7bf500a3f268a111b0cde644fd87d4ed9c7f
Mat Schmid <mats@stripe.com>
2025-04-24 11:02:25 -0400

    [LINK_MOBILE-122] Use nicknames for cards in Link (#4828)
    Resolves
    [LINK_MOBILE-122](https://jira.corp.stripe.com/browse/LINK_MOBILE-122)
    
    ## Summary
    
    This moves the `••••\(last4)` label to the subtitle, and adds a nickname
    to cards in the Link wallet. The nicknames either comes from the backend
    (user manually set a nickname in app.link.com), or we construct a
    reasonable default along the lines of `{brand} {funding}`.
    
    This mimics the behavior on web. We'd love to have this entire logic
    moved to the backend, and will work with that team to make those
    changes.
    
    ## Motivation
    
    Aligns with web
    
    ## Testing
    
    | Web | Mobile |
    |--------|--------|
    | ![Screenshot 2025-04-22 at 10 16
    16 AM](https://github.com/user-attachments/assets/c1f4d8fe-1aa5-4fba-9c1b-3810501cfb2f)
    | ![Simulator Screenshot - iPhone 16 - 2025-04-22 at 11 17
    51](https://github.com/user-attachments/assets/13cd9da9-ed35-4765-a471-ca51c1f61a16)
    |
    
    ## Changelog
    
    N/a

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/LinkStubs.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/LinkStubs.swift
@@ -37,10 +37,12 @@ extension LinkStubs {
                     brand: "visa",
                     networks: ["visa"],
                     last4: "1234",
+                    funding: .debit,
                     checks: nil)
                 ),
                 billingAddress: nil,
                 billingEmailAddress: nil,
+                nickname: nil,
                 isDefault: true
             ),
             ConsumerPaymentDetails(
@@ -51,10 +53,12 @@ extension LinkStubs {
                     brand: "mastercard",
                     networks: ["mastercard"],
                     last4: "4321",
+                    funding: .credit,
                     checks: .init(cvcCheck: .fail))
                 ),
                 billingAddress: nil,
                 billingEmailAddress: nil,
+                nickname: nil,
                 isDefault: false
             ),
             ConsumerPaymentDetails(
@@ -62,6 +66,7 @@ extension LinkStubs {
                 details: .bankAccount(bankAccount: .init(iconCode: nil, name: "test", last4: "1234")),
                 billingAddress: nil,
                 billingEmailAddress: nil,
+                nickname: nil,
                 isDefault: false
             ),
             ConsumerPaymentDetails(
@@ -72,10 +77,12 @@ extension LinkStubs {
                     brand: "discover",
                     networks: ["discover"],
                     last4: "1111",
+                    funding: .prepaid,
                     checks: nil)
                 ),
                 billingAddress: nil,
                 billingEmailAddress: nil,
+                nickname: "Patrick's card",
                 isDefault: false
             ),
         ]
