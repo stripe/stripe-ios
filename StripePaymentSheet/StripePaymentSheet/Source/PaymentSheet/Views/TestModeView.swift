4f6db802b2498a2eebe2a216b95d76343d650c1e
Lemuel Cardenas <lemuel@stripe.com>
2025-05-02 13:59:40 -0700

    Update StripePaymentSheet to use 'TEST' instead of 'TESTMODE' for test flows

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/TestModeView.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/TestModeView.swift
@@ -23,7 +23,7 @@ class TestModeView: UIView {
 
     private lazy var testLabel: UILabel = {
         let label = UILabel()
-        label.text = "TEST MODE"
+        label.text = "TEST"
         label.textColor = UIColor.testModeTextColor
         label.textAlignment = .center
         label.adjustsFontForContentSizeCategory = true
