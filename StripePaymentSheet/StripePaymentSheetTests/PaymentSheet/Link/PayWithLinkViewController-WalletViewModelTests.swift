23dcab5210a09c3a6f8e8ac8c83b400e3de3d08a
Mat Schmid <mats@stripe.com>
2025-05-14 14:57:32 -0400

    [LINK_MOBILE-162] Link signup screen button tweaks (#4878)
    ## Summary
    
    Adds a disabled `Log in or sign up` button when just the email field is
    shown. The remaining logic doesn't change - if a known email is
    detected, we automatically go to the OTP verification screen, otherwise
    we expand the other fields and the button switches to `Agree and
    continue`.
    
    ## Motivation
    
     [LINK_MOBILE-162](https://jira.corp.stripe.com/browse/LINK_MOBILE-162)
    
    ## Testing
    
    
    https://github.com/user-attachments/assets/63801086-7158-4039-a306-1e91cb5b5422
    
    ## Changelog
    
    N/a

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/Link/PayWithLinkViewController-WalletViewModelTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/Link/PayWithLinkViewController-WalletViewModelTests.swift
@@ -239,11 +239,18 @@ extension PayWithLinkViewController_WalletViewModelTests {
         linkPMOSFU: Bool? = nil
     ) throws -> PayWithLinkViewController.WalletViewModel {
         let (intent, elementsSession) = try isSettingUp
-        ? makeSetupIntentAndElementsSession(linkFundingSources: linkFundingSources, linkPassthroughModeEnabled: linkPassthroughModeEnabled)
-            : makePaymentIntentAndElementsSession(linkFundingSources: linkFundingSources, linkPassthroughModeEnabled: linkPassthroughModeEnabled, linkPMOSFU: linkPMOSFU)
+        ? PayWithLinkTestHelpers.makeSetupIntentAndElementsSession(
+            linkFundingSources: linkFundingSources,
+            linkPassthroughModeEnabled: linkPassthroughModeEnabled
+        )
+        : PayWithLinkTestHelpers.makePaymentIntentAndElementsSession(
+            linkFundingSources: linkFundingSources,
+            linkPassthroughModeEnabled: linkPassthroughModeEnabled,
+            linkPMOSFU: linkPMOSFU
+        )
 
         var paymentSheetConfiguration = PaymentSheet.Configuration()
-        if let linkPMOSFU {
+        if linkPMOSFU != nil {
             paymentSheetConfiguration.shouldReadPaymentMethodOptionsSetupFutureUsage = true
         }
 
@@ -268,75 +275,5 @@ extension PayWithLinkViewController_WalletViewModelTests {
             ),
             paymentMethods: paymentMethods
         )
-    }
-
-    private func makePaymentIntentAndElementsSession(
-        linkFundingSources: [String] = ["CARD"],
-        linkPassthroughModeEnabled: Bool? = nil,
-        linkPMOSFU: Bool? = nil
-    ) throws -> (Intent, STPElementsSession) {
-        // Link settings don't live in the PaymentIntent object itself, but in the /elements/sessions API response
-        // So we construct a minimal response (see STPPaymentIntentTest.testDecodedObjectFromAPIResponseMapping) to parse them
-        var paymentIntentJson = try XCTUnwrap(STPTestUtils.jsonNamed(STPTestJSONPaymentIntent))
-        if let linkPMOSFU {
-            paymentIntentJson["payment_method_options"] = [(linkPassthroughModeEnabled ?? false ? "card" : "link"): ["setup_future_usage": "off_session"]]
-        }
-        let orderedPaymentJson = ["card", "link"]
-        let paymentIntentResponse = [
-            "payment_intent": paymentIntentJson,
-            "ordered_payment_method_types": orderedPaymentJson,
-        ] as [String: Any]
-
-        var linkSettingsJson: [String: Any] = ["link_funding_sources": linkFundingSources]
-
-        if let linkPassthroughModeEnabled {
-            linkSettingsJson["link_passthrough_mode_enabled"] = linkPassthroughModeEnabled
-        }
-
-        let response = [
-            "payment_method_preference": paymentIntentResponse,
-            "link_settings": linkSettingsJson,
-            "session_id": "abc123",
-        ] as [String: Any]
-        let elementsSession = try XCTUnwrap(
-            STPElementsSession.decodedObject(fromAPIResponse: response)
-        )
-        let paymentIntentJSON = elementsSession.allResponseFields[jsonDict: "payment_method_preference"]?[jsonDict: "payment_intent"]
-        let paymentIntent = STPPaymentIntent.decodedObject(fromAPIResponse: paymentIntentJSON)!
-
-        return (Intent.paymentIntent(paymentIntent), elementsSession)
-    }
-
-    private func makeSetupIntentAndElementsSession(
-        linkFundingSources: [String] = ["CARD"],
-        linkPassthroughModeEnabled: Bool? = nil
-    ) throws -> (Intent, STPElementsSession) {
-        // Link settings don't live in the PaymentIntent object itself, but in the /elements/sessions API response
-        // So we construct a minimal response (see STPPaymentIntentTest.testDecodedObjectFromAPIResponseMapping) to parse them
-        let setupIntentJson = try XCTUnwrap(STPTestUtils.jsonNamed(STPTestJSONSetupIntent))
-        let orderedSetupJson = ["card", "link"]
-        let setupIntentResponse = [
-            "setup_intent": setupIntentJson,
-            "ordered_payment_method_types": orderedSetupJson,
-        ] as [String: Any]
-
-        var linkSettingsJson: [String: Any] = ["link_funding_sources": linkFundingSources]
-
-        if let linkPassthroughModeEnabled {
-            linkSettingsJson["link_passthrough_mode_enabled"] = linkPassthroughModeEnabled
-        }
-
-        let response = [
-            "payment_method_preference": setupIntentResponse,
-            "link_settings": linkSettingsJson,
-            "session_id": "abc123",
-        ] as [String: Any]
-        let elementsSession = try XCTUnwrap(
-            STPElementsSession.decodedObject(fromAPIResponse: response)
-        )
-        let setupIntentJSON = elementsSession.allResponseFields[jsonDict: "payment_method_preference"]?[jsonDict: "setup_intent"]
-        let setupIntent = STPSetupIntent.decodedObject(fromAPIResponse: setupIntentJSON)!
-
-        return (Intent.setupIntent(setupIntent), elementsSession)
     }
 }
