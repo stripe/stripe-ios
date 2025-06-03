5731431e081523027fe260a7d98d0947fbfd0ea2
Till Hellmund <tillh@stripe.com>
2025-06-03 13:52:53 -0400

    Fix incorrect primary button height for Link UI

--- StripePaymentSheet/StripePaymentSheet/Source/Internal/Link/Extensions/ConfirmButton+Link.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/Internal/Link/Extensions/ConfirmButton+Link.swift
@@ -10,20 +10,30 @@ import UIKit
 
 extension ConfirmButton {
 
+    private static let minimumLabelHeight: CGFloat = 24
+    private static let minimumButtonHeight: CGFloat = 44
+
     static func makeLinkButton(
         callToAction: CallToActionType,
         compact: Bool = false,
         didTap: @escaping () -> Void
     ) -> ConfirmButton {
+        let directionalLayoutMargins = compact ? LinkUI.compactButtonMargins : LinkUI.buttonMargins
+
+        let height = Self.minimumLabelHeight
+            + directionalLayoutMargins.top
+            + directionalLayoutMargins.bottom
+
+        var appearance = LinkUI.appearance
+        appearance.primaryButton.height = max(height, Self.minimumButtonHeight)
+
         let button = ConfirmButton(
             callToAction: callToAction,
-            appearance: LinkUI.appearance,
+            appearance: appearance,
             didTap: didTap
         )
 
-        button.directionalLayoutMargins = compact
-            ? LinkUI.compactButtonMargins
-            : LinkUI.buttonMargins
+        button.directionalLayoutMargins = directionalLayoutMargins
 
         return button
     }
