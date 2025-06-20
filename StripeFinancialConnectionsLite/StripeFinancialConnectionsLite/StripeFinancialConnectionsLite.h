2899dd0729b719508cb51c89fa00ff4e726a7218
Mat Schmid <mats@stripe.com>
2025-06-20 10:04:21 -0400

    Add module for FC Lite

---
+++ StripeFinancialConnectionsLite/StripeFinancialConnectionsLite/StripeFinancialConnectionsLite.h
@@ -0,0 +1,18 @@
+//
+//  StripeFinancialConnectionsLite.h
+//  StripeFinancialConnectionsLite
+//
+//  Created by Mat Schmid on 6/20/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StripeFinancialConnectionsLite.
+FOUNDATION_EXPORT double StripeFinancialConnectionsLiteVersionNumber;
+
+//! Project version string for StripeFinancialConnectionsLite.
+FOUNDATION_EXPORT const unsigned char StripeFinancialConnectionsLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StripeFinancialConnectionsLite/PublicHeader.h>
+
+
