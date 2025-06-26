d0b4ec35f8c6aa2c3205f04a358ab67fad64d00b
Mat Schmid <mats@stripe.com>
2025-06-26 15:01:39 -0400

    Revert "Extract FC Lite into its own module" (#5054)
    Reverts stripe/stripe-ios#5028

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/FCLiteImplementationTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/FCLiteImplementationTests.swift
@@ -1,8 +1,8 @@
 //
 //  FCLiteImplementationTests.swift
-//  StripeFinancialConnectionsLiteTests
+//  StripePaymentSheetTests
 //
-//  Created by Mat Schmid on 6/20/25.
+//  Created by Mat Schmid on 2025-03-28.
 //
 
 @_spi(STP) import StripeCore
@@ -11,7 +11,7 @@ import XCTest
 class FCLiteImplementationTests: XCTestCase {
     func testFCLiteImplementationAvailable() {
         let FinancialConnectionsLiteImplementation: FinancialConnectionsSDKInterface.Type? =
-            NSClassFromString("StripeFinancialConnectionsLite.FCLiteImplementation")
+            NSClassFromString("StripePaymentSheet.FCLiteImplementation")
             as? FinancialConnectionsSDKInterface.Type
         XCTAssertNotNil(FinancialConnectionsLiteImplementation)
     }
