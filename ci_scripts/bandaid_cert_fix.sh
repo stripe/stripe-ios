8d6ec7688b98f42b05f8ca11b8eaec6dc0f60220
George Birch <gbirch@stripe.com>
2025-07-09 17:16:07 -0500

    new script

--- ci_scripts/bandaid_cert_fix.sh
+++ ci_scripts/bandaid_cert_fix.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-mkdir -p certs && cd certs
-wget https://www.apple.com/certificateauthority/AppleRootCA-G3.cer
-openssl x509 -inform DER -in AppleRootCA-G3.cer -out AppleRootCA-G3.pem
-sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain AppleRootCA-G3.pem
+sudo security delete-certificate -Z 'F16CD3C54C7F83CEA4BF1A3E6A0819C8AAA8E4A1528FD144715F350643D2DF3A' /Library/Keychains/System.keychain
+sudo security delete-certificate -Z 'CE057691D730F89CA25E916F7335F4C8A15713DCD273A658C024023F8EB809C2' /Library/Keychains/System.keychain
+sudo security delete-certificate -Z 'DCF21878C77F4198E4B4614F03D696D89C66C66008D4244E1B99161AAC91601F' /Library/Keychains/System.keychain
+
+security find-certificate -a -c 'Apple Worldwide Developer Relations Certification Authority' -Z
+
+curl -o ~/AppleWWDRCAG3.cer https://www.apple.com/certificateauthority/AppleWWDRCAG3.cer
+sudo security add-certificates -k /Library/Keychains/System.keychain ~/AppleWWDRCAG3.cer
 
 
-wget https://www.apple.com/certificateauthority/AppleWWDRCAG3.cer
-openssl x509 -inform DER -in AppleWWDRCAG3.cer -out AppleWWDRCAG3.pem
-sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain AppleWWDRCAG3.pem
+curl -o ~/DeveloperIDG2CA.cer https://www.apple.com/certificateauthority/DeveloperIDG2CA.cer
+sudo security add-certificates -k /Library/Keychains/System.keychain ~/DeveloperIDG2CA.cer
