197f4f8c9935b0d494a19e600071eb7ef404fe75
joyceqin-stripe <joyceqin@stripe.com>
2025-07-11 09:58:59 -0700

    Increase default checkbox animation speed (#5137)
    ## Summary
    <!-- Simple summary of what was changed. -->
    Animate showing/hiding the default checkbox in 0.2
    ## Motivation
    <!-- Why are you making this change? If it's for fixing a bug, if
    possible, please include a code snippet or example project that
    demonstrates the issue. -->
    [MOBILESDK-3263](https://jira.corp.stripe.com/browse/MOBILESDK-3263)
    ## Testing
    <!-- How was the code tested? Be as specific as possible. -->
    <!-- Ignored Tests: Did you newly ignore a test in this PR? If so,
    please open an R4 incident so that the test can be re-enabled as soon as
    possible-->
    From quality review before:
    https://docs.google.com/document/d/1Z184luPIYHppw7J9sC4lOqAK1O1zDGWIpwpYlrTaLnQ/edit?tab=t.0#bookmark=id.kdtoxi8zlnbz
    
    
    
    https://github.com/user-attachments/assets/0978485e-3855-45c6-b0a7-2765bc76a45c
    
    
    
    ## Changelog
    <!-- Is this a notable change that affects users? If so, add a line to
    `CHANGELOG.md` and prefix the line with one of the following:
        - [Added] for new features.
        - [Changed] for changes in existing functionality.
        - [Deprecated] for soon-to-be removed features.
        - [Removed] for now removed features.
        - [Fixed] for any bug fixes.
        - [Security] in case of vulnerabilities.
    -->
    N/A

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheetFormFactory/PaymentSheetFormFactory.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheetFormFactory/PaymentSheetFormFactory.swift
@@ -633,7 +633,13 @@ extension PaymentSheetFormFactory {
             )
         ) { value in
             isSaving.value = value
-            defaultCheckbox?.view.isHidden = !value
+            if let defaultCheckbox {
+                UIView.transition(with: defaultCheckbox.view, duration: 0.2,
+                                  options: .transitionCrossDissolve,
+                                  animations: {
+                    defaultCheckbox.view.isHidden = !value
+                })
+            }
         }
 
         isSaving.value =
