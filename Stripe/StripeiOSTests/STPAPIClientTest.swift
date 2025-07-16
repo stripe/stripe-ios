9f48c1396a6d6c29e6b7c1b66fd7009e4ca9c5dc
Joyce Qin <joyceqin@stripe.com>
2025-07-15 17:05:25 -0700

    lint

--- Stripe/StripeiOSTests/STPAPIClientTest.swift
+++ Stripe/StripeiOSTests/STPAPIClientTest.swift
@@ -13,8 +13,8 @@ import XCTest
 @testable@_spi(STP) import StripeCore
 @testable@_spi(STP) import StripePayments
 @testable@_spi(STP) @_spi(CustomerSessionBetaAccess) import StripePaymentSheet
-@testable@_spi(STP) import StripePaymentsUI
 @testable import StripePaymentsTestUtils
+@testable@_spi(STP) import StripePaymentsUI
 
 class STPAPIClientTest: XCTestCase {
     func testSharedClient() {
