42ff76083a29e12d6023ab819a29ab6d48888ce2
Joyce Qin <joyceqin@stripe.com>
2025-07-16 16:59:00 -0700

    revert PaymentSheet+API

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheet+API.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheet+API.swift
@@ -160,21 +160,6 @@ extension PaymentSheet {
             completion(makePaymentSheetResult(for: status, error: error), nil)
         }
 
-        switch intent {
-        // MARK: ↪ PaymentIntent
-        case .paymentIntent(let paymentIntent):
-            STPAnalyticsClient.sharedClient.clientAttributionMetadata["payment_intent_creation_flow"] = "standard"
-            STPAnalyticsClient.sharedClient.clientAttributionMetadata["payment_method_selection_flow"] = paymentIntent.automaticPaymentMethods?.enabled ?? false ? "automatic" : "merchant_specified"
-        // MARK: ↪ SetupIntent
-        case .setupIntent(let setupIntent):
-            STPAnalyticsClient.sharedClient.clientAttributionMetadata["payment_intent_creation_flow"] = "standard"
-            STPAnalyticsClient.sharedClient.clientAttributionMetadata["payment_method_selection_flow"] = setupIntent.automaticPaymentMethods?.enabled ?? false ? "automatic" : "merchant_specified"
-        // MARK: ↪ Deferred Intent
-        case .deferredIntent(let intentConfig):
-            STPAnalyticsClient.sharedClient.clientAttributionMetadata["payment_intent_creation_flow"] = "deferred"
-            STPAnalyticsClient.sharedClient.clientAttributionMetadata["payment_method_selection_flow"] = intentConfig.paymentMethodTypes?.isEmpty ?? true ? "automatic" : "merchant_specified"
-        }
-
         switch paymentOption {
         // MARK: - Apple Pay
         case .applePay:
@@ -282,6 +267,7 @@ extension PaymentSheet {
                     : intentConfirmParamsFromSavedPaymentMethod?.confirmPaymentMethodOptions
 
                 let paymentIntentParams = makePaymentIntentParams(confirmPaymentMethodType: .saved(paymentMethod, paymentOptions: paymentOptions), paymentIntent: paymentIntent, configuration: configuration)
+
                 paymentHandler.confirmPayment(
                     paymentIntentParams,
                     with: authenticationContext,
