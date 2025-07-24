351991ad065af1f30ea1dea66a313359137c7d25
Michael Liberatore <michael@lickability.net>
2025-07-24 13:53:20 -0400

    Initial add of the CryptoOnramp Example project

---
+++ Example/CryptoOnramp Example/CryptoOnramp Example/CryptoOnramp_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoOnramp_ExampleApp.swift
+//  CryptoOnramp Example
+//
+//  Created by Michael Liberatore on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoOnramp_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
