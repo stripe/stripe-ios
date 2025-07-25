c7f780b737ba6108f8a6e1230fc80f8c9bb5916c
Joyce Qin <joyceqin@stripe.com>
2025-07-25 14:21:40 -0700

    generate session id at beginning of network tests

--- StripePayments/StripePaymentsTestUtils/STPNetworkStubbingTestCase.swift
+++ StripePayments/StripePaymentsTestUtils/STPNetworkStubbingTestCase.swift
@@ -32,7 +32,7 @@ import XCTest
     open override func setUp() {
         super.setUp()
 
-        AnalyticsHelper.shared.clearSessionID()
+        AnalyticsHelper.shared.generateSessionID()
 
         recordingMode = ProcessInfo.processInfo.environment["STP_RECORD_NETWORK"] != nil
         disableMocking = ProcessInfo.processInfo.environment["STP_NO_NETWORK_MOCKS"] != nil
