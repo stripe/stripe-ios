c4ece04a336ef7ed5c2c102dc7d8a8c0250f7694
Ken Ackerson <ken@lickability.net>
2025-07-31 12:32:57 -0400

    Update DocumentFileUploadViewControllerTest.swift

--- StripeIdentity/StripeIdentityTests/Unit/NativeComponents/ViewControllers/DocumentFileUploadViewControllerTest.swift
+++ StripeIdentity/StripeIdentityTests/Unit/NativeComponents/ViewControllers/DocumentFileUploadViewControllerTest.swift
@@ -38,7 +38,7 @@ final class DocumentFileUploadViewControllerTest: XCTestCase {
         let vc = makeViewController()
         // Allows front
         XCTAssertEqual(vc.viewModel.listViewModel?.items.count, 1)
-        XCTAssertEqual(vc.viewModel.listViewModel?.items[0].text, "Front of identity card")
+        XCTAssertEqual(vc.viewModel.listViewModel?.items[0].text, "Front of identity document")
     }
 
     func testIdCardBack() {
@@ -50,8 +50,8 @@ final class DocumentFileUploadViewControllerTest: XCTestCase {
         let vc = makeViewController()
         // Allows front and back
         XCTAssertEqual(vc.viewModel.listViewModel?.items.count, 2)
-        XCTAssertEqual(vc.viewModel.listViewModel?.items[0].text, "Front of identity card")
-        XCTAssertEqual(vc.viewModel.listViewModel?.items[1].text, "Back of identity card")
+        XCTAssertEqual(vc.viewModel.listViewModel?.items[0].text, "Front of identity document")
+        XCTAssertEqual(vc.viewModel.listViewModel?.items[1].text, "Back of identity document")
 
         // Verify button is only enabled after both front and back images are uploaded
         XCTAssertEqual(vc.buttonState, .disabled)
