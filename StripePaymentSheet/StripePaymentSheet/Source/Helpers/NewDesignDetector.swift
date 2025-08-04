c0b84d407d086d7053c97cbf427e622096430a19
David Estes <davidestes@stripe.com>
2025-08-04 13:34:05 -0700

    Adjust layout for iOS 26

---
+++ StripePaymentSheet/StripePaymentSheet/Source/Helpers/NewDesignDetector.swift
@@ -0,0 +1,28 @@
+//
+//  NewDesignDetector.swift
+//  StripePaymentSheet
+//
+//  Created by David Estes on 8/4/25.
+//
+
+import Foundation
+
+// Our SDK runs inside a user's app, we can't explicitly opt into or out of the new "Liquid Glass" design.
+// Instead, we'll do our best to detect which design to use, and adjust the default UI spacing and icons accordingly.
+@_spi(STP) public class NewDesignDetector {
+    static var newDesignEnabled = {
+        // If the app was built with Xcode 26 or later (which includes Swift compiler 6.2)...
+        #if compiler(>=6.2)
+        // And we're running on iOS 26 or later...
+        if #available(iOS 26.0, *) {
+            // And the app hasn't opted out of the new design...
+            if !(Bundle.main.infoDictionary?["UIDesignRequiresCompatibility"] as? Bool ?? false) {
+                // Then assume we're using the new design!
+                return true
+            }
+        }
+        #endif
+        // Otherwise, use the old design
+        return false
+    }()
+}
