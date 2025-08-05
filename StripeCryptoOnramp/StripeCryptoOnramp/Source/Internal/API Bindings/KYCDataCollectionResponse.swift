2540f4d6e53277ddde6dea328770a67ac72a1ed7
Michael Liberatore <michael@lickability.net>
2025-08-05 16:10:52 -0400

    Adds missing docs

--- StripeCryptoOnramp/StripeCryptoOnramp/Source/Internal/API Bindings/KYCDataCollectionResponse.swift
+++ StripeCryptoOnramp/StripeCryptoOnramp/Source/Internal/API Bindings/KYCDataCollectionResponse.swift
@@ -9,10 +9,20 @@ import Foundation
 
 /// Codable model representing a response from the `/v1/crypto/internal/kyc_data_collection` endpoint.
 struct KYCDataCollectionResponse: Codable {
+
+    /// The identifier of the user.
     let personId: String
+
+    /// The user’s first name.
     let firstName: String
+
+    /// The user’s last name.
     let lastName: String
+
+    /// A list of the user’s nationalities, if any were provided.
     let nationalities: [String]
+
+    /// The country in which the user currently resides.
     let residenceCountry: String
 
     // MARK: - Decodable
