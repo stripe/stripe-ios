1d44ae1216fb044b8404bb1df180e5316fd3f7b3
Joyce Qin <joyceqin@stripe.com>
2025-08-05 20:21:19 -0700

    remove startPassiveHCaptchaChallengeIfNecessary

--- StripePayments/StripePayments/Source/Helpers/STPHCaptchaChallenge.swift
+++ StripePayments/StripePayments/Source/Helpers/STPHCaptchaChallenge.swift
@@ -7,20 +7,6 @@
 
 import Foundation
 
-@_spi(STP) public func startPassiveHCaptchaChallengeIfNecessary(siteKey: String?, rqdata: String?, completion: @escaping (String?) -> Void) {
-    guard let siteKey,
-          let hcaptcha = try? HCaptcha(apiKey: siteKey, passiveApiKey: true, baseURL: URL(string: "http://localhost"), rqdata: rqdata) else {
-        completion(nil)
-        return
-    }
-    hcaptcha.didFinishLoading {
-        hcaptcha.validate { result in
-            let token = try? result.dematerialize()
-            completion(token)
-        }
-    }
-}
-
 @_spi(STP) public func fetchPassiveHCaptchaToken(siteKey: String?, rqdata: String?) async -> String? {
     return await withCheckedContinuation { continuation in
         guard let siteKey,
