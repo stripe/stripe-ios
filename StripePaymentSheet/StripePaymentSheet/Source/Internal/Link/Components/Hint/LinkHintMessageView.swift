6a654bc2069043d859a8cc71750115f6ba4223f4
Mat Schmid <mats@stripe.com>
2025-08-14 16:18:31 -0400

    Add LinkHintMessageViewSnapshotTests

--- StripePaymentSheet/StripePaymentSheet/Source/Internal/Link/Components/Hint/LinkHintMessageView.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/Internal/Link/Components/Hint/LinkHintMessageView.swift
@@ -5,14 +5,14 @@
 //  Created by Mat Schmid on 8/14/25.
 //
 
-import UIKit
 @_spi(STP) import StripeCore
 @_spi(STP) import StripeUICore
+import UIKit
 
 /// For internal SDK use only
 @objc(STP_Internal_LinkHintMessageView)
 final class LinkHintMessageView: UIView {
-    
+
     private struct Constants {
         static let spacing: CGFloat = LinkUI.smallContentSpacing
         static let margins: NSDirectionalEdgeInsets = LinkUI.compactButtonMargins
@@ -20,7 +20,7 @@ final class LinkHintMessageView: UIView {
         static let cornerRadius: CGFloat = LinkUI.cornerRadius
         static let minimumHeight: CGFloat = LinkUI.minimumButtonHeight
     }
-    
+
     var text: String? {
         get {
             return textLabel.text
@@ -29,22 +29,22 @@ final class LinkHintMessageView: UIView {
             textLabel.text = newValue
         }
     }
-    
+
     private lazy var iconView: UIImageView = {
         let imageView = UIImageView()
         imageView.tintColor = .linkIconTertiary
         imageView.contentMode = .scaleAspectFit
         imageView.setContentHuggingPriority(.required, for: .horizontal)
         imageView.setContentCompressionResistancePriority(.required, for: .horizontal)
-        
+
         NSLayoutConstraint.activate([
             imageView.widthAnchor.constraint(equalToConstant: Constants.iconSize.width),
             imageView.heightAnchor.constraint(equalToConstant: Constants.iconSize.height),
         ])
-        
+
         return imageView
     }()
-    
+
     private lazy var textLabel: UILabel = {
         let label = UILabel()
         label.textColor = .linkTextTertiary
@@ -53,21 +53,21 @@ final class LinkHintMessageView: UIView {
         label.numberOfLines = 0
         return label
     }()
-    
+
     init(message: String) {
         super.init(frame: .zero)
         setupUI()
         configureImage()
         self.text = message
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func setupUI() {
         let stackView = UIStackView(arrangedSubviews: [iconView, textLabel])
-        
+
         stackView.axis = .horizontal
         stackView.spacing = Constants.spacing
         stackView.alignment = .center
@@ -75,19 +75,19 @@ final class LinkHintMessageView: UIView {
         stackView.directionalLayoutMargins = Constants.margins
         stackView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(stackView)
-        
+
         NSLayoutConstraint.activate([
             stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
             stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
             stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
         ])
-        
+
         backgroundColor = .linkSurfaceSecondary
         layer.cornerRadius = Constants.cornerRadius
 
         heightAnchor.constraint(greaterThanOrEqualToConstant: Constants.minimumHeight).isActive = true
     }
-    
+
     private func configureImage() {
         iconView.image = Image.icon_info.makeImage(template: true)
     }
