7a542bceda7d756db73863114b88f1f880d8ee6b
Michael Liberatore <michael@lickability.net>
2025-08-19 14:42:00 -0400

    Adds payment tokens request + cleanup

--- Example/CryptoOnramp Example/CryptoOnramp Example/Example API Bindings/APIClient+Crypto.swift
+++ Example/CryptoOnramp Example/CryptoOnramp Example/Example API Bindings/APIClient+Crypto.swift
@@ -10,16 +10,33 @@ import Foundation
 extension APIClient {
     func fetchCustomerInfo(cryptoCustomerToken: String) async throws -> CustomerInformationResponse {
         guard let token = authToken else { throw APIError.missingAuthToken }
-        return try await request("customer_info", bearerToken: token, queryItems: [
-            URLQueryItem(name: "crypto_customer_token", value: cryptoCustomerToken)
-        ])
+        return try await request("customer_info", bearerToken: token, queryItems: [.cryptoCustomerToken(cryptoCustomerToken)])
     }
 
     func fetchCustomerWallets(cryptoCustomerToken: String) async throws -> CustomerWalletsResponse {
         guard let token = authToken else { throw APIError.missingAuthToken }
         return try await request("customer_wallets", bearerToken: token, queryItems: [
-            URLQueryItem(name: "crypto_customer_token", value: cryptoCustomerToken)
+            .cryptoCustomerToken(cryptoCustomerToken),
+            .pageSize(50)
         ])
+    }
+
+    func fetchPaymentTokens(cryptoCustomerToken: String) async throws -> PaymentTokensResponse {
+        guard let token = authToken else { throw APIError.missingAuthToken }
+        return try await request("payment_tokens", bearerToken: token, queryItems: [
+            .cryptoCustomerToken(cryptoCustomerToken),
+            .pageSize(50)
+        ])
+    }
+}
+
+private extension URLQueryItem {
+    static func cryptoCustomerToken(_ value: String) -> URLQueryItem {
+        URLQueryItem(name: "crypto_customer_token", value: value)
+    }
+
+    static func pageSize(_ value: Int) -> URLQueryItem {
+        URLQueryItem(name: "limit", value: String(value))
     }
 }
 
