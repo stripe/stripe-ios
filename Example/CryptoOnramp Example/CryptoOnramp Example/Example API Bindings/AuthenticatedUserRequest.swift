422c406ddcb5c70fdf674f9597c530a43a8615ad
Mat Schmid <mats@stripe.com>
2025-08-23 13:33:46 -0400

    Undo changes to AuthenticateUserRequest

--- Example/CryptoOnramp Example/CryptoOnramp Example/Example API Bindings/AuthenticatedUserRequest.swift
+++ Example/CryptoOnramp Example/CryptoOnramp Example/Example API Bindings/AuthenticatedUserRequest.swift
@@ -9,10 +9,4 @@ import Foundation
 
 struct AuthenticateUserRequest: Encodable {
     let email: String
-    let oauthScopes: String = "userinfo:read"
-
-    enum CodingKeys: String, CodingKey {
-        case email
-        case oauthScopes = "oauth_scopes"
-    }
 }
