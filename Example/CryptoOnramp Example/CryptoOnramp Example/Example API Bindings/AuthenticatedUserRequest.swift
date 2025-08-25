3ab9c1819b996a825f38d65e66b53006ad2709a5
Mat Schmid <mats@stripe.com>
2025-08-25 09:54:46 -0400

    Allow selecting OAuth scopes in Onramp example app (#5398)
    ## Summary
    
    Adds a picker on the root Onramp example app view to select requested
    OAuth scopes. These are then passed into our example backend API to
    create a Link auth intent.
    
    ## Motivation
    
    OAuth support
    
    ## Testing
    
    
    https://github.com/user-attachments/assets/ffec17f2-77d0-4991-b057-5eb72a5a4627
    
    
    ## Changelog
    
    N/a

--- Example/CryptoOnramp Example/CryptoOnramp Example/Example API Bindings/AuthenticatedUserRequest.swift
+++ Example/CryptoOnramp Example/CryptoOnramp Example/Example API Bindings/AuthenticatedUserRequest.swift
@@ -9,4 +9,15 @@ import Foundation
 
 struct AuthenticateUserRequest: Encodable {
     let email: String
+    let oauthScopes: String
+
+    enum CodingKeys: String, CodingKey {
+        case email
+        case oauthScopes = "oauth_scopes"
+    }
+
+    init(email: String, oauthScopes: [OAuthScopes] = OAuthScopes.inlineScope) {
+        self.email = email
+        self.oauthScopes = oauthScopes.map(\.rawValue).joined(separator: ",")
+    }
 }
