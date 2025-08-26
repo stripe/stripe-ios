82ad946045255244d5494f2cd1a55795b3f9b890
Till Hellmund <tillh@stripe.com>
2025-08-26 14:29:30 -0400

    Add snapshot for signup opt-in checkbox

--- Stripe/StripeiOSTests/LinkInlineSignupElementSnapshotTests.swift
+++ Stripe/StripeiOSTests/LinkInlineSignupElementSnapshotTests.swift
@@ -81,6 +81,14 @@ class LinkInlineSignupElementSnapshotTests: STPSnapshotTestCase {
         verify(sut)
     }
 
+    func testSignupOptInFeatureEnabled_unchecked() {
+        let sut = makeSUT(
+            linkAccountEmailAddress: "unknown@stripe.com",
+            signupOptInFeatureEnabled: true
+        )
+        verify(sut)
+    }
+
     // MARK: Textfield only mode
 
     func testDefaultState_textFieldsOnly() {
@@ -189,7 +197,8 @@ extension LinkInlineSignupElementSnapshotTests {
         preFillName: String? = nil,
         preFillPhone: String? = nil,
         showCheckbox: Bool = true,
-        allowsDefaultOptIn: Bool = false
+        allowsDefaultOptIn: Bool = false,
+        signupOptInFeatureEnabled: Bool = false
     ) -> LinkInlineSignupElement {
         var configuration = PaymentSheet.Configuration()
         configuration.merchantDisplayName = "[Merchant]"
@@ -213,7 +222,7 @@ extension LinkInlineSignupElementSnapshotTests {
             showCheckbox: showCheckbox,
             accountService: MockAccountService(),
             allowsDefaultOptIn: allowsDefaultOptIn,
-            signupOptInFeatureEnabled: false,
+            signupOptInFeatureEnabled: signupOptInFeatureEnabled,
             signupOptInInitialValue: false,
             linkAccount: linkAccount,
             country: country
