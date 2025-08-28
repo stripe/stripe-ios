ae65c5e9054f6adf4c1cdfa620185e890e42cad1
George Birch <gbirch@stripe.com>
2025-08-28 13:04:41 -0500

    Embedded - Don't show the change button for the disclosure style (#5304)
    ## Summary
    The disclosure style doesn't support a visual "selected" state.
    Therefore, we omit the change button to avoid visual confusion
    
    ## Motivation
    https://jira.corp.stripe.com/browse/MOBILESDK-3890
    
    ## Testing
    <!-- How was the code tested? Be as specific as possible. -->
    <!-- Ignored Tests: Did you newly ignore a test in this PR? If so,
    please open an R4 incident so that the test can be re-enabled as soon as
    possible-->
    
    ## Changelog
    N/A

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentElement+Internal.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentElement+Internal.swift
@@ -431,13 +431,7 @@ extension EmbeddedPaymentElement: EmbeddedFormViewControllerDelegate {
 
         // Show change button if the newly selected row needs it
         if let currentlySelectedType = embeddedPaymentMethodsView.selectedRowButton?.type{
-            let changeButtonState = getChangeButtonState(for: currentlySelectedType)
-            if changeButtonState.shouldShowChangeButton {
-                embeddedPaymentMethodsView.selectedRowButton?.addChangeButton(sublabel: changeButtonState.sublabel)
-                embeddedPaymentMethodsView.selectedRowChangeButtonState = (true, changeButtonState.sublabel)
-            } else {
-                embeddedPaymentMethodsView.selectedRowChangeButtonState = (false, nil)
-            }
+            updateChangeButtonAndSublabelState(for: currentlySelectedType)
         }
 
         embeddedFormViewController.dismiss(animated: true) {
@@ -450,13 +444,7 @@ extension EmbeddedPaymentElement: EmbeddedFormViewControllerDelegate {
     func embeddedFormViewControllerDidContinue(_ embeddedFormViewController: EmbeddedFormViewController) {
         // Show change button if the selected row needs it
         if let newSelectedType = embeddedPaymentMethodsView.selectedRowButton?.type {
-            let changeButtonState = getChangeButtonState(for: newSelectedType)
-            if changeButtonState.shouldShowChangeButton {
-                embeddedPaymentMethodsView.selectedRowButton?.addChangeButton(sublabel: changeButtonState.sublabel)
-                embeddedPaymentMethodsView.selectedRowChangeButtonState = (true, changeButtonState.sublabel)
-            } else {
-                embeddedPaymentMethodsView.selectedRowChangeButtonState = (false, nil)
-            }
+            updateChangeButtonAndSublabelState(for: newSelectedType)
         }
         embeddedFormViewController.dismiss(animated: true)
         informDelegateIfPaymentOptionUpdated()
@@ -465,19 +453,23 @@ extension EmbeddedPaymentElement: EmbeddedFormViewControllerDelegate {
         }
     }
 
-    func getChangeButtonState(for type: RowButtonType) -> (shouldShowChangeButton: Bool, sublabel: String?) {
-        guard let _paymentOption, let displayData = paymentOption else {
-            return (false, nil)
+    // Updates whether or not the change button shows and what sublabel (if any) the selected row button shows
+    func updateChangeButtonAndSublabelState(for type: RowButtonType) {
+        guard let _paymentOption,
+              let displayData = paymentOption,
+              case .new = type,
+              selectedFormViewController != nil else {
+            embeddedPaymentMethodsView.selectedRowChangeButtonState = (false, nil)
+            return
         }
-        // Show change button for new PMs that have a valid form
-        let shouldShowChangeButton: Bool = {
-            if case .new = type, selectedFormViewController != nil {
-               return true
-            }
-            return false
-        }()
 
-        // Add a sublabel to the selected row for cards and us bank account like "Visa 4242"
+        // Don't show the change button for the disclosure style
+        let shouldShowChangeButton = configuration.appearance.embeddedPaymentElement.row.style != .flatWithDisclosure
+        if shouldShowChangeButton {
+            embeddedPaymentMethodsView.selectedRowButton?.addChangeButton()
+        }
+
+        // Add a sublabel
         let sublabel: String? = {
             switch type.paymentMethodType {
             case .stripe(.card):
@@ -493,8 +485,12 @@ extension EmbeddedPaymentElement: EmbeddedFormViewControllerDelegate {
                 return nil
             }
         }()
+        // Only overwrite the existing sublabel if we have a new one
+        if let sublabel {
+            embeddedPaymentMethodsView.selectedRowButton?.setSublabel(text: sublabel)
+        }
 
-        return (shouldShowChangeButton: shouldShowChangeButton, sublabel: sublabel)
+        embeddedPaymentMethodsView.selectedRowChangeButtonState = (shouldShowChangeButton, sublabel)
     }
 }
 
