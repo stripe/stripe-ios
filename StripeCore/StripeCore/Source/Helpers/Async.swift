847eb746fc2233aade76a8534334d754ca6bf87c
George Birch <gbirch@stripe.com>
2025-09-09 17:49:38 -0400

    reinstate stp flag on promises

--- StripeCore/StripeCore/Source/Helpers/Async.swift
+++ StripeCore/StripeCore/Source/Helpers/Async.swift
@@ -34,7 +34,7 @@
 import Foundation
 
 // ⛔️ DEPRECATED: Futures are not fully thread safe and can cause crashes. Use Swift concurrency instead. ⛔️
-public class Future<Value> {
+@_spi(STP) public class Future<Value> {
     public typealias Result = Swift.Result<Value, Error>
 
     fileprivate var result: Result? {
@@ -125,7 +125,7 @@ public class Future<Value> {
 }
 
 // ⛔️ DEPRECATED: Promises are not fully thread safe and can cause crashes. Use Swift concurrency instead. ⛔️
-public class Promise<Value>: Future<Value> {
+@_spi(STP) public class Promise<Value>: Future<Value> {
     public override init() {
         super.init()
     }
