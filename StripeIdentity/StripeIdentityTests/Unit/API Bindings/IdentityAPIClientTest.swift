64e053883abdc70d7ea6f298bb3916aa767c2817
George Birch <gbirch@stripe.com>
2025-09-09 17:36:17 -0400

    lint

--- StripeIdentity/StripeIdentityTests/Unit/API Bindings/IdentityAPIClientTest.swift
+++ StripeIdentity/StripeIdentityTests/Unit/API Bindings/IdentityAPIClientTest.swift
@@ -8,12 +8,11 @@
 
 import OHHTTPStubs
 import OHHTTPStubsSwift
+@_spi(DEPRECATED) import StripeCore
 @_spi(STP) import StripeCoreTestUtils
 import XCTest
 
 // swift-format-ignore
-@testable @_spi(STP) import StripeCore
-
 @testable import StripeIdentity
 
 final class IdentityAPIClientTest: APIStubbedTestCase {
