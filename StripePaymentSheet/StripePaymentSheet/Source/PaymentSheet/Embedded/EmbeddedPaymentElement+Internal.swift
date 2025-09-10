3ceb8a8489a4bcafdf463279c56455632d811927
Joyce Qin <joyceqin@stripe.com>
2025-09-10 14:33:23 -0400

    lint

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentElement+Internal.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Embedded/EmbeddedPaymentElement+Internal.swift
@@ -530,7 +530,6 @@ extension EmbeddedPaymentElement {
 
         embeddedPaymentMethodsView.isUserInteractionEnabled = false
 
-        
         let hcaptchaToken = await passiveCaptchaChallenge?.fetchToken()
         let (result, deferredIntentConfirmationType) = await PaymentSheet.confirm(
             configuration: configuration,
