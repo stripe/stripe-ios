60c1a651e464f45920c0009b2e66458db8d3c2af
Joyce Qin <joyceqin@stripe.com>
2025-09-18 11:07:04 -0400

    remove redundant radar options in copy

--- StripePayments/StripePayments/Source/API Bindings/Models/PaymentIntents/STPPaymentIntentParams.swift
+++ StripePayments/StripePayments/Source/API Bindings/Models/PaymentIntents/STPPaymentIntentParams.swift
@@ -295,7 +295,6 @@ extension STPPaymentIntentParams: NSCopying {
         copy.receiptEmail = receiptEmail
         copy.savePaymentMethod = savePaymentMethod
         copy.setAsDefaultPM = setAsDefaultPM
-        copy.radarOptions = radarOptions
         copy.returnURL = returnURL
         copy.setupFutureUsage = setupFutureUsage
         copy.useStripeSDK = useStripeSDK
