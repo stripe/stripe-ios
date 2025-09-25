73fc1b82f3e953baf83ef222ddbc49b41577b36d
Michael Liberatore <michael@lickability.net>
2025-09-25 11:48:00 -0400

    Capitalizes Link

--- StripeCryptoOnramp/StripeCryptoOnramp/Source/Components/CryptoOnrampCoordinator+Error.swift
+++ StripeCryptoOnramp/StripeCryptoOnramp/Source/Components/CryptoOnrampCoordinator+Error.swift
@@ -26,7 +26,7 @@ public extension CryptoOnrampCoordinator {
         /// A crypto customer ID is missing but required.
         case missingCryptoCustomerID
 
-        /// The link account is not in a verified state.
+        /// The Link account is not in a verified state.
         case linkAccountNotVerified
 
         public var errorDescription: String? {
@@ -42,7 +42,7 @@ public extension CryptoOnrampCoordinator {
             case .missingCryptoCustomerID:
                 return "A crypto customer ID is missing but required. A crypto customer ID must either be provided to the Crypto Onramp Coordinator in the `create` API, or generated during the onramp flow by verifying a Link account using the `registerLinkUser`, `authenticateUser`, or `authorize` APIs."
             case .linkAccountNotVerified:
-                return "The customer’s link account is not in a verified state."
+                return "The customer’s Link account is not in a verified state."
             }
         }
     }
