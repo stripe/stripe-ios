7e0219f6ef2aa55806050ea8fd6afbdd05f9d4ec
Jeremy Kelleher <jkelle@stripe.com>
2025-10-01 11:36:29 -0400

    added ios 26 tag

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/Link/LinkNavigationBarSnapshotTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/Link/LinkNavigationBarSnapshotTests.swift
@@ -14,6 +14,7 @@ import UIKit
 @testable@_spi(STP) import StripePaymentSheet
 @testable@_spi(STP) import StripePaymentsUI
 
+// @iOS26
 class LinkNavigationBarSnapshotTests: STPSnapshotTestCase {
 
     func testDefault() {
