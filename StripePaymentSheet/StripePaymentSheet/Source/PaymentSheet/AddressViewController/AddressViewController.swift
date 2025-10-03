6191b8c41d000dcc9a28311441f5db505d3af0e2
David Estes <davidestes@stripe.com>
2025-10-02 17:54:41 -0700

    Actually don't show the checkbox at all

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/AddressViewController/AddressViewController.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/AddressViewController/AddressViewController.swift
@@ -179,8 +179,11 @@ public class AddressViewController: UIViewController {
 
         guard isCompatible else { return nil }
 
-        // Default to checked if shipping address (defaultValues) is empty and billing address has at least line1
-        let isSelectedByDefault = configuration.defaultValues.address.isEmpty && billingAddress.address.line1?.nonEmpty != nil
+        // Only show checkbox if billing address has at least line1
+        guard billingAddress.address.line1?.nonEmpty != nil else { return nil }
+
+        // Default to checked if shipping address (defaultValues) is empty
+        let isSelectedByDefault = configuration.defaultValues.address.isEmpty
 
         return CheckboxElement(
             theme: configuration.appearance.asElementsTheme,
