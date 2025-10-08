a1676bd72271493ca391649d0c414adba8e27319
Joyce Qin <joyceqin@stripe.com>
2025-10-08 10:52:36 -0400

    lint

--- StripePayments/StripePayments/Source/Captcha/PassiveCaptcha.swift
+++ StripePayments/StripePayments/Source/Captcha/PassiveCaptcha.swift
@@ -170,7 +170,6 @@ struct PassiveHCaptchaFactory: HCaptchaFactory {
     }
 }
 
-
 // All duration analytics are in milliseconds
 extension STPAnalyticsClient {
     func logPassiveCaptchaInit(siteKey: String) {
