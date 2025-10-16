9cdb000d1d4a4806ab6b297d90a8d589787990f9
George Birch <gbirch@stripe.com>
2025-10-16 12:41:10 -0500

    Refactor AfterpayPriceBreakdownView, use system image instead of asset

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/AfterpayPriceBreakdownView.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/AfterpayPriceBreakdownView.swift
@@ -21,9 +21,6 @@ class AfterpayPriceBreakdownView: UIView {
     private lazy var afterpayMarkImage: UIImage = {
         return PaymentSheetImageLibrary.afterpayLogo(currency: currency)
     }()
-    private lazy var infoImage: UIImage = {
-        return PaymentSheetImageLibrary.safeImageNamed("afterpay_icon_info", templateIfAvailable: true)
-    }()
 
     private lazy var infoURL: URL? = {
         let language = locale.stp_languageCode?.lowercased() ?? "en"
@@ -64,68 +61,24 @@ class AfterpayPriceBreakdownView: UIView {
     }
 
     private func makeAfterPayClearPayString() -> NSMutableAttributedString {
-        let stringAttributes = [
-            NSAttributedString.Key.font: theme.fonts.subheadline,
-            .foregroundColor: theme.colors.bodyText,
-        ]
         let template = STPLocalizedString(
             "Buy now or pay later with  <img/>",
             "Promotional text for Afterpay/Clearpay - the image tag will display the Afterpay or Clearpay logo. This text is displayed in a button that lets the customer pay with Afterpay/Clearpay"
         )
-
-        let resultingString = NSMutableAttributedString()
-        resultingString.append(NSAttributedString(string: ""))
-        guard let img = template.range(of: "<img/>") else {
-            return resultingString
-        }
-
-        var imgAppended = false
-
-        for (indexOffset, currCharacter) in template.enumerated() {
-            let currIndex = template.index(template.startIndex, offsetBy: indexOffset)
-            if img.contains(currIndex) {
-                if imgAppended {
-                    continue
-                }
-                imgAppended = true
-                let titleFont = stringAttributes[NSAttributedString.Key.font] as! UIFont
-                let clearPay = attributedStringOfImageWithoutLink(uiImage: afterpayMarkImage, font: titleFont)
-                let infoButton = attributedStringOfImageWithoutLink(uiImage: infoImage, font: titleFont)
-                resultingString.append(clearPay)
-                resultingString.append(NSAttributedString(string: "\u{00A0}\u{00A0}", attributes: stringAttributes))
-                resultingString.append(infoButton)
-            } else {
-                resultingString.append(NSAttributedString(string: String(currCharacter),
-                                                          attributes: stringAttributes))
-            }
-        }
-        return resultingString
-    }
-
-    private func attributedStringOfImageWithoutLink(uiImage: UIImage, font: UIFont, tintColor: UIColor? = nil) -> NSAttributedString {
-        let imageAttachment = NSTextAttachment()
-        imageAttachment.bounds = boundsOfImage(font: font, uiImage: uiImage)
-        if let tintColor {
-            imageAttachment.image = uiImage.withTintColor(tintColor, renderingMode: .alwaysTemplate)
-        } else {
-            imageAttachment.image = uiImage
-        }
-        return NSAttributedString(attachment: imageAttachment)
-    }
-
-    // https://stackoverflow.com/questions/26105803/center-nstextattachment-image-next-to-single-line-uilabel
-    private func boundsOfImage(font: UIFont, uiImage: UIImage) -> CGRect {
-        return CGRect(x: 0,
-                      y: (font.capHeight - uiImage.size.height).rounded() / 2,
-                      width: uiImage.size.width,
-                      height: uiImage.size.height)
+        return NSMutableAttributedString.bnplPromoString(
+            font: theme.fonts.subheadline,
+            foregroundColor: theme.colors.bodyText,
+            template: template,
+            partnerPlaceholder: "<img/>",
+            bnplLogoImage: afterpayMarkImage
+        )
     }
 
     @objc
     private func didTapInfoButton() {
         if let url = infoURL {
             let safariController = SFSafariViewController(url: url)
-            safariController.modalPresentationStyle = .overCurrentContext
+            safariController.modalPresentationStyle = .formSheet
             parentViewController?.present(safariController, animated: true)
         }
     }
