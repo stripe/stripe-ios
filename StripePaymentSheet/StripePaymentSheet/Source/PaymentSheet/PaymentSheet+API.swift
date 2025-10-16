2d3e793b2641f9c0567cd8adf0f43ed16607dda8
Joyce Qin <joyceqin@stripe.com>
2025-10-16 11:26:32 -0400

    cleanup

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheet+API.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheet+API.swift
@@ -734,14 +734,13 @@ extension PaymentSheet {
                 params = STPPaymentIntentParams(clientSecret: paymentIntent.clientSecret, paymentMethodType: paymentMethod.type)
                 params.paymentMethodId = paymentMethod.stripeId
                 params.paymentMethodOptions = paymentMethodoptions
-                params.clientAttributionMetadata = paymentMethodParams.clientAttributionMetadata
             } else {
                 params = STPPaymentIntentParams(clientSecret: paymentIntent.clientSecret)
                 params.paymentMethodParams = paymentMethodParams
                 params.paymentMethodOptions = paymentMethodoptions
-                params.clientAttributionMetadata = paymentMethodParams.clientAttributionMetadata
                 paymentMethodType = paymentMethodParams.type
             }
+            params.clientAttributionMetadata = paymentMethodParams.clientAttributionMetadata
             if let shouldSetAsDefaultPM {
                 params.setAsDefaultPM = NSNumber(value: shouldSetAsDefaultPM)
             }
@@ -792,12 +791,11 @@ extension PaymentSheet {
                     paymentMethodType: paymentMethod.type
                 )
                 params.paymentMethodID = paymentMethod.stripeId
-                params.clientAttributionMetadata = paymentMethodParams.clientAttributionMetadata
             } else {
                 params = STPSetupIntentConfirmParams(clientSecret: setupIntent.clientSecret)
                 params.paymentMethodParams = paymentMethodParams
-                params.clientAttributionMetadata = paymentMethodParams.clientAttributionMetadata
             }
+            params.clientAttributionMetadata = paymentMethodParams.clientAttributionMetadata
             if let shouldSetAsDefaultPM {
                 params.setAsDefaultPM = NSNumber(value: shouldSetAsDefaultPM)
             }
