532220e562fbf683ad0359a3e9d2ccbd6382fe98
bredmond-stripe <104025396+bredmond-stripe@users.noreply.github.com>
2025-10-21 11:17:01 -0400

    Add check-scanning component and callback support (#5721)
    ## Summary
    <!-- Simple summary of what was changed. -->
    Add check-scanning component and callback support
    
    * Add a new view controller for check-scanning which requires callback
    support
    * Add a fully generalized iOS implementation of the mobile supplemental
    function message protocol
    
    Keeping these in the same PR because the new use case helps prove and
    test the callback implementation (would otherwise need a test-only
    supplemental function to do that)
    
    ## Motivation
    <!-- Why are you making this change? If it's for fixing a bug, if
    possible, please include a code snippet or example project that
    demonstrates the issue. -->
    Supporting the check-scanning component in iOS
    
    ## Testing
    <!-- How was the code tested? Be as specific as possible. -->
    <!-- Ignored Tests: Did you newly ignore a test in this PR? If so,
    please open an R4 incident so that the test can be re-enabled as soon as
    possible-->
    Unit test coverage and manual simulator for a test app 
    
    ## Changelog
    <!-- Is this a notable change that affects users? If so, add a line to
    `CHANGELOG.md` and prefix the line with one of the following:
        - [Added] for new features.
        - [Changed] for changes in existing functionality.
        - [Deprecated] for soon-to-be removed features.
        - [Removed] for now removed features.
        - [Fixed] for any bug fixes.
        - [Security] in case of vulnerabilities.
    -->
    
    skip dead code check
    
    - Need to maintain a strong reference to `CheckScanController` even if
    it's not used

---
+++ StripeConnect/StripeConnect/Source/Internal/Webview/MessageHandlers/CallSupplementalFunctionMessageHandler.swift
@@ -0,0 +1,23 @@
+class CallSupplementalFunctionMessageHandler: ScriptMessageHandler<CallSupplementalFunctionMessageHandler.Payload> {
+    struct Payload: Decodable {
+        let functionName: SupplementalFunctionName
+        let invocationId: String
+        let args: SupplementalFunctionArgs
+
+        enum CodingKeys: String, CodingKey {
+            case functionName, invocationId, args
+        }
+
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            functionName = try container.decode(SupplementalFunctionName.self, forKey: .functionName)
+            invocationId = try container.decode(String.self, forKey: .invocationId)
+            let argsDecoder = try container.superDecoder(forKey: .args)
+            args = try SupplementalFunctionArgs.decode(from: argsDecoder, functionName: functionName)
+        }
+    }
+
+    init(analyticsClient: ComponentAnalyticsClient, didReceiveMessage: @escaping (Payload) -> Void) {
+        super.init(name: "callSupplementalFunction", analyticsClient: analyticsClient, didReceiveMessage: didReceiveMessage)
+    }
+}
