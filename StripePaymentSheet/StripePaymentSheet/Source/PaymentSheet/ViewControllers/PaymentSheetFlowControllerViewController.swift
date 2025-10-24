9950823acf3e9af0ce83e80c47778b550df6d3ce
Joyce Qin <joyceqin@stripe.com>
2025-10-24 16:22:50 -0400

    conform protocol

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/ViewControllers/PaymentSheetFlowControllerViewController.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/ViewControllers/PaymentSheetFlowControllerViewController.swift
@@ -23,6 +23,7 @@ class PaymentSheetFlowControllerViewController: UIViewController, FlowController
     let analyticsHelper: PaymentSheetAnalyticsHelper
     let loadResult: PaymentSheetLoader.LoadResult
     var passiveCaptchaChallenge: PassiveCaptchaChallenge?
+    var attestationConfirmationChallenge: AttestationConfirmationChallenge?
     var savedPaymentMethods: [STPPaymentMethod] {
         return savedPaymentOptionsViewController.savedPaymentMethods
     }
@@ -298,7 +299,8 @@ class PaymentSheetFlowControllerViewController: UIViewController, FlowController
             intent: intent,
             elementsSession: elementsSession,
             analyticsHelper: analyticsHelper,
-            passiveCaptchaChallenge: passiveCaptchaChallenge
+            passiveCaptchaChallenge: passiveCaptchaChallenge,
+            attestationConfirmationChallenge: attestationConfirmationChallenge
         ) { [weak self] confirmOption, _ in
             guard let self else { return }
             self.linkConfirmOption = confirmOption
