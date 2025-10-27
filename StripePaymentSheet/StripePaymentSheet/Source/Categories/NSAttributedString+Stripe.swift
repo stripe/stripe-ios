ffc8ae2b23164c30de3d10ee97a65d765a245177
George Birch <gbirch@stripe.com>
2025-10-27 17:23:28 -0500

    Scale BNPL promo string images with font size (#5737)
    ## Summary
    - Scale the BNPL logo and info icon with the font size
    - Reduce space between info icon and text
    - Use text color instead of icon color for info icon
    
    ## Motivation
    Better UI when using small or larger font
    
    ## Testing
    - updated snapshot tests
    - added unit test
    
    ## Changelog
    N/A

--- StripePaymentSheet/StripePaymentSheet/Source/Categories/NSAttributedString+Stripe.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/Categories/NSAttributedString+Stripe.swift
@@ -16,16 +16,23 @@ extension NSMutableAttributedString {
         }
     }
 
+    /// Generates an attributed for use in BNPL info context. Adds line spacing, an info icon at the end, and optionally substitutes a BNPL logo in for a placeholder in the template.
+    /// - Parameters:
+    ///    - template: The promotional text to be displayed, including a placeholder if needed (e.g. "Buy now or pay later with {partner}")
+    ///    - substitution: An optional tuple containing the placeholder text from the template to be replaced and the partner logo image to replace it with.
     static func bnplPromoString(
         font: UIFont,
         textColor: UIColor,
         infoIconColor: UIColor,
         template: String,
-        substitution: (String, UIImage)?
+        substitution: (placeholder: String, bnplLogo: UIImage)?
     ) -> NSMutableAttributedString {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 2
         let stringAttributes = [
             NSAttributedString.Key.font: font,
             .foregroundColor: textColor,
+            .paragraphStyle: paragraphStyle,
         ]
 
         let resultingString = NSMutableAttributedString()
@@ -48,8 +55,8 @@ extension NSMutableAttributedString {
                     }
                     imgAppended = true
 
-                    // Add BNPL logo
-                    let bnplLogo = Self.attributedStringOfImageWithoutLink(uiImage: bnplLogoImage, font: font)
+                    // Add BNPL logo. Use additioanl scale of 2x
+                    let bnplLogo = Self.attributedStringOfImageWithoutLink(uiImage: bnplLogoImage, font: font, additionalScale: 2.0)
                     resultingString.append(bnplLogo)
                 } else {
                     resultingString.append(NSAttributedString(string: String(currCharacter),
@@ -61,12 +68,12 @@ extension NSMutableAttributedString {
             resultingString.append(NSAttributedString(string: template, attributes: stringAttributes))
         }
 
-        // Add info icon
-        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 12)
+        // Add info icon. Use additional scale of 1.5x
+        let symbolConfig = UIImage.SymbolConfiguration(pointSize: font.pointSize)
         if let infoIconImage = UIImage(systemName: "info.circle", withConfiguration: symbolConfig)?
             .withTintColor(infoIconColor, renderingMode: .alwaysTemplate) {
-            let infoIcon = Self.attributedStringOfImageWithoutLink(uiImage: infoIconImage, font: font)
-            resultingString.append(NSAttributedString(string: "\u{00A0}\u{00A0}", attributes: stringAttributes))
+            let infoIcon = Self.attributedStringOfImageWithoutLink(uiImage: infoIconImage, font: font, additionalScale: 1.5)
+            resultingString.append(NSAttributedString(string: "\u{00A0}", attributes: stringAttributes))
             resultingString.append(infoIcon)
         } else {
             stpAssertionFailure("Failed to load system image info.circle")
@@ -75,21 +82,30 @@ extension NSMutableAttributedString {
         return resultingString
     }
 
+    // Returns an attributed string containing only a text attachment for the given image.
+    // The image is scaled so that its height matches the `.capHeight` of the font, and it is vertically centered.
+    // An additionalScale can be provided to make the image taller or shorter than the text.
     private static func attributedStringOfImageWithoutLink(
         uiImage: UIImage,
-        font: UIFont
+        font: UIFont,
+        additionalScale: CGFloat
     ) -> NSAttributedString {
         let imageAttachment = NSTextAttachment()
-        imageAttachment.bounds = boundsOfImage(font: font, uiImage: uiImage)
+        imageAttachment.bounds = boundsOfImage(font: font, uiImage: uiImage, additionalScale: additionalScale)
         imageAttachment.image = uiImage
         return NSAttributedString(attachment: imageAttachment)
     }
 
-    // https://stackoverflow.com/questions/26105803/center-nstextattachment-image-next-to-single-line-uilabel
-    private static func boundsOfImage(font: UIFont, uiImage: UIImage) -> CGRect {
-        return CGRect(x: 0,
-                      y: (font.capHeight - uiImage.size.height).rounded() / 2,
-                      width: uiImage.size.width,
-                      height: uiImage.size.height)
+    // Originally based on https://stackoverflow.com/questions/26105803/center-nstextattachment-image-next-to-single-line-uilabel
+    private static func boundsOfImage(font: UIFont, uiImage: UIImage, additionalScale: CGFloat) -> CGRect {
+        let scaledSize = uiImage.sizeMatchingFont(font, additionalScale: additionalScale)
+        // Calculate the difference in height between the scaled image and the font
+        let heightDifference = font.capHeight - scaledSize.height
+        // To vertically center the image, we want to vertically offset it by half of the height difference between it and the font
+        let verticalOffset = heightDifference.rounded() / 2
+        return CGRect(
+            origin: .init(x: 0, y: verticalOffset),
+            size: scaledSize
+        )
     }
 }
