9c79ac14497b3ed9daf18be3ae97f1ce1c745c1e
Nick Porter <porter@stripe.com>
2025-10-29 15:35:44 -0600

    Revert "deprecate savePaymentMethod"
    This reverts commit 19bd963af360a214dced81fae040cb88ccc43409.

--- StripePayments/StripePayments/Source/API Bindings/Models/PaymentIntents/STPPaymentIntentParams.swift
+++ StripePayments/StripePayments/Source/API Bindings/Models/PaymentIntents/STPPaymentIntentParams.swift
@@ -79,7 +79,6 @@ public class STPPaymentIntentParams: NSObject {
     /// `@YES` to save this PaymentIntent’s PaymentMethod or Source to the associated Customer,
     /// if the PaymentMethod/Source is not already attached.
     /// This should be a boolean NSNumber, so that it can be `nil`
-    @available(*, deprecated, message: "Use setupFutureUsage instead")
     @objc(savePaymentMethod)
     public var savePaymentMethod_objc: NSNumber? {
         get {
@@ -91,7 +90,6 @@ public class STPPaymentIntentParams: NSObject {
 
     /// `true` to save this PaymentIntent’s PaymentMethod or Source to the associated Customer,
     /// if the PaymentMethod/Source is not already attached.
-    @available(*, deprecated, message: "Use setupFutureUsage instead")
     public var savePaymentMethod: Bool?
 
     /// `@YES` to set this PaymentIntent’s PaymentMethod as the associated Customer's default
