b9e0f3fbfc972ef1cb90a4002f64de7c5b16591c
Joyce Qin <joyceqin@stripe.com>
2025-12-02 11:48:24 -0500

    lint

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheet+PaymentMethodAvailability.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/PaymentSheet+PaymentMethodAvailability.swift
@@ -40,7 +40,7 @@ extension PaymentSheet {
         .swish,
         .twint,
         .multibanco,
-        .payPay
+        .payPay,
     ]
 
     /// A list of `STPPaymentMethodType` that can be saved in PaymentSheet
