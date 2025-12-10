2aefe3c081584ea681357fe1285f4d0489a5459b
Nick Porter <porter@stripe.com>
2025-12-10 16:27:34 -0700

    Make it more clear

--- Example/PaymentSheet Example/PaymentSheet Example/PlaygroundController.swift
+++ Example/PaymentSheet Example/PaymentSheet Example/PlaygroundController.swift
@@ -873,7 +873,7 @@ extension PlaygroundController {
             "link_mode": settings.linkEnabledMode.rawValue,
             "use_manual_confirmation": settings.integrationType == .deferred_mc,
             "require_cvc_recollection": settings.requireCVCRecollection == .on,
-            "is_confirmation_token": settings.confirmationMode == .confirmationToken && settings.integrationType != .normal,
+            "is_confirmation_token": settings.confirmationMode == .confirmationToken && !settings.integrationType.isIntentFirst,
             "customer_session_component_name": "mobile_payment_element",
             "customer_session_payment_method_save": settings.paymentMethodSave.rawValue,
             "customer_session_payment_method_remove": settings.paymentMethodRemove.rawValue,
