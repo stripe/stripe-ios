2861695c8dbfadeb7a9aed4ea53d40d1e6e9ac9b
Nick Porter <88012362+porter-stripe@users.noreply.github.com>
2025-12-10 11:22:04 -0700

    Update TextFieldElement+CardTest.swift

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/TextFieldElement+CardTest.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/TextFieldElement+CardTest.swift
@@ -398,9 +398,7 @@ class TextFieldElementCardTest: STPNetworkStubbingTestCase {
     }
 
     func testCardSectionElement_cardFundingFiltering_showsWarningButRemainsValid() {
-        let originalPublishableKey = STPAPIClient.shared.publishableKey
         STPAPIClient.shared.publishableKey = STPTestingDefaultPublishableKey
-        defer { STPAPIClient.shared.publishableKey = originalPublishableKey }
 
         // Set up a CardSectionElement with funding filter that only allows debit:
         let cardSection = CardSectionElement(
