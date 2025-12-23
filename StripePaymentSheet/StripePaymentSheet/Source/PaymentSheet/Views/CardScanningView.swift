cf7751c3522e23acdd7d2f2028e268c53750e3ee
George Birch <gbirch@stripe.com>
2025-12-23 14:12:25 -0800

    fix cardscanningview required init

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/CardScanningView.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/CardScanningView.swift
@@ -270,7 +270,7 @@ class CardScanningView: UIView {
     }
 
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
