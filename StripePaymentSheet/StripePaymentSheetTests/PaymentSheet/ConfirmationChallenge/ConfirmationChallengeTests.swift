fb88ba07e7392173e2e9ea862b0ff9056c597446
Joyce Qin <joyceqin@stripe.com>
2025-12-23 10:37:03 -0500

    tests

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/ConfirmationChallenge/ConfirmationChallengeTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/ConfirmationChallenge/ConfirmationChallengeTests.swift
@@ -236,4 +236,43 @@ class ConfirmationChallengeTests: XCTestCase {
         // should return nil due to timeout
         XCTAssertNil(assertion)
     }
+
+    // MARK: - makeRadarOptions Payment Method Type Tests
+    func testMakeRadarOptionsForCard() async throws {
+        let confirmationChallenge = ConfirmationChallenge(enablePassiveCaptcha: true, enableAttestation: true, elementsSession: elementsSession, stripeAttest: stripeAttest)
+        await confirmationChallenge.setTimeout(timeout: 30)
+        // wait to make sure that the tokens will be ready by the time we call makeRadarOptions
+        try await Task.sleep(nanoseconds: 6_000_000_000)
+        let radarOptions = await confirmationChallenge.makeRadarOptions(for: .card)
+        // Card payment methods should return radar options
+        XCTAssertNotNil(radarOptions)
+        XCTAssertNotNil(radarOptions?.hcaptchaToken)
+        XCTAssertNotNil(radarOptions?.iosVerificationObject)
+        await confirmationChallenge.complete()
+    }
+
+    func testMakeRadarOptionsForLink() async throws {
+        let confirmationChallenge = ConfirmationChallenge(enablePassiveCaptcha: true, enableAttestation: true, elementsSession: elementsSession, stripeAttest: stripeAttest)
+        await confirmationChallenge.setTimeout(timeout: 30)
+        // wait to make sure that the tokens will be ready by the time we call makeRadarOptions
+        try await Task.sleep(nanoseconds: 6_000_000_000)
+        let radarOptions = await confirmationChallenge.makeRadarOptions(for: .link)
+        // Link payment methods should return radar options
+        XCTAssertNotNil(radarOptions)
+        XCTAssertNotNil(radarOptions?.hcaptchaToken)
+        XCTAssertNotNil(radarOptions?.iosVerificationObject)
+        await confirmationChallenge.complete()
+    }
+
+    func testMakeRadarOptionsForUSBankAccount() async throws {
+        let confirmationChallenge = ConfirmationChallenge(enablePassiveCaptcha: true, enableAttestation: true, elementsSession: elementsSession, stripeAttest: stripeAttest)
+        await confirmationChallenge.setTimeout(timeout: 30)
+        // wait to make sure that the tokens would be ready if they were needed
+        try await Task.sleep(nanoseconds: 6_000_000_000)
+        let radarOptions = await confirmationChallenge.makeRadarOptions(for: .USBankAccount)
+        // US Bank Account payment methods should not return radar options
+        XCTAssertNil(radarOptions)
+        await confirmationChallenge.complete()
+    }
+
 }
