6a8cd6db07192ceffa2bdbac5ae4e050bbe3e56f
George Birch <gbirch@stripe.com>
2026-01-21 14:53:59 -0600

    Document iOS 26 confirm button text color changes (#5940)
    ## Summary
    Add comments explaining intended but confusing confirm button text color
    change
    
    ## Motivation
    <!-- Why are you making this change? If it's for fixing a bug, if
    possible, please include a code snippet or example project that
    demonstrates the issue. -->
    
    ## Testing
    <!-- How was the code tested? Be as specific as possible. -->
    <!-- Ignored Tests: Did you newly ignore a test in this PR? If so,
    please open an R4 incident so that the test can be re-enabled as soon as
    possible-->
    
    ## Changelog
    <!-- Is this a notable change that affects users? If so, add a line to
    `CHANGELOG.md` and prefix the line with one of the following:
        - [Added] for new features.
        - [Changed] for changes in existing functionality.
        - [Deprecated] for soon-to-be removed features.
        - [Removed] for now removed features.
        - [Fixed] for any bug fixes.
        - [Security] in case of vulnerabilities.
    -->

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/ConfirmButton.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/ConfirmButton.swift
@@ -503,6 +503,8 @@ class ConfirmButton: UIControl {
         }
 
         // if foreground is set prefer that over a dynamic contrasting color in all other states
+        // Note: With the default .systemBlue button color in dark mode, background.contrastingColor is white
+        //      in iOS <26 and black in iOS >26 due to system color changes. This is expected and intended.
         return overriddenForegroundColor ?? background.contrastingColor
     }
 
