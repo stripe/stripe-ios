31da167998903fb99977a473cdab12e957d6220f
davidme-stripe <52758633+davidme-stripe@users.noreply.github.com>
2026-01-21 13:58:02 -0800

    Update copyright year in STPPaymentMethodTwintParams.swift

--- StripePayments/StripePayments/Source/API Bindings/Models/PaymentMethods/Types/STPPaymentMethodTwintParams.swift
+++ StripePayments/StripePayments/Source/API Bindings/Models/PaymentMethods/Types/STPPaymentMethodTwintParams.swift
@@ -2,7 +2,7 @@
 //  STPPaymentMethodTwintParams.swift
 //  StripePayments
 //
-//  Copyright © 2024 Stripe, Inc. All rights reserved.
+//  Copyright © 2026 Stripe, Inc. All rights reserved.
 //
 
 import Foundation
