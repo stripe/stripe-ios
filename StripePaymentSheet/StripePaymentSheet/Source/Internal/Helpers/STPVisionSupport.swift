18e8901bd9b948b77ab82571e162f8fcfbde4e42
David Estes <davidestes@stripe.com>
2026-02-03 15:23:39 -0800

    Move out helper file

--- StripePaymentSheet/StripePaymentSheet/Source/Internal/Helpers/STPVisionSupport.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/Internal/Helpers/STPVisionSupport.swift
@@ -11,14 +11,14 @@ import UIKit
 /// Creates a fallback ASPresentationAnchor when no window is available.
 /// On visionOS, UIWindow() without a scene is deprecated, so we find a scene first.
 func stp_makeFallbackPresentationAnchor() -> ASPresentationAnchor {
+    #if !os(visionOS)
+    return ASPresentationAnchor()
+    #else
     if let windowScene = UIApplication.shared.connectedScenes
         .compactMap({ $0 as? UIWindowScene })
         .first {
         return UIWindow(windowScene: windowScene)
     }
-    #if os(visionOS)
-    fatalError("No window scene available for ASPresentationAnchor on visionOS")
-    #else
-    return ASPresentationAnchor()
+    fatalError()
     #endif
 }
