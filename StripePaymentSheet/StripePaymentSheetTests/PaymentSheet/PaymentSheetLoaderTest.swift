198ad2daccedd5dc41f30e27e8170a357757f7dc
Yuki <yuki@stripe.com>
2026-02-18 13:51:20 -0800

    Don't load Customer for CustomerSession; it's already on the v1/e/s response!

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentSheetLoaderTest.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentSheetLoaderTest.swift
@@ -20,6 +20,7 @@ final class PaymentSheetLoaderTest: STPNetworkStubbingTestCase {
     override func setUp() {
         super.setUp()
         self.apiClient = STPAPIClient(publishableKey: STPTestingDefaultPublishableKey)
+        LinkAccountContext.shared.account = nil
     }
     lazy var configuration: PaymentSheet.Configuration = {
         var config = PaymentSheet.Configuration()
@@ -34,7 +35,6 @@ final class PaymentSheetLoaderTest: STPNetworkStubbingTestCase {
         super.tearDown()
     }
 
-    @MainActor
     func testPaymentSheetLoadWithPaymentIntent() async throws {
         let expectation = XCTestExpectation(description: "Load w/ PaymentIntent")
         let types = ["ideal", "card", "bancontact"]
@@ -607,6 +607,58 @@ final class PaymentSheetLoaderTest: STPNetworkStubbingTestCase {
             }
         }
         await fulfillment(of: [loadExpectation], timeout: STPTestingNetworkRequestTimeout)
+    }
+
+    func testLoaderLooksUpLink_EphemeralKey() async throws {
+        var configuration = self.configuration
+        // A hardcoded test Customer w/ email and attached card, us bank account
+        let testCustomerID = "cus_TqanA973bOrpoP"
+
+        // Create a new CS for the Customer
+        let customerAndEphemeralKey = try await STPTestingAPIClient.shared().fetchCustomerAndEphemeralKey(customerID: testCustomerID, merchantCountry: "us")
+        configuration.customer = .init(id: testCustomerID, ephemeralKeySecret: customerAndEphemeralKey.ephemeralKeySecret)
+        // This should disable us bank account, a delayed payment method
+        configuration.allowsDelayedPaymentMethods = false
+
+        // Loading w/ ^ customer...
+        let loadResult = try await PaymentSheetLoader.load(
+            mode: .deferredIntent(._testValue()),
+            configuration: configuration,
+            analyticsHelper: .init(integrationShape: .flowController, configuration: configuration),
+            integrationShape: .flowController
+        )
+        // ...should have 1 saved pm (us bank is filtered out)
+        XCTAssertEqual(loadResult.savedPaymentMethods.count, 1)
+        // ...and looks up link
+        XCTAssertNotNil(LinkAccountContext.shared.account)
+        XCTAssertEqual(LinkAccountContext.shared.account?.email, "yuki@stripe.com")
+    }
+
+    func testLoaderLooksUpLink_CustomerSession() async throws {
+        var configuration = self.configuration
+        // A hardcoded test Customer w/ email and attached card, us bank account
+        let testCustomerID = "cus_TqanA973bOrpoP"
+
+        // Create a new CS for the Customer
+        let customerSession = try await STPTestingAPIClient.shared().fetchCustomerAndCustomerSessionClientSecret(customerID: testCustomerID, merchantCountry: "us")
+        configuration.customer = .init(id: testCustomerID, customerSessionClientSecret: customerSession.customerSessionClientSecret)
+        // This should disable us bank account, a delayed payment method
+        configuration.allowsDelayedPaymentMethods = false
+
+        // Loading w/ ^ customer...
+        let loadResult = try await PaymentSheetLoader.load(
+            mode: .deferredIntent(._testValue()),
+            configuration: configuration,
+            analyticsHelper: .init(integrationShape: .flowController, configuration: configuration),
+            integrationShape: .flowController
+        )
+        // ...should have 1 saved pm (us bank is filtered out)
+        XCTAssertEqual(loadResult.savedPaymentMethods.count, 1)
+        // ...and the email
+        XCTAssertEqual(loadResult.elementsSession.customer?.email, "yuki@stripe.com")
+        // ...and looks up link
+        XCTAssertNotNil(LinkAccountContext.shared.account)
+        XCTAssertEqual(LinkAccountContext.shared.account?.email, "yuki@stripe.com")
     }
 
     func testLoadPerformance() {
