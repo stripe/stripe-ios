a238d17f05ca44746658355e60fa22cc9a4d264f
Yuki <yuki@stripe.com>
2026-02-27 14:07:59 -0800

    Fetch Customer earlier for Ephemeral Key (#6107)
    ## Summary
    Retrieve the Customer immediately, in parallel with fetching Elements
    Sessions.
    
    ## Motivation
    See
    https://docs.google.com/document/d/1nQdXDF4tzWEWCSGcy0XEq6pzDl6NuHz_0_8O3soCkj0/edit?tab=t.0#heading=h.d5dzy3udlijc
    
    ## Testing
    Relying on existing tests 
    
    ## Changelog
    I'm going to merge this at the same time as some future PRs related to
    latency improvements; will add changelog in one of the later PRs.

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentSheetLoaderTest.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentSheetLoaderTest.swift
@@ -5,6 +5,8 @@
 //  Created by Yuki Tokuhiro on 6/24/23.
 //
 
+import OHHTTPStubs
+import OHHTTPStubsSwift
 @testable@_spi(STP) import StripeCore
 @testable@_spi(STP) import StripeCoreTestUtils
 @testable@_spi(STP) import StripePayments
@@ -614,7 +616,7 @@ final class PaymentSheetLoaderTest: STPNetworkStubbingTestCase {
         // A hardcoded test Customer w/ email and attached card, us bank account
         let testCustomerID = "cus_TqanA973bOrpoP"
 
-        // Create a new CS for the Customer
+        // Create a new EK for the Customer
         let customerAndEphemeralKey = try await STPTestingAPIClient.shared().fetchCustomerAndEphemeralKey(customerID: testCustomerID, merchantCountry: "us")
         configuration.customer = .init(id: testCustomerID, ephemeralKeySecret: customerAndEphemeralKey.ephemeralKeySecret)
         // This should disable us bank account, a delayed payment method
@@ -632,6 +634,34 @@ final class PaymentSheetLoaderTest: STPNetworkStubbingTestCase {
         // ...and looks up link
         XCTAssertNotNil(LinkAccountContext.shared.account)
         XCTAssertEqual(LinkAccountContext.shared.account?.email, "yuki@stripe.com")
+    }
+
+    func test_loader_doesnt_fetch_Customer_when_default_billing_email() async throws {
+        var configuration = self.configuration
+        // A hardcoded test Customer w/ yuki@stripe.com email and attached card, us bank account
+        let testCustomerID = "cus_TqanA973bOrpoP"
+
+        // Create a new EK for the Customer
+        let customerAndEphemeralKey = try await STPTestingAPIClient.shared().fetchCustomerAndEphemeralKey(customerID: testCustomerID, merchantCountry: "us")
+        configuration.customer = .init(id: testCustomerID, ephemeralKeySecret: customerAndEphemeralKey.ephemeralKeySecret)
+        configuration.defaultBillingDetails.email = "davidestes@stripe.com"
+
+        // Stub the customer endpoint to fail if called
+        stub(condition: isPath("/v1/customers/\(testCustomerID)")) { _ in
+            XCTFail("Customer endpoint should not be called when default billing email is provided")
+            return HTTPStubsResponse(data: Data(), statusCode: 500, headers: nil)
+        }
+
+        // Loading w/ ^ customer...
+        _ = try await PaymentSheetLoader.load(
+            mode: .deferredIntent(._testValue()),
+            configuration: configuration,
+            analyticsHelper: .init(integrationShape: .flowController, configuration: configuration),
+            integrationShape: .flowController
+        )
+        // ...and looks up link, preferring the default email provided
+        XCTAssertNotNil(LinkAccountContext.shared.account)
+        XCTAssertEqual(LinkAccountContext.shared.account?.email, configuration.defaultBillingDetails.email)
     }
 
     func testLoaderLooksUpLink_CustomerSession() async throws {
