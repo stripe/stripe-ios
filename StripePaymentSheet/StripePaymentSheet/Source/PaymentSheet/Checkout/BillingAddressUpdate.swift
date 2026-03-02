f0a589973b6509804b7db96e897c8caa9d8085b9
Nick Porter <porter@stripe.com>
2026-03-02 14:03:30 -0700

    start on address apis

---
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Checkout/BillingAddressUpdate.swift
@@ -0,0 +1,30 @@
+//
+//  BillingAddressUpdate.swift
+//  StripePaymentSheet
+//
+//  Created by Nick Porter on 3/2/26.
+//  Copyright © 2026 Stripe, Inc. All rights reserved.
+//
+
+import Foundation
+
+@_spi(CheckoutSessionsPreview)
+extension Checkout {
+    /// Parameters for ``updateBillingAddress(_:)``.
+    public struct BillingAddressUpdate {
+        /// The customer's full name.
+        public let name: String?
+
+        /// The customer's billing address.
+        public let address: Address
+
+        /// Creates a billing address update.
+        /// - Parameters:
+        ///   - name: The customer's full name.
+        ///   - address: The customer's billing address.
+        public init(name: String? = nil, address: Address) {
+            self.name = name
+            self.address = address
+        }
+    }
+}
