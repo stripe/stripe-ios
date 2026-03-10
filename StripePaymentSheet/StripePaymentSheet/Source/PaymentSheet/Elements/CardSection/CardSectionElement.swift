925fa660785aaa9ac2df2e727e570b56994681a0
Joyce Qin <joyceqin@stripe.com>
2026-03-09 23:48:49 -0400

    has allowed

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Elements/CardSection/CardSectionElement.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Elements/CardSection/CardSectionElement.swift
@@ -333,11 +333,11 @@ final class CardSectionElement: ContainerElement {
             hasInteractedWithCBCElement = true
         }
 
-        let noAllowedBrands = cardBrands.filter({ cardBrandFilter.isAccepted(cardBrand: $0) }).isEmpty
+        let hasAllowedBrand = !cardBrands.filter({ cardBrandFilter.isAccepted(cardBrand: $0) }).isEmpty
         let shouldShow = panElement.isEditing
             && cardBrands.count > 1
             && !hasInteractedWithCBCElement
-            && !noAllowedBrands
+            && hasAllowedBrand
 
         // If the CBC tooltip has not been installed in the view, set it up
         if cbcTooltip.superview == nil {
