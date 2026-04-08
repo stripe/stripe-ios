118c33532031c1207ab93be1cdc2dcaa4a556034
John Woo <99628984+wooj-stripe@users.noreply.github.com>
2026-04-08 13:36:00 -0700

    Vend card art in CustomerSheet's api (#6316)
    ## Summary
    Adds ability to fetch card art behind client side flag
    
    ## Motivation
    Card art testing
    
    ## Testing
    Manually
    
    ## Changelog
    <!-- Is this a notable change that affects users? If so, add a line to
    `CHANGELOG.md` and prefix the line with one of the following:
        - [Added] for new features.
        - [Changed] for changes in existing functionality.
        - [Deprecated] for soon-to-be removed features.
        - [Removed] for now removed features.
        - [Fixed] for any bug fixes.
        - [Security] in case of vulnerabilities.
    -->

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/CustomerSheet/CustomerSheetConfiguration.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/CustomerSheet/CustomerSheetConfiguration.swift
@@ -108,9 +108,14 @@ extension CustomerSheet {
         /// A Stripe payment method was selected
         case paymentMethod(paymentMethod: STPPaymentMethod, paymentOptionDisplayData: PaymentOptionDisplayData)
 
+        /// Temporary flag for card art development/testing. Will be removed before GA.
+        @_spi(STP) public static var _cardArtEnabled: Bool = false
+
         /// Create a PaymentOptionSelection for a saved payment method.
         public static func paymentMethod(_ paymentMethod: STPPaymentMethod) -> PaymentOptionSelection {
-            let data = PaymentOptionDisplayData(image: paymentMethod.makeIcon(), label: paymentMethod.paymentSheetLabel)
+            let paymentMethodIcon = paymentMethod.makeIcon()
+            let image = paymentMethod.cardArtImage(cardArtEnabled: _cardArtEnabled) ?? paymentMethodIcon
+            let data = PaymentOptionDisplayData(image: image, label: paymentMethod.paymentSheetLabel)
             return .paymentMethod(paymentMethod: paymentMethod, paymentOptionDisplayData: data)
         }
 
