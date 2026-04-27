248657a99c371e7b48ce5832064d4d2cc97461fc
sophiah2-stripe <sophiah@stripe.com>
2026-04-26 21:17:07 -0700

    resolve errors

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentMethodAvailabilityTests.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/PaymentMethodAvailabilityTests.swift
@@ -23,7 +23,7 @@ final class PaymentMethodAvailabilityTests: XCTestCase {
             linkSettings: ._testValue(brand: .link)
         )
         var configuration = PaymentSheet.Configuration()
-        configuration.link = .init(brand: .notlink)
+        configuration.link.brand = .notlink
 
         XCTAssertEqual(configuration.resolvedLinkBrand(elementsSession: elementsSession), .notlink)
     }
