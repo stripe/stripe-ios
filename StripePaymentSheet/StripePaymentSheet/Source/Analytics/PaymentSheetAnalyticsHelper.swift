ce2341a32530e1bf8aafb2c0cb233263e406c0fe
Nick Porter <88012362+porter-stripe@users.noreply.github.com>
2026-05-04 10:06:56 -0600

    Remove AP selector from PaymentSheet complete (#6369)
    ## Summary
    - Removes the AP selector inside of PaymentSheet
    - Removes the ability to update PaymentSheet while open (no longer
    needed)
    - Removes any related code
    
    ## Motivation
    - Removing in favor of the standalone element
    
    ## Testing
    - CI
    
    ## Changelog
    N/A

--- StripePaymentSheet/StripePaymentSheet/Source/Analytics/PaymentSheetAnalyticsHelper.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/Analytics/PaymentSheetAnalyticsHelper.swift
@@ -472,20 +472,6 @@ final class PaymentSheetAnalyticsHelper {
         log(event: .shopPayWebviewCancelled, params: ["did_receive_ece_click": didReceiveECEClick])
     }
 
-    // MARK: - Adaptive Pricing
-
-    func logAdaptivePricingCurrencySelectorInit(isStandaloneElement: Bool) {
-        log(event: .adaptivePricingCurrencySelectorInit, params: ["is_standalone_element": isStandaloneElement])
-    }
-
-    func logAdaptivePricingCurrencyToggled() {
-        log(event: .adaptivePricingCurrencyToggled)
-    }
-
-    func logAdaptivePricingCurrencyToggledFailed(error: Error) {
-        log(event: .adaptivePricingCurrencyToggledFailed, error: error)
-    }
-
     func log(
         event: STPAnalyticEvent,
         duration: TimeInterval? = nil,
