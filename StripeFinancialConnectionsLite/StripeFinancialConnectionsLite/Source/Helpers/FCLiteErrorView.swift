88b8d62c55ee0d9768592c4aebed3d607635f0e3
Mat Schmid <mats@stripe.com>
2026-05-20 11:54:25 -0400

    Extract FC Lite into its own module

--- StripeFinancialConnectionsLite/StripeFinancialConnectionsLite/Source/Helpers/FCLiteErrorView.swift
+++ StripeFinancialConnectionsLite/StripeFinancialConnectionsLite/Source/Helpers/FCLiteErrorView.swift
@@ -1,6 +1,6 @@
 //
 //  FCLiteErrorView.swift
-//  StripePaymentSheet
+//  StripeFinancialConnectionsLite
 //
 //  Created by Mat Schmid on 2025-03-19.
 //
--- StripeFinancialConnectionsLite/StripeFinancialConnectionsLite/Source/Helpers/FCLiteModalPresentationWrapper.swift
+++ StripeFinancialConnectionsLite/StripeFinancialConnectionsLite/Source/Helpers/FCLiteModalPresentationWrapper.swift
@@ -1,6 +1,6 @@
 //
 //  FCLiteModalPresentationWrapper.swift
-//  StripePaymentSheet
+//  StripeFinancialConnectionsLite
 //
 //  Created by Mat Schmid on 2025-03-19.
 //
