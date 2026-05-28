2d871905e47ee0d475201faff142835294034968
Michael Liberatore <michael@lickability.net>
2026-05-28 13:09:41 -0400

    `APIErrorContext` cleanup

--- StripeCryptoOnramp/StripeCryptoOnramp/Source/Components/Errors/APIErrorContext.swift
+++ StripeCryptoOnramp/StripeCryptoOnramp/Source/Components/Errors/APIErrorContext.swift
@@ -82,77 +82,37 @@ public extension APIErrorContextProviding {
 public struct APIErrorContext {
 
     /// The backend `reason` value associated with this error, if one is available.
-    public let reason: String?
+    public var reason: String?
 
     /// The SDK operation that was running when this error occurred.
-    public let operation: String
+    public var operation: String
 
     /// The bundle identifier for the app using the SDK, if one is available.
-    public let appIdentifier: String?
+    public var appIdentifier: String?
 
     /// The Stripe mode associated with this error, if it can be determined.
-    public let mode: String?
+    public var mode: String?
 
     /// The Stripe iOS SDK version.
-    public let sdkVersion: String
+    public var sdkVersion: String
 
     /// The backend API error code associated with this error, if one is available.
-    public let apiErrorCode: String?
+    public var apiErrorCode: String?
 
     /// The backend API error type associated with this error, if one is available.
-    public let apiErrorType: String?
+    public var apiErrorType: String?
 
     /// The backend developer-facing API error message associated with this error, if one is available.
-    public let apiErrorMessage: String?
+    public var apiErrorMessage: String?
 
     /// The backend user-facing API error message associated with this error, if one is available.
-    public let apiUserMessage: String?
+    public var apiUserMessage: String?
 
     /// A URL to documentation for this error, if one is available.
-    public let docURL: URL?
+    public var docURL: URL?
 
     /// The original error that was mapped to this error.
-    public let underlyingError: Swift.Error
-
-    /// Creates shared API error context.
-    ///
-    /// - Parameters:
-    ///   - reason: The backend `reason` value associated with this error, if one is available.
-    ///   - operation: The SDK operation that was running when this error occurred.
-    ///   - appIdentifier: The bundle identifier for the app using the SDK, if one is available.
-    ///   - mode: The Stripe mode associated with this error, if it can be determined.
-    ///   - sdkVersion: The Stripe iOS SDK version.
-    ///   - apiErrorCode: The backend API error code associated with this error, if one is available.
-    ///   - apiErrorType: The backend API error type associated with this error, if one is available.
-    ///   - apiErrorMessage: The backend developer-facing API error message associated with this error, if one is available.
-    ///   - apiUserMessage: The backend user-facing API error message associated with this error, if one is available.
-    ///   - docURL: A URL to documentation for this error, if one is available.
-    ///   - underlyingError: The original error that was mapped to this error.
-    public init(
-        reason: String?,
-        operation: String,
-        appIdentifier: String?,
-        mode: String?,
-        sdkVersion: String,
-        apiErrorCode: String?,
-        apiErrorType: String?,
-        apiErrorMessage: String?,
-        apiUserMessage: String?,
-        docURL: URL?,
-        underlyingError: Swift.Error
-    ) {
-        self.reason = reason
-        self.operation = operation
-        self.appIdentifier = appIdentifier
-        self.mode = mode
-        self.sdkVersion = sdkVersion
-        self.apiErrorCode = apiErrorCode
-        self.apiErrorType = apiErrorType
-        self.apiErrorMessage = apiErrorMessage
-        self.apiUserMessage = apiUserMessage
-        self.docURL = docURL
-        self.underlyingError = underlyingError
-    }
+    public var underlyingError: Swift.Error
 
     /// The Stripe API request ID associated with this error, if one is available.
     public var requestID: String? {
