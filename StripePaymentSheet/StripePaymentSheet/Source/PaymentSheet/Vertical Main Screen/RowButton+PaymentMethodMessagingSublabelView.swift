b10cd6a985819869b55185dd3675637ad24d35fd
George Birch <gbirch@stripe.com>
2026-06-02 17:43:53 -0500

    Use correct UserInterfaceStyle when showing PaymentMethodMessaging modal in sublabel view (#6540)
    ## Summary
    - Use correct UserInterfaceStyle when showing PaymentMethodMessaging
    modal in sublabel view
    - Simplify derivation of UserInterfaceStyle for BNPLFormHeaderView by
    just checking the style in the view (which is already set according to
    the config)
    
    ## Motivation
    <!-- Why are you making this change? If it's for fixing a bug, if
    possible, please include a code snippet or example project that
    demonstrates the issue. -->
    
    ## Testing
    - existing unit and snapshot tests
    
    ## Changelog
    <!-- Is this a notable change that affects users? If so, add a line to
    `CHANGELOG.md` and prefix the line with one of the following:
        - [Added] for new features.
        - [Changed] for changes in existing functionality.
        - [Deprecated] for soon-to-be removed features.
        - [Removed] for now removed features.
        - [Fixed] for any bug fixes.
        - [Security] in case of vulnerabilities.
    -->

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Vertical Main Screen/RowButton+PaymentMethodMessagingSublabelView.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Vertical Main Screen/RowButton+PaymentMethodMessagingSublabelView.swift
@@ -134,7 +134,7 @@ extension RowButton {
                 return
             }
 
-            PMMEInfoModal.present(infoUrl: infoUrl, style: .automatic, from: self)
+            PMMEInfoModal.present(infoUrl: infoUrl, style: traitCollection.isDarkMode ? .alwaysDark : .alwaysLight, from: self)
         }
     }
 }
