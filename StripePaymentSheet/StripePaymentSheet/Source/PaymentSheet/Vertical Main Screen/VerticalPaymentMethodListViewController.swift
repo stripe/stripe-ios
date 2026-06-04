3eaadf1dcd7132cfe73d02abc1cdaa8b6068b856
George Birch <gbirch@stripe.com>
2026-06-04 11:22:04 -0500

    PMM Experiment — Add analytics; wire up form header to data source (#6542)
    ## Summary
    - add analytics (not exposure logging — that was done in a previous PR)
    - final wiring — enable the experiment form header by attaching it to
    the data source
    
    ## Motivation
    
    https://docs.google.com/document/d/1pi9vehSvFpT80lgEYwBmE_NCcDE5nPTnRrhBrYqlzUQ/edit?tab=t.0#heading=h.4y6tyjyk6w3h
    
    ## Testing
    - added analytics tests
    
    ## Changelog
    <!-- Is this a notable change that affects users? If so, add a line to
    `CHANGELOG.md` and prefix the line with one of the following:
        - [Added] for new features.
        - [Changed] for changes in existing functionality.
        - [Deprecated] for soon-to-be removed features.
        - [Removed] for now removed features.
        - [Fixed] for any bug fixes.
        - [Security] in case of vulnerabilities.
    -->
    
    ---------
    
    Co-authored-by: Bitrise CI <mobile-sdk-team@stripe.com>

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Vertical Main Screen/VerticalPaymentMethodListViewController.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Vertical Main Screen/VerticalPaymentMethodListViewController.swift
@@ -250,7 +250,10 @@ class VerticalPaymentMethodListViewController: UIViewController {
 
     func didTap(rowButton: RowButton, selection: RowButtonType) {
         guard let delegate else { return }
-        let shouldSelect = !delegate.willDisplayForm(rowButton.type)
+        // We set shouldSelect to be false if the selection is not actually changing to avoid de-selecting and then re-selecting the row
+        // We intentionally do not guard / exit early on this condition to preserve the call to delegate.didTapPaymentMethod()
+        //      and the analytics logging it does, which historically has been called in this scenario.
+        let shouldSelect = !delegate.willDisplayForm(rowButton.type) && currentSelection != selection
         if shouldSelect {
             // Deselect previous row
             rowButtons.forEach {
