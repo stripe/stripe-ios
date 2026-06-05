c3b02869dcd546881c09e16405dd2e12e2954de3
Mat Schmid <mats@stripe.com>
2026-06-04 21:36:19 -0400

    Move supportedPaymentMethods to LinkConfiguration

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Link/LinkConfiguration.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Link/LinkConfiguration.swift
@@ -8,21 +8,38 @@
 import Foundation
 
 /// Injectible configuration for Link behavior and content.
-@_spi(STP)
+@_spi(STP) @_spi(LinkControllerPreview)
 public struct LinkConfiguration {
 
     /// Custom hint message to display in the wallet view. If `nil`, no hint will be shown.
-    public let hintMessage: String?
+    @_spi(STP) public let hintMessage: String?
 
     /// Whether to allow the user to log out. When `false`, the logout menu button will be hidden.
-    public let allowLogout: Bool
+    @_spi(STP) public let allowLogout: Bool
+
+    /// The payment method types to support in the Link sheet. If `nil`, all available types are shown.
+    @_spi(LinkControllerPreview) public let supportedPaymentMethodTypes: [LinkPaymentMethodType]?
 
     /// Creates a new instance of `LinkConfiguration`.
     /// - Parameters:
     ///   - hintMessage: Custom hint message to display in the wallet view. If `nil`, no hint will be shown.
     ///   - allowLogout: Whether to allow the user to log out. When `false`, the logout menu button will be hidden. Defaults to `true`.
-    public init(hintMessage: String? = nil, allowLogout: Bool = true) {
+    @_spi(STP) public init(
+        hintMessage: String? = nil,
+        allowLogout: Bool = true
+    ) {
         self.hintMessage = hintMessage
         self.allowLogout = allowLogout
+        self.supportedPaymentMethodTypes = nil
+    }
+
+    /// Creates a new instance of `LinkConfiguration`.
+    /// - Parameter supportedPaymentMethodTypes: The payment method types to support in the Link sheet. If `nil`, all available types are shown.
+    @_spi(LinkControllerPreview) public init(
+        supportedPaymentMethodTypes: [LinkPaymentMethodType]? = nil
+    ) {
+        self.hintMessage = nil
+        self.allowLogout = true
+        self.supportedPaymentMethodTypes = supportedPaymentMethodTypes
     }
 }
