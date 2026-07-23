431dc011718c71700ebe70cfe588f5f3cf1a0355
Joyce Qin <joyceqin@stripe.com>
2026-07-23 18:55:32 -0400

    remove redundant check

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Checkout/Express Checkout Element/ExpressCheckoutElementView.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Checkout/Express Checkout Element/ExpressCheckoutElementView.swift
@@ -120,7 +120,6 @@ extension Checkout.ExpressCheckoutElementView {
             switch button {
             case .applePay:
                 if configuration.applePayConfiguration != nil
-                    && session.elementsSession.isApplePayEnabled
                     && StripeAPI.deviceSupportsApplePay() {
                     buttons.append(.applePay)
                 }
