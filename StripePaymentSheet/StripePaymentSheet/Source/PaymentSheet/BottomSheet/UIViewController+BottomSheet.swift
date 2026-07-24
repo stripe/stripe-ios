a484d96afcccd2e0f932c0718109e3ccc9750e5b
Nick Porter <porter@stripe.com>
2026-07-24 11:11:00 -0600

    Remove dispatch queue

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/BottomSheet/UIViewController+BottomSheet.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/BottomSheet/UIViewController+BottomSheet.swift
@@ -38,12 +38,8 @@ extension UIViewController {
         }
 
         // Prevent the presenting view from regaining focus when editing ends in the bottom sheet.
-        // Both calls are dispatched together (preserving their relative order) so endEditing's
-        // state mutation happens outside the current SwiftUI view update pass.
-        DispatchQueue.main.async { [weak self] in
-            self?.viewIfLoaded?.endEditing(true)
-            self?.present(viewControllerToPresent, animated: true, completion: completion)
-        }
+        viewIfLoaded?.endEditing(true)
+        present(viewControllerToPresent, animated: true, completion: completion)
     }
 
     var bottomSheetController: BottomSheetViewController? {
