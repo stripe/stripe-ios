a5568fb8295965bf7fb1036a018430f2b370ccbd
George Birch <gbirch@stripe.com>
2026-08-05 11:17:00 -0700

    fix linting

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Checkout/Checkout.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Checkout/Checkout.swift
@@ -359,7 +359,7 @@ extension Checkout {
            let address = billingDetails.address {
             try await updateBillingTaxRegionIfNecessary(address: address)
         }
-        
+
         // Set the passed default shipping address IF it is non-nil and the country is in the allowed shipping countries
         if let shippingDetails = defaults.shippingDetails,
            let address = shippingDetails.address,
