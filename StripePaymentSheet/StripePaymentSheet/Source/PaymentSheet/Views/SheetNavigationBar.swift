ed1df62d2208338ef1711c110c15c4d124164272
George Birch <gbirch@stripe.com>
2026-08-18 14:01:16 -0700

    [PaymentSheet] Isolate Link coordinators

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/SheetNavigationBar.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Views/SheetNavigationBar.swift
@@ -11,6 +11,7 @@ import Foundation
 @_spi(STP) import StripeUICore
 import UIKit
 
+@MainActor
 protocol SheetNavigationBarDelegate: AnyObject {
     func sheetNavigationBarDidClose(_ sheetNavigationBar: SheetNavigationBar)
     func sheetNavigationBarDidBack(_ sheetNavigationBar: SheetNavigationBar)
