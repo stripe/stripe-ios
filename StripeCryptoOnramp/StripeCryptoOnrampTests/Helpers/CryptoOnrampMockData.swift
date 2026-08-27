78a94c22589a6474c2dcbf18476c00d7be3b28c6
Michael Liberatore <michael@lickability.net>
2026-08-27 16:40:50 -0400

    Adds mock data for test

--- StripeCryptoOnramp/StripeCryptoOnrampTests/Helpers/CryptoOnrampMockData.swift
+++ StripeCryptoOnramp/StripeCryptoOnrampTests/Helpers/CryptoOnrampMockData.swift
@@ -65,6 +65,23 @@ enum ConfirmUserAttestationResponseMock: String, MockData {
     case confirmUserAttestationResponse_200 = "ConfirmUserAttestationResponse_200"
 }
 
+enum RetrievePartnerTermsResponseMock: String, MockData {
+    var bundle: Bundle { return Bundle(for: ClassForBundle.self) }
+
+    typealias ResponseType = PartnerTerms
+
+    case retrievePartnerTermsResponse_Required_200 = "RetrievePartnerTermsResponse_Required_200"
+    case retrievePartnerTermsResponse_NotRequired_200 = "RetrievePartnerTermsResponse_NotRequired_200"
+}
+
+enum ConfirmPartnerTermsResponseMock: String, MockData {
+    var bundle: Bundle { return Bundle(for: ClassForBundle.self) }
+
+    typealias ResponseType = EmptyResponse
+
+    case confirmPartnerTermsResponse_200 = "ConfirmPartnerTermsResponse_200"
+}
+
 enum WalletOwnershipChallengeResponseMock: String, MockData {
     var bundle: Bundle { return Bundle(for: ClassForBundle.self) }
 
