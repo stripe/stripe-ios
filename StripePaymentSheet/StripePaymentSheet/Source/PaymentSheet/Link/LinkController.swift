2e2cb3a6e61f3d15a425a52e1ef640312020b95a
Jeremy Kelleher <jkelle@stripe.com>
2026-08-28 13:34:04 -0400

    Rename Link LPMs Parsed Type from Unknown to Generic  (#6515)
    ## Summary
    Renames the `unknown` cases describing Link LPMs we don't have explicit
    parsing logic for to `generic`.
    
    ## Motivation
    `Unknown` LPMs communicates the wrong idea to the reader of our Link
    LPMs code. The LPM exists and it's known to the backend, it's just
    parsed generically by our mobile SDK. This PR renames the concept
    introduced in https://github.com/stripe/stripe-ios/pull/6248
    
    ## Testing
    Manual testing to ensure the LPMs still render are usable

--- StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Link/LinkController.swift
+++ StripePaymentSheet/StripePaymentSheet/Source/PaymentSheet/Link/LinkController.swift
@@ -192,7 +192,7 @@ import UIKit
         case .bankAccount(let bankAccount):
             let iconCode = PaymentSheetImageLibrary.bankIconCode(for: bankAccount.name)
             return PaymentSheetImageLibrary.bankIcon(for: iconCode, iconStyle: .filled)
-        case .unparsable:
+        case .generic:
             return Self.linkIcon
         }
     }
@@ -220,7 +220,7 @@ import UIKit
         }
 
         let type: PaymentMethodPreview.PaymentMethodType = switch selectedPaymentDetails.details {
-        case .card, .unparsable:
+        case .card, .generic:
             .card
         case .bankAccount:
             .bankAccount
