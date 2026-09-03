9f6e3c3ecf354048f88423225fb9d34983d54138
Joyce Qin <joyceqin@stripe.com>
2026-09-03 14:34:06 -0400

    fix

--- StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/Checkout/CheckoutTestHelpers.swift
+++ StripePaymentSheet/StripePaymentSheetTests/PaymentSheet/Checkout/CheckoutTestHelpers.swift
@@ -450,15 +450,6 @@ class MockCheckoutSessionWalletUpdater: CheckoutSessionWalletUpdater {
         continuation = nil
     }
 
-    func resume() {
-        guard let sessionToReturn else {
-            continuation?.resume(throwing: CheckoutError.unknown(debugDescription: "MockCheckoutSessionWalletUpdater has no session configured"))
-            continuation = nil
-            return
-        }
-        continuation?.resume(returning: sessionToReturn)
-        continuation = nil
-    }
 }
 
 extension CheckoutController.ApplePayConfirmationParameters {
