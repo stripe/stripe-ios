eb448244e1b54a1839486e068200a1aebdfff9d7
Michael Liberatore <michael@lickability.net>
2026-09-10 17:10:21 -0400

    Adds a color extension containing the colors used on the Message screen

---
+++ StripeCryptoOnramp/StripeCryptoOnramp/Source/Internal/UIColor+CryptoOnramp.swift
@@ -0,0 +1,35 @@
+//
+//  UIColor+CryptoOnramp.swift
+//  StripeCryptoOnramp
+//
+//  Created by Michael Liberatore on 9/10/26.
+//
+
+@_spi(STP) import StripeUICore
+import UIKit
+
+extension UIColor {
+
+    /// The primary background surface.
+    static let surfacePrimary = dynamic(light: UIColor(hex: 0xFFFFFF), dark: UIColor(hex: 0x1C1C1E))
+
+    /// The secondary surface used behind icons and controls.
+    static let surfaceSecondary = dynamic(light: UIColor(hex: 0xF5F5F5), dark: UIColor(hex: 0x262626))
+
+    /// The foreground color for primary text and icons.
+    static let textPrimary = dynamic(light: UIColor(hex: 0x171717), dark: UIColor(hex: 0xFFFFFF))
+
+    /// The foreground color for tertiary text.
+    static let textTertiary = dynamic(light: UIColor(hex: 0x707070), dark: UIColor(hex: 0xD4D4D4))
+
+    /// The background surface for error indicators.
+    static let surfaceCritical = UIColor(hex: 0xE61947)
+}
+
+private extension UIColor {
+    convenience init(hex: UInt) {
+        self.init(red: CGFloat((hex >> 16) & 0xFF) / 255,
+                  green: CGFloat((hex >> 8) & 0xFF) / 255,
+                  blue: CGFloat(hex & 0xFF) / 255, alpha: 1)
+    }
+}
