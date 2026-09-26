69538056d50d53359262af795a983b25a390d869
xuhui <xuhui@stripe.com>
2026-09-26 01:25:58 +0000

    Remove Link signup skip from microdeposit UI test

--- Example/PaymentSheet Example/PaymentSheetUITest/CustomerSheetUITest.swift
+++ Example/PaymentSheet Example/PaymentSheetUITest/CustomerSheetUITest.swift
@@ -396,8 +396,6 @@ class CustomerSheetUITest: XCTestCase {
         app.otherElements["consent_manually_verify_label"].links.firstMatch.tap()
         try! fillUSBankData_microdeposits(app)
 
-        skipLinkSignup(app)
-
         let doneManualEntry = app.buttons["success_done_button"]
         XCTAssertTrue(doneManualEntry.waitForExistence(timeout: timeout))
         doneManualEntry.tap()
